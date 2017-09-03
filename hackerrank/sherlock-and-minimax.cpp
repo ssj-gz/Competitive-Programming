e4bb90db989a085137058852973f914b406e7108
Simon St James <kdedevel@etotheipiplusone.com>
2017-09-03 20:58:20 +0100

    A bit of cleanup and documentation for Sherlock and MiniMax.

--- hackerrank/sherlock-and-minimax.cpp
+++ hackerrank/sherlock-and-minimax.cpp
@@ -7,74 +7,70 @@
 
 using namespace std;
 
-int bruteForce(const vector<int>& a, int P, int Q)
-{
-    int largestMin = -1;
-    int largestMinIndex = -1;
-    for (int i = P; i <= Q; i++)
-    {
-        int minDiff = numeric_limits<int>::max();
-        for (const auto value : a)
-        {
-            minDiff = min(minDiff, abs(value - i));
-        }
-        if (minDiff > largestMin)
-        {
-            largestMin = minDiff;
-            largestMinIndex = i;
-        }
-    }
-    assert(largestMinIndex != -1);
-    return (largestMinIndex); 
-}
-
 int optimised(const vector<int>& aOriginal, int64_t P, int64_t Q)
 {
     vector<int64_t> a(aOriginal.begin(), aOriginal.end());
+    // Insert "sentinel" values of a at the beginning and end - helps to cut down
+    // on all the annoying edge cases!
+    // We use int64_t for a to avoid difficulties with dealing with arithmetic with
+    // min and max integers.
     a.insert(a.begin(), numeric_limits<int>::min());
     a.push_back(numeric_limits<int>::max());
 
-    int largestMinPos = -1;
-    int largestMin = std::numeric_limits<int>::min();
+    int largestMinGapPos = -1;
+    int largestMinGap = std::numeric_limits<int>::min();
+
+    auto snapToPQ =[P, Q](int64_t pos)
+    {
+        pos = max(pos, P);
+        pos = min(pos, Q);
+        return pos;
+    };
 
     for (int i = 1; i < a.size(); i++)
     {
-        int64_t centrePos = a[i - 1] + (a[i] - a[i - 1]) / 2;
-        //cout << " i: " << i << " a[i - 1] " << a[i - 1] << endl;
-        //cout << " centrePos unadjusted: " << centrePos << endl;
-        centrePos = max(centrePos, P);
-        centrePos = min(centrePos, Q);
-        //cout << " i: " << i << " a[i]: " << a[i] << " centrePos: " << centrePos << endl;
-        if (centrePos < a[i - 1] || centrePos > a[i])
+        const int64_t centrePos = a[i - 1] + (a[i] - a[i - 1]) / 2;
+        const int64_t centrePosInPQ = snapToPQ(centrePos);
+        if (centrePosInPQ < a[i - 1] || centrePosInPQ > a[i])
         {
-            //cout << " out of range" << endl;
+            // Snapping it means it is no longer between a[i - 1] and a[i] (definitely not a 
+            // centre point!) - move on.
             continue;
         }
-        int64_t gap = numeric_limits<int>::max();
-        assert(centrePos >= a[i - 1]);
-        assert(centrePos <= a[i]);
-        if (a[i - 1] != numeric_limits<int>::min())
-        {
-            gap  = min(gap, centrePos - a[i - 1]);
-        }
-        if (a[i] != numeric_limits<int>::max())
-        {
-            gap = min(gap, a[i] - centrePos);
-        }
-        //cout << " gap: " << gap << endl;
-        if (gap > largestMin)
-        {
-            //cout << " found new best: " << gap << " at " << centrePos << endl;
-            largestMinPos = centrePos;
-            largestMin = gap;
-        }
 
+        int64_t minGap = numeric_limits<int>::max();
+        if (a[i - 1] != numeric_limits<int>::min()) // Ignore sentinel at beginning.
+        {
+            minGap  = min(minGap, centrePosInPQ - a[i - 1]);
+        }
+        if (a[i] != numeric_limits<int>::max()) // Ignore sentinel at end.
+        {
+            minGap = min(minGap, a[i] - centrePosInPQ);
+        }
+        if (minGap > largestMinGap)
+        {
+            largestMinGapPos = centrePosInPQ;
+            largestMinGap = minGap;
+        }
     }
-    return largestMinPos;
+    return largestMinGapPos;
 }
 
 int main()
 {
+    // Pretty easy one, though with some annoying edge cases if you are not careful!
+    // Firstly, sort a, and note that if a[i - 1] <= K <= a[i], then, in computing
+    //   min over all v in a { |v - K| }
+    // we can ignore all other v = a[j] besides j = i - 1 j = i; if j < i - 1,
+    // then |a[j] - K| > |a[i - 1] - K| so cannot be min, and if j > i, then 
+    // |a[j] - K| > |a[i] - K|, so cannot be min.
+    // So it seems that we need only examine pairs of successive values a[i - 1] and a[i].
+    // The point K that maximises the min distance from a[i - 1] and a[i] is at the centre point
+    // of a[i - 1] and a[i] i.e. K = (a[i] + a[i - 1]) / 2; of course, this might lie outside the bounds
+    // of P and Q, so we may have to snap K to lie in the range P <= K <= Q and take that as our best for
+    // this a[i - 1] and a[i].
+    // And that's about it, really: just find the best K over all a[i - 1] and a[i]. 
+    // At the time of writing, this challenge is rated "Hard", for some reason!
     int N;
     cin >> N;
     vector<int> a(N);
@@ -83,29 +79,9 @@ int main()
         cin >> a[i];
     }
     sort(a.begin(), a.end());
-//#define ALL_PAIRS
-#ifdef ALL_PAIRS
-    for (int P = 1; P < a.back() + 100; P++)
-        for (int Q = P; Q < a.back() + 100; Q++)
-#else
     int P, Q;
     cin >> P >> Q;
-    // Make P, Q zero-relative.
-    //P--;
-    //Q--; 
-#endif
 
-    {
-        //cout << " P: " << P << " Q: " << Q << endl;
-        const int optimisedResult = optimised(a, P, Q);
-        //cout << "optimisedResult: " << optimisedResult << endl;
-        cout << optimisedResult << endl;
-    
-//#define BRUTE_FORCE
-#ifdef BRUTE_FORCE
-    const int bruteForceResult = bruteForce(a, P, Q);
-    cout << "bruteForceResult: " << bruteForceResult << endl;
-    assert(bruteForceResult == optimisedResult);
-#endif
-    }
+    const int optimisedResult = optimised(a, P, Q);
+    cout << optimisedResult << endl;
 }
