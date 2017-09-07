a4a9101b58858a4fa3c8797e5011cf878340b062
Simon St James <kdedevel@etotheipiplusone.com>
2017-09-07 11:20:19 +0100

    Another generic snapshot of Heap.

--- experiments/heap.cpp
+++ experiments/heap.cpp
@@ -1,13 +1,14 @@
 #include "memorypool.h"
 
-template <typename ValueType, typename Compare>
+template <typename ValueType, typename Compare, typename KeyDecrease>
 class Heap
 {
     public:
-        Heap(int maxElements, Compare comparator)
+        Heap(int maxElements, Compare comparator, KeyDecrease keyDecreaser)
             : m_maxElements{maxElements}, 
               m_memoryPool{sizeof(ValueType), maxElements},
-              m_comparator{comparator}
+              m_comparator{comparator},
+              m_keyDecreaser{keyDecreaser}
         {
             m_handles.reserve(maxElements);
 
@@ -19,8 +20,13 @@ class Heap
                 Handle()
                 {
                 }
+                const ValueType& value() const
+                {
+                }
             private:
                 bool m_isValid = false;
+                ValueType* m_value;
+                friend class Heap;
         };
         Handle add(const ValueType& value)
         {
@@ -35,8 +41,17 @@ class Heap
         {
             return ValueType();
         }
+        Handle minHandle()
+        {
+            return Handle();
+        }
         void extractMin()
         {
+        }
+        template <typename DecreaseBy>
+        void decreaseKey(const Handle& valueHandle, const DecreaseBy& decreaseBy)
+        {
+            m_keyDecreaser(*(valueHandle.m_value), decreaseBy);
         }
     private:
         int m_maxElements = -1;
@@ -49,10 +64,29 @@ class Heap
         vector<Handle> m_handles;
         vector<Element> m_elements;
         Compare m_comparator;
+        KeyDecrease m_keyDecreaser;
+};
 
+struct A
+{
+    public:
+        A(int value)
+            : keyValue{value}
+        {
+        }
+
+        int keyValue = -1;
 };
 
 
 int main()
 {
+    auto decreaseKey = [](A& value, int decreaseKeyBy) { value.keyValue -= decreaseKeyBy; };
+    auto comparator = [](const A& lhs, const A& rhs) { return lhs.keyValue < rhs.keyValue; };
+    Heap<A, decltype(comparator), decltype(decreaseKey)> heapOfA(100, comparator, decreaseKey);
+    using Handle = decltype(heapOfA)::Handle;
+
+    A a(3);
+    Handle aHandle = heapOfA.add(a);
+    heapOfA.decreaseKey(aHandle, 5);
 }
