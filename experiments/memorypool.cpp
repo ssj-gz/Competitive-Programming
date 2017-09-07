bf84ce7c0e66d208dbde0f35a7efcc67148db06a
Simon St James <kdedevel@etotheipiplusone.com>
2017-09-07 10:08:17 +0100

    Very crude memory pool implementation - single initial chunk only i.e. doesn't grow dynamically.

---
+++ experiments/memorypool.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+#define VERIFY_DEALLOCS
+
+using namespace std;
+
+class MemoryPool
+{
+    public:
+        MemoryPool(int chunkSize, int numChunks)
+            : m_chunkSize{chunkSize}, m_numChunks{numChunks}
+        {
+            m_memoryVector.resize(m_chunkSize * m_numChunks);
+            m_freeChunks.reserve(m_numChunks);
+            m_memoryBegin = static_cast<unsigned char*>(m_memoryVector.data());
+            cout << " m_memoryBegin: " << static_cast<void*>(m_memoryBegin) << endl;
+            for (int i = 0; i < m_numChunks; i++)
+            {
+                m_freeChunks.push_back(m_memoryBegin + i * m_chunkSize);
+            }
+            cout << " floop: " << m_freeChunks.front() << endl;
+
+        }
+        void* allocChunk()
+        {
+            assert(m_freeChunks.size() > 0);
+            void *allocedChunk = static_cast<unsigned char*>(m_freeChunks.front());
+            m_freeChunks.front() = m_freeChunks.back();
+            m_freeChunks.pop_back();
+            return allocedChunk;
+        }
+        void dealloc(void* chunkToDealloc)
+        {
+#ifdef VERIFY_DEALLOCS
+            assert(std::find(m_freeChunks.begin(), m_freeChunks.end(), static_cast<unsigned char*>(chunkToDealloc)) == m_freeChunks.end());
+#endif
+            m_freeChunks.push_back(static_cast<unsigned char*>(chunkToDealloc));
+        }
+    private:
+        int m_chunkSize = -1;
+        int m_numChunks = -1;
+        vector<unsigned char> m_memoryVector;
+        vector<unsigned char*> m_freeChunks;
+        unsigned char* m_memoryBegin = nullptr;
+};
+
+int main()
+{
+    MemoryPool m(100, 3);
+    void *blee = m.allocChunk();
+    void *bloo = m.allocChunk();
+    void *blaa = m.allocChunk();
+    cout << "Got chunks: " << blee << "," << bloo << "," << blaa << endl;
+    cout << "gloop: " << (static_cast<unsigned char*>(blaa) - static_cast<unsigned char*>(bloo)) << endl;
+    m.dealloc(blaa);
+    m.dealloc(blaa);
+    void *blii = m.allocChunk();
+    cout << "Got chunk: " << blii << endl;
+
+}
