64618664bf528c9d0899dd3b940abc5c07287d3a
Simon St James <kdedevel@etotheipiplusone.com>
2017-09-13 10:41:17 +0100

    More progress on Heap, though currently not working.

--- experiments/heap.cpp
+++ experiments/heap.cpp
@@ -7,12 +7,11 @@ class Heap
         Heap(int maxElements, Compare comparator, KeyDecrease keyDecreaser)
             : m_maxElements{maxElements}, 
               m_memoryPool{sizeof(ValueType), maxElements},
+              m_elements(maxElements),
+              m_handles(maxElements),
               m_comparator{comparator},
               m_keyDecreaser{keyDecreaser}
         {
-            m_handles.reserve(maxElements);
-
-
         }
         class Handle
         {
@@ -31,7 +30,9 @@ class Heap
         Handle add(const ValueType& value)
         {
             assert(m_numElements < m_maxElements);
-            m_elements[m_numElements].value = new (m_memoryPool.allocChunk()) ValueType(value);
+            void* blah = m_memoryPool.allocChunk();
+            cout << " blah: " << blah << endl;
+            m_elements[m_numElements].value = new (blah) ValueType(value);
             onKeyDecreased(m_numElements);
             m_numElements++;
 
@@ -44,12 +45,12 @@ class Heap
         const ValueType& min()
         {
             assert(m_numElements > 0);
-            return *(m_elements[0]->value);
+            return *(m_elements[0].value);
         }
         void extractMin()
         {
             assert(m_numElements > 0);
-            m_memoryPool.dealloc(m_elements[0]->value);
+            m_memoryPool.dealloc(m_elements[0].value);
             m_elements[0] = m_elements[m_numElements - 1];
             m_numElements--;
             minHeapify(0);
@@ -86,7 +87,7 @@ class Heap
             {
                 indexOfSmallest = leftIndex;
             }
-            if (rightIndex < m_numElements && m_comparator(*m_elements[rightIndex], *m_elements[indexOfSmallest]))
+            if (rightIndex < m_numElements && m_comparator(*m_elements[rightIndex].value, *m_elements[indexOfSmallest].value))
             {
                 indexOfSmallest = rightIndex;
             }
@@ -143,7 +144,25 @@ int main()
     Heap<A, decltype(comparator), decltype(decreaseKey)> heapOfA(100, comparator, decreaseKey);
     using Handle = decltype(heapOfA)::Handle;
 
-    A a(3);
-    Handle aHandle = heapOfA.add(a);
-    heapOfA.decreaseKey(aHandle, 5);
+    //A a1(3);
+    //Handle a1Handle = heapOfA.add(a1);
+    //A a2(2);
+    //Handle a2Handle = heapOfA.add(a2);
+    //cout << "blee: " << heapOfA.min().keyValue << endl;
+    vector<A> as;
+    for (int i = 0; i < 10; i++)
+    {
+        as.emplace_back(rand() % 10);
+        heapOfA.add(as.back());
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "a[" << i << "] = " << as[i].keyValue << endl;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << " min: " << heapOfA.min().keyValue << endl;
+        heapOfA.extractMin();
+    }
+
 }
