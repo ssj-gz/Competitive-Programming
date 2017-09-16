5c184bdd25f9c0648ceead11cf5e95b8ca71a593
Simon St James <kdedevel@etotheipiplusone.com>
2017-09-16 13:35:00 +0100

    Successful (though messy!) submission of Stone Division Revisited.

--- hackerrank/stone-division-revisited.cpp
+++ hackerrank/stone-division-revisited.cpp
@@ -7,60 +7,53 @@
 
 using namespace std;
 
-void findMaxMoves(const vector<int64_t>& s, int sElementBeginIndex, int64_t numMovesSoFar, int depth, map<int64_t, int64_t>& pileSizeHistogram, int64_t& highestMoves)
+map<int64_t, int64_t> lookup;
+
+int64_t findMaxMoves(const vector<int64_t>& s, int sElementBeginIndex, int64_t numMovesSoFar, int depth, const map<int64_t, int64_t>& pileSizeHistogram, int64_t& highestMoves)
 {
-    if (numMovesSoFar > highestMoves)
-        highestMoves = numMovesSoFar;
     const string indent(depth, ' ');
-    cout << indent << "findMaxMoves: numMovesSoFar: " << numMovesSoFar << endl;
-    //int numMoves = 0;
-    int sElementIndex = sElementBeginIndex;
-    int64_t largestPileSize = 0;
-    auto pileSizeHistogramReverseIter = pileSizeHistogram.rbegin();
-    while (pileSizeHistogramReverseIter != pileSizeHistogram.rend())
+    int64_t numMoves = 0;
+    for (auto& pileSizeFrequency : pileSizeHistogram)
     {
-        if (pileSizeHistogramReverseIter->second > 0)
-        {
-            largestPileSize = pileSizeHistogramReverseIter->first;
-            break;
-        }
-        pileSizeHistogramReverseIter++;
-    }
-    cout << indent << "largestPileSize: " << largestPileSize << endl;
-    while (sElementIndex < s.size())
-    {
-        const auto sElement = s[sElementIndex];
-        cout << indent << "sElement: " << sElement << endl;
-        if (sElement > largestPileSize)
-            break;
+        const int64_t pileSize = pileSizeFrequency.first;
+        //cout << indent << "pileSize " << pileSize << " occurs " << pileSizeFrequency.second << " times" << endl;
+        //cout << "pileSize: " << pileSize << endl;
+        if (pileSizeFrequency.second == 0)
+            continue;
 
-        for (auto& pileSizeFrequency : pileSizeHistogram)
-        {
-            const int64_t pileSize = pileSizeFrequency.first;
-            cout << indent << "pileSize " << pileSize << " occurs " << pileSizeFrequency.second << " times" << endl;
-            if (pileSizeFrequency.second == 0)
-                continue;
+        int64_t maxMovesForThisPile = 0;
 
-            if ((pileSize % sElement) == 0 && pileSize != sElement)
+        auto sElementIter = s.begin();
+        while (*sElementIter < pileSize && sElementIter != s.end())
+        {
+            const auto sElement = *sElementIter;
+            //cout << indent << " sElement: " << sElement << endl;
+
+            if ((pileSize % sElement) == 0)
             {
                 const int64_t originalNumPiles = pileSizeFrequency.second;
                 const int64_t numNewPiles = (pileSize / sElement) * originalNumPiles;
                 const int64_t newPilesSize = sElement;
-                cout << indent << "divisible: newPilesSize: " << newPilesSize << " numNewPiles: " << numNewPiles << endl;
+                //cout << indent << "divisible by " << sElement << " : newPilesSize: " << newPilesSize << " numNewPiles: " << numNewPiles << " originalNumPiles: " << originalNumPiles << endl;
 
-                assert(pileSizeHistogram[pileSize] != 0);
-                pileSizeHistogram[newPilesSize] += numNewPiles;
-                pileSizeHistogram[pileSize] = 0;
+                map<int64_t, int64_t> newPileSizeHistogram;
+                newPileSizeHistogram[newPilesSize] = 1;
+                auto lookedup = lookup.find(newPilesSize);
+                if (lookedup == lookup.end())
+                {
+                    const int64_t maxMoves = findMaxMoves(s, sElementBeginIndex, numMovesSoFar + originalNumPiles, depth + 1, newPileSizeHistogram, highestMoves);
+                    lookup[newPilesSize] = maxMoves;
+                    lookedup = lookup.find(newPilesSize);
+                }
+                maxMovesForThisPile = max(maxMovesForThisPile, originalNumPiles + numNewPiles * lookedup->second);
 
-                findMaxMoves(s, sElementBeginIndex, numMovesSoFar + originalNumPiles, depth + 1, pileSizeHistogram, highestMoves);
-
-                pileSizeHistogram[pileSize] = originalNumPiles;
-                pileSizeHistogram[newPilesSize] -= numNewPiles;
             }
+            sElementIter++;
         }
-
-        sElementIndex++;
+        //cout << indent << "maxMovesForThisPile: " << maxMovesForThisPile << endl;
+        numMoves += maxMovesForThisPile;
     }
+    return numMoves;
 }
 
 int main()
@@ -69,7 +62,8 @@ int main()
     cin >> q;
     for (int t = 0; t < q; t++)
     {
-        cout << " t: " << t << endl;
+        lookup.clear();
+        //cout << " t: " << t << endl;
         int64_t n, m;
         cin >> n >> m;
         vector<int64_t> s(m);
@@ -78,11 +72,30 @@ int main()
             cin >> s[i];
         }
         sort(s.begin(), s.end());
+#if 0
+        auto sIter = s.begin();
+        while (sIter != s.end())
+        {
+            bool sIterErased = false;
+            for (auto sDiviserIter = s.begin(); sDiviserIter != sIter; sDiviserIter++)
+            {
+                if (*sIter != *sDiviserIter && *sDiviserIter != 1 && ((*sIter % *sDiviserIter) == 0))
+                {
+                    cout << "removing " << *sIter << " as it is a multiple of " << *sDiviserIter << endl;
+                    //sIter = s.erase(sIter);
+                    //sIterErased = true;
+                    break;
+                }
+            }
+            if (!sIterErased)
+                sIter++;
+        }
+#endif
         map<int64_t, int64_t> pileSizeHistogram;
         pileSizeHistogram[n] = 1;
         int64_t highestMoves = 0;
-        findMaxMoves(s, 0, 0, 0, pileSizeHistogram, highestMoves);
-        cout << highestMoves << endl;
+        const int64_t blah = findMaxMoves(s, 0, 0, 0, pileSizeHistogram, highestMoves);
+        cout << blah << endl;
     }
 
 }
