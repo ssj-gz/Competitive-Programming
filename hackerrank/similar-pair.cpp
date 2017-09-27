6f670b8d47ac2f771e4dfa95eb1aba31ab7acba5
Simon St James <kdedevel@etotheipiplusone.com>
2017-09-27 14:06:14 +0100

    Some cleanup and documentation for Similar Pair.

--- hackerrank/similar-pair.cpp
+++ hackerrank/similar-pair.cpp
@@ -69,7 +69,6 @@ class NumberTracker
                 numCellsInThisRow *= 2;
                 powerOf2 /= 2;
             }
-            //cout << "m_nextPowerOf2: " << m_nextPowerOf2 << endl;
 #ifdef VERIFY_NUMBER_TRACKER
             m_numOfNumber.resize(m_nextPowerOf2 + 1);
 #endif
@@ -91,7 +90,6 @@ class NumberTracker
             {
                 dbgNum += m_numOfNumber[i];
             }
-            //cout << " countNumbersInRange: start: " << start << " end: " << end << " num: " << num << " dbgNum: " << dbgNum << endl;
             assert(num == dbgNum);
 #endif
             return num;
@@ -110,7 +108,6 @@ class NumberTracker
         vector<vector<Cell>> m_cellMatrix;
         void addOrRemoveNumber(int n, bool add)
         {
-            //cout << "addOrRemoveNumber: " << n << " : " << add << endl;
             int cellRow = 0;
             int powerOf2 = m_nextPowerOf2;
             while (cellRow < m_cellMatrix.size())
@@ -121,23 +118,9 @@ class NumberTracker
                 cellRow++;
                 powerOf2 /= 2;
             }
-            //cout << "cell matrix:" << endl;
-            for (int i = 0; i < m_cellMatrix.size(); i++)
-            {
-                //cout << "row: " << i << " ";
-                for (const auto cell : m_cellMatrix[i])
-                {
-                    //cout << cell.numNumbersInRange << " ";
-                }
-                //cout << endl;
-            }
-#ifdef VERIFY_NUMBER_TRACKER
-            m_numOfNumber[n] += (add ? 1 : -1);
-#endif
         }
         int countNumbersInRange(int start, int end, int cellRow, int powerOf2)
         {
-            //cout << " countNumbersInRange: start: " << start << " end: " << end << " cellRow: " << cellRow << " powerOf2: " << powerOf2 << endl;
             if (end < start)
                 return 0;
             if (end == start)
@@ -150,15 +133,13 @@ class NumberTracker
             {
                 return countNumbersInRange(start, end, cellRow + 1, powerOf2 / 2);
             }
-            const int powerOf2AfterStart = ((start % powerOf2) == 0) ? start : (start / powerOf2 + 1) * powerOf2;
-            const int powerOf2BeforeEnd = ((end % powerOf2) == 0) ? end : (end / powerOf2) * powerOf2;
             int numberInRange = 0;
             if ((start % powerOf2) == 0)
             {
+                // Advance start by one complete cell at a time, summing the contents of that cell, then recurse on the rest.
                 while (start <= end - powerOf2)
                 {
                     const int cellContents = m_cellMatrix[cellRow][start/powerOf2].numNumbersInRange;
-                    //cout << " from start, start = " << start << " end = " << end << " adding contents of cell " << cellRow << "," << start/powerOf2 << " = " << cellContents << endl;
                     numberInRange += cellContents;
                     start += powerOf2;
                 }
@@ -166,19 +147,24 @@ class NumberTracker
             }
             if (((end + 1) % powerOf2) == 0)
             {
+                // Unadvance end by one complete cell at a time, summing the contents of that cell, then recurse on the rest.
                 while (start <= end - powerOf2)
                 {
                     const int cellContents = m_cellMatrix[cellRow][end/powerOf2].numNumbersInRange;
-                    //cout << " from end, start = " << start << " end = " << end << " adding contents of cell " << cellRow << "," << end/powerOf2 << " = " << cellContents << endl;
                     numberInRange += cellContents;
                     end -= powerOf2;
                 }
                 return numberInRange + countNumbersInRange(start, end, cellRow + 1, powerOf2 / 2);
             }
+            // Neither start nor end is a multiple of powerOf2: sum up the complete cells in between (if any) ...
+            const int powerOf2AfterStart = (start / powerOf2 + 1) * powerOf2;
+            const int powerOf2BeforeEnd =  (end / powerOf2) * powerOf2;
             for (int cellIndex = powerOf2AfterStart / powerOf2; cellIndex < powerOf2BeforeEnd / powerOf2; cellIndex++)
             {
                 numberInRange += m_cellMatrix[cellRow][cellIndex].numNumbersInRange;
             }
+            // ... and then split into two, and recurse: for each of two split regions, at least one of the start or end will be a multiple of powerOf2, so they
+            // will not split further.
             return numberInRange + countNumbersInRange(start, powerOf2AfterStart - 1, cellRow + 1, powerOf2 / 2) + countNumbersInRange(powerOf2BeforeEnd, end, cellRow + 1, powerOf2 / 2);
         }
         bool isPowerOf2(int n)
@@ -187,9 +173,6 @@ class NumberTracker
                 return false;
             return isPowerOf2(n / 2);
         }
-#ifdef VERIFY_NUMBER_TRACKER
-        vector<int> m_numOfNumber;
-#endif
 };
 
 int64_t numSimilarPairs(Node* root, int k, NumberTracker& numberTracker)
@@ -240,7 +223,6 @@ int64_t numSimilarPairs(Node* root, int k, NumberTracker& numberTracker)
         else
         {
             childReturnValue = currentStackFrame.numSimilarPairs;
-            //cout << " node: " << currentStackFrame.currentNode << " returning: " << childReturnValue << endl;
             numberTracker.removeNumber(currentStackFrame.currentNode->nodeNumber);
             stackFrames.pop();
         }
@@ -255,25 +237,6 @@ int64_t numSimilarPairs(Node* root, int k, NumberTracker& numberTracker)
 
 int main()
 {
-#if 0
-    while (true)
-    {
-        const int trackerSize = 1000;
-        NumberTracker tracker(trackerSize);
-        const int randomNumber = rand() % trackerSize;
-        cout << " randomNumber: " << randomNumber << endl;
-        tracker.addNumber(randomNumber);
-        for (int start = 0; start < trackerSize; start++)
-        {
-            for (int end = start; end < trackerSize; end++)
-            {
-                tracker.countNumbersInRange(start, end);
-            }
-        }
-    }
-
-    return 0;
-#endif
     int n, k;
     cin >> n >> k;
 
@@ -308,14 +271,6 @@ int main()
     NumberTracker numberTracker(n);
     const int64_t numSimilarPairs = ::numSimilarPairs(root, k, numberTracker);
     cout << numSimilarPairs << endl;
-
-#ifdef BRUTE_FORCE
-    vector<Node*> ancestors;
-    const int numSimilarPairsBruteForce = ::numSimilarPairsBruteForce(root, k, ancestors);
-    cout << "numSimilarPairs: " << numSimilarPairs << " numSimilarPairsBruteForce: " << numSimilarPairsBruteForce << endl;
-    assert(numSimilarPairsBruteForce == numSimilarPairs);
-    cout << numSimilarPairsBruteForce << endl;
-#endif
 
 }
 
