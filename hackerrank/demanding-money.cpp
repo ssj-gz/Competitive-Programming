a1517511a9f484d2bba75ea3d6f037f3ac75b495
Simon St James <kdedevel@etotheipiplusone.com>
2017-10-05 10:46:11 +0100

    Cleaned up and documented "Demanding Money".

--- hackerrank/demanding-money.cpp
+++ hackerrank/demanding-money.cpp
@@ -22,39 +22,32 @@ vector<vector<pair<int64_t, int64_t>>> memoisedResultForHouseIndexAndAvailableHo
 
 pair<int64_t, int64_t> findMaxMoney(const int currentHouseIndex, const uint64_t availableHouses, const vector<House>& houses)
 {
-    //cout << " currentHouseIndex: " << currentHouseIndex << " availableHouses: " << availableHouses << endl;
     if (currentHouseIndex == houses.size())
     {
         return {0, 1};
     }
-    const int leadingHousesToSkip = max(static_cast<int>(houses.size() - memoiseLastNHouses), 0);
-    const int memoiseHouseIndex = currentHouseIndex - leadingHousesToSkip;
-    //cout << "leadingHousesToSkip + memoiseHouseIndex: " << (leadingHousesToSkip + memoiseHouseIndex) << endl;
-    const auto memoiseHouseAvailable = availableHouses >> static_cast<uint64_t>(leadingHousesToSkip + memoiseHouseIndex);
+    // Work out the memo-isation parameters.
+    const int leadingHousesToAlwaysSkip = max(static_cast<int>(houses.size() - memoiseLastNHouses), 0);
+    const int memoiseHouseIndex = currentHouseIndex - leadingHousesToAlwaysSkip;
+    const auto memoiseHouseAvailable = availableHouses >> static_cast<uint64_t>(leadingHousesToAlwaysSkip + memoiseHouseIndex);
     const bool canUseMemoisable = (memoiseHouseIndex >= 0);
-    if (canUseMemoisable)
+    assert(!canUseMemoisable || (memoiseHouseIndex < memoiseLastNHouses && memoiseHouseAvailable >= 0 && memoiseHouseAvailable < memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex].size()));
+    if (canUseMemoisable && memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable].first != -1)
     {
-        //cout << "fleep currentHouseIndex: " << currentHouseIndex << " availableHouses: " << availableHouses << " memoiseHouseIndex: " << memoiseHouseIndex << " memoiseHouseAvailable: " << memoiseHouseAvailable << " memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex].size(): " << memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex].size() << endl;
-    }
-    //assert(!canUseMemoisable || (memoiseHouseIndex < memoiseLastNHouses && memoiseHouseAvailable >= 0 && memoiseHouseAvailable < memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex].size()));
-    if (true && canUseMemoisable && memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable].first != -1)
-    {
-        //cout << "using memo-ised result:" << memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable].first << "," <<  memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable].second << endl;
         return memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable];
     }
-    const auto resultIfNotChoseThis = findMaxMoney(currentHouseIndex + 1, availableHouses, houses);
 
+    // Don't choose this house, and recurse.
+    const auto resultIfNotChoseThis = findMaxMoney(currentHouseIndex + 1, availableHouses, houses);
     const bool houseIsAvailable = ((availableHouses & (static_cast<uint64_t>(1) << currentHouseIndex)) != 0);
     if (!houseIsAvailable)
     {
         if (canUseMemoisable)
-        {
-            //cout << "Storing " << resultIfNotChoseThis.first << "," << resultIfNotChoseThis.second << " at " << " memoiseHouseIndex: " << memoiseHouseIndex << " memoiseHouseAvailable: " << memoiseHouseAvailable << endl;
             memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable] = resultIfNotChoseThis;
-        }
         return resultIfNotChoseThis;
     }
 
+    // Choose this house, and recurse.
     uint64_t availableHousesIfChoseThis = availableHouses;
     for (const auto neighbour : houses[currentHouseIndex].neighbours)
     {
@@ -78,10 +71,8 @@ pair<int64_t, int64_t> findMaxMoney(const int currentHouseIndex, const uint64_t 
         result = { resultIfChoseThis.first, resultIfChoseThis.second + resultIfNotChoseThis.second};
     }
 
-    //cout << "currentHouseIndex: " << currentHouseIndex << " availableHouses: " << availableHouses << " result: " << result.first << "," << result.second << endl;
     if (canUseMemoisable)
     {
-            //cout << "Storing " << result.first << "," << result.second << " at " << " memoiseHouseIndex: " << memoiseHouseIndex << " memoiseHouseAvailable: " << memoiseHouseAvailable << endl;
         memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable] = result;
     }
 
@@ -90,6 +81,12 @@ pair<int64_t, int64_t> findMaxMoney(const int currentHouseIndex, const uint64_t 
 
 int main()
 {
+    // Fundamentally easy: it's basically a brute-force search with some memo-isation on the last memoiseLastNHouses houses.
+    // We use an int64_t to denote a bitmask of the houses that Kilgrave can still visit (availableHouses).
+    // If currentHouseIndex is amongst the last memoiseLastNHouses houses, the we take the binary representation of the availability
+    // of the last (houses.size() - currentHouseIndex <= memoiseLastNHouses) houses (memoiseHouseAvailable), and store the result
+    // that we eventually calculate in memoisedResultForHouseIndexAndAvailableHouses[memoiseHouseIndex][memoiseHouseAvailable], where memoiseHouseIndex
+    // is the index from 0 ... memoiseLastNHouses - 1 of currentHouseIndex in the last memoiseLastNHouses to memo-ise.
     int N, M;
     cin >> N >> M;
 
@@ -122,5 +119,4 @@ int main()
     const auto result = findMaxMoney(0, availableHouses, houses);
 
     cout << result.first << " " << result.second << endl;
-
 }
