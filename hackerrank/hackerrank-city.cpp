7ed7eb27d09d0d59b0bf611ea4ad9e8a8c9105d2
Simon St James <kdedevel@etotheipiplusone.com>
2017-10-21 13:33:30 +0100

    Nope - not right - not sure what's missing.

--- hackerrank/hackerrank-city.cpp
+++ hackerrank/hackerrank-city.cpp
@@ -1,11 +1,55 @@
+// Simon St James (ssjgz) - 2017-10-12 11:42
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+const int64_t modulus = 10'000'000'007ULL;
+
+class ModNum
+{
+    public:
+        ModNum(int64_t n)
+            : m_n{n}
+        {
+        }
+        ModNum& operator+=(const ModNum& other)
+        {
+            m_n = (m_n + other.m_n) % ::modulus;
+            return *this;
+        }
+        ModNum& operator*=(const ModNum& other)
+        {
+            m_n = (m_n * other.m_n) % ::modulus;
+            return *this;
+        }
+        int64_t value() const { return m_n; };
+    private:
+        int64_t m_n;
+};
+
+ModNum operator+(const ModNum& lhs, const ModNum& rhs)
+{
+    ModNum result(lhs);
+    result += rhs;
+    return result;
+}
+
+ModNum operator*(const ModNum& lhs, const ModNum& rhs)
+{
+    ModNum result(lhs);
+    result *= rhs;
+    return result;
+}
+
+ostream& operator<<(ostream& os, const ModNum& toPrint)
+{
+    os << toPrint.value();
+    return os;
+}
+
 int main()
 {
-    const int64_t modulus = 1000'000'007ULL;
     int N;
     cin >> N;
 
@@ -15,10 +59,10 @@ int main()
         cin >> A[i];
     }
 
-    int64_t numNodes = 1;
-    int64_t sumOfAllPaths = 0;
-    int64_t sumOfPathsToCorner = 0;
-    int64_t sumCornerToCorner = 0;
+    ModNum numNodes = 1;
+    ModNum sumOfAllPaths = 0;
+    ModNum sumOfPathsToCorner = 0;
+    ModNum sumCornerToCorner = 0;
 
 
     for (int i = 0; i < N; i++)
@@ -27,15 +71,14 @@ int main()
         cout << "sumOfAllPaths: " << sumOfAllPaths << endl;
         cout << "sumOfPathsToCorner: " << sumOfPathsToCorner << endl;
         cout << "sumCornerToCorner: " << sumCornerToCorner << endl;
-        int64_t newNumNodes = 4 * numNodes + 2;
-        int64_t newSumOfAllPaths = 4 * sumOfAllPaths + 
+        ModNum newNumNodes = 4 * numNodes + 2;
+        ModNum newSumOfAllPaths = 4 * sumOfAllPaths + 
             (3 * numNodes * A[i] + 2 * sumOfPathsToCorner) * 4 + 
             2 * 6 * numNodes * sumOfPathsToCorner + 
             (3 + 2 + 3 + 3 + 2 + 3) * (A[i] * numNodes * numNodes) +
             A[i];
-        newSumOfAllPaths %= modulus;
-        int64_t newSumOfPathsToCorner = 4 * sumOfPathsToCorner + ((3 + 2 + 3) * A[i] + sumCornerToCorner) * numNodes + 3 * A[i] + 2 * sumCornerToCorner;
-        int64_t newSumCornerToCorner = 2 * sumCornerToCorner + 3 * A[i];
+        ModNum newSumOfPathsToCorner = 4 * sumOfPathsToCorner + (2 * A[i] + sumCornerToCorner) + (A[i] + sumCornerToCorner) + numNodes * ((3 + 3 + 2) * A[i] + sumCornerToCorner);
+        ModNum newSumCornerToCorner = 2 * sumCornerToCorner + 3 * A[i];
 
         cout << "newNumNodes: " << newNumNodes << endl;
         cout << "newSumOfAllPaths: " << newSumOfAllPaths << endl;
