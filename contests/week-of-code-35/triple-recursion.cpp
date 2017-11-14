bfb1fc9f27ecda14f18a49ed4eca16b54a83e1e1
Simon St James <kdedevel@etotheipiplusone.com>
2017-11-14 07:22:30 +0000

    Simplify Triple Recursion.

--- contests/week-of-code-35/triple-recursion.cpp
+++ contests/week-of-code-35/triple-recursion.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -12,31 +13,19 @@ int main()
 
     a[0][0] = m;
 
-    for (int diag = 1; diag < n; diag++)
-    {
-        a[diag][diag] = a[diag - 1][diag - 1] + k;
-    }
-
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = j + 1; i < n; i++)
-        {
-            a[i][j] = a[i - 1][j] - 1;
-        }
-    }
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = 0; i < j; i++)
-        {
-            a[i][j] = a[i][j - 1] - 1;
-        }
-    }
-
     for (int j = 0; j < n; j++)
     {
         for (int i = 0; i < n; i++)
         {
-            cout << a[i][j];
+            int& cellValue = a[i][j];
+            if (i == j && i != 0)
+                cellValue = a[i - 1][j - 1] + k;
+            else if (i > j)
+                cellValue = a[i - 1][j] - 1;
+            else if (i < j && j != 0)
+                cellValue = a[i][j - 1] - 1;
+
+            cout << cellValue;
             if (i != n - 1)
                 cout << " ";
         }
