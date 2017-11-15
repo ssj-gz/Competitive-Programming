883ebe4b223fb1e6b8552f65ce6fbc43cbd0418f
Simon St James <kdedevel@etotheipiplusone.com>
2017-11-15 22:03:18 +0000

    Refactor for readability, and also make it possible to deal with huge Aij (although this can't happen in this particular problem!)

--- contests/week-of-code-35/3d-surface-area.cpp
+++ contests/week-of-code-35/3d-surface-area.cpp
@@ -12,9 +12,9 @@ int main()
 
     vector<vector<int>> A(H, vector<int>(W, -1));
 
-    for (int row = 0; row < H; row++)
+    for (auto row = 0; row < H; row++)
     {
-        for (int col = 0; col < W; col++)
+        for (auto col = 0; col < W; col++)
         {
             cin >> A[row][col];
             assert(A[row][col] >= 1);
@@ -22,34 +22,34 @@ int main()
     }
 
     int64_t totalCost = 0;
-    for (int row = 0; row < H; row++)
+    for (auto row = 0; row < H; row++)
     {
-        for (int col = 0; col < W; col++)
+        for (auto col = 0; col < W; col++)
         {
             totalCost += 2; // Top and bottom surface of this cell.
 
             // The "sides" of each square that is stacked on this cell.
-            for (int depth = 1; depth <= A[row][col]; depth++)
-            {
-                int exposedSides = 4;
-                if (col > 0 && A[row][col - 1] >= depth)
-                {
-                    exposedSides--;
-                }
-                if (col < W - 1 && A[row][col + 1] >= depth)
-                {
-                    exposedSides--;
-                }
-                if (row > 0 && A[row - 1][col] >= depth)
-                {
-                    exposedSides--;
-                }
-                if (row < H - 1 && A[row + 1][col] >= depth)
-                {
-                    exposedSides--;
-                }
-                totalCost += exposedSides;
-            }
+            auto exposedSideFaces = 0;
+            const auto stackHeight = A[row][col];
+            // Num exposed left faces of this stack.
+            const auto stackToLeftHeight = (col > 0 ? A[row][col - 1] : 0);
+            if (stackHeight > stackToLeftHeight)
+                exposedSideFaces += stackHeight - stackToLeftHeight;
+            // Right faces.
+            const auto stackToRightHeight = (col < W - 1 ? A[row][col + 1] : 0);
+            if (stackHeight > stackToRightHeight)
+                exposedSideFaces += stackHeight - stackToRightHeight;
+            // Back faces.
+            const auto stackBehindHeight = (row > 0 ? A[row - 1][col] : 0);
+            if (stackHeight > stackBehindHeight)
+                exposedSideFaces += stackHeight - stackBehindHeight;
+            // Front front.
+            const auto stackInFrontHeight = (row < H - 1 ? A[row + 1][col] : 0);
+            if (stackHeight > stackInFrontHeight)
+                exposedSideFaces += stackHeight - stackInFrontHeight;
+
+            assert(exposedSideFaces >= 0);
+            totalCost += exposedSideFaces;
         }
     }
     cout << totalCost << endl;
