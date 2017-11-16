337bd814541031708f3fab7c2c7d32b563b8521c
Simon St James <kdedevel@etotheipiplusone.com>
2017-11-16 11:49:06 +0000

    Generic snapshot - gah, finding (and maintaining) max in range is O(nlog(n))!

--- contests/week-of-code-35/matrix-land.cpp
+++ contests/week-of-code-35/matrix-land.cpp
@@ -134,6 +134,18 @@ int64_t maxSum(const vector<vector<int64_t>>& A)
         const auto bestGobbleToLeftFrom = findMaxSubarraySumEndingAt(A[row], rowOfZeros);
         const auto bestGobbleToRightFrom = findMaxSubarraySumEndingAtReversed(A[row], rowOfZeros);
 
+#if 0
+        vector<int64_t> cumulativeDescendAtScore;
+        {
+            int64_t cumulativeScore = 0;
+            for (int col = 0; col < numCols; col++)
+            {
+                cumulativeScore += lookup[row + 1][col];
+                cumulativeDescendAtScore.push_back(cumulativeScore);
+            }
+        }
+#endif
+
         int startCol = 0;
         while (startCol < numCols)
         {
@@ -150,11 +162,14 @@ int64_t maxSum(const vector<vector<int64_t>>& A)
             cout << "  bestGobbleSum: " << bestGobbleSum << " bestGobbleStartIndex: " << bestGobbleStartIndex << " bestGobbleEndIndex: " << bestGobbleEndIndex << endl;
             // Descend in the middle of gobble-range.
             int64_t bestForStartCol = std::numeric_limits<int64_t>::min();
-            for (int i = bestGobbleStartIndex; i <= bestGobbleEndIndex; i++)
+             for (int i = bestGobbleStartIndex; i <= bestGobbleEndIndex; i++)
             {
                 const int64_t scoreIfDescendHere = bestGobbleSum + lookup[row + 1][startCol];
                 bestForStartCol = max(bestForStartCol, scoreIfDescendHere);
             }
+
+            //const auto bestScoreIfDescendInGobbleRange = bestGobbleSum +  cumulativeDescendAtScore[bestGobbleEndIndex] - (bestGobbleStartIndex > 0 ? cumulativeDescendAtScore[bestGobbleStartIndex - 1] : 0);
+            //bestForStartCol = max(bestForStartCol, bestScoreIfDescendInGobbleRange);
             cout << "  bestForStartCol after descend in gobble-range: " << bestForStartCol << endl;
             // Descend to the left of gobble-range.
             if (bestGobbleStartIndex > 0)
@@ -171,10 +186,11 @@ int64_t maxSum(const vector<vector<int64_t>>& A)
             }
             cout << "  bestForStartCol: " << bestForStartCol << endl;
             // All columns in the gobble range have the same max attainable score if we start there TODO - no, this is wrong!
-            for (int i = bestGobbleStartIndex; i <= bestGobbleEndIndex; i++)
-            {
-                lookup[row][i] = max(lookup[row][i], bestForStartCol);
-            }
+            lookup[row][startCol] = max(lookup[row][startCol], bestForStartCol);
+            //for (int i = bestGobbleStartIndex; i <= bestGobbleEndIndex; i++)
+            //{
+                //lookup[row][i] = max(lookup[row][i], bestForStartCol);
+            //}
             //startCol = bestGobbleEndIndex + 1;
             startCol++;
         }
@@ -223,68 +239,6 @@ int64_t maxSumBruteForce(int startRow, int startCol, const vector<vector<int64_t
     return best;
 
 
-}
-
-void blah(const vector<int64_t>& a, const vector<int64_t>& b)
-{
-    assert(a.size() == b.size());
-    cout << "blah: a: " << endl;
-    for (const auto x : a)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-    cout << "b: " << endl;
-    for (const auto x : b)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-    int64_t bestSum = std::numeric_limits<int64_t>::min();
-    int64_t bestB = b.front();
-    int64_t cumulativeSum = 0;
-    int startPoint = 0;
-    //cout << "blah" << endl;
-    for (int endPoint = 0; endPoint < a.size(); endPoint++)
-    {
-        //cout << " endPoint: " << endPoint << endl;
-        if (b[endPoint] > bestSum )
-        {
-            //cout << " found better, apparently" << endl;
-            bestSum = a[endPoint] + b[endPoint];
-            bestB = b[endPoint];
-            cumulativeSum = a[endPoint];
-            startPoint = endPoint;
-        }
-        else
-        {
-            cumulativeSum += a[endPoint];
-            bestSum = cumulativeSum + bestB;
-        }
-        //cout << " bestSum: " << bestSum << " cumulativeSum: " << cumulativeSum << " bestB: " << bestB << endl;
-        {
-            // Verify.
-            int64_t bestSumDebug = std::numeric_limits<int64_t>::min();
-            int cumulativeSum = 0;
-            int startPointDebug = 0;
-            for (int startPoint = endPoint; startPoint >= 0; startPoint--)
-            {
-                cumulativeSum += a[startPoint];
-                if (cumulativeSum + b[startPoint] > bestSumDebug)
-                {
-                    bestSumDebug = cumulativeSum + b[startPoint];
-                    startPointDebug = startPoint;
-                }
-                else if (cumulativeSum + b[startPoint] == bestSumDebug)
-                {
-                    startPointDebug = startPoint;
-                }
-            }
-            //cout << " bestSumDebug: " << bestSumDebug << endl;
-            assert(bestSum == bestSumDebug);
-            assert(startPointDebug == startPoint);
-        }
-    }
 }
 
 int main()
