e87c57e5e018e2a26ab9bc3aab70ad50bf7974a4
Simon St James <kdedevel@etotheipiplusone.com>
2017-11-18 21:45:33 +0000

    More cleanup.

--- contests/week-of-code-35/airports.cpp
+++ contests/week-of-code-35/airports.cpp
@@ -14,91 +14,6 @@
 
 using namespace std;
 
-bool areAirportsArranged(const vector<int>& airports, int minDistance)
-{
-    //cout << "areAirportsArranged; minDistance: " << minDistance << " airports:" << endl;
-    //for (const auto x : airports)
-    //{
-        //cout << x << " ";
-    //}
-    //cout << endl;
-    set<int> reachedAirportIndices;
-    vector<int> airportIndicesToExplore;
-    airportIndicesToExplore.push_back(0);
-    reachedAirportIndices.insert(0);
-
-    while (!airportIndicesToExplore.empty())
-    {
-        vector<int> nextAirportIndicesToExplore;
-        for (const auto& airportIndexToExplore : airportIndicesToExplore)
-        {
-            const auto airportPos = airports[airportIndexToExplore];
-            for (int j = 0; j < airports.size(); j++)
-            {
-                if (abs(airports[j] - airportPos) >= minDistance)
-                {
-                    if (reachedAirportIndices.find(j) == reachedAirportIndices.end())
-                    {
-                        reachedAirportIndices.insert(j);
-                        nextAirportIndicesToExplore.push_back(j);
-                    }
-                }
-            }
-        }
-        airportIndicesToExplore = nextAirportIndicesToExplore;
-    }
-
-    const auto areArranged = reachedAirportIndices.size() == airports.size();
-    //cout << "areArranged? " << areArranged << endl;
-
-    return areArranged;
-}
-
-vector<int> findAirportArrangementWithCost(vector<int>& airports, int nextAirportIndex, int cost, int minDistance, bool moveOnlyFirstAndLast)
-{
-    if (cost < 0)
-    {
-        return vector<int>();
-    }
-    if (nextAirportIndex == airports.size())
-    {
-        if (areAirportsArranged(airports, minDistance))
-        {
-            return airports;
-        }
-        return vector<int>();
-    }
-    if (!moveOnlyFirstAndLast || nextAirportIndex == 0 || nextAirportIndex == airports.size() - 1)
-    {
-        const int originalPos = airports[nextAirportIndex];
-        for (int newAirportPos = originalPos - cost; newAirportPos <= originalPos + cost; newAirportPos++)
-        {
-            const int costOfThisMove = abs(newAirportPos - originalPos);
-            airports[nextAirportIndex] = newAirportPos;
-
-            const auto arrangedAirports = (findAirportArrangementWithCost(airports, nextAirportIndex + 1, cost - costOfThisMove, minDistance, moveOnlyFirstAndLast));
-            if ((!arrangedAirports.empty()))
-            {
-                //cout << "Can arrange airports with cost: " << cost << endl;
-                return arrangedAirports;
-            }
-        }
-        airports[nextAirportIndex] = originalPos;
-    }
-    else
-    {
-        return findAirportArrangementWithCost(airports, nextAirportIndex + 1, cost, minDistance, moveOnlyFirstAndLast);
-    }
-
-    return vector<int>();
-}
-
-vector<int> findAirportArrangementWithCost(const vector<int>& airports, int cost, int minDistance, bool moveOnlyFirstAndLast)
-{
-    vector<int> airportsCopy(airports);
-    return findAirportArrangementWithCost(airportsCopy, 0, cost, minDistance, moveOnlyFirstAndLast);
-}
-
 class MaxTracker
 {
     public:
@@ -133,7 +48,6 @@ class MaxTracker
 
 vector<int> findResult(const vector<int>& airportAddedOnDay, int minDistance)
 {
-    //cout << "findResult - minDistance: " << minDistance << endl;
     vector<int> results;
     vector<int> airports;
     int leftEndpoint = numeric_limits<int>::max();
@@ -151,6 +65,7 @@ vector<int> findResult(const vector<int>& airportAddedOnDay, int minDistance)
     {
         return airportPos < leftEndpoint + minDistance && airportPos > rightEndpoint - minDistance;
     };
+    // Some utils for dealing with set<> ugliness - though still just as ugly, I suppose!
     auto hasNext = [](const set<int>& collection, set<int>::iterator setIter)
     {
         if (setIter == collection.end())
@@ -183,7 +98,6 @@ vector<int> findResult(const vector<int>& airportAddedOnDay, int minDistance)
     set<int> airportsNotCovered;
     for (int i = 0; i < airportAddedOnDay.size(); i++)
     {
-
         const int airportPos = airportAddedOnDay[i];
 
         if (airportPos == leftEndpoint)
@@ -213,124 +127,105 @@ vector<int> findResult(const vector<int>& airportAddedOnDay, int minDistance)
 
         vector<int> newlyUncoveredAirportPositions;
 
+        // Should we add this as an "uncovered" airport? There's a bit of finickiness with whether it's an endpoint: 
+        // we should add it to uncovered if it is and there is a duplicate of it already in the set of airports.
         if ((airportPos != leftEndpoint || leftEndPointDuplicated) && (airportPos != rightEndpoint || rightEndPointDuplicated) &&
-            (isUncovered(airportPos))
-                )
+                (isUncovered(airportPos))
+           )
         {
             newlyUncoveredAirportPositions.push_back(airportPos);
         }
-            //cout << "end points changed - removing stuff from set!" << endl;
-            if (oldRightEndPoint != rightEndpoint)
+        if (oldRightEndPoint != rightEndpoint)
+        {
+            // The right endpoint has been moved rightwards; erase the now-covered airports from the beginning of
+            // airportsNotCovered (updating maxDiffOfSuccessiveUncoveredPairs as we go) ...
+            while (!airportsNotCovered.empty() && *airportsNotCovered.begin() <= rightEndpoint - minDistance)
             {
-                //cout << "right end point changed" << endl;
-                //assert(rightEndpoint == oldRightEndPoint);
-                while (!airportsNotCovered.empty() && *airportsNotCovered.begin() <= rightEndpoint - minDistance)
+                if (hasNext(airportsNotCovered, airportsNotCovered.begin()))
                 {
-                    //cout << "removing: " << *airportsNotCovered.begin() << " due to change right end point" << endl;
-                    if (hasNext(airportsNotCovered, airportsNotCovered.begin()))
+                    const int diffToRemove = next(airportsNotCovered, airportsNotCovered.begin()) -  *airportsNotCovered.begin();
+                    maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
+                }
+                airportsNotCovered.erase(airportsNotCovered.begin());
+            }
+            // ... and maybe add the previous endpoint as an uncovered airport.
+            if ((i != 0) && (isUncovered(oldRightEndPoint) && oldRightEndPoint != leftEndpoint))
+            {
+                newlyUncoveredAirportPositions.push_back(oldRightEndPoint);
+            }
+        }
+        if (oldLeftEndPoint != leftEndpoint)
+        {
+            // Left endpoint has been moved leftwards; erase the now-covered airports from the end of airportsNotCovered, etc.
+            // Same as the right endpoint case, really, except dealing with iterators is more ugly!
+            if (!airportsNotCovered.empty())
+            {
+                auto highestIter = airportsNotCovered.end();
+                highestIter--;
+                while (true)
+                {
+                    if (airportsNotCovered.empty())
+                        break;
+                    if (*highestIter >= leftEndpoint + minDistance)
                     {
-                        const int diffToRemove = next(airportsNotCovered, airportsNotCovered.begin()) -  *airportsNotCovered.begin();
-                        //cout << " removing diff: " << diffToRemove << " due to change right end point";
-                        maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
+                        if (hasPrevious(airportsNotCovered, highestIter))
+                        {
+                            const int diffToRemove = *highestIter - previous(airportsNotCovered, highestIter);
+                            maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
+                        }
+
+                        highestIter = airportsNotCovered.erase(highestIter);
+                        if (!airportsNotCovered.empty())
+                            highestIter--;
                     }
-                    airportsNotCovered.erase(airportsNotCovered.begin());
-                }
-                if ((i != 0) && (/*oldRightEndPointDuplicated && */ isUncovered(oldRightEndPoint) && oldRightEndPoint != leftEndpoint))
-                {
-                    //cout << "adding oldRightEndPoint: " << oldRightEndPoint << " to airportsNotCovered oldRightEndPointDuplicated: " << oldRightEndPointDuplicated<< endl;
-                    newlyUncoveredAirportPositions.push_back(oldRightEndPoint);
-                }
-                if (oldRightEndPoint != rightEndpoint && !airportsNotCovered.empty())
-                {
-                    const int reduceBy = rightEndpoint - oldRightEndPoint;
-                    //bestCostForInner -= reduceBy;
-                    //cout << " bestCostForInner reduced by " << reduceBy << " to " << bestCostForInner << " due to change in right endpoint" << endl;
-                    //assert(bestCostForInner >= 0);
+                    else
+                    {
+                        break;
+                    }
                 }
             }
-            if (oldLeftEndPoint != leftEndpoint)
+            if ((i != 0) && (isUncovered(oldLeftEndPoint) && (oldLeftEndPoint != rightEndpoint)))
             {
-                //cout << "left end point changed" << endl;
-                if (!airportsNotCovered.empty())
-                {
-                    auto highestIter = airportsNotCovered.end();
-                    highestIter--;
-                    while (true)
-                    {
-                        if (airportsNotCovered.empty())
-                            break;
-                        if (*highestIter >= leftEndpoint + minDistance)
-                        {
-                            //cout << "removing: " << *highestIter << " due to change left end point" << endl;
-                            if (hasPrevious(airportsNotCovered, highestIter))
-                            {
-                                const int diffToRemove = *highestIter - previous(airportsNotCovered, highestIter);
-                                //cout << " removing diff: " << diffToRemove << " due to change left end point";
-                                maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
-                            }
-
-                            highestIter = airportsNotCovered.erase(highestIter);
-                            if (!airportsNotCovered.empty())
-                                highestIter--;
-                        }
-                        else
-                        {
-                            //cout << "Not removing " << *highestIter << " due to left end point change" << endl;
-                            break;
-                        }
-                    }
-                }
-                //assert(leftEndpoint == oldLeftEndPoint);
-                if ((i != 0) && (/*oldLeftEndPointDuplicated &&*/ isUncovered(oldLeftEndPoint) && (oldLeftEndPoint != rightEndpoint)))
-                {
-                    //cout << "adding oldLeftEndPoint: " << oldLeftEndPoint << " to airportsNotCovered oldLeftEndPointDuplicated: " << oldLeftEndPointDuplicated  << endl;
-                    newlyUncoveredAirportPositions.push_back(oldLeftEndPoint);
-                }
-                if (oldLeftEndPoint != leftEndpoint && !airportsNotCovered.empty())
-                {
-                    const int reduceBy = oldLeftEndPoint - leftEndpoint;
-                    //bestCostForInner -= reduceBy;
-                    //cout << " bestCostForInner reduced by " << reduceBy << " to " << bestCostForInner << " due to change in left endpoint" << endl;
-                    //assert(bestCostForInner >= 0);
-                }
+                newlyUncoveredAirportPositions.push_back(oldLeftEndPoint);
             }
+        }
 
-            for (const auto uncoveredAirportPos : newlyUncoveredAirportPositions)
+        for (const auto uncoveredAirportPos : newlyUncoveredAirportPositions)
+        {
+            if (airportsNotCovered.find(uncoveredAirportPos) == airportsNotCovered.end())
             {
-                if (airportsNotCovered.find(uncoveredAirportPos) == airportsNotCovered.end())
+                airportsNotCovered.insert(uncoveredAirportPos);
+                auto newUncoveredIter = airportsNotCovered.find(uncoveredAirportPos);
+                const bool hasLeft = newUncoveredIter != airportsNotCovered.begin();
+                int left = -1;
+                if (hasLeft)
                 {
-                    airportsNotCovered.insert(uncoveredAirportPos);
-                    auto newUncoveredIter = airportsNotCovered.find(uncoveredAirportPos);
-                    const bool hasLeft = newUncoveredIter != airportsNotCovered.begin();
-                    int left = -1;
-                    if (hasLeft)
-                    {
-                        newUncoveredIter--;
-                        left = *newUncoveredIter;
-                        newUncoveredIter++;
-                    }
+                    newUncoveredIter--;
+                    left = *newUncoveredIter;
                     newUncoveredIter++;
-                    const bool hasRight = newUncoveredIter != airportsNotCovered.end();
-                    int right = -1;
-                    if (hasRight)
-                    {
-                        right = *newUncoveredIter;
-                    }
-                    if (hasLeft && hasRight)
-                    {
-                        // Broken up a previously successive pair; remove their contribution.
-                        maxDiffOfSuccessiveUncoveredPairs.remove(right - left);
-                    } 
-                    if (hasLeft)
-                    {
-                        maxDiffOfSuccessiveUncoveredPairs.add(uncoveredAirportPos - left);
-                    }
-                    if (hasRight)
-                    {
-                        maxDiffOfSuccessiveUncoveredPairs.add(right - uncoveredAirportPos);
-                    }
+                }
+                newUncoveredIter++;
+                const bool hasRight = newUncoveredIter != airportsNotCovered.end();
+                int right = -1;
+                if (hasRight)
+                {
+                    right = *newUncoveredIter;
+                }
+                if (hasLeft && hasRight)
+                {
+                    // Broken up a previously successive pair; remove their contribution.
+                    maxDiffOfSuccessiveUncoveredPairs.remove(right - left);
+                } 
+                if (hasLeft)
+                {
+                    maxDiffOfSuccessiveUncoveredPairs.add(uncoveredAirportPos - left);
+                }
+                if (hasRight)
+                {
+                    maxDiffOfSuccessiveUncoveredPairs.add(right - uncoveredAirportPos);
                 }
             }
+        }
 
 
         bestCostForInner = 0;
