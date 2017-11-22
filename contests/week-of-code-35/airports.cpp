85adac42e0b2c69c06f1c618a2bf5f017e4f8c0c
Simon St James <kdedevel@etotheipiplusone.com>
2017-11-22 09:01:11 +0000

    Begin to tidy up the hideousness of the min/max/prev/next bits of dealing with sets.

--- contests/week-of-code-35/airports.cpp
+++ contests/week-of-code-35/airports.cpp
@@ -44,6 +44,120 @@ class MaxTracker
 
 };
 
+template <typename T>
+class Set
+{
+    public:
+        void add(const T& value)
+        {
+            m_set.insert(value);
+            if (!m_hasMaxValue || value > m_maxValue)
+            {
+                m_maxValue = value;
+                m_hasMaxValue = true;
+            }
+            if (!m_hasMinValue || value < m_minValue)
+            {
+                m_minValue = value;
+                m_hasMinValue = true;
+            }
+            validate();
+        }
+        void remove(const T& value)
+        {
+            auto valueIter = m_set.find(value);
+            assert(valueIter != m_set.end());
+            valueIter = m_set.erase(valueIter);
+            if (m_set.empty())
+            {
+                m_hasMinValue = false;
+                m_hasMaxValue = false;
+            }
+            else
+            {
+                if (valueIter == m_set.end())
+                {
+                    assert(value == m_maxValue);
+                    assert(value != m_minValue);
+                    valueIter--;
+                    m_maxValue = *valueIter;
+                }
+                else
+                {
+                    assert(value != m_maxValue);
+                    if (value == m_minValue)
+                    {
+                        m_minValue = *valueIter;
+                    }
+                }
+            }
+            validate();
+        }
+        bool contains(const T& value) const
+        {
+            return (m_set.find(value) != m_set.end());
+        }
+        typename set<T>::iterator find(const T& value)
+        {
+            return m_set.find(value);
+        }
+        bool empty() const
+        {
+            return m_set.empty();
+        }
+        T min() const
+        {
+            assert(!m_set.empty());
+            return m_minValue;
+        }
+        T max() const
+        {
+            assert(!m_set.empty());
+            return m_maxValue;
+        };
+        bool hasNext(typename set<T>::const_iterator valueIter) const
+        {
+            return !m_set.empty() && valueIter != m_set.end() && *valueIter != m_maxValue;
+        }
+        T next(typename set<T>::const_iterator valueIter) const
+        {
+            assert(hasNext(valueIter));
+            valueIter++;
+            return *valueIter;
+        }
+        bool hasPrevious(typename set<T>::const_iterator valueIter) const
+        {
+            return !m_set.empty() && valueIter != m_set.begin();
+        }
+        T previous(typename set<T>::const_iterator valueIter) const
+        {
+            assert(hasPrevious(valueIter));
+            valueIter--;
+            return *valueIter;
+        }
+        void validate()
+        {
+            return;
+            vector<T> v(m_set.begin(), m_set.end());
+            assert(v.empty() == m_set.empty());
+            if (v.empty())
+                return;
+            sort(v.begin(), v.end());
+            assert(m_hasMinValue);
+            assert(v.front() == m_minValue);
+            assert(m_hasMaxValue);
+            assert(v.back() == m_maxValue);
+            assert(!hasNext(find(m_maxValue)));
+            assert(!hasPrevious(find(m_minValue)));
+        }
+    private:
+        set<T> m_set;
+        T m_maxValue;
+        bool m_hasMaxValue = false;
+        T m_minValue;
+        bool m_hasMinValue = false;
+};
+
 vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay, int minDistance)
 {
     vector<int> results;
@@ -63,37 +177,7 @@ vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay
     {
         return airportPos < leftEndpoint + minDistance && airportPos > rightEndpoint - minDistance;
     };
-    // Some utils for dealing with set<> ugliness - though still just as ugly, I suppose!
-    auto hasNext = [](const set<int>& collection, set<int>::iterator setIter)
-    {
-        if (setIter == collection.end())
-            return false;
-        setIter++;
-        if (setIter == collection.end())
-            return false;
-        return true;
-    };
-    auto hasPrevious = [](const set<int>& collection, set<int>::iterator setIter)
-    {
-        if (collection.empty())
-            return false;
-        if (setIter == collection.begin())
-            return false;
-        return true;
-    };
-    auto next = [&hasNext](const set<int>& collection, set<int>::iterator setIter)
-    {
-        assert(hasNext(collection, setIter));
-        setIter++;
-        return *setIter;
-    };
-    auto previous = [&hasPrevious](const set<int>& collection, set<int>::iterator setIter)
-    {
-        assert(hasPrevious(collection, setIter));
-        setIter--;
-        return *setIter;
-    };
-    set<int> airportsNotCovered;
+    Set<int> airportsNotCovered;
     for (int i = 0; i < airportAddedOnDay.size(); i++)
     {
         const int airportPos = airportAddedOnDay[i];
@@ -132,14 +216,20 @@ vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay
         {
             // The right endpoint has been moved rightwards; erase the now-covered airports from the beginning of
             // airportsNotCovered (updating maxDiffOfSuccessiveUncoveredPairs as we go) ...
-            while (!airportsNotCovered.empty() && *airportsNotCovered.begin() <= rightEndpoint - minDistance)
+            while (!airportsNotCovered.empty() && airportsNotCovered.min() <= rightEndpoint - minDistance)
             {
-                if (hasNext(airportsNotCovered, airportsNotCovered.begin()))
+                const auto oldMin = airportsNotCovered.min();
+                //cout << "Erasing from left: " <<  oldMin << endl;
+                airportsNotCovered.remove(oldMin);
+                if (!airportsNotCovered.empty())
                 {
-                    const int diffToRemove = next(airportsNotCovered, airportsNotCovered.begin()) -  *airportsNotCovered.begin();
+                    const int diffToRemove = airportsNotCovered.min() - oldMin;
+                    //cout << "removing from left diffToRemove: " << diffToRemove << endl;
+
+                    assert(diffToRemove > 0);
                     maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
+
                 }
-                airportsNotCovered.erase(airportsNotCovered.begin());
             }
             // ... and maybe add the previous endpoint as an uncovered airport.
             if ((i != 0) && (isUncovered(oldRightEndPoint) && oldRightEndPoint != leftEndpoint))
@@ -151,30 +241,19 @@ vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay
         {
             // Left endpoint has been moved leftwards; erase the now-covered airports from the end of airportsNotCovered, etc.
             // Same as the right endpoint case, really, except dealing with iterators is more ugly!
-            if (!airportsNotCovered.empty())
+            while (!airportsNotCovered.empty() && airportsNotCovered.max() >= leftEndpoint + minDistance)
             {
-                auto highestIter = airportsNotCovered.end();
-                highestIter--;
-                while (true)
-                {
-                    if (airportsNotCovered.empty())
-                        break;
-                    if (*highestIter >= leftEndpoint + minDistance)
-                    {
-                        if (hasPrevious(airportsNotCovered, highestIter))
-                        {
-                            const int diffToRemove = *highestIter - previous(airportsNotCovered, highestIter);
-                            maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
-                        }
+                const auto oldMax = airportsNotCovered.max();
+                //cout << "Erasing from right: " <<  oldMax << endl;
 
-                        highestIter = airportsNotCovered.erase(highestIter);
-                        if (!airportsNotCovered.empty())
-                            highestIter--;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                airportsNotCovered.remove(oldMax);
+                if (!airportsNotCovered.empty())
+                {
+                    const int diffToRemove = oldMax - airportsNotCovered.max();
+                    assert(diffToRemove > 0);
+                    maxDiffOfSuccessiveUncoveredPairs.remove(diffToRemove);
+                    //cout << "removing from right diffToRemove: " << diffToRemove << endl;
+
                 }
             }
             if ((i != 0) && (isUncovered(oldLeftEndPoint) && (oldLeftEndPoint != rightEndpoint)))
@@ -187,14 +266,16 @@ vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay
         // and keep maxDiffOfSuccessiveUncoveredPairs up-to-date.
         for (const auto uncoveredAirportPos : newlyUncoveredAirportPositions)
         {
-            if (airportsNotCovered.find(uncoveredAirportPos) == airportsNotCovered.end())
+            if (!airportsNotCovered.contains(uncoveredAirportPos))
             {
-                airportsNotCovered.insert(uncoveredAirportPos);
+                airportsNotCovered.add(uncoveredAirportPos);
                 const auto newUncoveredIter = airportsNotCovered.find(uncoveredAirportPos);
-                const bool hasLeft = hasPrevious(airportsNotCovered, newUncoveredIter);
-                const int left = (hasLeft ? previous(airportsNotCovered, newUncoveredIter) : -1);
-                const bool hasRight = hasNext(airportsNotCovered, newUncoveredIter);
-                const int right = (hasRight ? next(airportsNotCovered, newUncoveredIter) : -1);
+                const bool hasLeft = airportsNotCovered.hasPrevious(newUncoveredIter);
+                const int left = (hasLeft ? airportsNotCovered.previous(newUncoveredIter) : -1);
+                const bool hasRight = airportsNotCovered.hasNext(newUncoveredIter);
+                const int right = (hasRight ? airportsNotCovered.next(newUncoveredIter) : -1);
+                //cout << "uncoveredAirportPos: " << uncoveredAirportPos << " hasLeft? " << hasLeft << " left: " << left << " hasRight: " << hasRight << " right: " << right << endl;
+
                 if (hasLeft && hasRight)
                 {
                     // Broken up a previously successive pair; remove their contribution.
@@ -218,10 +299,10 @@ vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay
         if (!airportsNotCovered.empty())
         {
             bestCostForInner = numeric_limits<int>::max();
-            const int minUncoveredAirport = *airportsNotCovered.begin();
-            auto maxUncoveredAirportIter = airportsNotCovered.end();
-            maxUncoveredAirportIter--;
-            const int maxUncoveredAirport = *maxUncoveredAirportIter;
+            const int minUncoveredAirport = airportsNotCovered.min();
+            const int maxUncoveredAirport = airportsNotCovered.max();
+            //cout << "minUncoveredAirport: " << minUncoveredAirport << " maxUncoveredAirport: " << maxUncoveredAirport << endl;
+
             // If left endpoint extended to cover everything.
             bestCostForInner = min(bestCostForInner,  (leftEndpoint + minDistance) - minUncoveredAirport);
             // If right end point extended to cover everything.
@@ -250,6 +331,46 @@ vector<int> findMinCostOfArrangementForDays(const vector<int>& airportAddedOnDay
 
 int main(int argc, char** argv)
 {
+#if 0
+    Set<int> s;
+    s.add(3);
+    cout << "Added 3" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.add(6);
+    cout << "Added 6" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.add(4);
+    cout << "Added 4" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.add(5);
+    cout << "Added 5" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    cout << "has next 5? " << s.hasNext(s.find(5)) << endl;
+    cout << "next 5: " << s.next(s.find(5)) << endl;
+    cout << "has next 6? " << s.hasNext(s.find(6)) << endl;
+    cout << "has previous 5? " << s.hasPrevious(s.find(5)) << endl;
+    cout << "previous 5: " << s.previous(s.find(5)) << endl;
+    cout << "has previous 3? " << s.hasPrevious(s.find(3)) << endl;
+    s.remove(4);
+    cout << "removed 4" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.remove(3);
+    cout << "removed 3" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.remove(6);
+    cout << "removed 6" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.add(7);
+    cout << "added 7" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.remove(7);
+    cout << "removed 7" << endl;
+    cout << "s max: " << s.max() << " s min: " << s.min() << endl;
+    s.remove(5);
+    cout << "removed 5" << endl;
+    cout << "empty? " << s.empty() << endl;
+#endif
+
     ios::sync_with_stdio(false);
     int Q;
     cin >> Q;
