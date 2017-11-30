53395a08ac6f83b3da204ff17f2db4138a820555
Simon St James <kdedevel@etotheipiplusone.com>
2017-11-30 15:59:48 +0000

    Document and simplify Nimble Game.

--- hackerrank/nimble-game.cpp
+++ hackerrank/nimble-game.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 int main()
 {
+    // Took me a while, as it is tempting to consider this game from a "square-centric" point
+    // of view, whereas you really have to take a coin-centric one!
+    //
+    // Take a single-coin example: this is plainly equal to a Nim-game with one pile, where
+    // the pile size is equal to the square that the coin is on (moving the coin to the square
+    // j squares to the left is equivalent to removing j stones from the pile).
+    //
+    // With multiple coins, we simply have a Nim-game with multiple piles, where each pile corresponds
+    // to a coin, and has stones equal to the coin's square.
+    //
+    // Thus, we could use the logic, for each square i:
+    //
+    //  const auto pileSize = i;
+    //  for (int j = 0; j < numCoinsInSquare; j++)
+    //  {
+    //      stonePileXorSum ^= pileSize;
+    //  }
+    //
+    // but since numCoinsInSquare can be very large (~10^9), this will be too slow.  So we note that
+    // pileSize ^ pileSize = 0; pileSize ^ pileSize ^ pileSize = pileSize; pileSize ^ pileSize ^ pileSize ^ pileSize = 0
+    // etc to see that we only care about whether numCoinsInSquare is odd or even; if odd, xor stonePileXorSum with it;
+    // otherwise, leave stonePileXorSum unchanged for this square.
     int T;
     cin >> T;
 
@@ -20,9 +42,9 @@ int main()
             int numCoinsInSquare;
             cin >> numCoinsInSquare;
 
-            const auto pileSize = i;
-            for (int j = 0; j < (numCoinsInSquare % 2); j++)
+            if ((numCoinsInSquare % 2) == 1)
             {
+                const auto pileSize = i;
                 stonePileXorSum ^= pileSize;
             }
         }
