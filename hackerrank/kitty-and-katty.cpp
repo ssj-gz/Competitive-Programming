5c343b97c655ae7ce1c7ee26cf4b5c22eaf6238b
Simon St James <kdedevel@etotheipiplusone.com>
2017-12-01 20:02:43 +0000

    Documented and cleaned up Kitty and Katty.

--- hackerrank/kitty-and-katty.cpp
+++ hackerrank/kitty-and-katty.cpp
@@ -6,79 +6,38 @@
 
 using namespace std;
 
-enum WinState {KittyWin = 0, KattyWin = 1, Unknown};
-enum Player {Kitty = 0, Katty = 1};
-
-map<pair<array<int, 3>, Player>, WinState> blah;
-
-WinState getWinState(const array<int, 3>& state, Player player, int indent = 0)
-{
-    const string indentation = string(indent, ' ');
-    cout << indentation << "state: " << state[0] << "," << state[1] << "," << state[2] << " player: " << (player == Kitty ? "Kitty" : "Katty") << endl;
-    if (blah.find({state, player}) != blah.end())
-    {
-        //cout << "returning memo-ised";
-        return blah[{state, player}];
-    }
-    WinState winState = Unknown;
-    const int numStones = state[0] + state[1] + state[2];
-    if (numStones == 1)
-    {
-        if (state[0] == 1)
-        {
-            winState = static_cast<WinState>(1 - player);
-        }
-        else if (state[1] == 1)
-        {
-            winState = KittyWin;
-        }
-        else 
-        {
-            assert(state[2] == 1);
-            winState = KattyWin;
-        }
-
-    }
-    else
-    {
-        array<int, 3> stateCopy = state;
-        bool haveWinningMove = false;
-        for (int x = 0; x < 3; x++)
-        {
-            if (stateCopy[x] > 0)
-            {
-                stateCopy[x]--;
-                for (int y = 0; y < 3; y++)
-                {
-                    if (stateCopy[y] > 0)
-                    {
-                        stateCopy[y]--;
-                        const int differenceMod3 = (3 + x - y) % 3;
-                        stateCopy[differenceMod3]++;
-                        const auto moveWinState = getWinState(stateCopy, static_cast<Player>(1 - player), indent + 1);
-                        stateCopy[differenceMod3]--;
-                        stateCopy[y]++;
-
-                        if (static_cast<int>(moveWinState) == static_cast<int>(player))
-                            haveWinningMove = true;
-                    }
-                }
-                stateCopy[x]++;
-            }
-        }
-        if (haveWinningMove)
-            winState = static_cast<WinState>(player);
-        else
-            winState = static_cast<WinState>(1 - player);
-    }
-    assert(winState != Unknown);
-    blah[{state, player}] = winState;
-    cout << indentation  << "state: " << state[0] << "," << state[1] << "," << state[2] << " is a winner for " << (winState == KittyWin ? "Kitty" : "Katty") << endl;
-    return winState;
-}
-
 int main()
 {
+    // Fairly easy one, though I did a brute-force verification as practice :)
+    //
+    // The trick is to notice that :
+    //
+    //  a) the numbers on the blocks are more-or-less irrelevant; only their value mod 3 is important; thus
+    //  we can model the problem as three piles of blocks, where pile A has the blocks whose values mod 3 are 0; B has those whose value is 2; and C has 
+    //  those whose value is 3; 
+    //
+    //  b) after each move, the number of blocks always blocks reduces by exactly 1 (easily verified); and
+    //
+    //  c) the person who can make the last move always wins.
+    //
+    // To see c), let's look at all the states the penultimate move can be in i.e. all the contents of the 3 piles when there are exactly two blocks remaining.
+    //
+    //  i) 2 0 0 ii) 0 2 0 iii) 0 0 2 iv) 1 1 0 v) 1 0 1 vi) 0 1 1
+    //     A B C     A B C      A B C     A B C    A B C     A B C
+    //
+    // For i), ii) and iii), the only play is to take the two blocks and add one block to pile A (since the two blocks have the same value mod 3); pile A is the "0 mod 3"
+    // pile, and so the player who makes this move wins i.e. the player who makes the final move wins.
+    //
+    // For iv),  we can take the two remaining blocks in either order: taking the one from A then the one from B means we need to add to pile C (0 - 1 is 2, mod 3);
+    // taking the one from B then the one from A means we add to pile B (1 - 0 is 1, mod 3) - thus if the current player is Kitty, she makes the move that adds to B;
+    // if current player is Katty, she makes the move that adds to C.  Either way, the player who makes the final move wins.
+    //
+    // v) and vi) are basically identical to iv) - we have two orders for picking the last two blocks, and the current player can ensure that the last
+    // block is added to B or C and so should choose the one that makes them win.
+    //
+    // So in all cases, the current player i.e. the one who makes the final move is the winner.  Coupled with a), we see that, if n >= 2,
+    // then Kitty makes the last move if and only if n is even i.e. for n >= 2, Kitty wins if and only if n is even.  She also wins if n == 1.
+    
     int T;
     cin >> T;
 
@@ -87,25 +46,12 @@ int main()
         int n;
         cin >> n;
 
-        if (n == 1 || n == 2 || ((n % 2) == 0))
+        if (n == 1 || ((n % 2) == 0))
             cout << "Kitty";
         else
             cout << "Katty";
         cout << endl;
     }
-#if 0
-    for (int n = 1; n <= 50; n++)
-    {
-        cout << "Computing for n = " << n << endl;
-        array<int, 3> state = {{0, 0, 0}};
-        for (int j = 1; j <= n; j++)
-        {
-            state[j % 3]++;
-        }
-        const auto winState = getWinState(state, Kitty);
-        cout << "n: " << n << " winner: " << (winState == KittyWin ? "Kitty" : "Katty") << endl;
-    }
-#endif
 }
 
 
