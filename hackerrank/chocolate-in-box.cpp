5dced3ae88f047f29c6244c1ab4fa866d9790a02
Simon St James <kdedevel@etotheipiplusone.com>
2017-12-06 07:56:20 +0000

    Document Chocolate in Box.

--- hackerrank/chocolate-in-box.cpp
+++ hackerrank/chocolate-in-box.cpp
@@ -6,23 +6,31 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
+    // Easy 70(!) points - just note that this is basically Nim in disguise, and that
+    // a winning first move is just one that makes the Nim sum of the number of chocolates
+    // in each container zero.  Then note that, for each container, we can find the 
+    // nim sum of all other containers except this one very easily, then compute the number of chocolates
+    // that would need to be in this container for the nim sum to be zero, and make sure
+    // that that number is less than the original number of chocolates in this container i.e.
+    // that we could make this container have the required number of chocolates in one move.
+    int numContainers;
+    cin >> numContainers;
 
     int nimSum = 0;
-    vector<int> a(N);
-    for (int i = 0; i < N; i++)
+    vector<int> numChocsInContainer(numContainers);
+    for (int containerIndex = 0; containerIndex < numContainers; containerIndex++)
     {
-        cin >> a[i];
-        nimSum ^= a[i];
+        cin >> numChocsInContainer[containerIndex];
+        nimSum ^= numChocsInContainer[containerIndex];
     }
 
     const int originalNimSum = nimSum;
     int numWinningFirstMoves = 0;
-    for (int i = 0; i < N; i++)
+    for (int containerIndex = 0; containerIndex < numContainers; containerIndex++)
     {
-        const int nimSumWithoutContainer = originalNimSum ^ a[i];
-        if (nimSumWithoutContainer < a[i])
+        const int nimSumWithoutContainer = originalNimSum ^ numChocsInContainer[containerIndex];
+        const int numInThisContainerToMakeNimSumZero = nimSumWithoutContainer;
+        if (numInThisContainerToMakeNimSumZero < numChocsInContainer[containerIndex])
         {
             numWinningFirstMoves++;
         }
