91c03b014a594a0fe962231e5b1cadb7f0e11063
Simon St James <kdedevel@etotheipiplusone.com>
2017-12-14 21:45:45 +0000

    Generic snapshot.

--- contests/world-codesprint-12/keko-the-brilliant.cpp
+++ contests/world-codesprint-12/keko-the-brilliant.cpp
@@ -58,7 +58,7 @@ bool canMakeRight(Node* node, Node* parent, int maxRInParents, const vector<bool
 
     if (nodeIdsThatCanChange[node->id])
     {
-        //cout << "Made a change - node " << (node->id + 1) << " from " << node->r << " to " << maxRInParents << endl;
+        cout << "Made a change - node " << (node->id + 1) << " from " << node->r << " to " << maxRInParents << endl;
         node->r = maxRInParents;
     }
 
@@ -105,7 +105,7 @@ int main()
         vector<vector<bool>> allChoices;
         generateChoices(emptyChoices, 0, numChanges, allChoices);
 
-        //cout << "numChanges: " << numChanges << " choices:" << allChoices.size() << endl;
+        cout << "numChanges: " << numChanges << " choices:" << allChoices.size() << endl;
         for (const auto& choice : allChoices)
         {
             for (int i = 0; i < n; i++)
@@ -113,20 +113,20 @@ int main()
                 nodes[i].r = r[i];
             }
 
-            //cout << "Checking if can make right" << endl;
+            cout << "Checking if can make right" << endl;
             if (canMakeRight(rootNode, nullptr, 0, choice))
             {
-                //cout << "Wee" << endl;
+                cout << "Wee" << endl;
                 for (int i = 0; i < n; i++)
                 {
-                    //cout << "Node: " << (i + 1) << " parent: " << (nodes[i].parent ? nodes[i].parent->id + 1 : -1) << " r: " << nodes[i].r << endl;
+                    cout << "Node: " << (i + 1) << " parent: " << (nodes[i].parent ? nodes[i].parent->id + 1 : -1) << " r: " << nodes[i].r << endl;
                 }
                 cout << numChanges << endl;
                 return 0;
             }
 
         }
-        //cout << endl;
+        cout << endl;
 
         numChanges++;
 
