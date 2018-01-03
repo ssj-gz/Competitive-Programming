f0733cdcf1aeed8f91cf0ea49f3647f95472f0ba
Simon St James <kdedevel@etotheipiplusone.com>
2018-01-03 10:02:20 +0000

    Generic snapshot of Ticket to Ride.

--- experiments/ticket-to-ride.cpp
+++ experiments/ticket-to-ride.cpp
@@ -11,6 +11,25 @@ struct Node
     bool doNotExplore = false;
 };
 
+int countDescendants(Node* node, Node* parentNode)
+{
+    if (node->doNotExplore)
+        return 0;
+    int numDescendants = 1; // Current node.
+
+    for (const auto& child : node->neighbours)
+    {
+        if (child == parentNode)
+            continue;
+        if (child->doNotExplore)
+            continue;
+
+        numDescendants += countDescendants(child, node);
+    }
+
+    return numDescendants;
+}
+
 vector<Node*> getDescendants(Node* node, Node* parentNode)
 {
     vector<Node*> descendants;
@@ -26,28 +45,68 @@ vector<Node*> getDescendants(Node* node, Node* parentNode)
         if (child->doNotExplore)
             continue;
 
-        //descendants.push_back(child);
         const auto& childDescendants = getDescendants(child, node);
         descendants.insert(descendants.end(), childDescendants.begin(), childDescendants.end());
     }
 
-#if 0
-    cout << "Node: " << node->index << " descendants: ";
-    for (const auto& node : descendants)
-    {
-        cout << node->index << " ";
-    }
-    cout << endl;
-#endif
-
     return descendants;
+}
+
+int findCentroidAux(Node* currentNode, Node* parentNode, const int totalNodes, Node** centroid)
+{
+    if (currentNode->doNotExplore)
+        return 0;
+    int numDescendents = 1;
+
+    bool childHasTooManyDescendants = false;
+
+    for (const auto& child : currentNode->neighbours)
+    {
+        if (child == parentNode)
+            continue;
+        if (child->doNotExplore)
+            continue;
+
+        const auto numChildDescendants = findCentroidAux(child, currentNode, totalNodes, centroid);
+        if (numChildDescendants > totalNodes / 2)
+        {
+            // Not the centroid, but can't break here - must continue processing children.
+            childHasTooManyDescendants = true;
+        }
+
+        numDescendents += numChildDescendants;
+    }
+
+    if (!childHasTooManyDescendants)
+    {
+        // No child has more than totalNodes/2 descendants, but what about the remainder of the graph?
+        const auto nonChildDescendants = totalNodes - numDescendents;
+        if (nonChildDescendants <= totalNodes / 2)
+        {
+            assert(centroid);
+            *centroid = currentNode;
+        }
+    }
+
+    return numDescendents;
 }
 
 Node* findCentroid(Node* startNode)
 {
+    const auto totalNumNodes = countDescendants(startNode, nullptr);
+    Node* centroid = nullptr;
+    findCentroidAux(startNode, nullptr, totalNumNodes, &centroid);
+    assert(centroid);
+    return centroid;
+}
+
+
+#if 0
+Node* findCentroidBruteForce(Node* startNode, int numNodes)
+{
     int numInGraph = 1;
-    vector<Node*> allNodes = getDescendants(startNode, nullptr);
-    const auto numNodes = allNodes.size();
+    //vector<Node*> allNodes = getDescendants(startNode, nullptr);
+    //const auto numNodes = countDescendants(startNode, nullptr);
 
     if (numNodes == 1)
         return startNode;
@@ -71,6 +130,7 @@ Node* findCentroid(Node* startNode)
     assert(false);
     return nullptr;
 }
+#endif
 
 void decompose(Node* startNode, vector<vector<int>>& blee, int indentLevel = 0)
 {
@@ -81,16 +141,19 @@ void decompose(Node* startNode, vector<vector<int>>& blee, int indentLevel = 0)
         assert(node1 != node2);
         if (node1->index > node2->index)
             swap(node1, node2);
-        cout << "Counted " << node1->index << "," << node2->index << endl;
         blee[node1->index][node2->index]++;
         blee[node2->index][node1->index]++;
     };
     const string indent(indentLevel, ' ');
-    cout << indent << "Decomposing graph containing " << startNode->index << endl;
+    //cout << indent << "Decomposing graph containing " << startNode->index << endl;
+    const auto numNodes = countDescendants(startNode, nullptr);
     Node* centroid = findCentroid(startNode);
-    const auto numNodes = getDescendants(centroid, nullptr).size();
-    cout << indent << " centroid: " << centroid->index << " num nodes: " << numNodes << endl;
-    cout << " indentLevel: " << indentLevel << " numNodes: " << numNodes << endl;
+    //cout << indent << " centroid: " << centroid->index << " num nodes: " << numNodes << endl;
+    //cout << " indentLevel: " << indentLevel << " numNodes: " << numNodes << endl;
+
+    static int numNodesTotal = 0;
+    numNodesTotal += numNodes;
+    cout << "numNodesTotal: " << numNodesTotal << endl;
 
     if (centroid->doNotExplore)
         return;
@@ -120,8 +183,9 @@ void decompose(Node* startNode, vector<vector<int>>& blee, int indentLevel = 0)
 int main()
 {
     srand(time(0));
-    const int maxNodes = 10;
+    const int maxNodes = 10000;
     const int numNodes = (rand() % maxNodes) + 1;
+    //const int numNodes = 20000;
     vector<Node> nodes(numNodes);
 
     int nodeIndex = 0;
@@ -168,6 +232,7 @@ int main()
             if (i == j)
                 continue;
             cout << "i: " << i << " j: " << j << " blee: " << blee[i][j] << endl;
+            assert(blee[i][j] == 1);
         }
     }
     
