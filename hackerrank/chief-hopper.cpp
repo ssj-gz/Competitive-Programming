ab609752a2b2b5d8b17833c490f316e7b058569c
Simon St James <kdedevel@etotheipiplusone.com>
2018-01-23 11:19:12 +0000

    Tidied up Chief Hopper.

--- hackerrank/chief-hopper.cpp
+++ hackerrank/chief-hopper.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 int main()
 {
-#if 1
     int N;
     cin >> N;
 
@@ -16,146 +15,29 @@ int main()
     {
         cin >> heights[i];
     }
-#endif
-    //while (true)
+
+    int startEnergy = 0;
+    for (int i = N - 1; i >= 0; i--)
     {
-#if 0
-        const int N = rand() % 10 + 1;
-        vector<int> heights;
-        for (int i = 0; i < N; i++)
+        if (i >= 1)
         {
-            heights.push_back(rand() % 10000 + 1);
-        }
-#endif
-
-#if 0
-        int64_t startEnergy = 0;
-        while (true)
-        {
-            int64_t energy = startEnergy;
-            cout << "startEnergy: " << startEnergy << endl;
-            for (int i = 0; i < N; i++)
-            {
-                cout << "i: " << i << " energy: " << energy << endl;
-                if (heights[i] > energy)
-                {
-                    energy -= (heights[i] - energy);
-                } 
-                else
-                {
-                    energy += (energy - heights[i]);
-                }
-
-                //if (energy < 0)
-                //break;
-                int64_t dbgEnergy = (startEnergy << (i + 1));
-                for (int j = 0; j <= i; j++)
-                {
-                    dbgEnergy -= (heights[j] << (i - j));
-                }
-                cout << "Wee: " << energy << " woo: " << dbgEnergy << " glah: " << -(dbgEnergy - (startEnergy << (i + 1))) << endl;
-                assert(energy == dbgEnergy);
-
-            }
-
-            cout << "Done - final energy: " << energy << endl;
-
-            if (energy >= 0)
-            {
-                cout << startEnergy << endl;
-                break;
-            }
-
-
-            startEnergy++;
-        }
-
-        auto fleep = [&heights]()
-        {
-            int sausage = 0;
-            const int N = heights.size();
-            for (int i = 0; i < N; i++)
-            {
-                sausage += (1 << (N - 1 - i)) * heights[i];
-                cout << "i: " << i << " sausage: " << sausage << endl;
-            }
-            return sausage;
-
-        };
-
-        const int sausage = fleep();
-
-#if 0
-        for (int i = N - 1; i >= 0; i--)
-        {
-            auto blee = heights[i];
-
-            heights[i] = 0;
-            int k = i;
-            while (blee > 0 && k >= 0)
-            {
-                if ((blee % 2) == 1)
-                {
-                    heights[k]++;
-                }
-
-                if (k > 0)
-                {
-                    blee >>= 1;
-                    k--;
-                }
-                else
-                {
-                    break;
-                }
-
-                cout << "i: " << i << " k: " << k << " blee: " << blee << endl;
-            }
-            //if (i == 0)
-            heights[0] += blee;
-        }
-#endif
-#endif
-        int flipple = 0;
-        for (int i = N - 1; i >= 0; i--)
-        {
-            if (i >= 1)
-            {
             heights[i - 1] += heights[i] / 2;
-            }
-            else
-            {
-                flipple = heights[i] / 2;
-            }
-            heights[i] %= 2;
         }
-        for (const auto h : heights)
+        else
         {
-            if (h != 0)
-            {
-                flipple++;
-                break;
-            }
+            startEnergy = heights[i] / 2;
         }
-
-#if 0
-        cout << "Final heights: " << endl;
-        for (const auto h : heights)
-        {
-            cout << h << " ";
-        }
-        cout << endl;
-#endif
-
-        //const int pickle = fleep();
-        //cout << "sausage: " << sausage << " pickle: " << pickle << endl;
-        //assert(sausage == pickle);
-
-        //const int blah = (startEnergy * (1 << N)) - sausage;
-        //cout << "blah: " << blah << endl;
-
-        //cout << "startEnergy: " << startEnergy << " flipple: " << flipple << endl;
-        //assert(startEnergy == flipple);
-        cout << flipple << endl;
+        heights[i] %= 2;
     }
+
+    for (const auto h : heights)
+    {
+        if (h != 0)
+        {
+            startEnergy++;
+            break;
+        }
+    }
+
+    cout << startEnergy << endl;
 }
