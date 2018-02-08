ffce9758c1e83404e4ff294b231a589fa85f8adf
Simon St James <kdedevel@etotheipiplusone.com>
2018-02-08 08:55:57 +0000

    Snapshot of Brute Force "A Race Against Time".

--- contests/week-of-code-36/a-race-against-time.cpp
+++ contests/week-of-code-36/a-race-against-time.cpp
@@ -1,8 +1,58 @@
+// Simon St James (ssjgz) - 2018-02-08
+#define BRUTE_FORCE
+//#define SUBMISSION
+#ifdef SUBMISSION
+#define NDEBUG
+#undef BRUTE_FORCE
+#endif
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <cassert>
 
 using namespace std;
+
+int64_t minCostBruteForce(const vector<int>& heights, const vector<int64_t>& prices)
+{
+    const int n = heights.size();
+    vector<int64_t> minCostStartingWithStudent(n);
+    minCostStartingWithStudent.back() = 0; // Just run instantly to finish line.
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        cout << "i: " << i << endl;
+        int64_t minCostStartingHere = numeric_limits<int64_t>::max();
+        bool forcedExchange = false;
+        for (int nextStudent = i + 1; nextStudent < n; nextStudent++)
+        {
+            minCostStartingHere = (nextStudent - i) + prices[nextStudent] + abs(heights[nextStudent] - heights[i]) + minCostStartingWithStudent[nextStudent];
+            cout << " nextStudent: " << nextStudent << " minCostStartingHere becomes " << minCostStartingHere << endl;
+            if (heights[nextStudent] > heights[i])
+            {
+                cout << "  Forced exchange!" << endl;
+                forcedExchange = true;
+                break;
+            }
+        }
+        if (!forcedExchange)
+        {
+            // Run to finish line.
+            minCostStartingHere = min(minCostStartingHere, static_cast<int64_t>(n - i));
+            cout << " Run straight to finish line" << endl;
+        }
+        cout << " minCostStartingHere: " << minCostStartingHere << endl;
+        minCostStartingWithStudent[i] = minCostStartingHere;
+
+
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "minCostStartingWithStudent[" << i << "] = " << minCostStartingWithStudent[i] << endl;
+    }
+
+    return minCostStartingWithStudent[0];
+}
 
 int main()
 {
@@ -17,7 +67,7 @@ int main()
         cin >> heights[i];
     }
 
-    vector<int> prices(N);
+    vector<int64_t> prices(N);
     prices[0] = 0; // No charge for Madison.
 
     for (int i = 1; i <= N - 1; i++)
@@ -25,18 +75,9 @@ int main()
         cin >> prices[i];
     }
 
-    vector<int64_t> totalSumAndPriceForLastM(N);
-    totalSumAndPriceForLastM.back() = 0; // No charge for last guy.
-
-    for (int i = N - 2; i >= 0; i--)
-    {
-        int64_t lowestSumAndPriceFromHere = numeric_limits<int64_t>::max();
-        const int 
-        if (i + 2 < N)
-        {
-            const int64_t costOfSkipping = lowestSumAndPriceFromHere[i + 2] + 
-        }
-        
-    }
+#ifdef BRUTE_FORCE
+    const auto resultBruteForce = minCostBruteForce(heights, prices);
+    cout << "resultBruteForce: " << resultBruteForce << endl;
+#endif
 
 }
