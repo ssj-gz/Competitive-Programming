4756dc11f330faec31141966ce409ff51e9c9457
Simon St James <kdedevel@etotheipiplusone.com>
2018-02-10 08:22:20 +0000

    Generic snapshot - still incorrect!

--- contests/week-of-code-36/expert-computation.cpp
+++ contests/week-of-code-36/expert-computation.cpp
@@ -6,6 +6,19 @@
 
 using namespace std;
 
+int64_t calcF(int i, const vector<int64_t>& h, const vector<int64_t>& c, const vector<int64_t>& l)
+{
+    int64_t maxInRange = numeric_limits<int64_t>::min();
+    for (int j = 1; j <= i - l[i]; j++)
+    {
+        const auto blah = h[j] * c[i] - c[j] * h[i];
+        //cout << " j: " << j << " blah: " << blah << endl;
+        maxInRange = max(maxInRange, blah);
+    }
+
+    return maxInRange;
+}
+
 int main()
 {
     int n;
@@ -25,20 +38,15 @@ int main()
     vector<int64_t> c(n + 1);
     vector<int64_t> l(n + 1);
 
-    vector<int64_t> G(n + 1);
-    G[1] = 0;
 
     h[1] = x[1];
     c[1] = y[1];
     l[1] = z[1];
 
     vector<int64_t> F(n + 1);
-    int64_t maxInRange = 0;
-    for (int j = 1; j <= 1 - l[j]; j++)
-    {
-        maxInRange = max(maxInRange, h[j] * c[1] - c[j] * h[1]);
-    }
-    F[1] = maxInRange;
+    F[1] = calcF(1, h, c, l);
+    vector<int64_t> G(n + 1);
+    G[1] = F[1];
 
     //vector<int64_t> maxHCCH(n);
     //maxHCCH[0] = h[0] 
@@ -52,18 +60,12 @@ int main()
         c[i] = y[i] ^ G[i - 1];
         l[i] = z[i] ^ G[i - 1];
 
-        //int64_t maxInRange = numeric_limits<int64_t>::min();
-        int64_t maxInRange = 0;
-        for (int j = 1; j <= i - l[i]; j++)
-        {
-            maxInRange = max(maxInRange, h[j] * c[i] - c[j] * h[i]);
-        }
-        F[i] = maxInRange;
+        F[i] = calcF(i, h, c, l);
 
         G[i] = (G[i - 1] + F[i]) % M;
 
         //cout << "i: " << i << " h: " << h[i] << " c: " << c[i] << " l: " << l[i] <<  " F: " << F[i] << " G: " << G[i] << endl;
-        //assert(l[i] <= i - 1);
+        assert(l[i] <= i - 1);
     }
 
     cout << G[n] << endl;
