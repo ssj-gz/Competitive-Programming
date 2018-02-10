dad2136ce56c65816b445e6544c5605df33d08fa
Simon St James <kdedevel@etotheipiplusone.com>
2018-02-10 12:33:03 +0000

    Generic snapshot.

--- contests/week-of-code-36/expert-computation.cpp
+++ contests/week-of-code-36/expert-computation.cpp
@@ -10,30 +10,60 @@ using namespace std;
 
 const int64_t M = 1000000007UL;
 
-int64_t calcF(const int i, const vector<int64_t>& h, const vector<int64_t>& c, const vector<int64_t>& l)
+bool assertOnExit = false;
+
+int64_t calcF(const int i, const vector<int64_t>& h, const vector<int64_t>& c, const vector<int64_t>& l, bool quiet = false)
 {
-    //cout << "calcF - i: " << i << endl;
+    if (!quiet)
+        cout << "calcF - i: " << i << endl;
     int64_t maxInRange = numeric_limits<int64_t>::min();
     for (int j = 1; j <= i - l[i]; j++)
     {
         const int64_t blah = h[j] * c[i] - c[j] * h[i];
         maxInRange = max(maxInRange, blah);
-        //cout << " j: " << j << " blah: " << blah << " maxInRange: " << maxInRange << endl;
+        if (!quiet)
+            cout << " j: " << j << " blah: " << blah << " maxInRange: " << maxInRange << endl;
     }
 
     return maxInRange;
 }
+void blibble(const int i, const vector<int64_t>& h, const vector<int64_t>& c, const vector<int64_t>& l, bool quiet = false)
+{
+    int64_t maxInRange = numeric_limits<int64_t>::min();
+    int maxJ = -1;
+    static int oldMaxJ = -1;
+    for (int j = 1; j <= i; j++)
+    {
+        const int64_t blah = h[j] * c[i] - c[j] * h[i];
+        if (blah > maxInRange)
+        {
+            maxInRange = blah;
+            maxJ = j;
+        }
+
+        if (!quiet)
+            cout << "blibble j: " << j << " blah: " << blah << " maxInRange: " << maxInRange << endl;
+    }
+
+    cout << "i: " << i << " maxJ: " << maxJ << endl;
+    if (maxJ < oldMaxJ)
+    {
+        cout << "******** oldMaxJ: " << oldMaxJ << endl;
+        assertOnExit = true;
+    }
+    oldMaxJ = maxJ;
+}
 
 bool generateTestcaseAux(int n)
 {
-    cout << "Trying to generate a testcase of length " << n << endl;
+    //cout << "Trying to generate a testcase of length " << n << endl;
     vector<int64_t> x(n + 1);
     vector<int64_t> y(n + 1);
     vector<int64_t> z(n + 1);
 
     const int maxRandomNumber = 100;
-    x[1] = rand() % maxRandomNumber;
-    y[1] = rand() % maxRandomNumber;
+    x[1] = rand() % maxRandomNumber + 1;
+    y[1] = rand() % maxRandomNumber + 1;
     z[1] = 0;
 
     vector<int64_t> h(n + 1);
@@ -45,7 +75,7 @@ bool generateTestcaseAux(int n)
     l[1] = z[1];
 
     vector<int64_t> F(n + 1);
-    F[1] = calcF(1, h, c, l);
+    F[1] = calcF(1, h, c, l, true);
     vector<int64_t> G(n + 1);
     G[1] = F[1];
 
@@ -61,7 +91,9 @@ bool generateTestcaseAux(int n)
         random_shuffle(randomNumbers.begin(), randomNumbers.end());
         for (int j = 0; j < maxRandomNumber; j++)
         {
-            x[i] = randomNumbers[j];
+            //x[i] = randomNumbers[j];
+            const auto nextH = h[i - 1] + randomNumbers[j];
+            x[i] = nextH ^ G[i - 1];
             h[i] = x[i] ^ G[i - 1];
             if (1 <= h[i] && h[i] <= 10'000'000 && h[i] > h[i - 1])
             {
@@ -76,6 +108,8 @@ bool generateTestcaseAux(int n)
         for (int j = 0; j <maxRandomNumber; j++)
         {
             y[i] = randomNumbers[j];
+            //const auto nextC = rand() % (1000) + 1;
+            //y[i] = nextC ^ G[i - 1];
             c[i] = y[i] ^ G[i - 1];
             if (1 <= c[i] && c[i] <= 10'000'000)
             {
@@ -89,7 +123,9 @@ bool generateTestcaseAux(int n)
         random_shuffle(randomNumbers.begin(), randomNumbers.end());
         for (int j = 0; j < maxRandomNumber; j++)
         {
-            z[i] = randomNumbers[j];
+            //z[i] = randomNumbers[j];
+            const int nextL = randomNumbers[j] % i;
+            z[i] = nextL ^ G[i - 1];
             l[i] = z[i] ^ G[i - 1];
             if (0 <= l[i] && l[i] <= i - 1)
             {
@@ -103,7 +139,9 @@ bool generateTestcaseAux(int n)
         c[i] = y[i] ^ G[i - 1];
         l[i] = z[i] ^ G[i - 1];
 
-        F[i] = calcF(i, h, c, l);
+        F[i] = calcF(i, h, c, l, true);
+        //if (F[i] >= 0)
+            //return false;
         if (F[i] < 0)
         {
             const auto blee = (F[i] / -M) + 1;
@@ -119,6 +157,15 @@ bool generateTestcaseAux(int n)
         assert(1 <= h[i] && h[i] <= 10'000'000);
         assert(h[i] > h[i - 1]);
     }
+
+    int numNonZeroZs = 0;
+    for (const auto a : z)
+    {
+        if (a != 0)
+            numNonZeroZs++;
+    }
+    if (numNonZeroZs < 2)
+        return false;
 
     cout << n << endl;
     for (int i = 1; i <= n; i++)
@@ -136,7 +183,8 @@ bool generateTestcaseAux(int n)
 
 void generateTestcase()
 {
-    const int n = rand() % 5 + 2;
+    //const int n = rand() % 7 + 2;
+    const int n = 8;
     while (true)
     {
         if (generateTestcaseAux(n))
@@ -198,8 +246,10 @@ int main(int argc, char** argv)
         l[i] = z[i] ^ G[i - 1];
 
         F[i] = calcF(i, h, c, l);
+        blibble(i, h, c, l);
         if (F[i] < 0)
         {
+            cout << "F was -ve" << endl;
             const auto blee = (F[i] / -M) + 1;
             F[i] += blee * M;
         }
@@ -215,5 +265,7 @@ int main(int argc, char** argv)
     }
 
     cout << G[n] << endl;
+
+    assert(!assertOnExit);
 
 }
