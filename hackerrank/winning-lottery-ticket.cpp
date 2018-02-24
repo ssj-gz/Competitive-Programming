b1f1d9cae395859e98ee20c5a9ae27f220a63a90
Simon St James <kdedevel@etotheipiplusone.com>
2018-02-24 09:58:08 +0000

    Minor tweaks and renamings to Winning Lottery Ticket.

--- hackerrank/winning-lottery-ticket.cpp
+++ hackerrank/winning-lottery-ticket.cpp
@@ -9,45 +9,45 @@ int main()
 {
     // Easy one - for each ticket, reduce it to a binary number 10-digit B, where the ith digit
     // of B is 1 if and only if the ticket contains the decimal number i.  Call this B
-    // the "digit digest".
+    // the "digit bitmask".
     // Then a pair of tickets is a Winning Pair if and only if the bitwise OR of their
-    // digits is a digest representing the ticket 012345789 (i.e. a 10-digit binary number where
+    // digits is a bitmask representing the ticket 012345789 (i.e. a 10-digit binary number where
     // every bit is 1).  It is then a simple matter to count all winning pairs.
     int numTickets;
     cin >> numTickets;
 
-    vector<int64_t> numTicketsWithDigitDigest(1 << 10);
+    const uint32_t fullDigitsBitmask = (1 << 10) - 1;
+    vector<int64_t> numTicketsWithDigitBitmask(fullDigitsBitmask + 1);
 
     for (int i = 0; i < numTickets; i++)
     {
         string ticket;
         cin >> ticket;
 
-        uint32_t ticketDigitDigest = 0;
+        uint32_t ticketDigitBitmask = 0;
         for (const auto digitChar : ticket)
         {
             const uint32_t digit = digitChar - '0';
-            ticketDigitDigest |= (1 << digit);
-            assert(ticketDigitDigest >= 0 && ticketDigitDigest < numTicketsWithDigitDigest.size());
+            ticketDigitBitmask |= (1 << digit);
 
         }
-        numTicketsWithDigitDigest[ticketDigitDigest]++;
+        assert(ticketDigitBitmask >= 0 && ticketDigitBitmask < numTicketsWithDigitBitmask.size());
+        numTicketsWithDigitBitmask[ticketDigitBitmask]++;
     }
 
-    const uint32_t fullDigitsDigest = (1 << 10) - 1;
     int64_t numWinningPairs = 0;
-    for (int ticket1Digest = 0; ticket1Digest < numTicketsWithDigitDigest.size(); ticket1Digest++)
+    for (int ticket1Bitmask = 0; ticket1Bitmask < numTicketsWithDigitBitmask.size(); ticket1Bitmask++)
     {
-        for (int ticket2Digest = ticket1Digest + 1; ticket2Digest < numTicketsWithDigitDigest.size(); ticket2Digest++)
+        for (int ticket2Bitmask = ticket1Bitmask + 1; ticket2Bitmask < numTicketsWithDigitBitmask.size(); ticket2Bitmask++)
         {
-            if ((ticket1Digest | ticket2Digest) == fullDigitsDigest)
+            if ((ticket1Bitmask | ticket2Bitmask) == fullDigitsBitmask)
             {
-                numWinningPairs += numTicketsWithDigitDigest[ticket1Digest] * numTicketsWithDigitDigest[ticket2Digest];
+                numWinningPairs += numTicketsWithDigitBitmask[ticket1Bitmask] * numTicketsWithDigitBitmask[ticket2Bitmask];
             }
         }
     }
-    // Pairs of tickets that each have full digit digests.
-    numWinningPairs += numTicketsWithDigitDigest[fullDigitsDigest] * (numTicketsWithDigitDigest[fullDigitsDigest] - 1) / 2;
+    // Pairs of tickets that each have full digit Bitmasks.
+    numWinningPairs += numTicketsWithDigitBitmask[fullDigitsBitmask] * (numTicketsWithDigitBitmask[fullDigitsBitmask] - 1) / 2;
 
     cout << numWinningPairs << endl; 
 }
