97b9b6b556f434b85ceb56d91ed57f0a988959d3
Simon St James <kdedevel@etotheipiplusone.com>
2018-02-28 16:16:51 +0000

    Documentation for "Cloudy Day".

--- hackerrank/cloudy-day.cpp
+++ hackerrank/cloudy-day.cpp
@@ -8,6 +8,21 @@ using namespace std;
 
 int main()
 {
+    // Very easy one - for each town from left to right, find the set of clouds that are covering that town
+    // i.e. the number of intervals (where each interval is the region covered by a cloud - its startPoint to endPoint)
+    // that contain the town's location.  There is a well-known algorithm for this involving sorting the intervals by their
+    // left endpoint, and separately by their right: maintain an iterator (nextCloudIter) to the first interval that hasn't covered a town yet
+    // (or which hasn't been shown not to cover a town) - when we reach a new town, iterate through all clouds in order
+    // of their startPoint to find all that have their startPoint occur before the town's location (these may cover the town), and add them to
+    // cloudsCoveringCurrentPosByEndpoint.
+    // After doing this, find all clouds in cloudsCoveringCurrentPosByEndpoint whose endpoint ends before the current town location - easy as they
+    // are sorted by their endpoints!
+    // After this, cloudsCoveringCurrentPosByEndpoint is precisely the set of clouds that covers the current town.
+    // If there is exactly one cloud c covering the current town, then make a note that removing c will make the population of the current town
+    // sunny (i.e. add the population to populationCoveredJustByThisCloud).  
+    // If there are no clouds covering the current town, then this town will always be sunny, so add it to populationOriginallySunny.
+    // The final result is populationOriginallySunny plus the population made sunny by removing the cloud that makes the most new people sunny
+    // i.e. the highest populationCoveredJustByThisCloud.
     int numTowns;
     cin >> numTowns;
 
