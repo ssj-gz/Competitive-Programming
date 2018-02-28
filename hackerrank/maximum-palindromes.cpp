3dc02f889bcfb05ca13d5f7198b16516ac5454da
Simon St James <kdedevel@etotheipiplusone.com>
2018-02-28 18:18:57 +0000

    Documented and cleaned-up "Maximum Palindromes".

--- hackerrank/maximum-palindromes.cpp
+++ hackerrank/maximum-palindromes.cpp
@@ -51,9 +51,6 @@ ostream& operator<<(ostream& os, const ModNum& toPrint)
     return os;
 }
 
-
-
-
 vector<int64_t> factorialLookup;
 vector<int64_t> factorialInverseLookup;
 
@@ -106,6 +103,35 @@ void buildFactorialLookups(int maxN)
 
 int main()
 {
+    // Fairly easy one - would have gotten it first time if I'd sat down with pen and paper, but oh well :)
+    // Firstly: note that a string s can be made into a palindrome if and only if:
+    //
+    //  i) s contains an even number of each letter; or
+    // ii) s contains an even number of each letter except for precisely one letter which occurs an odd number of times.
+    //
+    // In the case of ii), the palindrome is of odd length and is formed around the letter that occurs an odd number of times.
+    //
+    // Now, the number of such palindromes, if all the letters were distinct, would be (|s| / 2)! for case i), and again (|s| / 2)!
+    // (integer division rounds down) for case ii).  Intuitively, we consider only half the palindrome, and take all the permutations
+    // of letters in this half-palindrome: the other half is obviously the reverse of the first half.
+    //
+    // In the case where we have duplicated letters, we will overcount - if s = aaaa, there is one palindrome, not (|s| / 2)! == 2! = 4
+    // palindromes! Dealing with this is easy: if the letter x occurs n(x) times in the half-palindrome, then it will  contribute a factor
+    // of n(x)! to the overcount, so we must divide by this at the end; "denominatorInverse" represents the productive of these divisions
+    // over all letters.
+    //
+    // So, anyway - if the range contains an even number of each letters, then the can form a palindrome with the same size as the size
+    // of the range, and work out the number of ways of doing so using the above.
+    //
+    // If the range contains some letters that occur and odd number of times, then the situation is more complex: for each such letter
+    // x we can form a palindrome of odd length around x by removing precisely one of each other letter y that occurs an odd number
+    // of times, leading to a case ii) situation.  The length of such a palindrome is numLettersInRange - numLettersWithOddOccurrencesInRange + 1.
+    // We can work out, for each x, the number of distinct palindromes of this max length centred around x fairly easily by again 
+    // considering "half-palindromes", where here a half-palindrome is all letters to the left of the central x (the letters to the right of 
+    // this x must then be a mirror of this half-palindrome).  As it happens, the overcount is exactly the same as in the case
+    // where all letters occur an even number of times and so we must again simply multiply our result by denominatorInverse.
+    // So we can easily find the number of palindromes of max length centred around x for each x that occurs and odd number of times,
+    // and we just need to sum these.  And that's about it!
     string s;
     cin >> s;
 
@@ -138,9 +164,6 @@ int main()
 
     buildFactorialLookups(maxN);
 
-    //cout << "numQueries: " << numQueries << endl;
-
-
     for (int q = 0; q < numQueries; q++)
     {
         int l, r;
@@ -151,42 +174,43 @@ int main()
 
         LetterHistogram letterHistogramForRange = letterHistogramForPrefixSize[r + 1] - letterHistogramForPrefixSize[l];
         int numLettersWithOddOccurrencesInRange = 0;
-        ModNum denominator = 1;
+        ModNum denominatorInverse = 1;
         for (int letterIndex = 0; letterIndex < numLetters; letterIndex++)
         {
             const int numOccurencesOfLetterInRange = letterHistogramForRange.letterCount[letterIndex];
             if ((numOccurencesOfLetterInRange % 2) == 1)
                 numLettersWithOddOccurrencesInRange++;
 
-            denominator *= factorialInverseLookup[numOccurencesOfLetterInRange / 2];
+            // Divide by the number of occurrence of the letter in the half-palindrome, factorial'd - note that this works
+            // whether numOccurencesOfLetterInRange is odd or even!
+            denominatorInverse *= factorialInverseLookup[numOccurencesOfLetterInRange / 2];
         }
         ModNum numWithMaxPalindrome = 0;
         const int numLettersInRange = r - l + 1;
         if (numLettersWithOddOccurrencesInRange == 0)
         {
+            // Every letter occurs an even number of times; the max length of the palindrome is numLettersInRange.
             assert((numLettersInRange % 2) == 0);
-            numWithMaxPalindrome = factorialLookup[numLettersInRange / 2] * denominator;
+            const int numInHalfPalindrome = numLettersInRange / 2;
+            numWithMaxPalindrome = factorialLookup[numInHalfPalindrome] * denominatorInverse;
         }
         else
         {
+            // At least one letter occurs an odd number of times; the max length of the palindrome is numLettersInRange - numLettersWithOddOccurrencesInRange + 1.
             ModNum numerator = 0;
             for (int letterIndex = 0; letterIndex < numLetters; letterIndex++)
             {
                 const int numOccurencesOfLetterInRange = letterHistogramForRange.letterCount[letterIndex];
                 if ((numOccurencesOfLetterInRange % 2) == 1)
                 {
+                    // Form an odd-length palindrome around this letter, "removing" all occurrences of the other letters that have
+                    // an odd number of occurrences in this range.
                     const int numInHalfPalindrome = (numLettersInRange - numLettersWithOddOccurrencesInRange) / 2;
-                    ModNum palindromeContribToNumerator = ModNum(factorialLookup[numInHalfPalindrome]) /* * factorialInverseLookup[numOccurencesOfLetterInRange / 2]*/;
-
-                    numerator += palindromeContribToNumerator;
+                    numWithMaxPalindrome += factorialLookup[numInHalfPalindrome] * denominatorInverse;
                 }
             }
-            numWithMaxPalindrome = numerator * denominator;
-
         }
 
         cout << numWithMaxPalindrome.value() << endl;
     }
-
-
 }
