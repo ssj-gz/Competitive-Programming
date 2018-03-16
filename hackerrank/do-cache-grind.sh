69093ffe9fcffbc8e3753457a9a1040565fb32b6
Simon St James <kdedevel@etotheipiplusone.com>
2018-03-16 20:53:32 +0000

    Allow the executable sent to valgrind to be passed as a command-line argument (defaulting to ./a.out).

--- hackerrank/do-cache-grind.sh
+++ hackerrank/do-cache-grind.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
+if [[ $# -gt 0 ]]; then
+  export EXECUTABLE=$1
+else
+  export EXECUTABLE=./a.out
+fi
+
+
 rm cachegrind.out.*
-valgrind --tool=cachegrind ./a.out 
+valgrind --tool=cachegrind $EXECUTABLE
