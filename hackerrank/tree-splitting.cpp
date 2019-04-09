f05d8cbfd724c2616e719fade8d4470cf1058b05
Simon St James <kdedevel@etotheipiplusone.com>
2019-04-09 10:07:48 +0100

    Begin laying groundwork for optimised solution - do Heavy-Light decomposition.

--- hackerrank/tree-splitting.cpp
+++ hackerrank/tree-splitting.cpp
@@ -9,20 +9,53 @@ struct Node
     vector<Node*> neighbours;
     vector<Node*> children;
     Node* parent = nullptr;
+    int originalNumDescendants = 0;
 };
 
 
-void fixParentChildAndHeights(Node* node, Node* parent = nullptr)
+int fixParentChildAndCountDescendants(Node* node, Node* parentNode)
 {
-    node->parent = parent;
-
-    node->children.erase(remove(node->children.begin(), node->children.end(), parent), node->children.end());
+    node->originalNumDescendants = 1;
+    if (parentNode)
+        node->children.erase(find(node->children.begin(), node->children.end(), parentNode));
 
     for (auto child : node->children)
+        node->originalNumDescendants += fixParentChildAndCountDescendants(child, node);
+
+    return node->originalNumDescendants;
+}
+
+
+vector<vector<Node*>> heavyChains;
+
+// Build up heavyChains; move the heavy child of each node to the front of that node's children.
+void doHeavyLightDecomposition(Node* node, bool followedHeavyEdge)
+{
+    if (followedHeavyEdge)
     {
-        fixParentChildAndHeights(child, node);
+        // Continue this chain.
+        heavyChains.back().push_back(node);
+    }
+    else
+    {
+        // Start a new chain.
+        heavyChains.push_back({node});
+    }
+    if (!node->children.empty())
+    {
+        auto heaviestChildIter = max_element(node->children.begin(), node->children.end(), [](const Node* lhs, const Node* rhs)
+                {
+                return lhs->originalNumDescendants < rhs->originalNumDescendants;
+                });
+        iter_swap(node->children.begin(), heaviestChildIter);
+        auto heavyChild = node->children.front();
+        doHeavyLightDecomposition(heavyChild, true);
+
+        for (auto lightChildIter = node->children.begin() + 1; lightChildIter != node->children.end(); lightChildIter++)
+            doHeavyLightDecomposition(*lightChildIter, false);
     }
 }
+
 
 int numInComponent(Node* nodeInComponent, Node* previousNode = nullptr)
 {
@@ -62,6 +95,27 @@ vector<int> bruteForce(vector<Node>& nodes, const vector<int>& queries)
     return queryResults;
 }
 
+vector<int> findSolutionOptimised(vector<Node>& nodes, const vector<int>& queries)
+{
+    Node* rootNode = &(nodes.front());
+    fixParentChildAndCountDescendants(rootNode, nullptr);
+
+    doHeavyLightDecomposition(rootNode, false);
+    vector<int> queryResults;
+    int previousAnswer = 0;
+    for (int encryptedNodeIndex : queries)
+    {
+        const int nodeIndex = (encryptedNodeIndex ^ previousAnswer) - 1;
+
+        Node* nodeToRemove = &(nodes[nodeIndex]);
+        const int thisAnswer = -1; // TODO
+        queryResults.push_back(thisAnswer);
+
+        previousAnswer = thisAnswer;
+    }
+    return queryResults;
+
+}
 
 int main()
 {
