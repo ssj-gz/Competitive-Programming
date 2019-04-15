44bc8397dc61a02d3d86a3ffc1405aec0cd9a5f4
Simon St James <kdedevel@etotheipiplusone.com>
2019-04-15 21:58:04 +0100

    Various renamings for Repetitive K Sums.

--- hackerrank/repetitive-k-sums.cpp
+++ hackerrank/repetitive-k-sums.cpp
@@ -1,5 +1,5 @@
 // Simon St James (ssjgz) - 2019-04-15
-#define SUBMISSION
+//#define SUBMISSION
 #define BRUTE_FORCE
 #ifdef SUBMISSION
 #undef BRUTE_FORCE
@@ -16,14 +16,14 @@
 
 using namespace std;
 
-class ChoiceIndices
+class Choice
 {
     public:
-        ChoiceIndices(int numIndices)
+        Choice(int numIndices)
             : m_indices(numIndices)
         {
         }
-        ChoiceIndices() = default;
+        Choice() = default;
         int& operator[](int i)
         {
             return m_indices[i];
@@ -41,7 +41,7 @@ class ChoiceIndices
 };
 struct Sum
 {
-    ChoiceIndices choiceIndices;
+    Choice choiceIndices;
     int64_t value = 0;
 };
 
@@ -51,24 +51,24 @@ bool operator<(const Sum& lhs, const Sum& rhs)
     return lhs.value < rhs.value;
 }
 
-void findSums(const vector<int64_t>& a, ChoiceIndices& indices, int indexToChange, int maxValueOfIndex, vector<Sum>& destSums)
+void findSums(const vector<int64_t>& a, Choice& choice, int indexToChange, int maxValueOfIndex, vector<Sum>& destSums)
 {
     if (indexToChange == -1)
     {
         int64_t value = 0;
-        for (int i = 0; i < indices.numIndices(); i++)
+        for (int i = 0; i < choice.numIndices(); i++)
         {
-            value += a[indices[i]];
+            value += a[choice[i]];
         }
         Sum sum;
         sum.value = value;
-        sum.choiceIndices = indices;
+        sum.choiceIndices = choice;
         destSums.push_back(sum);
         //cout << "--" << endl;
-        //cout << "indices: " << endl;
-        //for (int i = 0; i < indices.numIndices(); i++)
+        //cout << "choice: " << endl;
+        //for (int i = 0; i < choice.numIndices(); i++)
         //{
-            //cout << indices[i] << " ";
+            //cout << choice[i] << " ";
         //}
         //cout << endl;
         //cout << "sum: " << value << endl;
@@ -77,16 +77,16 @@ void findSums(const vector<int64_t>& a, ChoiceIndices& indices, int indexToChang
 
     for (int indexValue = 0; indexValue <= maxValueOfIndex; indexValue++)
     {
-        indices[indexToChange] = indexValue;
-        findSums(a, indices, indexToChange - 1, indexValue, destSums); 
+        choice[indexToChange] = indexValue;
+        findSums(a, choice, indexToChange - 1, indexValue, destSums); 
     }
 }
 
 bool isSolutionCorrect(const vector<int64_t>& a, const vector<int64_t>& s, int k)
 {
-    ChoiceIndices indices(k);
+    Choice choice(k);
     vector<Sum> sums;
-    findSums(a, indices, k - 1, a.size() - 1, sums);
+    findSums(a, choice, k - 1, a.size() - 1, sums);
     sort(sums.begin(), sums.end());
     vector<int64_t> sortedSums;
     for (const auto& sum : sums)
@@ -108,10 +108,10 @@ int main(int argc, char* argv[])
         gettimeofday(&time,NULL);
         srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
 
-        //const int n = rand() % 10 + 1;
-        //const int k = rand() % n + 1;
-        const int n = 19;
-        const int k = 8;
+        const int n = rand() % 10 + 1;
+        const int k = rand() % n + 1;
+        //const int n = 19;
+        //const int k = 8;
         //const int maxValue = rand() % 100'000'000;
         const int maxValue = rand() % 100;
 
@@ -125,9 +125,9 @@ int main(int argc, char* argv[])
 
         cout << n << " " << k << endl;
 
-        ChoiceIndices indices(k);
+        Choice choice(k);
         vector<Sum> sums;
-        findSums(a, indices, k - 1, a.size() - 1, sums);
+        findSums(a, choice, k - 1, a.size() - 1, sums);
         stable_sort(sums.begin(), sums.end());
 
         for (const auto& x : sums)
@@ -166,48 +166,7 @@ int main(int argc, char* argv[])
 
         return 0;
     }
-#if 0
-    sort(a.begin(), a.end());
 
-    a = {1, 20, 21, 22, 23, 24, 25000 };
-
-    cout << "n: " << a.size() << " k: " << k << endl;
-    cout << "a:" << endl;
-    for (const auto x : a)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    ChoiceIndices indices(k);
-    vector<Sum> sums;
-    findSums(a, indices, k - 1, a.size() - 1, sums);
-    stable_sort(sums.begin(), sums.end());
-
-
-    cout << "sums: " << endl;
-    for (const auto& x : sums)
-    {
-        cout << x.value << " ";
-
-        cout << "(";
-        for (int i = 0; i < x.choiceIndices.numIndices(); i++)
-        {
-            cout << x.choiceIndices[i] << " ";
-        }
-        cout << ") "; 
-        cout << "(";
-        string indices(a.size(), '.');
-        for (int i = 0; i < x.choiceIndices.numIndices(); i++)
-        {
-            indices[x.choiceIndices[i]] = 'X';
-        }
-        cout << indices;
-        cout << ")" << endl;
-    }
-    cout << endl;
-    //assert(is_sorted(sums.begin(), sums.end()));
-#endif
     string line;
     getline(cin, line);
     istringstream tStream(line);
@@ -236,55 +195,51 @@ int main(int argc, char* argv[])
         }
 
 
-        ChoiceIndices indices(K);
+        Choice choice(K);
         vector<Sum> sums;
         vector<int64_t> dummyA(N);
-        findSums(dummyA, indices, K - 1, dummyA.size() - 1, sums);
-        vector<vector<ChoiceIndices>> choiceIndicesWithForLastIndex(N);
+        findSums(dummyA, choice, K - 1, dummyA.size() - 1, sums);
+        vector<vector<Choice>> choicesUsingFirstElements(N);
 
         for (const auto& sum : sums)
         {
-            choiceIndicesWithForLastIndex[sum.choiceIndices[K - 1]].push_back(sum.choiceIndices);
+            choicesUsingFirstElements[sum.choiceIndices[K - 1]].push_back(sum.choiceIndices);
         }
 
         sort(s.begin(), s.end());
         assert((s.front() % K) == 0);
         vector<int64_t> a(N);
         a[0] = s.front() / K;
-        std::priority_queue<int64_t, std::vector<int64_t>, std::greater<int64_t> >  blah;
-        blah.push(K * a[0]);
-        //cout << "First num: " << a[0] << endl;
+        std::priority_queue<int64_t, std::vector<int64_t>, std::greater<int64_t> >  expectedValuesUsingKnownElements;
+        expectedValuesUsingKnownElements.push(K * a[0]);
+
         int numKnownElementsOfA = 1;
         for (const auto x : s)
         {
-            //cout << "x: " << x << " top: " << (blah.empty() ? -1 : blah.top()) << endl;
-            if (blah.empty() || blah.top() != x)
+            if (expectedValuesUsingKnownElements.empty() || expectedValuesUsingKnownElements.top() != x)
             {
                 const int64_t newNum = x - (K - 1) * a[0];
-                //cout << "New!" << newNum << " x: " << x << endl;
                 a[numKnownElementsOfA] = newNum;
-                for (const auto& blee : choiceIndicesWithForLastIndex[numKnownElementsOfA])
+                for (const auto& choice : choicesUsingFirstElements[numKnownElementsOfA])
                 {
-                    int64_t value = 0;
+                    int64_t newValueUsingKnownElements = 0;
                     for (int i = 0; i < K; i++)
                     {
-                        assert(blee[i] <= numKnownElementsOfA);
-                        value += a[blee[i]];
+                        assert(choice[i] <= numKnownElementsOfA);
+                        newValueUsingKnownElements += a[choice[i]];
                     }
-                    //cout << " adding " << value << endl;
-                    blah.push(value);
+                    expectedValuesUsingKnownElements.push(newValueUsingKnownElements);
                 }
                 numKnownElementsOfA++;
-                blah.pop();
+                expectedValuesUsingKnownElements.pop();
                 if (numKnownElementsOfA == N)
                     break;
             }
             else
             {
-                blah.pop();
+                expectedValuesUsingKnownElements.pop();
             }
         }
-        //cout << "A:" << endl;
         for (const auto x : a)
         {
             cout << x << " ";
