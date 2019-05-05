16b99ceee2ff5bd33f95594f27766e93c3c11448
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-05 18:23:27 +0100

    Add elapsed time measurement (std::chrono) to Snippets.

--- hackerrank/snippets.cpp
+++ hackerrank/snippets.cpp
@@ -6,6 +6,16 @@ gettimeofday(&time,NULL);
 srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
 // End Initialise srand() using current time.
 
+// Elapsed time.
+#include <chrono>
+
+std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+
+std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
+std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
+// End Elapsed time.
+
 // Ukkonen's Algorithm.
 struct StateData
 {
