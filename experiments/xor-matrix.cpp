92d53a7a0d26e3b2d197253f07cb235909dcb3c4
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-06 11:14:59 +0100

    A bit of cleanup for Xor Matrix.

--- experiments/xor-matrix.cpp
+++ experiments/xor-matrix.cpp
@@ -7,119 +7,9 @@
 #endif
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cassert>
 
-#include <sys/time.h>
-
 using namespace std;
-
-vector<vector<vector<int64_t>>> blee;
-int narb = 0;
-
-vector<int64_t> glarp(int row, int col)
-{
-    if (row >= blee.size())
-    {
-        blee.resize(row + 1);
-    }
-    if (col >= blee[row].size())
-    {
-        blee[row].resize(col + 1);
-    }
-    if (!blee[row][col].empty())
-    {
-        return blee[row][col];
-    }
-    if (row == 0)
-    {
-        vector<int64_t> floop(narb, 0);
-        floop[col] = 1;
-        blee[row][col] = floop;
-
-        return floop;
-    }
-    const auto gleep1 = glarp(row - 1, col);
-    const auto gleep2 = glarp(row - 1, (col + 1) % narb);
-    vector<int64_t> floop(narb, 0);
-    for (int i = 0; i < narb; i++)
-    {
-        //floop[i] = gleep1[i] + gleep2[i];
-        floop[i] = (gleep1[i] + gleep2[i]) % 2;
-    }
-    blee[row][col] = floop;
-    return floop;
-}
-
-#if 0
-int main()
-{
-    map<string, int> blee;
-    //string current = "00100000000011100000000100100000000000001111000000111110000000000000000000010";
-    string current = "1010110101001011010101010000000000000000000000000000000000000000000000100011100111100111100111100111100111100111100111100111100111100111100111100111100111100111100111100111100111100111";
-    //string current = "111000101010101010101";
-    narb = current.size();
-    cout << "size: " << current.size() << endl;
-#if 0
-    int count = 0;
-    while (true)
-    {
-        cout << current << endl;
-        if (blee.find(current) != blee.end())
-        {
-            //cout << "Count: " << blee[current] << " repeated at: " << count << " gap: " << (count - blee[current]) << endl;
-        }
-        blee[current] = count;
-        string next;
-        for (int i = 0; i < current.size(); i++)
-        {
-            const int blah = current[i] - '0';
-            const int blah2 = current[(i + 1) % current.size()] - '0';
-            next.push_back('0' + (blah ^ blah2));
-        }
-        current = next;
-        count++;
-    }
-#endif
-#if 0
-    int row = 0;
-    uint64_t powerOf2 = 1;
-    while (true)
-    {
-        //cout << "row: " << row << " glarp: " << std::endl;
-        int blork = 0;
-        //if (row == powerOf2)
-        {
-            for (int i = 0; i < narb; i++)
-            {
-                const auto num = glarp(row, 0)[i];
-                cout 
-                    //<< " " 
-                    << num;
-                if (num == 1)
-                    blork++;
-                //cout << " " << (glarp(row, 0)[i] % 2);
-            }
-            cout << " row: " << row << " glarp: " << std::endl;
-            powerOf2 *= 2;
-        }
-        //cout << endl;
-        //if (blork == narb)
-            //break;
-        row++;
-    }
-#else
-    for (int col = 0; col < narb; col++)
-    {
-        for (int row = 0; row < 100; row++)
-        {
-            cout << glarp(row, col)[0];
-        }
-        cout << endl;
-    }
-#endif
-}
-#endif
 
 vector<int64_t> solutionBruteForce(const vector<int64_t>& originalA, int n, uint64_t m)
 {
@@ -168,26 +58,8 @@ vector<int64_t> solution(const vector<int64_t>& originalA, int n, uint64_t m)
     return currentRow;
 }
 
-#if 1
 int main(int argc, char* argv[])
 {
-    if (argc == 2)
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-
-        const int n = rand() % 20 + 1;
-        const int m = rand() % 1000 + 1;
-        cout << n << " " << m << endl;
-        for (int i = 0; i < n; i++)
-        {
-            cout << (rand() % 50 + 1) <<  " ";
-        }
-        cout << endl;
-        return 0;
-    }
     int n;
     cin >> n;
     uint64_t m;
@@ -220,6 +92,5 @@ int main(int argc, char* argv[])
     cout << endl;
     assert(bruteForceSolution == optimisedSolution);
 }
-#endif
 
 
