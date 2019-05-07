40a6e4b7eeb2cfea7963c8d767348dbc0a6d0639
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-07 09:23:17 +0100

    Splice in and adapt proper solution from Xor Matrix; add testcase generator; fix error in transformTable.

--- experiments/mixing-proteins.cpp
+++ experiments/mixing-proteins.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
+
+#include <sys/time.h>
 
 using namespace std;
+
+string calcRowOfMatrix(const string& originalProteinString, int n, uint64_t m)
+{
+    auto transform = [](char a, char b)
+    {
+        static const char transformTable[][4] =
+        {
+            {'A', 'B', 'C', 'D' },
+            {'B', 'A', 'D', 'C' },
+            {'C', 'D', 'A', 'B' },
+            {'D', 'C', 'B', 'A' }
+        };
+        return transformTable[a - 'A'][b - 'A'];
+
+    };
+
+    string currentRow(originalProteinString);
+
+    uint64_t powerOf2 = static_cast<uint64_t>(1) << static_cast<uint64_t>(63);
+    while (powerOf2 != 0)
+    {
+        if (m >= powerOf2)
+        {
+            // Set the row to be all the identity element : we'll update it by 
+            // transforming each element.
+            string currentPlusPowerOf2thRow(n, 'A');
+            for (int i = 0; i < n; i++)
+            {
+                currentPlusPowerOf2thRow[i] = transform(currentPlusPowerOf2thRow[i],  currentRow[i]);
+                currentPlusPowerOf2thRow[i] = transform(currentPlusPowerOf2thRow[i], currentRow[(i + powerOf2) % n]);
+            }
+            currentRow = currentPlusPowerOf2thRow;
+
+
+            m -= powerOf2;
+        }
+        powerOf2 >>= 1;
+    }
+    return currentRow;
+}
+
 
 string solveBruteForce(const string& originalProteinString, int N, int K)
 {
@@ -17,7 +61,7 @@ string solveBruteForce(const string& originalProteinString, int N, int K)
                 {'A', 'B', 'C', 'D' },
                 {'B', 'A', 'D', 'C' },
                 {'C', 'D', 'A', 'B' },
-                {'D', 'C', 'A', 'A' }
+                {'D', 'C', 'B', 'A' }
             };
             return transformTable[a - 'A'][b - 'A'];
 
@@ -33,6 +77,23 @@ string solveBruteForce(const string& originalProteinString, int N, int K)
 
 int main(int argc, char* argv[])
 {
+    if (argc == 2)
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+
+        const int N = rand() % 100 + 1;
+        const int K = rand() % 1000 + 1;
+        cout << N << " " << K << endl;
+        for (int i = 0; i < N; i++)
+        {
+            cout << static_cast<char>('A' + rand() % 4);
+        }
+        cout << endl;
+        return 0;
+
+    }
     int N, K;
     cin >> N >> K;
 
@@ -41,4 +102,7 @@ int main(int argc, char* argv[])
 
     const auto bruteForceSolution = solveBruteForce(proteinString, N, K);
     cout << "bruteForceSolution: " << bruteForceSolution << endl;
+    const auto optimisedSolution = calcRowOfMatrix(proteinString, N, K);
+    cout << "optimisedSolution: " << optimisedSolution << endl;
+    assert(bruteForceSolution == optimisedSolution);
 }
