3de6a71cfb56f186a789527516a24ef61093923a
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-08 13:39:52 +0100

    Brute-force implementation of P-Sequences.

---
+++ experiments/p-sequences.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+const int64_t modulus = 1'000'000'007ULL;
+
+class ModNum
+{
+    public:
+        ModNum(int64_t n = 0)
+            : m_n{n}
+        {
+        }
+        ModNum& operator+=(const ModNum& other)
+        {
+            m_n = (m_n + other.m_n) % ::modulus;
+            return *this;
+        }
+        ModNum& operator*=(const ModNum& other)
+        {
+            m_n = (m_n * other.m_n) % ::modulus;
+            return *this;
+        }
+        int64_t value() const { return m_n; };
+    private:
+        int64_t m_n;
+};
+
+ModNum operator+(const ModNum& lhs, const ModNum& rhs)
+{
+    ModNum result(lhs);
+    result += rhs;
+    return result;
+}
+
+ModNum operator*(const ModNum& lhs, const ModNum& rhs)
+{
+    ModNum result(lhs);
+    result *= rhs;
+    return result;
+}
+
+ostream& operator<<(ostream& os, const ModNum& toPrint)
+{
+    os << toPrint.value();
+    return os;
+}
+
+ModNum solutionBruteForce(int N, int P)
+{
+    ModNum result = 0;
+    vector<vector<ModNum>> firstNEndingOnP(N, vector<ModNum>(P + 1, 0));
+    for (int r = 0; r <= P; r++)
+    {
+        firstNEndingOnP[0][r] = 1;
+    }
+    for (int i = 1; i < N; i++)
+    {
+        for (int q = 1; q <= P; q++)
+        {
+            for (int r = 1; r * q <= P; r++)
+            {
+                firstNEndingOnP[i][q] += firstNEndingOnP[i - 1][r];
+            }
+        }
+    }
+
+    for (int r = 0; r <= P; r++)
+    {
+        result += firstNEndingOnP[N - 1][r];
+    }
+    return result;
+}
+
+
+int main(int argc, char* argv[])
+{
+    int N;
+    cin >> N;
+
+    int P;
+    cin >> P;
+
+    const auto bruteForceResult = solutionBruteForce(N, P);
+    cout << "bruteForceResult: " << bruteForceResult << endl;
+
+}
