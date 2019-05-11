95cdf34df1f16bd200c9455a685e2fadbb54686e
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-11 13:47:59 +0100

    Progress towards String Transmission - compute number of strings we can generate using up to K changes.

--- experiments/string-transmission.cpp
+++ experiments/string-transmission.cpp
@@ -37,29 +37,57 @@ bool isPeriodic(const string& a)
     return false;
 }
 
-void blah(string& alteredStringSoFar, int nextIndex, int K, const string& originalString, uint64_t& numFound)
+void blah(string& alteredStringSoFar, int nextIndex, int K, const string& originalString, uint64_t& numNonPeriodicFound, uint64_t& totalFound)
 {
     if (nextIndex == originalString.size())
     {
         if (numChanges(alteredStringSoFar, originalString) <= K && !isPeriodic(alteredStringSoFar))
         {
-            numFound++;
+            numNonPeriodicFound++;
         }
+        if (numChanges(alteredStringSoFar, originalString) <= K)
+            totalFound++;
         return;
     }
-    blah(alteredStringSoFar, nextIndex + 1, K, originalString, numFound);
+    blah(alteredStringSoFar, nextIndex + 1, K, originalString, numNonPeriodicFound, totalFound);
 
     alteredStringSoFar[nextIndex] = '0' + ('1' - alteredStringSoFar[nextIndex]);
-    blah(alteredStringSoFar, nextIndex + 1, K, originalString, numFound);
+    blah(alteredStringSoFar, nextIndex + 1, K, originalString, numNonPeriodicFound, totalFound);
     alteredStringSoFar[nextIndex] = '0' + ('1' - alteredStringSoFar[nextIndex]);
 } 
 
 uint64_t solveBruteForce(const string& binaryString, int N, int K)
 {
     string alteredStringSoFar = binaryString;
-    uint64_t numFound = 0;
-    blah(alteredStringSoFar, 0, K, binaryString, numFound);
-    return numFound;
+    uint64_t numNonPeriodicFound = 0;
+    uint64_t totalFound = 0;
+    blah(alteredStringSoFar, 0, K, binaryString, numNonPeriodicFound, totalFound);
+    cout << "totalFound: " << totalFound << endl;
+    return numNonPeriodicFound;
+}
+uint64_t solveOptimised(const string& binaryString, int N, int K)
+{
+    uint64_t result = 0;
+    vector<vector<uint64_t>> numOfLengthWithChanges(N + 1, vector<uint64_t>(K + 1, 0));
+    numOfLengthWithChanges[1][0] = 1;
+    numOfLengthWithChanges[1][1] = 1;
+    for (int i = 2; i <= N; i++)
+    {
+        for (int numChanges = 0; numChanges <= min(K, i); numChanges++)
+        {
+            numOfLengthWithChanges[i][numChanges] = numOfLengthWithChanges[i - 1][numChanges];
+            if (numChanges != 0)
+                numOfLengthWithChanges[i][numChanges] += numOfLengthWithChanges[i - 1][numChanges - 1];
+        }
+    }
+    uint64_t total = 0;
+    for (int numChanges = 0; numChanges <= K; numChanges++)
+    {
+        total += numOfLengthWithChanges[N][numChanges];
+    }
+    cout << "total optimised: " <<  total << endl;
+    cout << "GLoop: " << numOfLengthWithChanges[N][K] << endl;
+    return result;
 }
 
 int main(int argc, char* argv[])
@@ -79,6 +107,9 @@ int main(int argc, char* argv[])
 
         const auto solutionBruteForce = solveBruteForce(binaryString, N, K);
         cout << "solutionBruteForce: " << solutionBruteForce << endl;
+
+        const auto solutionOptimised = solveOptimised(binaryString, N, K);
+        cout << "solutionOptimised: " << solutionOptimised << endl;
     }
 
 
