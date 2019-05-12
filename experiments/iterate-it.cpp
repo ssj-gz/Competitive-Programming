30483eff4f7361d1a801f8fc67c826eaac93e137
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-12 14:24:29 +0100

    Further work on Iterate It!

--- experiments/iterate-it.cpp
+++ experiments/iterate-it.cpp
@@ -76,16 +76,6 @@ int solutionOptimised(const vector<int>& aOriginal)
     int numIterations = 0;
     const int numInBlock = 64;
     const int numBlocks = *std::max_element(a.begin(), a.end()) / numInBlock + 1;
-    vector<uint64_t> blocks(numBlocks);
-    for (const auto x : a)
-    {
-        const uint64_t glarp = (static_cast<uint64_t>(1) << static_cast<uint64_t>(x % numInBlock));
-        cout << "x: " << x << " glarp: " << glarp << " gleep: " << (x / numInBlock) << endl;
-        cout << "before: " << blocks[x / numInBlock] << endl;
-        blocks[x / numInBlock] |= glarp;
-        cout << "after: " << blocks[x / numInBlock] << endl;
-    }
-
     auto blocksToBinaryString = [numInBlock, numBlocks](const vector<uint64_t>& blocks)
     {
         string binaryString;
@@ -107,66 +97,137 @@ int solutionOptimised(const vector<int>& aOriginal)
         }
         return binaryString;
     };
-#if 1
-    vector<uint64_t> blocksShifted(numBlocks);
-    vector<vector<uint64_t>> blocksShiftedBy;
-    blocksShiftedBy.push_back(blocks);
-    for (int i = 1; i < numInBlock; i++)
+    while (!a.empty())
     {
-        blocksShiftedBy.push_back(blocksShiftedBy.back());
-        for (int blockNum = 0; blockNum < numBlocks; blockNum++)
+        vector<uint64_t> blocks(numBlocks);
+        for (const auto x : a)
         {
-            blocksShiftedBy.back()[blockNum] >>= 1;
-            if (blockNum + 1 < numBlocks)
+            blocks[x / numInBlock] |= (static_cast<uint64_t>(1) << static_cast<uint64_t>(x % numInBlock));
+        }
+
+        vector<uint64_t> blocksShifted(numBlocks);
+        vector<vector<uint64_t>> blocksShiftedBy;
+        blocksShiftedBy.push_back(blocks);
+        for (int i = 1; i < numInBlock; i++)
+        {
+            blocksShiftedBy.push_back(blocksShiftedBy.back());
+            for (int blockNum = 0; blockNum < numBlocks; blockNum++)
             {
-                //blocksShiftedBy.back()[blockNum] |= (blocksShiftedBy.back()[blockNum + 1] >> static_cast<uint64_t>(numInBlock - 1));
-                cout << "Next block: " << (blocksShiftedBy.back()[blockNum + 1]) << endl;
-                blocksShiftedBy.back()[blockNum] |= ((blocksShiftedBy.back()[blockNum + 1] & 1) << static_cast<uint64_t>(numInBlock - 1));
+                blocksShiftedBy.back()[blockNum] >>= 1;
+                if (blockNum + 1 < numBlocks)
+                {
+                    blocksShiftedBy.back()[blockNum] |= ((blocksShiftedBy.back()[blockNum + 1] & 1) << static_cast<uint64_t>(numInBlock - 1));
+                }
             }
         }
-    }
-#endif
 
-    for (int i = 0; i < numInBlock; i++)
-    {
-        const auto& blocks = blocksShiftedBy[i];
-        cout << "i: " << i << endl;
-        cout << "shifted as binary string: " << blocksToBinaryString(blocks) << endl;
-        vector<int> bBlah;
-        for (auto x : a)
+#if 0
+        for (int i = 0; i < numInBlock; i++)
         {
-            x -= i;
-            if (x >= 0)
-                bBlah.push_back(x);
+            const auto& blocks = blocksShiftedBy[i];
+            cout << "i: " << i << endl;
+            cout << "shifted as binary string: " << blocksToBinaryString(blocks) << endl;
+            vector<int> bBlah;
+            for (auto x : a)
+            {
+                x -= i;
+                if (x >= 0)
+                    bBlah.push_back(x);
+            }
+            vector<int> dbg;
+            for (int j = 0; j < numBlocks; j++)
+            {
+                const uint64_t block = blocks[j];
+                cout << "block[" << j << "]: " << block << endl;
+                for (int k = 0; k < numInBlock; k++)
+                {
+                    if (((block >> static_cast<uint64_t>(k)) & 1) == 1)
+                    {
+                        dbg.push_back(k + j * numInBlock);
+                    }
+                }
+            }
+            cout << "bBlah: " << endl;
+            for (const auto x : bBlah)
+            {
+                cout << " " << x;
+            }
+            cout << endl;
+            cout << "dbg: " << endl;
+            for (const auto x : dbg)
+            {
+                cout << " " << x;
+            }
+            cout << endl;
+            assert(bBlah == dbg);
         }
-        vector<int> dbg;
+#endif
+        vector<vector<int>> withRemainder(numInBlock);
+        for (const auto x : a)
+        {
+            withRemainder[x % numInBlock].push_back(x);
+        }
+        vector<uint64_t> bBlocks(a.size());
+        for (int i = 0; i < numInBlock; i++)
+        {
+            for (const auto x : withRemainder[i])
+            {
+                const auto numBlocksToShift = x / numInBlock;
+                for (int j = 0; j + numBlocksToShift < numBlocks; j++)
+                {
+                    bBlocks[j] |= blocksShiftedBy[i][j + numBlocksToShift];
+                }
+            }
+        }
+        vector<int> b;
         for (int j = 0; j < numBlocks; j++)
         {
-            const uint64_t block = blocks[j];
-            cout << "block[" << j << "]: " << block << endl;
+            const uint64_t block = bBlocks[j];
+            //cout << "block[" << j << "]: " << block << endl;
             for (int k = 0; k < numInBlock; k++)
             {
                 if (((block >> static_cast<uint64_t>(k)) & 1) == 1)
                 {
-                    dbg.push_back(k + j * numInBlock);
+                    b.push_back(k + j * numInBlock);
                 }
             }
         }
-        cout << "bBlah: " << endl;
-        for (const auto x : bBlah)
+        if (!b.empty() && b.front() == 0)
         {
-            cout << " " << x;
+            b.erase(b.begin());
         }
-        cout << endl;
-        cout << "dbg: " << endl;
-        for (const auto x : dbg)
-        {
-            cout << " " << x;
-        }
-        cout << endl;
-        assert(bBlah == dbg);
-    }
 
+        set<int> bDebugSet;
+        for (const auto x : a)
+        {
+            for (const auto y : a)
+            {
+                if (x != y)
+                {
+                    bDebugSet.insert(abs(x - y));
+                }
+            }
+        }
+        vector<int> bDebug(bDebugSet.begin(), bDebugSet.end());
+
+
+        cout << "bDebug: " << endl;
+        for (const auto x : bDebug)
+        {
+            cout << " " << x;
+        }
+        cout << endl;
+        cout << "b: " << endl;
+        for (const auto x : b)
+        {
+            cout << " " << x;
+        }
+        cout << endl;
+        assert(bDebug == b);
+
+        a = b;
+        numIterations++;
+    }
 
     return numIterations;
 }
