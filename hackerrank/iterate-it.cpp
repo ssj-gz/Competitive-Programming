13e275be17842195858b1b8450eaed05831bf272
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-13 11:02:45 +0100

    Clean up Iterate It!

--- hackerrank/iterate-it.cpp
+++ hackerrank/iterate-it.cpp
@@ -1,8 +1,6 @@
 // Simon St James (ssjgz) - 2019-05-12
 #define SUBMISSION
-#define BRUTE_FORCE
 #ifdef SUBMISSION
-#undef BRUTE_FORCE
 #define NDEBUG
 #endif
 #include <iostream>
@@ -11,70 +9,7 @@
 #include <algorithm>
 #include <cassert>
 
-#include <sys/time.h>
-#include <chrono>
-
 using namespace std;
-
-int solutionBruteForce(const vector<int>& aOriginal)
-{
-    vector<int> a(aOriginal);
-#if 0
-    std::sort(a.begin(), a.end());
-    a.erase(std::unique(a.begin(), a.end()), a.end());
-#endif
-    int rep = 0;
-    std::chrono::steady_clock::time_point totalBegin = std::chrono::steady_clock::now();
-    while (!a.empty())
-    {
-        const int originalSize = a.size();
-        std::chrono::steady_clock::time_point iterationBegin = std::chrono::steady_clock::now();
-
-
-#if 0
-        cout << "Iterate: a: " << endl;
-        for (const auto x : a)
-        {
-            cout << " " << x;
-        }
-        cout << endl;
-#endif
-        cout << " Iteration: " << rep << " brute force # elements: " << a.size() << " max element: " << *std::max_element(a.begin(), a.end()) << endl;
-#if 0
-        set<int> bSet;
-        for (const auto x : a)
-        {
-            for (const auto y : a)
-            {
-                if (x != y)
-                    bSet.insert(abs(x - y));
-            }
-        }
-        a.assign(bSet.begin(), bSet.end());
-#else
-        vector<bool> b(*std::max_element(a.begin(), a.end()) + 1, false);
-        for (const auto x : a)
-        {
-            for (const auto y : a)
-            {
-                if (x != y)
-                    b[abs(x - y)] = true;
-            }
-        }
-        a.clear();
-        for (int i = 0; i < b.size(); i++)
-        {
-            if (b[i])
-                a.push_back(i);
-        }
-#endif
-        rep++;
-
-        std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
-        std::cout << "ITeration of " << originalSize << " elements took : " << std::chrono::duration_cast<std::chrono::milliseconds>(end - iterationBegin).count() << "ms - total elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - totalBegin).count() << "ms"  <<std::endl;
-    }
-    return rep;
-}
 
 int gcd(int a, int b)
 {
@@ -93,40 +28,24 @@ int gcd(int a, int b)
     return b;
 }
 
-int solutionOptimised(const vector<int>& aOriginal)
+int findNumIterations(const vector<int>& aOriginal)
 {
-    set<int> blah(aOriginal.begin(), aOriginal.end());
-    vector<int> a(blah.begin(), blah.end());
+    set<int> aOriginalSet(aOriginal.begin(), aOriginal.end());
+    // Make a sorted and with no repetitions.  The sorting is not 
+    // strictly necessary.
+    vector<int> a(aOriginal.begin(), aOriginal.end());
     int numIterations = 0;
     const int numInBlock = 64;
     const int numBlocks = *std::max_element(a.begin(), a.end()) / numInBlock + 1;
-    auto blocksToBinaryString = [numInBlock, numBlocks](const vector<uint64_t>& blocks)
-    {
-        string binaryString;
-        for (int j = 0; j < numBlocks; j++)
-        {
-            const uint64_t block = blocks[j];
-            for (int k = 0; k < numInBlock; k++)
-            {
-                if (((block >> static_cast<uint64_t>(k)) & 1) == 1)
-                {
-                    binaryString.push_back('1');
-                }
-                else
-                {
-                    binaryString.push_back('0');
-                }
-            }
-        }
-        return binaryString;
-    };
+
     while (!a.empty())
     {
-        if (a.size() == *std::max_element(a.begin(), a.end()))
+        const int N = a.size();
+        if (N == *std::max_element(a.begin(), a.end()))
         {
             // A has converged to a solid block of consecutive numbers;
             // we can predict how many iterations remain, so do this and bail.
-            numIterations += a.size();
+            numIterations += N;
             break;
         }
         
@@ -134,7 +53,7 @@ int solutionOptimised(const vector<int>& aOriginal)
         // will be {x, y-x, y}, so (by induction) we can deduce the state of 
         // A after a given number of iterations.  Skip until the gap between
         // x and y is small.
-        if (a.size() == 3 && (a[2] - a[1] == a[0]))
+        if (N == 3 && (a[2] - a[1] == a[0]))
         {
             const int skipIterations = ((a[1] - a[0]) / a[0]) - 1;
             if (skipIterations > 0)
@@ -147,7 +66,7 @@ int solutionOptimised(const vector<int>& aOriginal)
 
         if (numIterations == 1)
         {
-            if (a.size() > 1)
+            if (N > 1)
             {
                 // Compute A / gcd(A) - this will have the same number of iterations
                 // remaining as the original A.
@@ -155,7 +74,7 @@ int solutionOptimised(const vector<int>& aOriginal)
                 // always be detectable after the first iteration, so no need to
                 // check it every iteration.
                 int gcdOfAllInA = a.front();
-                for (int i = 1; i < a.size(); i++)
+                for (int i = 1; i < N; i++)
                 {
                     gcdOfAllInA = gcd(gcdOfAllInA, a[i]);
                 }
@@ -187,6 +106,7 @@ int solutionOptimised(const vector<int>& aOriginal)
                 blocksShiftedBy.back()[blockNum] >>= 1;
                 if (blockNum + 1 < numBlocks)
                 {
+                    // Due to shifting, the first bit of the next block should become the last bit of the current block.
                     blocksShiftedBy.back()[blockNum] |= ((blocksShiftedBy.back()[blockNum + 1] & 1) << static_cast<uint64_t>(numInBlock - 1));
                 }
             }
@@ -234,36 +154,6 @@ int solutionOptimised(const vector<int>& aOriginal)
             assert(b.front() == 0);
             b.erase(b.begin());
         }
-
-#ifdef BRUTE_FORCE
-        set<int> bDebugSet;
-        for (const auto x : a)
-        {
-            for (const auto y : a)
-            {
-                if (x != y)
-                {
-                    bDebugSet.insert(abs(x - y));
-                }
-            }
-        }
-        vector<int> bDebug(bDebugSet.begin(), bDebugSet.end());
-#if 1
-        cout << "bDebug: " << endl;
-        for (const auto x : bDebug)
-        {
-            cout << " " << x;
-        }
-        cout << endl;
-        cout << "b: " << endl;
-        for (const auto x : b)
-        {
-            cout << " " << x;
-        }
-        cout << endl;
-#endif
-        assert(bDebug == b);
-#endif
 
         a = b;
         numIterations++;
@@ -342,33 +232,6 @@ int main(int argc, char* argv[])
     // more awkward logic and just use the "if |A| <= sqrt(N), compute B naively" approach.
     //
     // Kind of surprised at the (at the time of writing) < 10% completion rate.
-
-    if (argc == 2)
-    {
-
-#if 1
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-
-        const int n = rand() % 1000 + 1;
-        const int maxElement = rand() % 1000 + 1;
-#else
-        const int n = 25000;
-#endif
-        cout << n << endl;
-        for (int i = 0; i < n; i++)
-        {
-#if 1
-            cout << ((rand() % maxElement) + 1) << " ";
-#else
-            cout << (i * 2) << " ";
-#endif
-        }
-        cout << endl;
-        return 0;
-    }
     int n;
     cin >> n;
 
@@ -379,19 +242,5 @@ int main(int argc, char* argv[])
         assert(a[i] != 0);
     }
 
-#ifdef BRUTE_FORCE
-    const int bruteForceResult = solutionBruteForce(a);
-    cout << "bruteForceResult: " << bruteForceResult << endl;
-    const int optimisedResult = solutionOptimised(a);
-    cout << "optimisedResult: " << optimisedResult << endl;
-#else
-    cout << solutionOptimised(a) << endl;
-#endif
-
-
-#if 1
-#ifdef BRUTE_FORCE
-    assert(optimisedResult == bruteForceResult);
-#endif
-#endif
+    cout << findNumIterations(a) << endl;
 }
