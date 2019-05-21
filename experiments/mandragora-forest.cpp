963b8d3d98adadbc311c780f5444e3190703f3ad
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-21 14:35:04 +0100

    Further optimise Mandragora Forest's optimised solution to be O(n).

--- experiments/mandragora-forest.cpp
+++ experiments/mandragora-forest.cpp
@@ -54,20 +54,30 @@ int64_t solveBruteForce(const vector<int>& originalH)
 
 int64_t solveOptimised(const vector<int>& originalH)
 {
+    const int n = originalH.size();
+
     int64_t bestExperience = 0;
     auto sortedH = originalH;
     sort(sortedH.begin(), sortedH.end());
 
+    vector<int64_t> sumOfRemainingHealths(n + 1);
+    int64_t sumOfLastHealths = 0;
+    for (const auto h : sortedH)
+    {
+        sumOfLastHealths += h;
+    }
+    for (int numRemaining = n; numRemaining >= 0; numRemaining--)
+    {
+        sumOfRemainingHealths[numRemaining] = sumOfLastHealths;
+        if (n - numRemaining >= 0)
+            sumOfLastHealths -= sortedH[n - numRemaining];
+    }
+
     int64_t currentHealthIfOnlyEat = 1;
-    int64_t currentExperienceIfOnlyEat = 0;
-    const int n = originalH.size();
+    const int64_t currentExperienceIfOnlyEat = 0;
     for (int i = 0; i < n; i++)
     {
-        int64_t currentExperience = currentExperienceIfOnlyEat;
-        for (int j = i; j < n; j++)
-        {
-            currentExperience += currentHealthIfOnlyEat * sortedH[j];
-        }
+        const int64_t currentExperience = currentHealthIfOnlyEat * sumOfRemainingHealths[n - i];
         bestExperience = max(bestExperience, currentExperience);
         currentHealthIfOnlyEat++;
     }
