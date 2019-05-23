2ceadedf3ecc90f03832f0b8e473e7ea4a2ed7d7
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-23 10:51:09 +0100

    Document Superman Celebrates Diwali.

--- hackerrank/superman-celebrates-diwali.cpp
+++ hackerrank/superman-celebrates-diwali.cpp
@@ -8,6 +8,22 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    // "Hard"? "80 Points"?? Well, I can't really complain as I bungled it the first time
+    // I suppose (schoolboy error - the original way I calculated bestResultAfterSwitch
+    // added an extra factor of numBuildings to the computational complexity, which I missed
+    // completely XD).  It really *is* fundamentally easy, though!
+    //
+    // The recurrence relation for building maxScoreFromBuildingAndFloorLookup is very very simple
+    // - rescue the people on this floor and building; add the amount rescued if we either
+    // a) descend to the next floor of the current building or b) switch to a different building
+    // (with the resulting height loss), whichever is larger.
+    //
+    // The only "difficult" bit comes from calculating this latter (bestResultAfterSwitch) efficiently
+    // i.e. in O(1) instead of the O(numBuildings) it would be if we did it naively, but this is 
+    // easy with a bit of incremental updating of lookup helpers (maxScoreIfSwitchToBuildingLessThanCurrent;
+    // maxScoreIfSwitchToBuildingGreaterThan) - hopefully the code for this is self-explanatory.
+    //
+    // And that's it!
     auto readInt = []() { int x; cin >> x; assert(cin); return x; };
     const int numBuildings = readInt();
 
@@ -60,6 +76,7 @@ int main(int argc, char* argv[])
                     // Update more of the lookup stuff needed for efficient calculation of max score if we switch buildings.
                     maxScoreIfSwitchToBuildingLessThanCurrent = max(maxScoreIfSwitchToBuildingLessThanCurrent, maxScoreFromBuildingAndFloorLookup[buildingNum - 1][floorNumIfSwitchBuildings]);
                 }
+                // i.e. bestResultAfterSwitch = max[otherBuilding != buildingNum] {maxScoreFromBuildingAndFloorLookup[otherBuilding][floorNumIfSwitchBuildings]}.
                 bestResultAfterSwitch = max(maxScoreIfSwitchToBuildingGreaterThan[buildingNum], maxScoreIfSwitchToBuildingLessThanCurrent);
             }
 
@@ -70,6 +87,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    // Max rescued = max over all buildings of max rescued if we started on top floor of that building.
     int64_t maxRescued = 0;
     for (int buildingNum = 0; buildingNum < numBuildings; buildingNum++)
     {
