20ed9cf5d80b6f4ac3cf9870967b38f0755aa5f1
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-23 10:26:44 +0100

    Generic snapshot of Superman Celebrates Diwali - not working yet.
    Bit embarrassing XD

--- hackerrank/superman-celebrates-diwali.cpp
+++ hackerrank/superman-celebrates-diwali.cpp
@@ -17,7 +17,7 @@ int main()
     int buildingSwitchHeightLoss;
     cin >> buildingSwitchHeightLoss;
 
-    vector<vector<int>> numInBuildingOnFloor(numBuildings, vector<int>(buildingHeight + 1, 0));
+    vector<vector<int>> numInBuildingOnFloor(numBuildings + 1, vector<int>(buildingHeight + 1, 0));
 
     for (int buildingNum = 0; buildingNum < numBuildings; buildingNum++)
     {
@@ -36,19 +36,32 @@ int main()
     }
     assert(cin);
 
-    vector<vector<int64_t>> maxScoreFromBuildingAndFloorLookup(numBuildings, vector<int64_t>(buildingHeight + 1, 0));
+    vector<vector<int64_t>> maxScoreFromBuildingAndFloorLookup(numBuildings + 1, vector<int64_t>(buildingHeight + 1, 0));
 
     for (int floorNum = 1; floorNum <= buildingHeight; floorNum++)
     {
+        const int floorNumIfSwitchBuildings = floorNum - buildingSwitchHeightLoss;
+        const bool canSwitchBuildings = (floorNumIfSwitchBuildings >= 0);
+        vector<int64_t> maxScoreIfSwitchToBuildingGreaterThan(numBuildings + 1, 0);
+        if (canSwitchBuildings)
+        {
+            for (int otherBuildingNum = numBuildings - 1; otherBuildingNum >= 0; otherBuildingNum--)
+            {
+                maxScoreIfSwitchToBuildingGreaterThan[otherBuildingNum] = max(maxScoreIfSwitchToBuildingGreaterThan[otherBuildingNum + 1], maxScoreFromBuildingAndFloorLookup[otherBuildingNum + 1][floorNumIfSwitchBuildings]);
+                cout << "floorNum: " << floorNum <<" maxScoreIfSwitchToBuildingGreaterThan[" << otherBuildingNum << "] = " << maxScoreIfSwitchToBuildingGreaterThan[otherBuildingNum]<< endl;
+            }
+        }
+
+        int64_t maxScoreIfSwitchToBuildingLessThanCurrent = 0;
         for (int buildingNum = 0; buildingNum < numBuildings; buildingNum++)
         {
-            int64_t result = numInBuildingOnFloor[buildingNum][floorNum]; // Rescue these people.
-            cout << "buildingNum: " << buildingNum << " floorNum: " << floorNum << " rescue: " << result << endl;
+            //int64_t result = numInBuildingOnFloor[buildingNum][floorNum]; // Rescue these people.
+            //cout << "buildingNum: " << buildingNum << " floorNum: " << floorNum << " rescue: " << result << endl;
 
-            int64_t bestResultAfterMove = 0;
             // Stay in same building; descend to next floor.
-            bestResultAfterMove = max(bestResultAfterMove, maxScoreFromBuildingAndFloorLookup[buildingNum][floorNum - 1]);
-            if (floorNum - buildingSwitchHeightLoss >= 0)
+            const int64_t bestResultAfterDescend = maxScoreFromBuildingAndFloorLookup[buildingNum][floorNum - 1];
+            int64_t bestResultAfterSwitch = 0;
+            if (canSwitchBuildings)
             {
                 // Switch to other buildings.
                 const int numBuildings = numInBuildingOnFloor.size();
@@ -59,13 +72,24 @@ int main()
 
                     if (floorNum - buildingSwitchHeightLoss >= 0)
                     {
-                        bestResultAfterMove = max(bestResultAfterMove, maxScoreFromBuildingAndFloorLookup[otherBuildingNum][floorNum - buildingSwitchHeightLoss]);
+                        bestResultAfterSwitch = max(bestResultAfterSwitch, maxScoreFromBuildingAndFloorLookup[otherBuildingNum][floorNum - buildingSwitchHeightLoss]);
                     }
                 }
+                const auto bestResultAfterSwitchOptimised = max(maxScoreIfSwitchToBuildingGreaterThan[buildingNum], maxScoreIfSwitchToBuildingLessThanCurrent);
+                cout << "buildingNum: " << buildingNum << " floorNum: " << floorNum << " bestResultAfterSwitch: " << bestResultAfterSwitch << " bestResultAfterSwitchOptimised: " << bestResultAfterSwitchOptimised << " maxScoreIfSwitchToBuildingGreaterThan[buildingNum]: " << maxScoreIfSwitchToBuildingGreaterThan[buildingNum] << " maxScoreIfSwitchToBuildingLessThanCurrent: " << maxScoreIfSwitchToBuildingLessThanCurrent << endl;
+                assert(bestResultAfterSwitch == bestResultAfterSwitchOptimised);
             }
 
-            result += bestResultAfterMove;
+
+            const auto result = numInBuildingOnFloor[buildingNum][floorNum] + // Rescue these people.
+                               max(bestResultAfterDescend, bestResultAfterSwitch);
             maxScoreFromBuildingAndFloorLookup[buildingNum][floorNum] = result;
+
+            if (canSwitchBuildings && buildingNum > 0)
+            {
+                maxScoreIfSwitchToBuildingLessThanCurrent = max(maxScoreIfSwitchToBuildingLessThanCurrent, maxScoreFromBuildingAndFloorLookup[buildingNum - 1][floorNumIfSwitchBuildings]);
+            }
+            cout << "buildingNum: " << buildingNum << " floorNum: " << floorNum << " result: " << result << endl;
         }
     }
 
