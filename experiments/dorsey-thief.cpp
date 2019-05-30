a478f9a4fd65bb6314fd7e9825cbb72944f69be0
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-30 11:19:25 +0100

    Snapshot of Dorsey Thief - not working at all, yet!

--- experiments/dorsey-thief.cpp
+++ experiments/dorsey-thief.cpp
@@ -42,18 +42,43 @@ int64_t solveBruteForce(int N, int X, const vector<int>& v, const vector<int>& a
 int64_t solveOptimised(int N, int X, const vector<int>& v, const vector<int>& a)
 {
     vector<vector<int>> buyersOfGoldDecreasingPrice(X + 1);
+    vector<int> distinctGoldAmounts;
     for (int i = 0; i < N; i++)
     {
         if (a[i] > X)
             continue;
+        distinctGoldAmounts.push_back(a[i]);
         buyersOfGoldDecreasingPrice[a[i]].push_back(v[i]);
     }
+    sort(distinctGoldAmounts.begin(), distinctGoldAmounts.end());
+    distinctGoldAmounts.erase(std::unique(distinctGoldAmounts.begin(), distinctGoldAmounts.end()), distinctGoldAmounts.end());
     for (auto& prices : buyersOfGoldDecreasingPrice)
     {
         sort(prices.begin(), prices.end(), std::greater<>());
     }
+    vector<int64_t> bestPriceForSoldGold(X + 1, 0);
 
-    return 0;
+    for (const auto goldToSell : distinctGoldAmounts)
+    {
+        vector<int64_t> nextBestPriceForSoldGold = bestPriceForSoldGold;
+        const int totalBuyersForGold = buyersOfGoldDecreasingPrice[goldToSell].size();
+        for (int existingGold = 0; existingGold <= X; existingGold++)
+        {
+            int64_t priceOfSoldGold = 0;
+            int64_t amountOfSoldGold = 0;
+            for (int numBuyers = 1; numBuyers <= totalBuyersForGold; numBuyers++)
+            {
+                priceOfSoldGold += buyersOfGoldDecreasingPrice[goldToSell][numBuyers - 1];
+                amountOfSoldGold += goldToSell;
+                if (existingGold - amountOfSoldGold < 0)
+                    break;
+                nextBestPriceForSoldGold[existingGold - amountOfSoldGold] = max(nextBestPriceForSoldGold[existingGold - amountOfSoldGold], bestPriceForSoldGold[existingGold] + priceOfSoldGold);
+            }
+        }
+        bestPriceForSoldGold = nextBestPriceForSoldGold;
+    }
+
+    return bestPriceForSoldGold[X];
 }
 
 int main(int argc, char* argv[])
@@ -71,8 +96,8 @@ int main(int argc, char* argv[])
     }
 
     const auto resultBruteForce = solveBruteForce(N, X, v, a);
-    if (resultBruteForce != -1)
-        cout << "resultBruteForce: " << resultBruteForce << endl;
-    else
-        cout << "got caught!" << endl;
+    cout << "resultBruteForce: " << resultBruteForce << endl;
+
+    const auto resultOptimised = solveOptimised(N, X, v, a);
+    cout << "resultOptimised: " << resultOptimised << endl;
 }
