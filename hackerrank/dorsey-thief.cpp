881c3f2c620a81a3dae63645bc8b6582d042e754
Simon St James <kdedevel@etotheipiplusone.com>
2019-05-30 12:01:01 +0100

    Various tweaks and cleanups to Dorsey Thief.

--- hackerrank/dorsey-thief.cpp
+++ hackerrank/dorsey-thief.cpp
@@ -5,43 +5,6 @@
 
 using namespace std;
 
-void solveBruteForce(int passengerIndex, int remainingGold, int64_t profitSoFar, const vector<int>& v, const vector<int>& a, int64_t& bestProfit)
-{
-    //cout << "passengerIndex: " << passengerIndex << " remainingGold: " << remainingGold << " profitSoFar: " << profitSoFar << endl;
-    if (remainingGold < 0) 
-        return;
-    if (remainingGold == 0)
-    {
-        if (profitSoFar > bestProfit)
-        {
-            //cout << "Found new best profit: " << profitSoFar << endl;
-            bestProfit = profitSoFar;
-        }
-        return;
-    }
-
-    const int N = v.size();
-    if (passengerIndex == N)
-    {
-        // No more passengers left.
-        return;
-    }
-    // Don't buy from this passenger.
-    solveBruteForce(passengerIndex + 1, remainingGold, profitSoFar, v, a, bestProfit);
-    // Buy from this passenger.
-    solveBruteForce(passengerIndex + 1, remainingGold - a[passengerIndex], profitSoFar + v[passengerIndex], v, a, bestProfit);
-}
-
-int64_t solveBruteForce(int N, int X, const vector<int>& v, const vector<int>& a)
-{
-    int64_t bestProfit = -1;
-    solveBruteForce(0, X, 0, v, a, bestProfit);
-
-    return bestProfit;
-}
-
-int64_t numIterations = 0;
-
 int64_t findBestPriceForSellingAllGold(int N, int X, const vector<int>& v, const vector<int>& a)
 {
     vector<vector<int>> buyersOfGoldDecreasingPrice(X + 1);
@@ -55,6 +18,7 @@ int64_t findBestPriceForSellingAllGold(int N, int X, const vector<int>& v, const
     }
     sort(distinctGoldAmounts.begin(), distinctGoldAmounts.end());
     distinctGoldAmounts.erase(std::unique(distinctGoldAmounts.begin(), distinctGoldAmounts.end()), distinctGoldAmounts.end());
+
     for (auto& prices : buyersOfGoldDecreasingPrice)
     {
         sort(prices.begin(), prices.end(), std::greater<>());
@@ -81,8 +45,8 @@ int64_t findBestPriceForSellingAllGold(int N, int X, const vector<int>& v, const
                     // Can't sell more gold that we started with!
                     break;
                 }
-                bestPriceForNextSoldGold[existingGold + amountOfSoldGoldAmount] = max(bestPriceForNextSoldGold[existingGold + amountOfSoldGoldAmount], bestPriceForSoldGold[existingGold] + priceOfSoldGoldAmount);
-                numIterations++;
+                bestPriceForNextSoldGold[existingGold + amountOfSoldGoldAmount] = max(bestPriceForNextSoldGold[existingGold + amountOfSoldGoldAmount], 
+                                                                                      bestPriceForSoldGold[existingGold] + priceOfSoldGoldAmount);
             }
         }
         bestPriceForSoldGold = bestPriceForNextSoldGold;
