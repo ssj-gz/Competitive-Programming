a67672fe29a7b7083007f884db2d201637de339d
Simon St James <kdedevel@etotheipiplusone.com>
2019-06-04 12:50:08 +0100

    Begin work on Brute-Force version of Powers Game.
    Not working yet.  I think I've worked out *most* of the strategies, but
    there's some I'm still not sure of - this will help me figure them out
    :)

---
+++ experiments/powers-game.cpp
@@ -0,0 +1,449 @@
+// Simon St James (ssjgz) - 2017-12-03.  Framework for exploring "Optimal Play" Game Theory games, complete with example ("Move The Coins").
+#include <iostream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <limits>
+#include <algorithm>
+#include <sys/time.h>
+#include <cassert>
+
+using namespace std;
+
+//#define VERY_VERBOSE
+#define PRINT_COMPUTER_MOVES
+
+#define MOVE_COINS_EXAMPLE
+//#define MOVE_COINS_EXAMPLE_RANDOM
+#ifdef MOVE_COINS_EXAMPLE
+struct Node
+{
+    vector<Node*> children;
+    Node* parent = nullptr;
+    int numCoins = 0;
+    int nodeId = -1;
+    int height = -1;
+
+    vector<Node*> neighbours;
+};
+
+void fixTree(Node* rootNode, int height, Node* parent)
+{
+    rootNode->parent = parent;
+    rootNode->height = height;
+    for (Node* child : rootNode->neighbours)
+    {
+        if (child == parent)
+            continue;
+
+        rootNode->children.push_back(child);
+        fixTree(child, height + 1, rootNode);
+    }
+    rootNode->neighbours.clear();
+}
+
+void printTree(const vector<Node>& nodes)
+{
+    for (const auto& node : nodes)
+    {
+        cout << "Node: " << (node.nodeId + 1) << " coins: " << node.numCoins << " parent: " << (node.parent == nullptr ? -1 : node.parent->nodeId + 1) << " children: " ;
+        for (const auto& child : node.children)
+        {
+            cout << (child->nodeId + 1) << " ";
+
+        }
+        cout << endl;
+    }
+}
+
+vector<Node> nodes;
+#endif
+
+enum PlayState { Player1Win, Player1Lose, Draw };
+enum Player { Player1, Player2 };
+
+const string player1Name = "Player 1";
+const string player2Name = "Player 2";
+
+enum PlayerType { CPU, Random, Human };
+
+Player otherPlayer(Player player)
+{
+    if (player == Player1)
+        return Player2;
+    else
+        return Player1;
+}
+
+PlayState winForPlayer(Player player)
+{
+    if (player == Player1)
+        return Player1Win;
+    else
+        return Player1Lose;
+}
+
+PlayState loseForPlayer(Player player)
+{
+    if (player == Player1)
+        return Player1Lose;
+    else
+        return Player1Win;
+}
+
+class GameState;
+class Move;
+
+vector<Move> movesFor(Player currentPlayer, const GameState& gameState);
+
+class GameState
+{
+    public:
+        vector<int> numWithEachModulus;
+        int currentModulus = 0;
+        bool hasWinningPlayerOverride(Player currentPlayer) const
+        {
+            // Assume that a GameState that has no moves that lead to a Lose for the
+            // other player is a losing state (and vice-versa).
+            return false;
+        }
+        PlayState winningPlayerOverride(Player currentPlayer) const
+        {
+            assert(false);
+            return Player1Win;
+        }
+};
+
+bool operator<(const GameState& lhs, const GameState& rhs)
+{
+    if (lhs.numWithEachModulus != rhs.numWithEachModulus)
+        return (lhs.numWithEachModulus < rhs.numWithEachModulus);
+    else
+        return lhs.numWithEachModulus < rhs.numWithEachModulus;
+
+}
+
+class Move
+{
+    public:
+        bool add = false;
+        int modulus = 0;
+        static Move preferredMove(const vector<Move>& moves, PlayState moveOutcome, Player currentPlayer, const GameState& gameState)
+        {
+            assert(!moves.empty());
+            // Pick the first one, arbitrarily - feel free to add your own preferences here :)
+            return moves.front();
+        }
+};
+
+ostream& operator<<(ostream& os, const GameState& gameState)
+{
+    os << "currentModulus: " << gameState.currentModulus << " num in Pile: ";
+    for (int i = 1; i <= 17; i++)
+    {
+        os << i << ": = " << gameState.numWithEachModulus[i] << " ";
+    }
+    return os;
+}
+
+ostream& operator<<(ostream& os, const Move& move)
+{
+    cout << (move.add ? "Add " : "Subtract ") << move.modulus;
+    return os;
+}
+
+ostream& operator<<(ostream& os, Player player)
+{
+    if (player == Player1)
+        os << player1Name;
+    else
+        os << player2Name;
+
+    return os;
+}
+
+ostream& operator<<(ostream& os, PlayState playState)
+{
+    if (playState == Player1Win)
+        os << Player1 << " wins";
+    else if (playState == Player1Lose)
+        os << Player2 << " wins";
+    else
+        os << "Draw";
+
+    return os;
+}
+
+
+map<pair<GameState, Player>, PlayState> playStateForLookup;
+
+vector<Move> movesFor(Player currentPlayer, const GameState& gameState)
+{
+    vector<Move> moves;
+    for (int i = 1; i <= 17; i++)
+    {
+        if (gameState.numWithEachModulus[i] > 0)
+        {
+            Move add;
+            add.add = true;
+            add.modulus = i;
+            moves.push_back(add);
+
+            Move subtract;
+            subtract.add = false;
+            subtract.modulus = i;
+            moves.push_back(subtract);
+        }
+    }
+
+    return moves;
+}
+
+GameState gameStateAfterMove(const GameState& gameState, Player currentPlayer, const Move& move)
+{
+    GameState nextGameState = gameState;
+    if (move.add)
+    {
+        nextGameState.currentModulus = (nextGameState.currentModulus + move.modulus) % 17;
+    }
+    else
+    {
+        nextGameState.currentModulus = (nextGameState.currentModulus + 17 - move.modulus) % 17;
+    }
+    nextGameState.numWithEachModulus[move.modulus]--;
+    assert(nextGameState.currentModulus >= 0 && nextGameState.currentModulus < 17);
+    assert(nextGameState.numWithEachModulus[move.modulus] >= 0);
+
+    return nextGameState;
+}
+
+PlayState findWinnerAux(Player currentPlayer, const GameState& gameState, PlayerType player1Type, PlayerType player2Type, bool isBruteForceMoveSearch)
+{
+    const auto playThisMoveAsType = (currentPlayer == Player1 ? player1Type : player2Type);
+    const bool playThisMoveInteractively = !isBruteForceMoveSearch && (playThisMoveAsType == Human);
+    const bool playThisMoveRandomly = !isBruteForceMoveSearch && (playThisMoveAsType == Random);
+    if (isBruteForceMoveSearch && playStateForLookup.find({gameState, currentPlayer}) != playStateForLookup.end())
+    {
+        // Don't use the cache if we're interactive/ random: it will know all losing states from earlier dry-runs,
+        // and if the human player is in a losing state, won't give them a chance to make a move!
+        return playStateForLookup[{gameState, currentPlayer}];
+    }
+
+    // Assume a loss by default.
+    PlayState playState = loseForPlayer(currentPlayer);
+
+    vector<Move> winningMovesForCurrentPlayer;
+    vector<Move> losingMovesForCurrentPlayer;
+    vector<Move> drawingMovesForCurrentPlayer;
+
+
+    auto updatePlayStateFromMove = [&playState, &gameState, currentPlayer, player1Type, player2Type, &winningMovesForCurrentPlayer, &drawingMovesForCurrentPlayer, &losingMovesForCurrentPlayer]
+        (const Move& move, bool isBruteForceMoveSearch)
+        {
+            const auto newGameState = gameStateAfterMove(gameState, currentPlayer, move);
+            const auto result = findWinnerAux(otherPlayer(currentPlayer), newGameState, player1Type, player2Type, isBruteForceMoveSearch);
+            if (result == winForPlayer(currentPlayer))
+            {
+                winningMovesForCurrentPlayer.push_back(move);
+            }
+            else if (result == Draw)
+            {
+                drawingMovesForCurrentPlayer.push_back(move);
+            }
+            else
+            {
+                losingMovesForCurrentPlayer.push_back(move);
+            }
+            if (result == winForPlayer(currentPlayer))
+            {
+                playState = winForPlayer(currentPlayer);
+            }
+            else if (playState != winForPlayer(currentPlayer) && result == Draw)
+            {
+                playState = Draw;
+            }
+
+        };
+
+    auto readInt = [](const string& message)
+    {
+        while (true)
+        {
+            cout << message << endl;
+            int value;
+            cin >> value;
+
+            if (!cin)
+            {
+                cout << "Invalid input; please try again" << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            else
+                return value;
+        }
+    };
+
+    if (gameState.hasWinningPlayerOverride(currentPlayer))
+    {
+        playState = gameState.winningPlayerOverride(currentPlayer);
+
+    }
+    else
+    {
+        const vector<Move> availableMoves = movesFor(currentPlayer, gameState);
+
+        if (playThisMoveInteractively)
+        {
+            if (!availableMoves.empty())
+            {
+                stringstream messagePromptStream;
+                messagePromptStream << "Player " << currentPlayer << ", it is your move; game state is " << gameState << endl;
+                for (int i = 0; i < availableMoves.size(); i++)
+                {
+                    messagePromptStream << i << ":  " << availableMoves[i] << endl;
+                }
+                int moveIndex = -1;
+                while (true)
+                {
+                    moveIndex = readInt(messagePromptStream.str());
+                    if (moveIndex < 0 || moveIndex >= availableMoves.size())
+                    {
+                        cout << "Invalid input" << endl;
+                    }
+                    else
+                        break;
+                }
+                const auto chosenMove = availableMoves[moveIndex];
+                cout << "You chose move " << chosenMove << " game state is now: " <<  gameStateAfterMove(gameState, currentPlayer, chosenMove) << endl;
+                updatePlayStateFromMove(chosenMove, false);
+            }
+            else
+            {
+                cout << "Player " << currentPlayer << " - you have no moves available.  You lose; good-day, Sir!" << endl;
+            }
+        }
+        else if (playThisMoveRandomly)
+        {
+            if (!availableMoves.empty())
+            {
+                const int moveIndex = rand() % availableMoves.size();
+                const auto chosenMove = availableMoves[moveIndex];
+                cout << "Randomly chose move " << chosenMove << " game state is now: " <<  gameStateAfterMove(gameState, currentPlayer, chosenMove) << endl;
+                updatePlayStateFromMove(chosenMove, false);
+            }
+        }
+        else
+        {
+            if (!isBruteForceMoveSearch)
+            {
+#ifdef PRINT_COMPUTER_MOVES
+                cout << "Computer's turn; thinking ..." << endl;
+#endif
+            }
+            for (const auto& move : availableMoves)
+            {
+                const auto oldPlayState = playState;
+                updatePlayStateFromMove(move, true);
+
+#ifdef VERY_VERBOSE
+                cout << "The move " << move << " from state: " << gameState << " is a " << (playState == winForPlayer(currentPlayer) ? "Win" : (playState == Draw ? "Draw" : "Lose")) << " for player " << currentPlayer << endl;
+#endif
+
+            }
+
+            if (!isBruteForceMoveSearch && !availableMoves.empty())
+            {
+                Move chosenMove;
+                if (playState == winForPlayer(currentPlayer))
+                {
+                    assert(!winningMovesForCurrentPlayer.empty());
+                    chosenMove = Move::preferredMove(winningMovesForCurrentPlayer, playState, currentPlayer, gameState);
+                }
+                else if (playState == Draw)
+                {
+                    assert(!drawingMovesForCurrentPlayer.empty());
+                    chosenMove = Move::preferredMove(drawingMovesForCurrentPlayer, playState, currentPlayer, gameState);
+                }
+                else
+                {
+                    assert(!losingMovesForCurrentPlayer.empty());
+                    chosenMove = Move::preferredMove(losingMovesForCurrentPlayer, playState, currentPlayer, gameState);
+                }
+#ifdef PRINT_COMPUTER_MOVES
+                cout << "Computer (" << currentPlayer << ") played move: " << chosenMove << " and thinks it will " << (playState == winForPlayer(currentPlayer) ? "Win" : (playState == Draw ? "Draw" : "Lose")) << ". Game state now: " <<  gameStateAfterMove(gameState, currentPlayer, chosenMove) << endl;
+#endif
+                updatePlayStateFromMove(chosenMove, false);
+            }
+        }
+    }
+
+#ifdef VERY_VERBOSE
+    cout << "At game state: " << gameState << " with player " << currentPlayer << ", the player " << (playState == winForPlayer(currentPlayer) ? "Wins" : playState == winForPlayer(otherPlayer(currentPlayer)) ? "Loses" : "Draws") << endl;
+
+#endif
+    playStateForLookup[{gameState, currentPlayer}] = playState;
+
+    return playState;
+}
+
+PlayState findWinner(Player firstPlayer, const GameState& initialGameState, PlayerType player1Type = CPU, PlayerType player2Type = CPU)
+{
+    cout << "Initial game state: " << initialGameState << endl;
+    playStateForLookup.clear();
+    const auto result = findWinnerAux(firstPlayer, initialGameState, player1Type, player2Type, false);
+
+    if (player1Type == Human || player2Type == Human)
+    {
+        const auto interactivePlayer = (player1Type == Human ? Player1 : Player2);
+        cout << "Result of interactive game: you played as " << interactivePlayer << "; " << firstPlayer << " took the first turn; " << result << endl;
+    }
+
+    return result;
+}
+
+
+int main(int argc, char** argv)
+{
+    struct timeval time; 
+    gettimeofday(&time,NULL);
+    srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+
+    if (argc == 2)
+    {
+        return 0;
+    }
+
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < T; t++)
+    {
+        int N;
+        cin >> N;
+
+        GameState initialGameState;
+        initialGameState.numWithEachModulus.resize(17);
+        int powerOf2 = 1;
+        for (int i = 0; i < N; i++)
+        {
+            initialGameState.numWithEachModulus[powerOf2]++;
+            powerOf2 *= 2;
+        }
+        const auto result = findWinner(Player1, initialGameState);
+        if (result == Player1Win)
+        {
+            cout << "FIRST";
+        }
+        else
+        {
+            cout << "SECOND";
+        }
+        cout << endl;
+    }
+
+}
+
+
+
