9c4dfc6e9f2e2846a37e8626e5e3c0de736febce
Simon St James <kdedevel@etotheipiplusone.com>
2019-06-15 16:07:31 +0100

    Test case generator for Count Triplets.

--- hackerrank/interview-preparation-kit/count-triplets.cpp
+++ hackerrank/interview-preparation-kit/count-triplets.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <cassert>
 
+#include <sys/time.h>
+
 using namespace std;
 
 int64_t solveBruteForce(const vector<int64_t>& a, int64_t r)
@@ -31,6 +33,25 @@ int64_t solveBruteForce(const vector<int64_t>& a, int64_t r)
 
 int main(int argc, char* argv[])
 {
+    if (argc == 2)
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+
+        const int n = rand() % 100;
+        const int r = rand() % 10 + 1;
+        const int maxNum = rand() % 1000 + 1;
+
+        cout << n << " " << r << endl;
+        for (int i = 0; i < n; i++)
+        {
+            cout << (rand() % maxNum + 1) << " ";
+        }
+        cout << endl;
+
+        return 0;
+    }
     int n;
     cin >> n;
     int64_t r;
