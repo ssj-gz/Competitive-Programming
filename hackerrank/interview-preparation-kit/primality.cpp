5a65f36157abbe51baa84a3780d519d823d028a4
Simon St James <kdedevel@etotheipiplusone.com>
2019-06-15 17:40:42 +0100

    Some diagnostics for "Primality".

--- hackerrank/interview-preparation-kit/primality.cpp
+++ hackerrank/interview-preparation-kit/primality.cpp
@@ -23,12 +23,15 @@ int main()
     for (int factor = 2; factor <= rootMaxN; factor++)
     {
         const bool isFactorPrime = isPrime[factor];
+#if 0
         if (isFactorPrime)
         {
             primesUpToRootMaxN.push_back(factor);
         }
+#endif
         for (int multiple = factor * 2; multiple <= rootMaxN; multiple += factor)
         {
+#if 0
             if (!isPrime[multiple] && !isFactorPrime)
             {
                 // This multiple has already been marked, and since factor is not prime,
@@ -36,10 +39,26 @@ int main()
                 // prime factors of factor!).
                 break;
             }
+#endif
             isPrime[multiple] = false;
         }
     }
+#if 1
+    for (int i = 2; i <= rootMaxN; i++)
+    {
+        if (isPrime[i])
+        {
+            primesUpToRootMaxN.push_back(i);
+        }
+    }
+#endif
 
+    for (const auto x : primesUpToRootMaxN)
+    {
+        cout << x << " is prime" << endl;
+    }
+
+#if 0
     int p;
     cin >> p;
 
@@ -69,4 +88,5 @@ int main()
         }
     }
 
+#endif
 }
