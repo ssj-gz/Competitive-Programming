32097d1014ec54f2d1be4de22990e13882c39fa8
Simon St James <kdedevel@etotheipiplusone.com>
2019-06-15 12:20:03 +0100

    Document Minimum Swaps.

--- hackerrank/interview-preparation-kit/minimum-swaps.cpp
+++ hackerrank/interview-preparation-kit/minimum-swaps.cpp
@@ -13,6 +13,14 @@ using namespace std;
 
 int main()
 {
+    // Another easy one: it's hopefully obvious that the optimal algorithm
+    // simply sweeps from left to right: if the element at the current index
+    // is not the expected one (i.e. not equal to index + 1) then we need 
+    // to swap the element at this index with the expected element; otherwise
+    // we do nothing.
+    //
+    // The indexOf array - which we need to maintain in the face of swaps - 
+    // enables us to find the index of the expected element in O(1).
     int n;
     cin >> n;
 
