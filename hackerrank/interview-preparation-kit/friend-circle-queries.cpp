6ac14f042db1db08c789d5e437143e5a148fd198
Simon St James <kdedevel@etotheipiplusone.com>
2019-06-15 19:08:10 +0100

    Document Friend Circle Queries, and don't use the word "squashed" all over the place!
    Spin the squashing stuff out into its own new function
    (squashPersonIdsForQueries) where it's out of the way.

--- hackerrank/interview-preparation-kit/friend-circle-queries.cpp
+++ hackerrank/interview-preparation-kit/friend-circle-queries.cpp
@@ -6,30 +6,15 @@
 
 using namespace std;
 
-int main()
+struct Query
 {
-    // Easy one, and similar to a bunch of other challenges (Value of Friendship;
-    // Journey to the Moon; etc) - friendship is an equivalence relation, and so
-    // partitions the people into disjoint components.  Introducing a friendship
-    // between people in difference components merges the two components.
-    // We can do this merging efficiently using the well-known Disjoint Set Union
-    // algorithm to process all queries in O(Q log2 Q).
-    int Q;
-    cin >> Q;
+    int person1 = -1;
+    int person2 = -1;
+};
 
-    struct Query
-    {
-        int person1 = -1;
-        int person2 = -1;
-    };
 
-    vector<Query> queries(Q);
-    for (int q = 0; q < Q; q++)
-    {
-        cin >> queries[q].person1;
-        cin >> queries[q].person2;
-    }
-
+int squashPersonIdsForQueries(vector<Query>& queries)
+{
     map<int, int> originalToSquashedPersonIds;
     auto getSquashedPersonId = [&originalToSquashedPersonIds](int originalPerson)
     {
@@ -45,53 +30,76 @@ int main()
         }
     };
 
-    // Re-map the original person ids to the consecutive "squashed" ids 0, 1, ...
-    // This might seem like a micro-optimisation, but it should knock off a O(log2 Q)
-    // asymptotically.
     for (auto& query : queries)
     {
         query.person1 = getSquashedPersonId(query.person1);
         query.person2 = getSquashedPersonId(query.person2);
     }
 
+    return originalToSquashedPersonIds.size();
+}
+
+int main()
+{
+    // Easy one, and similar to a bunch of other challenges (Value of Friendship;
+    // Journey to the Moon; etc) - friendship is an equivalence relation, and so
+    // partitions the people into disjoint components.  Introducing a friendship
+    // between people in difference components merges the two components.
+    // We can do this merging efficiently using the well-known Disjoint Set Union
+    // algorithm to process all queries in O(Q log2 Q).
+    int Q;
+    cin >> Q;
+
+    vector<Query> queries(Q);
+    for (int q = 0; q < Q; q++)
+    {
+        cin >> queries[q].person1;
+        cin >> queries[q].person2;
+    }
+
+    // Re-map the original person ids to the consecutive "squashed" ids 0, 1, ...
+    // This might seem like a micro-optimisation, but it should knock off a O(log2 Q)
+    // asymptotically by allowing us to use a vector for componentForPersonId instead
+    // of a map.
+    const int numDistinctPersons =squashPersonIdsForQueries(queries);
+
     struct Component
     {
-        vector<int> squashedPersonIdsInComponent;
+        vector<int> personIdsInComponent;
     };
 
     // Have each person in their own component, initially.
-    const int numDistinctPersons = originalToSquashedPersonIds.size();
     vector<Component> components(numDistinctPersons);
-    vector<Component*> componentForSquashedPersonId(numDistinctPersons);
-    for (int squashedId = 0; squashedId < numDistinctPersons; squashedId++)
+    vector<Component*> componentForPersonId(numDistinctPersons);
+    for (int personId = 0; personId < numDistinctPersons; personId++)
     {
-        componentForSquashedPersonId[squashedId] = &(components[squashedId]);
-        components[squashedId].squashedPersonIdsInComponent.push_back(squashedId);
+        componentForPersonId[personId] = &(components[personId]);
+        components[personId].personIdsInComponent.push_back(personId);
     }
 
     int largestComponentSize = 1;
     for (const auto& query : queries)
     {
-        const auto componentPerson1 = componentForSquashedPersonId[query.person1];
-        const auto componentPerson2 = componentForSquashedPersonId[query.person2];
+        const auto componentPerson1 = componentForPersonId[query.person1];
+        const auto componentPerson2 = componentForPersonId[query.person2];
 
         if (componentPerson1 != componentPerson2)
         {
             // Standard Disjoint Set Union - absorb the smaller component into the larger component and update.
             // This changes what looks like a O(Q ^ 2) algorithm into an O(Q log2 Q) algorithm.
-            const auto absorbingComponent = (componentPerson1->squashedPersonIdsInComponent.size() > componentPerson2->squashedPersonIdsInComponent.size()) ? 
+            const auto absorbingComponent = (componentPerson1->personIdsInComponent.size() > componentPerson2->personIdsInComponent.size()) ? 
                 componentPerson1 : componentPerson2;
             const auto componentToAbsorb = (absorbingComponent == componentPerson1) ? componentPerson2 : componentPerson1;
 
-            for (auto squashedPersonId : componentToAbsorb->squashedPersonIdsInComponent)
+            for (auto personId : componentToAbsorb->personIdsInComponent)
             {
-                componentForSquashedPersonId[squashedPersonId] = absorbingComponent;
-                absorbingComponent->squashedPersonIdsInComponent.push_back(squashedPersonId);
+                componentForPersonId[personId] = absorbingComponent;
+                absorbingComponent->personIdsInComponent.push_back(personId);
             }
-            componentToAbsorb->squashedPersonIdsInComponent.clear();
+            componentToAbsorb->personIdsInComponent.clear();
 
 
-            largestComponentSize = std::max(largestComponentSize, static_cast<int>(absorbingComponent->squashedPersonIdsInComponent.size()));
+            largestComponentSize = std::max(largestComponentSize, static_cast<int>(absorbingComponent->personIdsInComponent.size()));
         }
 
         cout << largestComponentSize << endl;
