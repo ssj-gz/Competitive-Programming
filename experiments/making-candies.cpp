5037e12c95d9adc450010a8745f0fc67c586b487
Simon St James <kdedevel@etotheipiplusone.com>
2019-06-16 14:52:31 +0100

    Log the actions made in Making Candies.

--- experiments/making-candies.cpp
+++ experiments/making-candies.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <limits>
 
 using namespace std;
 
-void solveBruteForce(int64_t numWorkers, int64_t numMachines, int64_t costOfNewMachineOrWorker, int64_t numCandies, int64_t numCandiesToProduce, int numPassesSoFar, int64_t& bestNumPasses)
+struct Action
+{
+    int numWorkersBought = 0;
+    int numMachinesBought = 0;
+};
+
+void solveBruteForce(int64_t numWorkers, int64_t numMachines, int64_t costOfNewMachineOrWorker, int64_t numCandies, int64_t numCandiesToProduce, int numPassesSoFar, vector<Action>& actionsSoFar, int64_t& bestNumPasses)
 {
     if (numCandies >= numCandiesToProduce)
     {
-        if(numPassesSoFar <= bestNumPasses)
+        if(numPassesSoFar < bestNumPasses)
         {
             cout << "Found new best: " << numPassesSoFar << endl;
+            for (int i = 0; i < actionsSoFar.size(); i++)
+            {
+                cout << " day: " << i << " bought " << actionsSoFar[i].numWorkersBought << " workers and " << actionsSoFar[i].numMachinesBought << " machines" << endl;
+            }
             bestNumPasses = numPassesSoFar;
         }
         return;
@@ -26,7 +37,13 @@ void solveBruteForce(int64_t numWorkers, int64_t numMachines, int64_t costOfNewM
         const auto numCandiesAfterWorkers = numCandies -(numWorkersToBuy * costOfNewMachineOrWorker);
         for (int numMachinesToBuy = 0; numMachinesToBuy * costOfNewMachineOrWorker <= numCandiesAfterWorkers; numMachinesToBuy++)
         {
-            solveBruteForce(numWorkers + numWorkersToBuy, numMachines + numMachinesToBuy, costOfNewMachineOrWorker, numCandiesAfterWorkers - numMachinesToBuy * costOfNewMachineOrWorker, numCandiesToProduce, numPassesSoFar + 1, bestNumPasses);
+            Action action;
+            action.numWorkersBought = numWorkersToBuy;
+            action.numMachinesBought = numMachinesToBuy;
+
+            actionsSoFar.push_back(action);
+            solveBruteForce(numWorkers + numWorkersToBuy, numMachines + numMachinesToBuy, costOfNewMachineOrWorker, numCandiesAfterWorkers - numMachinesToBuy * costOfNewMachineOrWorker, numCandiesToProduce, numPassesSoFar + 1, actionsSoFar, bestNumPasses);
+            actionsSoFar.pop_back();
         }
     }
 }
@@ -34,7 +51,8 @@ void solveBruteForce(int64_t numWorkers, int64_t numMachines, int64_t costOfNewM
 int64_t solveBruteForce(int64_t m, int64_t w, int64_t p, int64_t n)
 {
     int64_t bestNumPasses = std::numeric_limits<int>::max();
-    solveBruteForce(w, m, p, 0, n, 0, bestNumPasses);
+    vector<Action> actionsSoFar;
+    solveBruteForce(w, m, p, 0, n, 0, actionsSoFar, bestNumPasses);
     return bestNumPasses;
 }
 
