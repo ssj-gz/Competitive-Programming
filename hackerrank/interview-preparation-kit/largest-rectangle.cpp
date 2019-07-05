65fe73a60b3369c95a4345ad48149882f2c25c9a
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-05 15:38:15 +0100

    Cleanup Largest Rectangle.

--- hackerrank/interview-preparation-kit/largest-rectangle.cpp
+++ hackerrank/interview-preparation-kit/largest-rectangle.cpp
@@ -1,46 +1,13 @@
 // Simon St James (ssjgz) - 2019-04-05
-#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <stack>
 #include <limits>
 #include <algorithm>
 
-#include <sys/time.h>
 #include <cassert>
 
 using namespace std;
-
-int64_t solveBruteForce(const vector<int>& heights)
-{
-    int64_t largestRectangle = 0;
-    const int n = heights.size();
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            int minHeight = std::numeric_limits<int>::max();
-            for (int k = i; k <= j; k++)
-            {
-                if (heights[k] < minHeight)
-                {
-                    minHeight = heights[k];
-                }
-            }
-            const int64_t rangeSize = (j - i + 1);
-            const int64_t rectangleSize = rangeSize * minHeight;
-            largestRectangle = max(largestRectangle, rectangleSize);
-        }
-    }
-
-    return largestRectangle;
-}
 
 vector<int> findIndexOfNextLowerThan(const vector<int>& heights)
 {
@@ -51,11 +18,9 @@ vector<int> findIndexOfNextLowerThan(const vector<int>& heights)
 
     for (int index = 0; index < n; index++)
     {
-        //cout << " findIndexOfNextLowerThan: index: " << index << " buildingHeightIndices.size(): " << buildingHeightIndices.size() << endl;
         while (!buildingHeightIndices.empty() && heights[buildingHeightIndices.top()] > heights[index])
         {
             indexOfNextLowerThan[buildingHeightIndices.top()] = index;
-            //cout << " Did an assignment to " << buildingHeightIndices.top() << " of " << index  << endl;
             buildingHeightIndices.pop();
         }
         buildingHeightIndices.push(index);
@@ -63,23 +28,34 @@ vector<int> findIndexOfNextLowerThan(const vector<int>& heights)
 
     return indexOfNextLowerThan;
 }
-
-int64_t solveOptimised(const vector<int>& heights)
+vector<int> findIndexOfPrevLowerThan(const vector<int>& heights)
 {
-    int64_t largestRectangle = 0;
+    // Leave the heavy-lifting to findIndexOfNextLowerThan after passing it
+    // a reversed copy of heights.
     const int n = heights.size();
-
-    const auto indexOfNextLowerThan = findIndexOfNextLowerThan(heights);
-
     const vector<int> reversedHeights(heights.rbegin(), heights.rend());
     auto indexOfPrevLowerThan = findIndexOfNextLowerThan(reversedHeights);
-    // Reverse order indexOfPrevLowerThan, and also "reverse" each index.
+
+    // Correct for the fact that we gave findIndexOfNextLowerThan a reversed version 
+    // of heights: Reverse order indexOfPrevLowerThan, and also "reverse" (flip from 
+    // left to right) each index.
     for (auto& index : indexOfPrevLowerThan)
     {
         if (index != -1)
             index = (n - 1) - index;
     }
     reverse(indexOfPrevLowerThan.begin(), indexOfPrevLowerThan.end());
+
+    return indexOfPrevLowerThan;
+}
+
+int64_t findLargestRectangle(const vector<int>& heights)
+{
+    int64_t largestRectangle = 0;
+    const int n = heights.size();
+
+    const auto indexOfNextLowerThan = findIndexOfNextLowerThan(heights);
+    const auto indexOfPrevLowerThan = findIndexOfPrevLowerThan(heights);
 
     for (int index = 0; index < n; index++)
     {
@@ -88,7 +64,6 @@ int64_t solveOptimised(const vector<int>& heights)
         const int64_t distanceToLeftWhereWeAreMin = (indexOfPrevLowerThan[index] == -1 ? index : index - indexOfPrevLowerThan[index] - 1);
         const int64_t distanceToRightWhereWeAreMin = (indexOfNextLowerThan[index] == -1 ? n - 1 - index : indexOfNextLowerThan[index] - index - 1);
         const int64_t largestRectangleWhereWeAreMin = (distanceToLeftWhereWeAreMin + 1 + distanceToRightWhereWeAreMin) * heights[index];
-        //cout << "index: " << index << " height: " << heights[index] << " indexOfNextLowerThan: " << indexOfNextLowerThan[index] << " indexOfPrevLowerThan: " << indexOfPrevLowerThan[index] << " distanceToRightWhereWeAreMin: " << distanceToRightWhereWeAreMin << endl;
 
         largestRectangle = max(largestRectangle, largestRectangleWhereWeAreMin);
     }
@@ -98,24 +73,6 @@ int64_t solveOptimised(const vector<int>& heights)
 
 int main(int argc, char* argv[])
 {
-    if (argc == 2)
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-        const int n = (rand() % 100) + 1;
-        const int maxHeight = rand() % 10000 + 1;
-
-        cout << n << endl;
-
-        for (int i = 0; i < n; i++)
-        {
-            cout << ((rand() % maxHeight) + 1) << " ";
-        }
-
-        return 0;
-    }
     int n;
     cin >> n;
 
@@ -126,15 +83,6 @@ int main(int argc, char* argv[])
         cin >> heights[i];
     }
 
-#ifdef BRUTE_FORCE
-    const auto solutionBruteForce = solveBruteForce(heights);
-    cout << "solutionBruteForce: " << solutionBruteForce << endl;
-    const auto solutionOptimised = solveOptimised(heights);
-    cout << "solutionOptimised: " << solutionOptimised << endl;
-    assert(solutionOptimised == solutionBruteForce);
-#else
-    const auto solutionOptimised = solveOptimised(heights);
-    cout << solutionOptimised << endl;
-#endif
+    cout << findLargestRectangle(heights) << endl;
 }
 
