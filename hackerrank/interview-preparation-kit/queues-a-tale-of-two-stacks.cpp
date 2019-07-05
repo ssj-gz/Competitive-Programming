db607c048f083f6f969cbf8d4c9506da1bbc1e04
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-05 18:04:54 +0100

    Initial attempt at Interview Preparation Kit: "Queues: A Tale of Two Stacks"

---
+++ hackerrank/interview-preparation-kit/queues-a-tale-of-two-stacks.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int q;
+    cin >> q;
+
+    stack<int> oldestOnTop;
+    stack<int> newestOnTop;
+
+    auto flushNewestOnTopToOldestOnTop = [&oldestOnTop, &newestOnTop]()
+    {
+        while (!newestOnTop.empty())
+        {
+            const int value = newestOnTop.top();
+            oldestOnTop.push(value);
+            newestOnTop.pop();
+        }
+    };
+
+    for (int i = 0; i < q; i++)
+    {
+        int type;
+        cin >> type;
+
+        switch(type)
+        {
+            case 1:
+                {
+                    int value;
+                    cin >> value;
+                    newestOnTop.push(value);
+                }
+                break;
+            case 2:
+                if (oldestOnTop.empty())
+                    flushNewestOnTopToOldestOnTop();
+                oldestOnTop.pop();
+                break;
+            case 3:
+                if (oldestOnTop.empty())
+                    flushNewestOnTopToOldestOnTop();
+                cout << oldestOnTop.top() << endl;
+                break;
+        }
+    }
+
+}
