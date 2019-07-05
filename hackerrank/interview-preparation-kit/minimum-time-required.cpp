6ed714ae7244430dc8ac59cd549e5f6049385a45
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-05 09:57:32 +0100

    Initial stab at optimised solution for Minimum Time Required.

--- hackerrank/interview-preparation-kit/minimum-time-required.cpp
+++ hackerrank/interview-preparation-kit/minimum-time-required.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+#include <cassert>
 
 using namespace std;
 
@@ -20,6 +23,52 @@ int64_t solveBruteForce(const vector<int>& machineTimeToProduce, int64_t goal)
     }
     return daysElapsed;
 }
+int64_t solveOptimised(const vector<int>& machineTimeToProduce, int64_t goal)
+{
+    const auto longestDaysToProduceOneItem = *std::max_element(machineTimeToProduce.begin(), machineTimeToProduce.end());
+    const auto maxDaysToProduceGoal = goal * longestDaysToProduceOneItem;
+
+    int64_t topDay = maxDaysToProduceGoal;
+    int64_t bottomDay = 0;
+
+    int64_t middleDay = -1;
+
+    auto numProducedAfterDays = [&machineTimeToProduce](int64_t daysElapsed)
+    {
+        int64_t numProduced = 0;
+        for (const auto daysToProduce : machineTimeToProduce)
+        {
+            numProduced += daysElapsed / daysToProduce;
+        }
+        return numProduced;
+    };
+    while (topDay - bottomDay > 1)
+    {
+        middleDay = bottomDay + (topDay - bottomDay) / 2;
+        cout << "topDay: " << topDay << " middleDay: " << middleDay << " bottomDay: " << bottomDay << endl;
+
+        const auto numProducedByMiddleDay = numProducedAfterDays(middleDay);
+        cout << " numProducedByMiddleDay: " << numProducedByMiddleDay << endl;
+        if (numProducedByMiddleDay < goal)
+        {
+            bottomDay = middleDay;
+        }
+        else
+        {
+            topDay = middleDay;
+        }
+    }
+    if (numProducedAfterDays(bottomDay) >= goal)
+    {
+        return bottomDay;
+    }
+    if (numProducedAfterDays(topDay) >= goal)
+    {
+        return topDay;
+    }
+    assert(false);
+    return middleDay;
+}
 
 int main(int argc, char* argv[])
 {
@@ -37,4 +86,7 @@ int main(int argc, char* argv[])
 
     const auto solutionBruteForce = solveBruteForce(machineTimeToProduce, goal);
     cout << "solutionBruteForce: " << solutionBruteForce << endl;
+    const auto solutionOptimised = solveOptimised(machineTimeToProduce, goal);
+    cout << "solutionOptimised: " << solutionOptimised << endl;
+    assert(solutionOptimised == solutionBruteForce);
 }
