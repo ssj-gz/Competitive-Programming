95b70488a412d845d9bbd33b08bee92c8f1bae3c
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-05 14:26:55 +0100

    Initial attempt at Interview Preparation Kit - "Balanced Brackets".
    Unsuccessful submission of Balanced Brackets.

---
+++ hackerrank/interview-preparation-kit/balanced-brackets.cpp
@@ -0,0 +1,68 @@
+// Simon St James (ssjg) - 2019-04-05
+#include <iostream>
+#include <stack>
+
+#include <cassert>
+
+using namespace std;
+
+bool isBalanced(const string& bracketString)
+{
+    std::stack<char> bracketStack;
+
+    for (const auto bracket : bracketString)
+    {
+        if (bracket == '{' || bracket == '[' || bracket == '(')
+        {
+            // Opening bracket.
+            bracketStack.push(bracket);
+        }
+        else
+        {
+            // Closing bracket.
+            if (bracketStack.empty())
+                return false;
+
+            const auto candidateMatchingBracket = bracketStack.top();
+            switch (bracket)
+            {
+                case '}':
+                    if (candidateMatchingBracket != '{')
+                        return false;
+                    break;
+                case ']':
+                    if (candidateMatchingBracket != '[')
+                        return false;
+                    break;
+                case ')':
+                    if (candidateMatchingBracket != '(')
+                        return false;
+                    break;
+                default:
+                    assert(false);
+            }
+            // Matched - pop the matching opening bracket off the stack.
+            bracketStack.pop();
+
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        string bracketString;
+        cin >> bracketString;
+
+        if (isBalanced(bracketString))
+            cout << "YES";
+        else
+            cout << "NO";
+        cout << endl;
+    }
+}
