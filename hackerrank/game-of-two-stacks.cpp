4a3c7bf6b43df5cc64ce85956d9d202e9f8654bd
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-06 14:17:11 +0100

    Documentation and readability tweaks for Game of Two Stacks.

--- hackerrank/game-of-two-stacks.cpp
+++ hackerrank/game-of-two-stacks.cpp
@@ -3,21 +3,45 @@
 #include <vector>
 #include <algorithm>
 
+#include <cassert>
+
 using namespace std;
 
 int main()
 {
-    int numGames;
-    cin >> numGames;
+    // Trivially easy.  Note first of all that the order we choose elements from
+    // A and B is irrelevant: only the number we chose from A and the number
+    // we choose from B affect the score and the number of moves.
+    // 
+    // We then use a two-pointer-esque kind of technique, where we initially 
+    // choose all of B, then winnow it back until the sum is <= limit.
+    //
+    // Then choose the next element of A, then, again, winnow the number
+    // chosen from B down (if necessary) until the sum is <= limit again.
+    //
+    // Since there are no negative elements, the number we choose from B
+    // is non-increasing as the number we choose from A increases, so
+    // this "winnowing", across all elements of A, contributes no more than
+    // O(m) to the run-time (this would not be the case if there were negative
+    // elements, though - that would be much harder!).
+    //
+    // There is a bit of hackage where we prepend a "0" to the beginning of A:
+    // this just makes the case where we pick no elements from A slightly less clunky,
+    // code-wise.
+    //
+    // And that's basically it!
+    //
+    // Slightly surprised by the low completion rate for this one (39.66% at the time
+    // of writing).
+
+    auto readInt = [](){ int x; cin >> x; assert(cin); return x; };
+    const int numGames = readInt();;
 
     for (int g = 0; g < numGames; g++)
     {
-        int n;
-        cin >> n;
-        int m;
-        cin >> m;
-        int x;
-        cin >> x;
+        const int n = readInt();
+        const int m = readInt();
+        const int limit = readInt();
 
         vector<int> a(n);
         for (int i = 0; i < n; i++)
@@ -30,27 +54,34 @@ int main()
             cin >> b[i];
         }
 
+        // Take every element in B, initially.
         int indexTakenFromB = m - 1;
         int64_t sumTakenFromB = 0;
         for (const auto bElement : b)
             sumTakenFromB += bElement;
 
-        a.insert(a.begin(), 0);
 
+        // A bit of a hack to make the case where we choose no elements
+        // from A a little less clunky.
+        a.insert(a.begin(), 0);
         int64_t sumTakenFromA = 0;
-        int largestNumMoves = 0;
         int numTakenFromA = 0;
+
+        int largestNumMoves = 0;
         for (const auto aElement : a)
         {
             sumTakenFromA += aElement;
-            while (indexTakenFromB != -1 && (sumTakenFromA + sumTakenFromB > x))
+            while (indexTakenFromB != -1 && (sumTakenFromA + sumTakenFromB > limit))
             {
+                // Winnow the amount taken from B down so we are below the limit.
                 sumTakenFromB -= b[indexTakenFromB];
                 indexTakenFromB--;
             }
-            if (sumTakenFromA + sumTakenFromB <= x)
+            if (sumTakenFromA + sumTakenFromB <= limit)
             {
-                const int numMoves = numTakenFromA + (indexTakenFromB + 1);
+                // This is a valid sequence of moves: update largestNumMoves.
+                const int numTakenFromB = (indexTakenFromB + 1);
+                const int numMoves = numTakenFromA + numTakenFromB;
                 largestNumMoves = max(largestNumMoves, numMoves);
             }
             numTakenFromA++;
