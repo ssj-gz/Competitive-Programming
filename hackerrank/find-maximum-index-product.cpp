a8a6ac166804887944ae052eb689867e104434da
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-06 16:01:45 +0100

    Initial attempt at Find Maximum Index Product; not yet working.

---
+++ hackerrank/find-maximum-index-product.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> findIndexOfNextGreaterThan(const vector<int>& a)
+{
+    std::stack<int> indices;
+    const int n = a.size();
+
+    vector<int> indexOfNextGreaterThan(n, -1);
+
+    for (int index = 0; index < n; index++)
+    {
+        while (!indices.empty() && a[indices.top()] < a[index])
+        {
+            indexOfNextGreaterThan[indices.top()] = index;
+            indices.pop();
+        }
+        indices.push(index);
+    }
+
+    return indexOfNextGreaterThan;
+}
+vector<int> findIndexOfPrevGreaterThan(const vector<int>& a)
+{
+    // Leave the heavy-lifting to findIndexOfNextGreaterThan after passing it
+    // a reversed copy of a.
+    const int n = a.size();
+    const vector<int> reversedArr(a.rbegin(), a.rend());
+    auto indexOfPrevGreaterThan = findIndexOfNextGreaterThan(reversedArr);
+
+    // Correct for the fact that we gave findIndexOfNextGreaterThan a reversed version 
+    // of a: Reverse order indexOfPrevGreaterThan, and also "reverse" (flip from 
+    // left to right) each index.
+    for (auto& index : indexOfPrevGreaterThan)
+    {
+        if (index != -1)
+            index = (n - 1) - index;
+    }
+    reverse(indexOfPrevGreaterThan.begin(), indexOfPrevGreaterThan.end());
+
+    return indexOfPrevGreaterThan;
+}
+
+
+int64_t findMaxIndexProduct(const vector<int>& a)
+{
+    const int n = a.size();
+
+    const auto indexOfNextGreaterThan = findIndexOfNextGreaterThan(a);
+    const auto indexOfPrevGreaterThan = findIndexOfPrevGreaterThan(a);
+
+    int64_t maxIndexProduct = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        const int64_t left_i = indexOfPrevGreaterThan[i] + 1;
+        const int64_t right_i = indexOfPrevGreaterThan[i] + 1;
+
+        const int64_t indexProduct_i = left_i * right_i;
+
+        maxIndexProduct = max(maxIndexProduct, indexProduct_i);
+    }
+
+    return maxIndexProduct;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << findMaxIndexProduct(a) << endl;
+}
+
+
