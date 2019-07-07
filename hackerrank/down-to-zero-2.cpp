ef65732b498616ef946586da5eadafb57ff056ad
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-07 08:49:30 +0100

    Major optimisations for Down to Zero 2:
    * Precompute all factors (<= sqrt(N)) for all N up to maxN using
      Sieve of Erastophenes
    * Precompute minDownToZeroLookup for all N, bottom-up - removes
      possibilty of stackoverflow

--- hackerrank/down-to-zero-2.cpp
+++ hackerrank/down-to-zero-2.cpp
@@ -7,25 +7,29 @@
 
 using namespace std;
 
-int findMinDownToZero(int N, vector<int>& minDownToZeroLookup)
+int findMinDownToZero(int N, vector<int>& minDownToZeroLookup, const vector<vector<int>>& factorsLookup)
 {
+    //cout << "N: " << N << endl;
     if (minDownToZeroLookup[N] != -1)
+    {
+        //cout << " returning cached" << endl;
         return minDownToZeroLookup[N];
+    }
 
 
     int minDownToZero = std::numeric_limits<int>::max();
-    for (int factor = 2; factor * factor <= N; factor++)
+    minDownToZero = min(minDownToZero, 1 + findMinDownToZero(N - 1, minDownToZeroLookup, factorsLookup));
+    for (const auto factor : factorsLookup[N])
     {
-        if ((N % factor) != 0)
-            continue;
+        if (factor * factor > N)
+            break;
 
         assert(N / factor == max(N / factor, factor));
-
-        minDownToZero = min(minDownToZero, 1 + findMinDownToZero(N / factor, minDownToZeroLookup));
+        minDownToZero = min(minDownToZero, 1 + findMinDownToZero(N / factor, minDownToZeroLookup, factorsLookup));
     }
 
-    minDownToZero = min(minDownToZero, 1 + findMinDownToZero(N - 1, minDownToZeroLookup));
 
+    //cout << " caching " << N << " = " << minDownToZero << endl; 
     minDownToZeroLookup[N] = minDownToZero;
 
     return minDownToZero;
@@ -40,15 +44,47 @@ int main()
     vector<int> minDownToZeroLookup(maxN + 1, -1);
     minDownToZeroLookup[0] = 0;
 
+    vector<vector<int>> factorsLookup(maxN);
+    for (int factor = 2; factor <= maxN; factor++)
+    {
+        for (int multiple = factor; multiple <= maxN; multiple += factor)
+        {
+            if (factor * factor <= multiple)
+            {
+                factorsLookup[multiple].push_back(factor);
+            }
+        }
+    }
+    //cout << "Built factorsLookup" << endl;
+#if 0
+    for (int i = 1; i <= maxN; i++)
+    {
+        cout << "N: " << i << " factors: " << endl;
+        for (const auto f : factorsLookup[i])
+        {
+            cout << f << " ";
+        }
+        cout << endl;
+    }
+#endif
+
+    for (int i = 1; i <= maxN; i++)
+    {
+#if 0
+        if ((i % 100) == 0)
+            cout << "i: " << i << " / " << maxN << endl;
+#endif
+        findMinDownToZero(i, minDownToZeroLookup, factorsLookup);
+    }
+
     for (int q = 0; q < Q; q++)
     {
         int N;
         cin >> N;
         assert(cin);
-        cout << "N: " << N << endl;
 
         assert(N <= maxN);
 
-        cout << findMinDownToZero(N, minDownToZeroLookup) << endl;
+        cout << findMinDownToZero(N, minDownToZeroLookup, factorsLookup) << endl;
     }
 }
