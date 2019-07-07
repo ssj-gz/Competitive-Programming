52e721d267efe970c632bd45bdec0a36576151f5
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-07 08:20:27 +0100

    Initial attempt at Down to Zero 2.
    Knocked up in a few mins - might just submit it now to see what happens
    :)
    
    Unsuccessful submission of "Down to Zero 2".

---
+++ hackerrank/down-to-zero-2.cpp
@@ -0,0 +1,48 @@
+// Simon St James (ssjgz) - 2019-04-07
+#include <iostream>
+#include <vector>
+#include <limits>
+
+#include <cassert>
+
+using namespace std;
+
+int findMinDownToZero(int N, vector<int>& minDownToZeroLookup)
+{
+    if (minDownToZeroLookup[N] != -1)
+        return minDownToZeroLookup[N];
+
+    int minDownToZero = 1 + findMinDownToZero(N - 1, minDownToZeroLookup);
+
+    for (int factor = 2; factor * factor <= N; factor++)
+    {
+        if ((N % factor) != 0)
+            continue;
+
+        assert(N / factor == max(N / factor, factor));
+
+        minDownToZero = min(minDownToZero, 1 + findMinDownToZero(N / factor, minDownToZeroLookup));
+    }
+
+    minDownToZeroLookup[N] = minDownToZero;
+
+    return minDownToZero;
+}
+
+int main()
+{
+    int Q;
+    cin >> Q;
+
+    const int maxN = 1'000'000;
+    vector<int> minDownToZeroLookup(maxN + 1, -1);
+    minDownToZeroLookup[0] = 0;
+
+    for (int q = 0; q < Q; q++)
+    {
+        int N;
+        cin >> N;
+
+        cout << findMinDownToZero(N, minDownToZeroLookup) << endl;
+    }
+}
