c849c02b5e1a8665243b4d7561a81fd011823b1a
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-09 18:25:13 +0100

    Testcase generator for "Kindergarten Adventures".

--- hackerrank/kindergarten-adventures.cpp
+++ hackerrank/kindergarten-adventures.cpp
@@ -1,5 +1,8 @@
+// Simon St James (ssjgz) - 2019-04-09
 #include <iostream>
 #include <vector>
+
+#include <sys/time.h>
 
 using namespace std;
 
@@ -28,8 +31,27 @@ vector<int> solveBruteForce(const vector<int>& extraTimeNeededForStudent)
     return numCompletedIfStartAt;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc == 2)
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+
+
+        const int n = rand() % 100 + 1;
+        cout << n << endl;
+
+        const int maxTime = rand() % 1000 + 1;
+        for (int i = 0; i < n; i++)
+        {
+            cout << ((rand() % maxTime) + 1) << " ";
+        }
+        cout << endl;
+
+        return 0;
+    }
     int n;
     cin >> n;
 
