9f62ecd9f2a9425a2ae4a341822c8f9f791c7057
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-09 13:16:58 +0100

    Working on a testcase generator for Simple Text Editor that should cause OOM on inefficient implementations.
    Currently not working - the testcase it generates can request an "Undo"
    when the undoStack is empty.

--- hackerrank/simple-text-editor.cpp
+++ hackerrank/simple-text-editor.cpp
@@ -1,6 +1,9 @@
 // Simon St James (ssjgz) - 2019-04-09
 #include <iostream>
 #include <stack>
+#include <vector>
+
+#include <sys/time.h>
 
 #include <cassert>
 
@@ -13,8 +16,149 @@ struct UndoableOp
     string opData;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc == 2)
+    {
+        // Try to generate a testcase that will trigger OOM if someone keeps
+        // all actual currentText's in the Undo stack, rather than just the operations
+        // needed to perform the Undos.
+        //
+        // Basic aim: keep currentText long!
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+
+
+        auto randomStringOfLength = [](int n)
+        {
+            string s;
+            for (int i = 1; i <= n; i++)
+            {
+                s.push_back('a' + rand() % 26);
+            }
+            return s;
+        };
+
+        struct Op
+        {
+            int opType;
+            string opArgString;
+            int opArgInt;
+        };
+
+        vector<Op> ops;
+
+        stack<UndoableOp> undoStack;
+        string currentText;
+
+        int sumOfCharsAdded = 0;
+        int sumOfCharsRemoved = 0;
+
+        while (currentText.length() <= 500'000)
+        {
+            const auto longString = randomStringOfLength(50'000 + (rand() % 50'000));
+
+            currentText += longString;
+            undoStack.push({UndoableOp::Append, longString});
+            ops.push_back({1, longString, -1});
+            sumOfCharsAdded += longString.length();
+        }
+
+        // TODO - update firstChangedCharIndexSincePrint with each op, and use it to
+        // with opType == 3 to get a bit more variety in what we are asked to print out.
+        int firstChangedCharIndexSincePrint = -1;
+
+        while (ops.size() <= 1'000'000 && sumOfCharsAdded < 1'000'000 && sumOfCharsRemoved < 2 * 1'000'000)
+        {
+            vector<int> allowableOpTypes;
+            assert(!undoStack.empty());
+            if (undoStack.top().opType == UndoableOp::Delete || currentText.size() - undoStack.top().opData.size() >= 400'000)
+            {
+                allowableOpTypes.push_back(4);
+            }
+            if (currentText.size() > 0)
+            {
+                allowableOpTypes.push_back(2);
+            }
+            allowableOpTypes.push_back(1);
+            if (currentText.size() > 0)
+                allowableOpTypes.push_back(3);
+
+            const int opType = allowableOpTypes[rand() % allowableOpTypes.size()];
+
+            if (opType == 1)
+            {
+                const int maxCharsToAdd = min(100, 1'000'000 - sumOfCharsAdded);
+                const int numCharsToAdd = (rand() % maxCharsToAdd ) + 1;
+                const string stringToAdd = randomStringOfLength(numCharsToAdd);
+
+                currentText += stringToAdd;
+                undoStack.push({UndoableOp::Append, stringToAdd});
+                ops.push_back({1, stringToAdd, -1});
+                sumOfCharsAdded += stringToAdd.length();
+            }
+            else if (opType == 2)
+            {
+                const int maxCharsToRemove = min<int>(100, currentText.size());
+                const int numToRemove = (rand() % maxCharsToRemove) + 1;
+                const string removedText = currentText.substr(currentText.size() - numToRemove);
+
+                currentText.erase(currentText.begin() + currentText.size() - numToRemove, currentText.end());
+                undoStack.push({UndoableOp::Delete, removedText});
+                ops.push_back({2, "", numToRemove});
+                sumOfCharsRemoved += numToRemove;
+            }
+            else if (opType == 3)
+            {
+                // TODO - use firstChangedCharIndexSincePrint for variety!
+                ops.push_back({3, "", (rand() % static_cast<int>(currentText.length()) + 1)});
+            }
+            else if (opType == 4)
+            {
+                assert(!undoStack.empty());
+                const auto& opToUndo = undoStack.top();
+                switch (opToUndo.opType)
+                {
+                    case UndoableOp::Append:
+                        {
+                            const int numToRemove = opToUndo.opData.size();
+                            assert(numToRemove <= currentText.size());
+                            currentText.erase(currentText.begin() + currentText.size() - numToRemove, currentText.end());
+                            break;
+                        }
+                    case UndoableOp::Delete:
+                        currentText += opToUndo.opData;
+                        break;
+                    default:
+                        assert(false);
+                        break;
+                }
+
+                undoStack.pop();
+                ops.push_back({4, "", -1});
+            }
+        }
+
+        cout << ops.size() << endl;
+        for (const auto& op : ops)
+        {
+            cout << op.opType;
+            if (op.opType == 1)
+            {
+                cout << op.opArgString;
+            }
+            else if (op.opType == 2 || op.opType == 3) 
+            {
+                cout << op.opArgInt;
+            }
+
+
+            cout << endl;
+        }
+
+        return 0;
+    }
     // Trivially easy.  The only potential snag is the memory usage needed for Undo-ability:
     // if we stored the complete current text for each Undoable
     // operation, we'd use O(n^2) memory under certain circumstances.
