182f52873e280791ca323b94768d033982483d60
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-10 15:50:23 +0100

    Documentation for "Jim and the Skyscraper".

--- hackerrank/jim-and-the-skyscrapers.cpp
+++ hackerrank/jim-and-the-skyscrapers.cpp
@@ -37,6 +37,7 @@ int64_t findNumValidPaths(const vector<int>& heightsOriginal)
             }
             else
             {
+                // We've completed a run of the same height: update numPaths accordingly.
                 numPaths += nChoose2(numOfSameHeightPoppedInARow) * 2;
                 numOfSameHeightPoppedInARow = 1;
             }
@@ -56,6 +57,25 @@ int64_t findNumValidPaths(const vector<int>& heightsOriginal)
 
 int main(int argc, char* argv[])
 {
+    // Pretty easy one, and similar to e.g. "Max Min Riddle"; "Largest Rectangle"; etc.
+    //
+    // We maintain a stack of heights encountered from left-to-right, and ensure it is non-decreasing:
+    // we do this by adding each new height h to the stack *after* popping all heights that
+    // are less than h from the stack.  Imagine if instead of just adding the heights, we 
+    // added pairs of the height plus the index of this height in heights.  It's hopefully clear
+    // that indices i and j, i < j, would both be on the stack at once if and only if there is a path
+    // between them that does not encounter any skyscraper taller than height[i].
+    //
+    // Further, it's hopefully clear that if the same height is present more than once in the stack,
+    // then these heights will be adjacent i.e. in a run of the same height.  Further further,
+    // each pair of the indices i < j with h[i] == h[j] will be on the stack if and only if
+    // there is a *valid* path between them.  If there are r of these in a row, then that will
+    // give rise to r choose 2 paths going from left to right, and (r choose 2) * 2 paths in total
+    // (there is a bijection between paths going left to right and paths going from right to left).
+    //
+    // So: we simply maintain the stack as described above, and count the number of runs of the same
+    // height that end up in the stack, and update numPaths as described above.  The whole process
+    // is O(n).
     int n;
     cin >> n;
 
