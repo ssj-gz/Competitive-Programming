52746279ff1797a706d826d0f9d622ca23c5471b
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-10 13:41:57 +0100

    Add some documentation for "Mr X and his Shots".

--- hackerrank/mr-x-and-his-shots.cpp
+++ hackerrank/mr-x-and-his-shots.cpp
@@ -40,7 +40,6 @@ class RangeTracker
             m_numActiveRangesAddedAfterIncrement = 0;
             while (!m_rangesByBegin.empty() && m_rangesByBegin.begin()->range.begin <= m_x)
             {
-                //cout << this << " adding new active range for x: " << m_x << endl;
                 const auto newActiveRange = *(m_rangesByBegin.begin());
                 m_activeRangesByEnd.insert(newActiveRange);
                 
@@ -50,7 +49,6 @@ class RangeTracker
             }
             while (!m_activeRangesByEnd.empty() && m_activeRangesByEnd.begin()->range.end < m_x)
             {
-                //cout << this << " removing active range for x: " << m_x << endl;
                 m_activeRangesByEnd.erase(m_activeRangesByEnd.begin());
             }
 
@@ -60,15 +58,16 @@ class RangeTracker
             return !m_rangesByBegin.empty() || !m_activeRangesByEnd.empty();
         }
     private:
+        // We add an "id" so that our sets may contain multiple copies of ranges
+        // with the same "begin" and/ or "end"!
         struct RangeWithId
         {
             Range range;
             int id = -1;
         };
+
         int m_x = 0;
-
         int m_numActiveRangesAddedAfterIncrement = 0;
-
 
         static bool compareRangeBegins(const RangeWithId& lhs, const RangeWithId& rhs)
         {
@@ -104,14 +103,13 @@ int64_t findNumShotsThatCanBeStopped(const vector<Range>& shots, const vector<Ra
         const int64_t newShotRangesActive = shotRangeTracker.numActiveRangesAddedAfterIncrement();
         const int64_t newPlayerRangesActive = playerRangeTracker.numActiveRangesAddedAfterIncrement();
 
-        // Update numShotsThatCanBeStopped based on the new shot ranges and player ranges that
+        // Update numShotsThatCanBeStopped based on the new shot ranges and new player ranges that
         // became active.
-        // Note that this overcounts.
+        // Note that this calculatin overcounts.
         numShotsThatCanBeStopped += newShotRangesActive * numPlayerRangesActive;
         numShotsThatCanBeStopped += newPlayerRangesActive * numShotRangesActive;
         // Remove the amount we overcounted by, above.
         numShotsThatCanBeStopped -= newPlayerRangesActive * newShotRangesActive;
-
     }
 
     return numShotsThatCanBeStopped;
@@ -119,6 +117,13 @@ int64_t findNumShotsThatCanBeStopped(const vector<Range>& shots, const vector<Ra
 
 int main(int argc, char* argv[])
 {
+    // Easy one, though there's a bit of subtlety with updating numShotsThatCanBeStopped
+    // involving overcounting.
+    //
+    // This is basically a "how many ranges surround a given point?" problem, where we have
+    // *two* lists of ranges.  RangeTracker handles these queries as we sweep from left to
+    // right across the cricket field, with "moving right" taking O(log2 max(numShots, numPlayers)) per move.
+    // Hopefully the code above is self-explanatory.
     int numShots;
     cin >> numShots;
 
