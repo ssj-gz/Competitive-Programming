c3f5a0d17427916cc3054177d147fe1713874b7b
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-20 13:54:51 +0100

    Remove yet more dead code from A Game of Primes.

--- codechef/a-game-of-primes.cpp
+++ codechef/a-game-of-primes.cpp
@@ -1,22 +1,11 @@
 // Simon St James (ssjgz) - 2019-07-20
-#define SUBMISSION
-#define VERIFY_RANGE_TRACKER
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#undef VERIFY_RANGE_TRACKER
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <set>
-#include <functional>
 #include <cmath>
-
 
 #include <cassert>
 
-#include <sys/time.h>
 
 using namespace std;
 
@@ -215,136 +204,6 @@ class RangeTracker
 #endif
 };
 
-vector<int> solveBruteForce(const vector<Query>& queries, int64_t K, const vector<int>& primesThatDivideK)
-{
-    const int maxRangeEnd = 100'000;
-    vector<int64_t> values(maxRangeEnd + 1, -1);
-    vector<int> queryResults;
-    int numQueries = 0;
-    for (const auto& query : queries)
-    {
-        cout << "query: " << numQueries << " out of " << queries.size() << endl;
-        if (query.queryType == '!')
-        {
-            for (int i = query.l; i <= query.r; i++)
-            {
-                if (values[i] == -1)
-                    values[i] = query.value;
-            }
-        }
-        else if (query.queryType == '?')
-        {
-            int num = 0;
-            for (int primeFactorOfKIndex = 0; primeFactorOfKIndex < primesThatDivideK.size(); primeFactorOfKIndex++)
-            {
-                bool hasFactor = false;
-                for (int i = query.l; i <= query.r; i++)
-                {
-                    if (values[i] == -1)
-                        continue;
-                    if ((values[i] % primesThatDivideK[primeFactorOfKIndex]) == 0)
-                    {
-                        hasFactor = true;
-                        break;
-                    }
-                }
-                if (hasFactor)
-                {
-                    num++;
-                }
-            }
-            queryResults.push_back(num);
-        }
-        numQueries++;
-    }
-    return queryResults;
-}
-
-class SegmentTree2
-{
-    public:
-        SegmentTree2(int maxRangeEnd)
-            : maxRangeEnd(maxRangeEnd),
-              tree(3 * maxRangeEnd, 0),
-              lazy(3 * maxRangeEnd, 0)
-        {
-            build(1, 0, maxRangeEnd - 1);
-        };
-        void setAllInRangeA(int left, int right, bool setOn)
-        {
-            range_update(1, 0, maxRangeEnd - 1, left, right, (setOn ? 2 : 1));
-        }
-        int getSumOfRange(int left, int right)
-        {
-            return query(1, 0, maxRangeEnd - 1, left, right);
-        }
-    private:
-        int maxRangeEnd = -1;
-        vector <int> tree;
-        vector <int> lazy;
-        void combine(int &v, const int &v1, const int &v2)
-        {
-            v = v1 + v2;
-        }
-
-        void build(int where, int left, int right)
-        {
-            if ( left > right ) return;
-            if ( left == right ) {
-                tree[where] = 0;
-                return;
-            }
-            int mid = (left+right)>>1;
-            build((where<<1), left, mid);
-            build((where<<1)+1, mid+1, right);
-            combine(tree[where], tree[(where<<1)], tree[(where<<1)+1]);
-        }
-
-        void push_down(int where, int left, int right)
-        {
-            if ( lazy[where] ) {
-                tree[where] = (lazy[where] == 1 ? 0 : (right - left + 1));
-                if ( left != right ) {
-                    lazy[(where<<1)] = lazy[where];
-                    lazy[(where<<1)+1] = lazy[where];
-                }
-                lazy[where] = 0;
-            }
-            return;
-        }
-
-        void range_update(int where, int left, int right ,int i, int j, int val)
-        {
-            push_down(where, left, right);
-            if ( left > right || left > j || right < i ) return;
-            if ( left >= i && right <= j ) {
-                lazy[where] = val;
-                push_down(where, left, right);
-                return;
-            }
-            int mid = (left+right)>>1;
-            range_update((where<<1), left, mid, i, j, val);
-            range_update((where<<1)+1, mid+1, right, i, j, val);
-            combine(tree[where], tree[(where<<1)], tree[(where<<1)+1]);
-        }
-
-        int query(int where, int left, int right, int i, int j)
-        {
-            push_down(where, left, right);
-            if ( left > right || left > j || right < i ) return 0;
-            if ( left >= i && right <= j ) 
-            {
-                return tree[where];
-            }
-            int mid = (left+right)>>1;
-            int ans = 0;
-            int ans1 = query((where<<1), left, mid, i, j);
-            int ans2 = query((where<<1)+1, mid+1, right, i, j);
-            combine(ans, ans1, ans2);
-            return ans;
-        }
-
-};
 vector<int> solveOptimised(const vector<Query>& queries, int64_t K, const vector<int>& primesThatDivideK)
 {
     vector<int> queryResults;
@@ -404,147 +263,6 @@ int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
 
-    if (false)
-    {
-        RangeTracker rangeTracker(1000);
-
-        rangeTracker.setRangeToOn(Range{3, 5});
-        rangeTracker.setRangeToOn(Range{1, 2});
-        rangeTracker.setRangeToOn(Range{6, 10});
-        rangeTracker.setRangeToOn(Range{3, 100});
-
-        rangeTracker.setRangeToOn(Range{200, 205});
-        rangeTracker.setRangeToOn(Range{207, 207});
-        rangeTracker.setRangeToOn(Range{207, 208});
-        rangeTracker.setRangeToOn(Range{170, 209});
-        rangeTracker.setRangeToOn(Range{50, 180});
-        rangeTracker.setRangeToOn(Range{60, 209});
-        rangeTracker.setRangeToOn(Range{62, 215});
-
-        while (true)
-        {
-            const int maxRangeEnd = (rand() % 1000) + 1;
-            const int numQueriesForThisRangeTracker = rand() % 1000 + 1;
-
-            RangeTracker rangeTracker(maxRangeEnd);
-            for (int q = 0; q < numQueriesForThisRangeTracker; q++)
-            {
-                int left = rand() % maxRangeEnd;
-                int right = rand() % maxRangeEnd;
-                if (right < left)
-                    swap(left, right);
-                if ((rand() % 2) == 0)
-                {
-                    rangeTracker.setRangeToOn(Range{left, right});
-                }
-                else
-                {
-                    rangeTracker.hasOnRangeOverlapping(Range{left, right});
-                }
-
-
-            }
-            cout << " performed " << numQueriesForThisRangeTracker << " queries with maxRangeEnd: " << maxRangeEnd << endl;
-
-        }
-
-        return 0;
-    }
-
-#if 0
-    if (true)
-    {
-        const int maxRangeEnd = 100'000;
-        auto combineSetValue = [](const int64_t& newValueToSetTo, const int64_t& earlierValueToSetTo)
-        {
-            return newValueToSetTo;
-        };
-
-        auto combineValues = [](const int& lhsValue, const int& rhsValue)
-        {
-            return lhsValue + rhsValue;
-        };
-
-        auto applySetValue = [](int64_t valueToSetTo, int& value)
-        {
-            value = valueToSetTo;
-        };
-
-        using NumPrimesTracker = SegmentTree<int, int64_t>;
-
-        NumPrimesTracker numPrimesTracker(maxRangeEnd, combineValues, applySetValue, combineSetValue);
-        numPrimesTracker.setInitialValues(vector<int>(maxRangeEnd, 0));
-        build(1, 0, maxRangeEnd - 1);
-
-
-        {
-            const int setL = 3;
-            const int setR = 100;
-            range_update(1, 0, maxRangeEnd - 1, setL, setR, 2);
-            numPrimesTracker.applyOperatorToAllInRange(setL, setR, true);
-
-            const int qL = 30;
-            const int qR = 50;
-            const int blee = ::query(1, 0, maxRangeEnd - 1, qL, qR);
-            const int blaa = numPrimesTracker.combinedValuesInRange(qL, qR);
-            cout << "Blee: " <<  blee << endl;
-            cout << "Blaa: " << blaa << endl;
-        }
-{
-            const int setL = 56;
-            const int setR = 63;
-            range_update(1, 0, maxRangeEnd - 1, setL, setR, 1);
-            numPrimesTracker.applyOperatorToAllInRange(setL, setR, false);
-
-            const int qL = 23;
-            const int qR = 80;
-            const int blee = ::query(1, 0, maxRangeEnd - 1, qL, qR);
-            const int blaa = numPrimesTracker.combinedValuesInRange(qL, qR);
-            cout << "Blee: " <<  blee << endl;
-            cout << "Blaa: " << blaa << endl;
-        }
-
-        return 0;
-    }
-#endif
-
-    if (argc == 2)
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-        const int K = (rand() % 1'000'000'000ULL) + 1;
-        const int Q = (rand() % 1000) + 1;
-        const int maxR = (rand() % 1000) + 1;
-        const int maxX = rand() % 1'000'000'000ULL + 1;
-
-        cout << K << " " << Q << endl;
-
-        for (int q = 0; q < Q; q++)
-        {
-            if (rand() % 2 == 0)
-            {
-                cout << "! ";
-                int l = rand() % maxR + 1;
-                int r = rand() % maxR + 1;
-                if (r < l)
-                    swap(l ,r);
-                cout << l << " " << r << " " << (rand() % maxX + 1) << endl;
-            }
-            else
-            {
-                cout << "? ";
-                int l = rand() % maxR + 1;
-                int r = rand() % maxR + 1;
-                if (r < l)
-                    swap(l ,r);
-                cout << l << " " << r << endl;
-            }
-        }
-        return 0;
-    }
-
     const int64_t maxXorK = 1'000'000'000ULL;
     const int sqrtMaxXorK = sqrt(maxXorK);
 
@@ -562,7 +280,6 @@ int main(int argc, char* argv[])
             }
         }
     }
-
 
     int64_t K;
     cin >> K;
@@ -622,31 +339,12 @@ int main(int argc, char* argv[])
         }
     }
 
-#ifdef BRUTE_FORCE
-    const auto solutionBruteForce = solveBruteForce(queries, K, primesThatDivideK);
-    cout << " solutionBruteForce: ";
-    for (const auto x : solutionBruteForce)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-    const auto solutionOptimised = solveOptimised(queries, K, primesThatDivideK);
-
-    cout << " solutionOptimised:  ";
-    for (const auto x : solutionOptimised)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-    assert(solutionOptimised == solutionBruteForce);
-#else
     const auto solutionOptimised = solveOptimised(queries, K, primesThatDivideK);
 
     for (const auto x : solutionOptimised)
     {
         cout << x << endl;
     }
-#endif
 
 
 }
