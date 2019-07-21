a5722642ea7d7ac36a62ad8abb36cae363dacc7a
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-21 12:15:30 +0100

    Another generic snapshot of "Number Game".

--- codechef/number-game.cpp
+++ codechef/number-game.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 
+#include <cassert>
+
 using namespace std;
 
 int main()
@@ -14,15 +16,55 @@ int main()
 
         string A;
         cin >> A;
+        cout << "A: " << A << endl;
 
         int modulus;
         cin >> modulus;
 
         int numStartingMoves = 0;
-        for (int i = 0; i < A.size(); i++)
+        vector<int> firstDigitsOfAMod = { 0 };
+        vector<int> lastDigitsOfAMod = { 0 };
+
+        int numOfDigitInA[10] = {};
+        int value = 0;
+        for (const auto digit : A)
+        {
+            const int digitValue = digit - '0';
+            numOfDigitInA[digitValue]++;
+            value = (value * 10 + digitValue) % modulus;
+            firstDigitsOfAMod.push_back(value);
+        }
+        value = 0;
+        int powerOf10 = 1;
+        for (auto digitReverseIter = A.crbegin(); digitReverseIter != A.crend(); digitReverseIter++)
+        {
+            const int digitValue = *digitReverseIter - '0';
+            value = (value + digitValue * powerOf10) % modulus;
+            lastDigitsOfAMod.push_back(value);
+
+            powerOf10 = (powerOf10 * 10) % modulus;
+        }
+
+        for (const auto x : lastDigitsOfAMod)
+        {
+            cout << " lastDigitsOfAMod: " << x << endl;
+        }
+        for (const auto x : firstDigitsOfAMod)
+        {
+            cout << " firstDigitsOfAMod: " << x << endl;
+        }
+
+
+        powerOf10 = 1;
+        for (int i = A.size() - 1; i >= 0; i--)
         {
             string aMinusDigit = A.substr(0, i) + A.substr(i + 1);
-            const int aMinusDigitMod = stoi(aMinusDigit) % modulus;
+            cout << "aMinusDigit: " << aMinusDigit << endl;
+            const int dbgAMinusDigitMod = stoi(aMinusDigit) % modulus;
+
+            const int aMinusDigitMod = (firstDigitsOfAMod[i] * powerOf10 + lastDigitsOfAMod[A.size() - i - 1]) % modulus;
+            cout << "i: " << i << " aMinusDigitMod: " << aMinusDigitMod << " dbgAMinusDigitMod: " << dbgAMinusDigitMod << endl;
+            assert(aMinusDigitMod == dbgAMinusDigitMod);
 
             vector<int> digitsInAMinusDigit;
             for (int digit = 0; digit <= 9; digit++)
@@ -57,6 +99,7 @@ int main()
             if (processed[0])
                 numStartingMoves++;
 
+            powerOf10 = (powerOf10 * 10) % modulus;
         }
         cout << numStartingMoves << endl;
 
