071a2e79e0dc7e0c63e9a5f3ebdf0e6b2b513ed0
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-21 17:44:56 +0100

    Cleanups/ refactorings for Number Game.

--- codechef/number-game.cpp
+++ codechef/number-game.cpp
@@ -3,11 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <map>
 #include <utility>
 
 #include <cassert>
-#include <sys/time.h>
 
 using namespace std;
 
@@ -15,70 +13,17 @@ enum Status { Unknown, Yes, No };
 
 int main(int argc, char* argv[])
 {
-    if (argc == 2)
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-
-        const int maxSumOfStringLengths = 1'000'000;
-        const int maxMod = 1000;
-        vector<string> strings;
-        int sumOfStringLengths = 0;
-        const int T = 100;
-        for (int t = 0; t < T; t++)
-        {
-            //const int stringLength = (rand() % (maxSumOfStringLengths - sumOfStringLengths) + 1);
-            int stringLength = (rand() % (30) + 2);
-            //int stringLength = (rand() % (10'000) + 2);
-            vector<int> digits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            if (t == 100)
-            {
-                assert(sumOfStringLengths < maxSumOfStringLengths);
-                stringLength = maxSumOfStringLengths - sumOfStringLengths;
-            }
-
-            random_shuffle(digits.begin(), digits.end());
-            const int numDistinctDigitsToUse = (rand() % 10) + 1;
-
-            string numberString;
-            for (int i = 0; i < stringLength; i++)
-            {
-                numberString.push_back('0' + rand() % numDistinctDigitsToUse);
-            }
-            strings.push_back(numberString);
-            sumOfStringLengths += stringLength;
-        }
-
-        if (strings.size() > 10000)
-            strings.resize(10000);
-
-        cout << strings.size() << endl;
-        for (int i = 0; i < strings.size(); i++)
-        {
-            const int mod = 800 + rand() % (maxMod - 800) + 1;
-            assert(mod <= maxMod);
-            cout << strings[i] << " " << mod << endl;
-        }
-        return 0;
-    }
     int T;
     cin >> T;
 
     for (int t = 0; t < T; t++)
     {
-
-        //cout << " t: " << t << " / " << T << endl;
         string A;
         cin >> A;
-        //cout << "A: " << A << endl;
 
         int modulus;
         cin >> modulus;
 
-        int numStartingMoves = 0;
         vector<int> firstDigitsOfAMod = { 0 };
         vector<int> lastDigitsOfAMod = { 0 };
 
@@ -144,8 +89,8 @@ int main(int argc, char* argv[])
             }
         }
 
-        vector<Status> canAppendAndMake0StartingWith(modulus, Unknown);
-        canAppendAndMake0StartingWith[0] = Yes;
+        vector<bool> canMake0ViaAppendsStartingWith(modulus, Unknown);
+        canMake0ViaAppendsStartingWith[0] = true;
 
         vector<vector<int>> canBeReachedByAppendFrom(modulus);
         for (int startValue = 0; startValue < modulus; startValue++)
@@ -157,38 +102,37 @@ int main(int argc, char* argv[])
             }
         }
 
+        vector<bool> visited(modulus, false);
         vector<int> toProcess = { 0 };
+        visited[0] = true;
+
         while (!toProcess.empty())
         {
             vector<int> nextToProcess;
             for (const auto p : toProcess)
             {
-                canAppendAndMake0StartingWith[p] = Yes;
+                canMake0ViaAppendsStartingWith[p] = true;
 
                 for (const auto reachedFrom : canBeReachedByAppendFrom[p])
                 {
-                    if (canAppendAndMake0StartingWith[reachedFrom] == Unknown)
+                    if (!visited[reachedFrom])
                     {
-                        canAppendAndMake0StartingWith[reachedFrom] = Yes;
+                        visited[reachedFrom] = true;
+
                         nextToProcess.push_back(reachedFrom);
                     }
                 }
             }
             toProcess = nextToProcess;
         }
+        int numStartingMovesWhereWeWin = 0;
         for (const auto startValue : valuesFromRemoving1Digit)
         {
-            //cout << " startValue: " << startValue << endl;
-            if (canAppendAndMake0StartingWith[startValue] == Yes)
+            if (canMake0ViaAppendsStartingWith[startValue])
             {
-                //cout << " yes" << endl;
-                numStartingMoves += numTimesCanMakeValueByRemoving1Digit[startValue];
-            }
-            else
-            {
-                //cout << " no" << endl;
+                numStartingMovesWhereWeWin += numTimesCanMakeValueByRemoving1Digit[startValue];
             }
         }
-        cout << numStartingMoves << endl; 
+        cout << numStartingMovesWhereWeWin << endl; 
     }
 }
