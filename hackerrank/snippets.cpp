206d5025d964a47be19f7a9011cbdb6aeef50335
Simon St James <kdedevel@etotheipiplusone.com>
2019-07-30 17:17:54 +0100

    Add a "fast integer input" function to snippets.cpp.
    Nabbed from https://stackoverflow.com/questions/14208472/input-faster-than-scanf-for-int-in-c-c

--- hackerrank/snippets.cpp
+++ hackerrank/snippets.cpp
@@ -904,5 +904,25 @@ int main(int argc, char* argv[])
 
 }
 
+// Begin fast integer input.
+#define gc getchar_unlocked
+
+void scan_integer( int &x )
+{
+    int c = gc();
+    x = 0;
+    int neg = 0;
+    for( ; ((c<48 || c>57) && c != '-'); c = gc() );
+    if( c=='-' ) {
+        neg=1;
+        c=gc();
+    }
+    for( ;c>47 && c<58; c = gc() ) {
+        x = (x << 1) + (x << 3) + c - 48;
+    }
+    if( neg )
+        x=-x;
+} 
 
 
+// End fast integer input.
