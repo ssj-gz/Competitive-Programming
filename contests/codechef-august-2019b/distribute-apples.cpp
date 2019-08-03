f0004a7adc9566e2f51aa270f3ea8138bddf0a61
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-03 14:59:59 +0100

    Clean up "Distribute Apples" a little.

--- contests/codechef-august-2019b/distribute-apples.cpp
+++ contests/codechef-august-2019b/distribute-apples.cpp
@@ -1,17 +1,9 @@
 // Simon St James (ssjgz) - 2019-08-03
-#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h>
 
 using namespace std;
 
@@ -24,63 +16,7 @@ T read()
     return toRead;
 }
 
-bool solveBruteForce(int64_t N, int64_t K)
-{
-    vector<int64_t> numInBoxEmployee1(K);
-    vector<int64_t> numInBoxEmployee2(K);
-
-    int64_t numApplesRemaining = N;
-
-    while (numApplesRemaining > 0)
-    {
-        // Employee1
-        for (int i = 0; i < K; i++)
-        {
-            numInBoxEmployee1[i]++;
-        }
-        // Employee2
-        {
-            const int64_t minInBox = *std::min_element(numInBoxEmployee2.begin(), numInBoxEmployee2.end());
-            vector<int64_t> indicesWithMinInBox;
-            for (int64_t i = 0; i < K; i++)
-            {
-                if (numInBoxEmployee2[i] == minInBox)
-                {
-                    indicesWithMinInBox.push_back(i);
-                }
-            }
-            const int64_t randIndexWithMinInBox = indicesWithMinInBox[rand() % indicesWithMinInBox.size()];
-            numInBoxEmployee2[randIndexWithMinInBox] += K;
-        }
-
-        numApplesRemaining -= K;
-    }
-
-    cout << "N: " << N << " K: " << K << endl;
-    cout << "numInBoxEmployee1: " << endl;
-    for (const auto x : numInBoxEmployee1)
-    {
-        cout << x << " ";
-    }
-    cout << endl << "numInBoxEmployee2: " << endl;
-    for (const auto x : numInBoxEmployee2)
-    {
-        cout << x << " ";
-    }
-    bool boxesEqual = true;
-    for (int64_t i = 0; i < K; i++)
-    {
-        if (numInBoxEmployee1[i] != numInBoxEmployee2[i])
-        {
-            cout << " found difference at index " << i << " - numInBoxEmployee1: " << numInBoxEmployee1[i] << " numInBoxEmployee2: " << numInBoxEmployee2[i] << endl;
-            boxesEqual = false;
-        }
-    }
-
-    return boxesEqual;
-}
-
-bool solveOptimised(int64_t numApples, int64_t numBoxes)
+bool calcCanBoxContentsDiffer(int64_t numApples, int64_t numBoxes)
 {
     assert((numApples % numBoxes) == 0);
     assert(numApples >= numBoxes);
@@ -94,48 +30,28 @@ bool solveOptimised(int64_t numApples, int64_t numBoxes)
     {
         // Have an equal number of apples in each box - in fact,
         // numApples / numBoxes of them, just like Candidate1!
-        return true;
+        // Box contents cannot differ.
+        return false;
     }
     else
     {
         // Have an unequal number of apples in each box - must
         // differ from Candidate1, all of whose boxes contain
         // the same number of apples.
-        return false;
+        return true;
     }
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        const int64_t K = rand() % 1000 + 1;
-        const int64_t N = ((rand() % 1000) + 1) * K;
-        cout << 1 << endl;
-        cout << N << " " << K << endl;
-        return 0;
-    }
-
     const int T = read<int>();
     for (int t = 0; t < T; t++)
     {
         const int64_t N = read<int64_t>();
         const int64_t K = read<int64_t>();
 
-#ifdef BRUTE_FORCE
-        const auto solutionBruteForce = solveBruteForce(N, K);
-        cout << "solutionBruteForce: " << (solutionBruteForce ? "NO" : "YES") << endl;
-        const auto solutionOptimised = solveOptimised(N, K);
-        cout << "solutionOptimised: " << (solutionOptimised ? "NO" : "YES") << endl;
-
-        assert(solutionOptimised == solutionBruteForce);
-#else
-        const auto solutionOptimised = solveOptimised(N, K);
-        cout << (solutionOptimised ? "NO" : "YES") << endl;
-#endif
+        const auto canBoxContentsDiffer = calcCanBoxContentsDiffer(N, K);
+        cout << (canBoxContentsDiffer ? "YES" : "NO") << endl;
     }
 }
 
