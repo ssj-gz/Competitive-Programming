d10fe434395a35afc8325ca576a89e9aef91d44e
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-08 10:23:26 +0100

    More minor renamings for CaGR.

--- contests/codechef-august-2019b/chef-and-gordon-ramsay.cpp
+++ contests/codechef-august-2019b/chef-and-gordon-ramsay.cpp
@@ -118,7 +118,7 @@ class SegmentTree
         }
 };
 
-void solveOptimisedAuxLCANoneOfA1A2A3(const vector<Node>& nodes, const Triple& P, int64_t& result)
+void solveOptimisedAuxLCAIsNotA2(const vector<Node>& nodes, const Triple& P, int64_t& result)
 {
     const bool isA2LessThanA1 = (P[1] < P[0]);
     const bool isA2LessThanA3 = (P[1] < P[2]);
@@ -206,11 +206,11 @@ int64_t solveOptimised2(vector<Node>& nodes, const Triple& P)
     SegmentTree nodeTracker(nodes.size() + 1);
     solveOptimisedAuxLCAIsA2(rootNode, nodeTracker, P, result);
 
-    solveOptimisedAuxLCANoneOfA1A2A3(nodes, P, result);
+    solveOptimisedAuxLCAIsNotA2(nodes, P, result);
     const bool isPMonotonic = (P[2] > P[1] && P[1] > P[0]) || (P[2] < P[1] && P[1] < P[0]);
     if (isPMonotonic)
     {
-        solveOptimisedAuxLCANoneOfA1A2A3(nodes, reversedP, result);
+        solveOptimisedAuxLCAIsNotA2(nodes, reversedP, result);
     }
     return result;
 }
