1fc03530a5f9f794efdfba6e91b2f4b6165635dd
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-09 18:50:42 +0100

    More cleanup for Zombie and the Caves.

--- contests/codechef-august-2019b/zombie-and-the-caves.cpp
+++ contests/codechef-august-2019b/zombie-and-the-caves.cpp
@@ -1,15 +1,9 @@
 // Simon St James (ssjgz) - 2019-08-03
-#define SUBMISSION
-#ifdef SUBMISSION
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h>
 
 using namespace std;
 
@@ -91,64 +85,6 @@ bool calcCanKillAllZombies(const vector<int64_t>& radiationPower, const vector<i
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        const int numCaves = rand() % 11 + 1;
-        const int maxHealth = rand() % 100 + 1;
-        const int maxRadiationPower = rand() % numCaves + 1;
-
-        cout << 1 << endl;
-        cout << numCaves << endl;
-
-        vector<int64_t> radiationPower;
-        for (int i = 0; i < numCaves; i++)
-        {
-            radiationPower.push_back((rand() % maxRadiationPower) + 1);
-        }
-        for (const auto& x : radiationPower)
-        {
-            cout << x << " ";
-        }
-        cout << endl;
-
-
-        const bool generateYES = ((rand() % 4) == 0);
-        if (generateYES)
-        {
-            vector<int64_t> radiationLevel(numCaves, 0);
-            for (int i = 0; i < numCaves; i++)
-            {
-                const int radiationRange = radiationPower[i];
-                const int radiationRangeLeft = max(0, i - radiationRange);
-                const int radiationRangeRight = min(numCaves - 1, i + radiationRange);
-
-                for (int j = radiationRangeLeft; j <= radiationRangeRight; j++)
-                {
-                    radiationLevel[j]++;
-                }
-            }
-            auto zombieHealth = radiationLevel;
-            random_shuffle(zombieHealth.begin(), zombieHealth.end());
-            for (const auto& x : zombieHealth)
-            {
-                cout << x << " ";
-            }
-            cout << endl;
-        }
-        else
-        {
-            for (int i = 0; i < numCaves; i++)
-            {
-                cout << ((rand() % maxHealth) + 1) << " ";
-            }
-            cout << endl;
-        }
-        return 0;
-    }
 
     const int T = read<int>();
     for (int t = 0; t < T; t++)
