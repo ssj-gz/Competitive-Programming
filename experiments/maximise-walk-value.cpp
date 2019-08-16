464ea7acc8f71f1fba7c4027e2cc5bd49b954e00
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-16 11:56:42 +0100

    A big chunk of cleanup for Maximise Walk Value, and a little documentation.

--- experiments/maximise-walk-value.cpp
+++ experiments/maximise-walk-value.cpp
@@ -1,8 +1,6 @@
 // Simon St James (ssjgz) - 2019-08-16
 #define SUBMISSION
-#define BRUTE_FORCE
 #ifdef SUBMISSION
-#undef BRUTE_FORCE
 #define NDEBUG
 #endif
 #include <iostream>
@@ -12,11 +10,9 @@
 
 #include <cassert>
 
-#include <sys/time.h>
-
 using namespace std;
 
-    template <typename T>
+template <typename T>
 T read()
 {
     T toRead;
@@ -36,7 +32,6 @@ struct CostInfo
 struct Node
 {
     vector<Node*> neighbours;
-    int id = -1;
 
     int cost = 0;
 
@@ -47,13 +42,14 @@ struct Node
 
 void doDfsFromSpecialNode(Node* currentNode, Node* parent, const int sourceSpecialNodeId)
 {
-    //cout << "doDfsFromSpecialNode: currentNode: " << currentNode->id << " sourceSpecialNodeId: " << sourceSpecialNodeId << " generatableCostsOnPathToSpecialNode.size(): " << currentNode->generatableCostsOnPathToSpecialNode.size() << endl;
-    assert(sourceSpecialNodeId < currentNode->generatableCostsOnPathToSpecialNode.size());
     auto& generatableCostsOnPathToSpecialNode = currentNode->generatableCostsOnPathToSpecialNode[sourceSpecialNodeId];
     if (parent)
     {
+        // We can generate precisely the set of costs that our parent did, just by not including our cost in the subset.
         generatableCostsOnPathToSpecialNode = parent->generatableCostsOnPathToSpecialNode[sourceSpecialNodeId];
         assert(generatableCostsOnPathToSpecialNode.size() == maxCost + 1);
+        // ... but we can also generate "cost from source special node to parent" + "currentNode->cost", by
+        // including currentNode in the subset!
         for (int i = maxCost; i >= currentNode->cost; i--)
         {
             if (generatableCostsOnPathToSpecialNode[i - currentNode->cost].canBeGenerated)
@@ -64,21 +60,14 @@ void doDfsFromSpecialNode(Node* currentNode, Node* parent, const int sourceSpeci
     }
     else
     {
+        // The only ones we can generate are 0 (by not including currentNode) and currentNode->cost
+        // (by including currentNode!)
         generatableCostsOnPathToSpecialNode.resize(maxCost + 1);
         generatableCostsOnPathToSpecialNode[0].canBeGenerated = true;
         generatableCostsOnPathToSpecialNode[currentNode->cost].canBeGenerated = true;
     }
 
-    //cout << "currentNode: " << currentNode->id << endl;
-    //cout << "Generatable costs from sourceSpecialNodeId: " << sourceSpecialNodeId << endl;
-#if 0
-    for (int cost = 0; cost <= maxCost; cost++)
-    {
-        if (generatableCostsOnPathToSpecialNode[cost].canBeGenerated)
-            cout << " " << cost << endl;
-    }
-#endif
-
+    // Update nextLowestGeneratableCost for our CostInfo.
     int nextLowestGeneratableCost = -1;
     for (int cost = 0; cost <= maxCost; cost++)
     {
@@ -87,6 +76,7 @@ void doDfsFromSpecialNode(Node* currentNode, Node* parent, const int sourceSpeci
             nextLowestGeneratableCost = cost;
     }
 
+    // Recurse.
     for (auto child : currentNode->neighbours)
     {
         if (child == parent)
@@ -127,6 +117,7 @@ PVValue findPVValue(const vector<CostInfo>& costInfo1, const vector<CostInfo>& c
         if (!costInfo[largestCost].canBeGenerated)
             largestCost = costInfo[largestCost].nextLowestGeneratableCost;
 
+        // We can always generate "0", so largestCost shoud never be -1.
         assert(largestCost != -1);
 
         return largestCost;
@@ -149,7 +140,6 @@ PVValue findPVValue(const vector<CostInfo>& costInfo1, const vector<CostInfo>& c
             nextCostIn2LessThanCostIn1 = costInfo2[nextCostIn2LessThanCostIn1].nextLowestGeneratableCost;
         while (nextCostIn2AtLeastCostIn1 != -1 && costInfo2[nextCostIn2AtLeastCostIn1].nextLowestGeneratableCost != -1 && costInfo2[nextCostIn2AtLeastCostIn1].nextLowestGeneratableCost >= costIn1)
             nextCostIn2AtLeastCostIn1 = costInfo2[nextCostIn2AtLeastCostIn1].nextLowestGeneratableCost;
-        //cout << " costIn1: " << costIn1 << " nextCostIn2LessThanCostIn1: " << nextCostIn2LessThanCostIn1 << " nextCostIn2AtLeastCostIn1: " << nextCostIn2AtLeastCostIn1 << endl;
 
         incorporateNewCostPair(costIn1, nextCostIn2LessThanCostIn1);
         incorporateNewCostPair(costIn1, nextCostIn2AtLeastCostIn1);
@@ -163,12 +153,10 @@ PVValue findPVValue(const vector<CostInfo>& costInfo1, const vector<CostInfo>& c
 
 int findBestPVValueForQuery(const Node* sourceNode, const Node* destNode, const int costLimit, const vector<Node*>& specialNodes)
 {
-    //cout << " query: sourceNode: " << sourceNode->id << " destNode: " << destNode->id << " costLimit: " << costLimit << endl;
     PVValue result;
 
     for (int specialNodeIndex = 0; specialNodeIndex < specialNodes.size(); specialNodeIndex++)
     {
-        //cout << " query: sourceNode: " << sourceNode->id << " destNode: " << destNode->id << " costLimit: " << costLimit << " specialNode: " << specialNodes[specialNodeIndex]->id  << endl;
         const auto resultWithThisPivot = findPVValue(sourceNode->generatableCostsOnPathToSpecialNode[specialNodeIndex], 
                                                      destNode->generatableCostsOnPathToSpecialNode[specialNodeIndex],
                                                      costLimit);
@@ -189,172 +177,17 @@ void buildLookupTables(vector<Node>& nodes, const vector<Node*>& specialNodes)
     {
         doDfsFromSpecialNode(specialNode, nullptr, specialNode->specialNodeIndex);
     }
-#ifndef NDEBUG
-    for (auto& node : nodes)
-    {
-        assert(node.generatableCostsOnPathToSpecialNode.size() == specialNodes.size());
-        for (int i = 0; i < specialNodes.size(); i++)
-        {
-            assert(node.generatableCostsOnPathToSpecialNode[i].size() == maxCost + 1);
-        }
-    }
-#endif
-}
-
-void findWeightListAlongPathAux(const Node* currentNode, const Node* parent, const Node* pathEnd, vector<int>& weightListSoFar, vector<int>& answer)
-{
-    weightListSoFar.push_back(currentNode->cost);
-    if (currentNode == pathEnd)
-        answer = weightListSoFar;
-
-    for (auto child : currentNode->neighbours)
-    {
-        if (child == parent)
-            continue;
-
-        findWeightListAlongPathAux(child, currentNode, pathEnd, weightListSoFar, answer);
-    }
-
-    weightListSoFar.pop_back();
-}
-
-vector<int> findCostsGeneratableFromList(const vector<int>& costList)
-{
-    vector<bool> useCost(costList.size(), false);
-
-    vector<int> results;
-
-    while (true)
-    {
-        int cost = 0;
-        for (int i = 0; i < costList.size(); i++)
-        {
-            if (useCost[i])
-                cost += costList[i];
-        }
-        results.push_back(cost);
-
-        int index = 0;
-        while (index < costList.size() && useCost[index] == true)
-        {
-            useCost[index] = false;
-            index++;
-        }
-        if (index == costList.size())
-            break;
-        useCost[index] = true;
-    }
-
-    sort(results.begin(), results.end());
-    results.erase(unique(results.begin(), results.end()), results.end());
-
-    return results;
-}
-
-vector<int> findWeightListAlongPath(const Node* pathBegin, const Node* pathEnd)
-{
-    vector<int> result;
-    vector<int> weightListSoFar;
-
-    findWeightListAlongPathAux(pathBegin, nullptr, pathEnd, weightListSoFar, result);
-
-    return result;
-}
-
-
-int solveQueryBruteForce(const Node* sourceNode, const Node* destNode, const int costLimit, const vector<Node*>& specialNodes)
-{
-    PVValue result;
-    for (auto specialNode : specialNodes)
-    {
-        const auto weightSumsGeneratedSourceToSpecial = findCostsGeneratableFromList(findWeightListAlongPath(sourceNode, specialNode)); 
-        const auto weightSumsGeneratedSpecialToDest = findCostsGeneratableFromList(findWeightListAlongPath(specialNode, destNode)); 
-
-        cout << "weightSumsGeneratedSourceToSpecial: " << endl;
-        for (const auto x : weightSumsGeneratedSourceToSpecial)
-            cout << x << " ";
-        cout << endl;
-        cout << "weightSumsGeneratedSpecialToDest: " << endl;
-        for (const auto x : weightSumsGeneratedSpecialToDest)
-            cout << x << " ";
-        cout << endl;
-
-        for (const auto cost1 : weightSumsGeneratedSourceToSpecial)
-        {
-            if (cost1 > costLimit)
-                continue;
-            for (const auto cost2 : weightSumsGeneratedSpecialToDest)
-            {
-                if (cost2 > costLimit)
-                    continue;
-                PVValue newPVValue = { abs(cost1 - cost2), cost1 + cost2 };
-                updatePVValue(result, newPVValue);
-            }
-        }
-    }
-
-    return result.maxSumForMinDiff;
 }
 
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        const int numNodes = rand() % 1000 + 1;
-        const int numSpecialNodes = rand() % (min(numNodes, 10)) + 1;
-        const int numQueries = rand() % 1000 + 1;
-
-        const int maxCostPerNode = rand() % 100 + 1;
-        const int maxCostInQuery = rand() % 1000 + 1;
-
-        cout << numNodes << " " << numSpecialNodes << " " << numQueries << endl;
-        for (int i = 0; i < numNodes - 1; i++)
-        {
-            cout << ((rand() % (i + 1)) + 1) << " ";
-        }
-        cout << endl;
-
-        vector<int> nodeIds;
-        for (int i = 0; i < numNodes; i++)
-        {
-            nodeIds.push_back(i + 1);
-            cout << ((rand() % maxCostPerNode)) << " ";
-        }
-        cout << endl;
-
-        random_shuffle(nodeIds.begin(), nodeIds.end());
-
-        for (int i = 0; i < numSpecialNodes; i++)
-        {
-            cout << (nodeIds[i]) << " ";
-        }
-        cout << endl;
-
-        for (int i = 0; i < numQueries; i++)
-        {
-            const int nodeId1 = (rand() % numNodes) + 1;
-            const int nodeId2 = (rand() % numNodes) + 1;
-            const int costLimit = rand() % maxCostInQuery;
-
-            cout << nodeId1 << " " << nodeId2 << " " << costLimit << endl; 
-        }
-
-        return 0;
-    }
 
     const int numNodes = read<int>();
     const int numSpecialNodes = read<int>();
     const int numQueries = read<int>();
 
     vector<Node> nodes(numNodes);
-    for (int i = 0; i < numNodes; i++)
-    {
-        nodes[i].id = i + 1;
-    }
 
     for (int i = 0; i < numNodes - 1; i++)
     {
@@ -362,14 +195,11 @@ int main(int argc, char* argv[])
 
         nodes[v].neighbours.push_back(&(nodes[i + 1]));
         nodes[i + 1].neighbours.push_back(&(nodes[v]));
-
-        //cout << "edge between: " << nodes[i + 1].id << " and " << nodes[v].id << endl;
     }
 
     for (int i = 0; i < numNodes; i++)
     {
         nodes[i].cost = read<int>();
-        //cout << "node cost: " << nodes[i].cost << endl;
     }
 
     vector<Node*> specialNodes;
@@ -378,7 +208,6 @@ int main(int argc, char* argv[])
     {
         specialNodes.push_back(&(nodes[read<int>() - 1]));
         specialNodes.back()->specialNodeIndex = i;
-        //cout << " special node: " << specialNodes.back()->id << endl;
     }
 
     buildLookupTables(nodes, specialNodes);
@@ -388,20 +217,11 @@ int main(int argc, char* argv[])
         Node* sourceNode = &(nodes[read<int>() - 1]);
         Node* destNode = &(nodes[read<int>() - 1]);
         int maxCost = read<int>();
-        assert(cin);
 
-#ifdef BRUTE_FORCE
-        const auto queryResultOptimised = findBestPVValueForQuery(sourceNode, destNode, maxCost, specialNodes);
-        cout << "queryResultOptimised: " << queryResultOptimised << endl;
-        const auto queryResultBruteForce = solveQueryBruteForce(sourceNode, destNode, maxCost, specialNodes);
-        cout << "solutionBruteForce: " << queryResultBruteForce << endl;
-
-        assert(queryResultOptimised == queryResultBruteForce);
-#else
         const auto queryResultOptimised = findBestPVValueForQuery(sourceNode, destNode, maxCost, specialNodes);
         cout << queryResultOptimised << endl;
-#endif
     }
+    assert(cin);
 }
 
 
