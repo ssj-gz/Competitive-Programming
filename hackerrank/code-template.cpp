369cf705891490e3b35119c371fc01db886bf047
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-21 10:06:35 +0100

    Move the Code Template out of snippets.cpp into its own file to make it easier to copy-n-paste.

---
+++ hackerrank/code-template.cpp
@@ -0,0 +1,76 @@
+// Simon St James (ssjgz) - 2019-XX-XX
+//#define SUBMISSION
+#define BRUTE_FORCE
+#ifdef SUBMISSION
+#undef BRUTE_FORCE
+#define NDEBUG
+#endif
+#include <iostream>
+#include <vector>
+
+#include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
+using namespace std;
+
+template <typename T>
+T read()
+{
+    T toRead;
+    cin >> toRead;
+    assert(cin);
+    return toRead;
+}
+
+#if 0
+SolutionType solveBruteForce()
+{
+    SolutionType result;
+    
+    return result;
+}
+#endif
+
+#if 0
+SolutionType solveOptimised()
+{
+    SolutionType result;
+    
+    return result;
+}
+#endif
+
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+        return 0;
+    }
+    
+    // TODO - read in testcase.
+    
+#ifdef BRUTE_FORCE
+#if 0
+    const auto solutionBruteForce = solveBruteForce();
+    cout << "solutionBruteForce: " << solutionBruteForce << endl;
+#endif
+#if 0
+    const auto solutionOptimised = solveOptimised();
+    cout << "solutionOptimised: " << solutionOptimised << endl;
+    
+    assert(solutionOptimised == solutionBruteForce);
+#endif
+#else
+    const auto solutionOptimised = solveOptimised();
+    cout << solutionOptimised << endl;
+#endif
+
+    assert(cin);
+}
