e0b5e62375f58a0d9354c43f40dbae3d4b0a9478
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-21 06:56:59 +0100

    More cleanup of Ambiguous Permutation.

--- codechef/ambiguous-permutation.cpp
+++ codechef/ambiguous-permutation.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <cassert>
-
-#include <sys/time.h>
 
 using namespace std;
 
@@ -26,79 +23,11 @@ bool isAmbiguous(const vector<int>& a)
         inversePermutation[a[i] - 1] = i + 1;
     }
 
-    return inversePermutation == a;
+    return (inversePermutation == a);
 }
-
 
 int main(int argc, char* argv[])
 {
-    if (argc == 2)
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-        int numGenerated = 0;
-        int numAmbiguous = 0;
-        int numNotAmbiguous = 0;
-
-        for (int t = 0; t < 20; t++)
-        {
-            const int N = 15;
-            vector<int> a(N);
-            for (int i = 0; i < N; i++)
-            {
-                a[i] = (i + 1);
-            }
-            const auto originalA = a;
-
-            const bool generateAmbiguous = ((rand() % 2) == 1);
-
-            if (generateAmbiguous)
-            {
-                numAmbiguous++;
-                vector<pair<int, int>> swaps;
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = i; j < N; j++)
-                    {
-                        swaps.push_back({i, j});
-                    }
-                }
-                random_shuffle(swaps.begin(), swaps.end());
-                assert(isAmbiguous(a));
-                for (const auto blah : swaps)
-                {
-                    swap(a[blah.first], a[blah.second]);
-                    if (!isAmbiguous(a))
-                    {
-                        swap(a[blah.first], a[blah.second]);
-                    }
-                }
-                assert(isAmbiguous(a));
-            }
-            else
-            {
-                numNotAmbiguous++;
-                while (isAmbiguous(a))
-                {
-                    random_shuffle(a.begin(), a.end());
-                }
-                assert(!isAmbiguous(a));
-            }
-            cout << N << endl;
-            for (const auto x : a)
-            {
-                cout << x << " ";
-            }
-            cout << endl;
-
-
-        }
-        cout << 0 << endl;
-        cerr << "Generated " << numGenerated << " ambiguous: " << numAmbiguous << " not ambiguous: " << numNotAmbiguous << endl;
-        return 0;
-    }
     while (true)
     {
         const int N = read<int>();
