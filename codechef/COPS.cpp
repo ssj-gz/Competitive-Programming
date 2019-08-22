44bb31961dcc8f2b252d60ba871804861c5ccbbe
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-22 05:59:37 +0100

    Some cleanup for COPS.

--- codechef/COPS.cpp
+++ codechef/COPS.cpp
@@ -1,17 +1,9 @@
-// Simon St James (ssjgz) - 2019-XX-XX
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
+// Simon St James (ssjgz) - 2019-08-22
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h>
 
 using namespace std;
 
@@ -24,54 +16,10 @@ T read()
     return toRead;
 }
 
-#if 0
-SolutionType solveBruteForce()
-{
-    SolutionType result;
-
-    return result;
-}
-#endif
-
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-
-    return result;
-}
-#endif
-
-
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        cout << 1 << endl;
-        const int M = rand() % 10 + 1;
-        const int x = rand() % 10 + 1;
-        const int y = rand() % 10 + 1;
-        cout << M << " " << x << " " << y << endl;
-        vector<int> allHouseNumbers;
-        for (int i = 1; i <= 100; i++)
-        {
-            allHouseNumbers.push_back(i);
-        }
-        random_shuffle(allHouseNumbers.begin(), allHouseNumbers.end());
-        for (int i = 0; i < M; i++)
-        {
-            cout << allHouseNumbers[i] << " ";
-        }
-        cout << endl;
-        return 0;
-    }
 
-    // TODO - read in testcase.
     const int numHouses = 100;
     const int T = read<int>();
 
@@ -82,15 +30,14 @@ int main(int argc, char* argv[])
         const int y = read<int>();
 
         vector<int> copLocations(M);
-        for (auto& location : copLocations)
-            location = read<int>() - 1;
-
+        for (auto& copLocation : copLocations)
+            copLocation = read<int>() - 1;
 
         const int houseSearchSpan = x * y;
 
         vector<bool> isHouseSafe(numHouses, true);
 
-        for (const auto copLocation : copLocations)
+        for (const auto& copLocation : copLocations)
         {
             for (int i = copLocation - houseSearchSpan; i <= copLocation + houseSearchSpan; i++)
             {
@@ -100,7 +47,7 @@ int main(int argc, char* argv[])
         }
 
         int numSafeHouses = 0;
-        for (const auto safe : isHouseSafe)
+        for (const auto& safe : isHouseSafe)
         {
             if (safe)
                 numSafeHouses++;
@@ -109,24 +56,7 @@ int main(int argc, char* argv[])
         cout << numSafeHouses << endl;
     }
 
-
     assert(cin);
-#ifdef BRUTE_FORCE
-#if 0
-    const auto solutionBruteForce = solveBruteForce();
-    cout << "solutionBruteForce: " << solutionBruteForce << endl;
-#endif
-#if 0
-    const auto solutionOptimised = solveOptimised();
-    cout << "solutionOptimised: " << solutionOptimised << endl;
-
-    assert(solutionOptimised == solutionBruteForce);
-#endif
-#else
-    const auto solutionOptimised = solveOptimised();
-    cout << solutionOptimised << endl;
-#endif
-
 }
 
 
