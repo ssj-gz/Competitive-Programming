093cf7f0a058f6d086a2372985c296d17cedbc37
Simon St James <kdedevel@etotheipiplusone.com>
2019-08-29 19:11:48 +0100

    Cleanup Bear and Candies 123.

--- codechef/bear-and-candies-123.cpp
+++ codechef/bear-and-candies-123.cpp
@@ -1,16 +1,10 @@
-// Simon St James (ssjgz) - 2019-XX-XX
-#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
+// Simon St James (ssjgz) - 2019-08-29
+//
+// Problem link: https://www.codechef.com/problems/CANDY123
+//
 #include <iostream>
-#include <vector>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -23,37 +17,9 @@ T read()
     return toRead;
 }
 
-#if 0
-SolutionType solveBruteForce()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
-
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        cout << 1 << endl;
-        cout << ((rand() % 1000) + 1) << " " << ((rand() % 1000) + 1) << endl;
-        return 0;
-    }
     
     const int T = read<int>();
 
