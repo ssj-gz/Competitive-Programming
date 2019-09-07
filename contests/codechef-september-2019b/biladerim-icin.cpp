38e9474bfb9ebdde7713a55e057d2a83f3cb9805
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-07 20:20:28 +0100

    A bit of exploration for "Biladerim Icin" .
    It's kind of leading somewhere, I think - it looks like we can use
    similar logic to P-Sequences to cut down on the computations.

--- contests/codechef-september-2019b/biladerim-icin.cpp
+++ contests/codechef-september-2019b/biladerim-icin.cpp
@@ -6,11 +6,12 @@
 #define BRUTE_FORCE
 #ifdef SUBMISSION
 #undef BRUTE_FORCE
-#define NDEBUG
+//#define NDEBUG
 #endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 #include <cassert>
 
@@ -202,10 +203,16 @@ int64_t solveOptimised(int64_t maxA, int64_t maxB, int64_t maxC)
     bool finished = false;
     for (int64_t B = 1; B <= maxB && !finished; B++)
     {
+        B = 899;
+        int64_t lastC = -1;
+        vector<int64_t> diffsBetweenCs;
+        vector<int64_t> lengthsOfRunsOfCs;
+        int64_t currentCRunLength = 1;
+
         for (int64_t A = 2; A <= maxA && !finished; A++)
         {
-            //cout << "B: " << B << " A: " << A << endl;
-            int64_t C = (B * B) / A;
+            cout << "B: " << B << " A: " << A << endl;
+            int64_t C = ((B * B) + 1) / (A - 1);
 
             auto isValidC = [&A, &B, maxC](const int C)
             {
@@ -213,16 +220,17 @@ int64_t solveOptimised(int64_t maxA, int64_t maxB, int64_t maxC)
                 return C >= 1 && C <= maxC && ((A - 1) * (C - 1) > B * B);
             };
 
-            //cout << " choosing initial C = " << C << " isValidC: " << isValidC(C) << endl;
+            cout << " choosing initial C = " << C << " isValidC: " << isValidC(C) << endl;
 
             while (isValidC(C - 1))
             {
-                //cout << " too high; decrementing" << endl;
+                assert(false);
+                cout << " too high; decrementing" << endl;
                 C--;
             }
             while (C < maxC && !isValidC(C))
             {
-                //cout << " too low; incrementing" << endl;
+                cout << " too low; incrementing" << endl;
                 C++;
             }
             if (!isValidC(C))
@@ -230,23 +238,67 @@ int64_t solveOptimised(int64_t maxA, int64_t maxB, int64_t maxC)
                 //cout << " Could not find C for A: " << A << " B: " << B << endl;
                 continue;
             }
-            //cout << "Found valid C: " << C << endl;
-            if (C == 2)
+            cout << "Found valid C: " << C << endl;
+            if (A - 1 >= sqrt(B * B + 1))
+            {
+                if (C == lastC)
+                {
+                    currentCRunLength++;
+                }
+                else
+                {
+                    lengthsOfRunsOfCs.push_back(currentCRunLength);
+                    currentCRunLength = 1;
+                }
+            }
+            else
+            {
+                if (lastC != -1)
+                {
+                    diffsBetweenCs.push_back(abs(C - lastC));
+                    if (diffsBetweenCs.back() == 0)
+                    {
+                        cout << "Whoo: A: " << A << " B: " << B << endl;
+                    }
+                }
+            }
+            //if (C == 2)
             {
                 // Done - all remaining A's paired with all possible C's will do.
                 //cout << "Found C == 2 for A: " << A << " B: " << B << " -  done" << endl;
                 //cout << "result so far: " << result << " maxA - A: " << (maxA - A) << " maxC: " << maxC << endl;
-                result += ModNum(maxA - A + 1) * ModNum(maxC - 1);
+                //result += ModNum(maxA - A + 1) * ModNum(maxC - 1);
                 //finished = true;
                 //assert(false);
-                break;
+                //break;
             }
-            else
+            //else
             {
                 result += maxC - C + 1;
             }
+            
+            lastC = C;
 
         }
+
+        cout << "B:" << B << endl;
+        cout << " diffsBetweenCs:" << endl;
+        for (const auto x : diffsBetweenCs)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+        reverse(lengthsOfRunsOfCs.begin(), lengthsOfRunsOfCs.end());
+        cout << " lengthsOfRunsOfCs:" << endl;
+        for (const auto x : lengthsOfRunsOfCs)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+        break;
+
+
+
     }
     
     return result.value();
@@ -301,5 +353,7 @@ int main(int argc, char* argv[])
 #endif
     }
 
+    // ∂  f(x) = fxx
+    // ∂x
     assert(cin);
 }
