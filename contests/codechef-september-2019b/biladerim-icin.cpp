2ed7064f599d4a09550dc5d440c2a06d9bb9258d
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-07 16:45:07 +0100

    A bit of random testing of the "negative <=> (a-1)*(c-1) <= b * b" hypothesis.

--- contests/codechef-september-2019b/biladerim-icin.cpp
+++ contests/codechef-september-2019b/biladerim-icin.cpp
@@ -26,54 +26,93 @@ T read()
     assert(cin);
     return toRead;
 }
+struct Blah
+{
+    Blah(double x, double y, double z)
+        : x(x), y(y), z(z)
+    {
+    }
+    double x, y;
+    double z;
+};
+
+bool operator<(const Blah& lhs, const Blah& rhs)
+{
+    if (lhs.z != rhs.z)
+        return lhs.z < rhs.z;
+    if (lhs.x != rhs.x)
+        return lhs.x < rhs.x;
+    return lhs.y < rhs.y;
+}
+ostream& operator<<(ostream& os, const Blah& blah)
+{
+    os << "z: " << blah.z << " x: " << blah.x << " y: " << blah.y << endl;
+    return os;
+}
 
 #if 1
 int64_t solveBruteForce()
 {
     // Looks like minimum of f will be < 0 if (a - 1) * (c - 1) < b * b.
     int64_t result = 0;
-    const int A = 51;
-    const int B = 10;
-    const int C = 3;
-
-    cout << "(A - 1) * (C - 1): " << (A - 1) * (C - 1) << endl;
-    cout << "B * B: " << (B * B) << endl;
-
-    cout << "sausage: " << (double)(C - 1) - (double)(B) * B / ((A - 1)) << endl; 
-
-    auto f = [A, B, C](double x, double y)
+    while (true)
     {
-        return ((double)A - 1) * x * x + 2 * (double)B * x * y + ((double)C - 1) * y * y;
-    };
-    cout << "bleep: " << f(-100, -8.333335) << endl;
+        const int A = rand() % 20 + 1;
+        const int B = rand() % 20 + 1;
+        const int C = rand() % 20 + 1;
 
-    struct Blah
-    {
-        double x, y;
-        double z;
-    };
-    vector<Blah> blahs;
 
-    const double range = 10;
-    for (double x = -range; x <= range; x += 0.01)
-    {
-        for (double y = -range; y <= range; y += 0.01)
+        const bool isSaddle = (A - 1) * (C - 1) <= B * B;
+        cout << "(A - 1) * (C - 1): " << (A - 1) * (C - 1) << endl;
+        cout << "B * B: " << (B * B) << endl;
+        cout << "isSaddle: " << isSaddle << endl;
+
+        //cout << "sausage: " << (double)(C - 1) - (double)(B) * B / ((A - 1)) << endl; 
+
+        auto f = [A, B, C](double x, double y)
         {
-            blahs.push_back({x, y, f(x, y)});
-        }
-    }
-    sort(blahs.begin(), blahs.end(), [](const auto lhs, const auto rhs)
-            {
-            if (lhs.z != rhs.z)
-            return lhs.z < rhs.z;
-            if (lhs.x != rhs.x)
-            return lhs.x < rhs.x;
-            return lhs.y < rhs.y;
-            });
+            return ((double)A - 1) * x * x + 2 * (double)B * x * y + ((double)C - 1) * y * y;
+        };
+        //cout << "bleep: " << f(-100, -8.333335) << endl;
 
-    for (const auto blah : blahs)
-    {
-        cout << "z: " << blah.z << " x: " << blah.x << " y: " << blah.y << endl;
+        //vector<Blah> blahs;
+
+        Blah minBlah = {0, 0, 3000};
+
+        const double range = 10;
+        for (double x = -range; x <= range; x += 0.01)
+        {
+            for (double y = -range; y <= range; y += 0.01)
+            {
+                //blahs.push_back({x, y, f(x, y)});
+                if (Blah(x, y, f(x,y)) < minBlah)
+                {
+                    minBlah = Blah(x, y, f(x,y));
+                }
+            }
+        }
+        cout << "minBlah: " << minBlah << endl;
+
+        const bool isMinNegative = (minBlah.z < -0.01);
+        cout << "minBlah negative: " << isMinNegative << "  isSaddle: " << isSaddle << endl;
+        assert(isMinNegative == isSaddle);
+#if 0
+        sort(blahs.begin(), blahs.end(), [](const auto lhs, const auto rhs)
+                {
+                if (lhs.z != rhs.z)
+                return lhs.z < rhs.z;
+                if (lhs.x != rhs.x)
+                return lhs.x < rhs.x;
+                return lhs.y < rhs.y;
+                });
+#endif
+
+#if 0
+        for (const auto blah : blahs)
+        {
+            cout << "z: " << blah.z << " x: " << blah.x << " y: " << blah.y << endl;
+        }
+#endif
     }
     
     return result;
