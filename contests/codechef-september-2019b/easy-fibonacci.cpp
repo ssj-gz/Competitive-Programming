d8978d02039efaeceeed4d247173adf52451ad00
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-07 07:51:27 +0100

    Snapshot of Easy Fibonacci.
    Probably close to submissible, now.

--- contests/codechef-september-2019b/easy-fibonacci.cpp
+++ contests/codechef-september-2019b/easy-fibonacci.cpp
@@ -18,63 +18,97 @@ T read()
     return toRead;
 }
 
+int solveBruteForce(int64_t N)
+{
+    int64_t fn_2_mod10 = 0;
+    int64_t fn_1_mod10 = 1;
+
+    vector<int> a;
+    a.push_back(fn_2_mod10);
+    a.push_back(fn_1_mod10);
+    for (int i = 0; i < N - 2; i++)
+    {
+        //cout << "fn: " <<  fn << " mod 10: " << (fn % 10) << endl;
+        int64_t fn_mod10 = (fn_2_mod10 + fn_1_mod10) % 10;
+        a.push_back(fn_mod10);
+
+        fn_2_mod10 = fn_1_mod10;
+        fn_1_mod10 = fn_mod10;
+    }
+
+    while (a.size() > 1)
+    {
+        vector<int> b;
+        for (int i = 1; i < a.size(); i += 2)
+        {
+            b.push_back(a[i]);
+        }
+        a = b;
+        cout << "new A:" << endl;
+        for (const auto x : a)
+        {
+            cout << " " << x;
+        }
+        cout << endl;
+    }
+    return a.front();
+}
+
 
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    
+
+    vector<int> fibonacciMod10Cycle;
+
+    int64_t fn_2_mod10 = 0;
+    int64_t fn_1_mod10 = 1;
+    fibonacciMod10Cycle.push_back(fn_2_mod10);
+    fibonacciMod10Cycle.push_back(fn_1_mod10);
+
+    int i = 0;
+    while (true)
+    {
+        //cout << "fn: " <<  fn << " mod 10: " << (fn % 10) << endl;
+        int64_t fn_mod10 = (fn_2_mod10 + fn_1_mod10) % 10;
+        fibonacciMod10Cycle.push_back(fn_mod10);
+        cout << "n: " << (i + 2) << " fn_mod10: " << fn_mod10 << endl;
+
+
+        fn_2_mod10 = fn_1_mod10;
+        fn_1_mod10 = fn_mod10;
+
+        if (fn_2_mod10 == 0 && fn_1_mod10 == 1)
+        {
+            fibonacciMod10Cycle.pop_back();
+            fibonacciMod10Cycle.pop_back();
+            break;
+        }
+        i++;
+    }
+
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
     {
         const int64_t N = read<int64_t>(); 
 
-#if 0
-        int64_t fn_2 = 0;
-        int64_t fn_1 = 1;
-        cout << (fn_2 % 10) << endl;
-        cout << (fn_1 % 10) << endl;
-
-        int64_t fn_2_mod10 = 0;
-        int64_t fn_1_mod10 = 1;
-
-        for (int i = 0; i < N - 2; i++)
+        cout << "fibonacciMod10Cycle: " << endl;
+        for (int i = 0; i < fibonacciMod10Cycle.size(); i++)
         {
-            int64_t fn = fn_2 + fn_1;
-            cout << "fn: " <<  fn << " mod 10: " << (fn % 10) << endl;
-            int64_t fn_mod10 = (fn_2_mod10 + fn_1_mod10) % 10;
-            cout << "fn_mod10: " << fn_mod10 << endl;
-
-            assert(fn_mod10 == (fn % 10));
-
-            fn_2 = fn_1;
-            fn_1 = fn;
-
-            fn_2_mod10 = fn_1_mod10;
-            fn_1_mod10 = fn_mod10;
+            cout << " N: " << (i + 1) << " f: " << fibonacciMod10Cycle[i] << endl;
         }
-#endif
-        vector<int> a(N);
-        for (int i = 0; i < N; i++)
+        int64_t largestPowerOf2 = 1;
+        while (largestPowerOf2 <= N)
         {
-            a[i] = i;
+            largestPowerOf2 <<= 1;
         }
+        largestPowerOf2 >>= 1;
+        const int64_t fibonacciIndex = (largestPowerOf2 - 1);
 
-        while (a.size() > 1)
-        {
-            vector<int> b;
-            for (int i = 1; i < a.size(); i += 2)
-            {
-                b.push_back(a[i]);
-            }
-            a = b;
-            cout << "new A:" << endl;
-            for (const auto x : a)
-            {
-                cout << " " << x;
-            }
-            cout << endl;
-        }
+        const auto solutionBruteForce = solveBruteForce(N);
+        cout << "solutionBruteForce: " << solutionBruteForce << endl;
+        cout << "solutionOptimised: " << fibonacciMod10Cycle[fibonacciIndex % fibonacciMod10Cycle.size()] << endl;
     }
 
 
