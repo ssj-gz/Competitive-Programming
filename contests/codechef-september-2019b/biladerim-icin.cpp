87da9171da808abf24e194f7916bc3df3537c0f6
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-09 12:52:53 +0100

    Yet more cleanup for "Biladerim Icin".

--- contests/codechef-september-2019b/biladerim-icin.cpp
+++ contests/codechef-september-2019b/biladerim-icin.cpp
@@ -2,12 +2,6 @@
 // 
 // Solution to: https://www.codechef.com/SEPT19B/problems/LAPD
 //
-#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -15,8 +9,6 @@
 #include <limits>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -100,7 +92,6 @@ int64_t divCeiling(int64_t x, int64_t y)
 
 struct CForA
 {
-    int64_t A = -1;
     int64_t C = std::numeric_limits<int64_t>::max();
     ModNum cumulativeC;
 };
@@ -121,56 +112,42 @@ struct LookupForB
 
 vector<LookupForB> computeLookups(int64_t maxB)
 {
-    //cout << "computeLookups: " << endl;
     vector<LookupForB> lookup(maxB + 1);
-
 
     for (int B = 1; B <= maxB; B++)
     {
-        //cout << "B: " << B << endl;
         const int64_t maxA = B * B + 1;
         const int64_t sqrtMaxA = sqrt(maxA);
-        //cout << "sqrtMaxA: " << sqrtMaxA << endl;
-        //cout << "B: " << B << endl;
-        //B = 20;
         auto& lookupForB = lookup[B];
         if (B == 1)
         {
+            // Ugly - special case for B == 1.
             lookupForB.cForA.resize(1);
-            lookupForB.cForA[0].A = 0;
             lookupForB.repetitionsOfC.push_back({std::numeric_limits<int64_t>::max(), 1, 1});
             lookupForB.repetitionsOfC.push_back({3, 1, 2});
         }
         else if (B == 2)
         {
+            // Ugly - special case for B == 2.
             lookupForB.cForA.resize(sqrtMaxA + 1);
-            lookupForB.cForA[2].A = 2;
             lookupForB.cForA[2].C = 6;
             lookupForB.repetitionsOfC.push_back({4, 1, 3});
             lookupForB.repetitionsOfC.push_back({3, 2, 5});
         }
         else
         {
-            const int64_t sqrtMaxA = sqrt(maxA);
             lookupForB.cForA.resize(sqrtMaxA + 1);
-            //cout << "B: " << B << " maxA: " << maxA << " sqrt(maxA): " << sqrtMaxA << endl;
             for (int A = 2; A <= sqrtMaxA; A++)
             {
                 const int C = divCeiling(B * B + 1, A - 1) + 1;
-                lookupForB.cForA[A].A = A;
                 lookupForB.cForA[A].C = C;
-                //cout << "A: " << A << " cForA: " << C << endl;
             }
             const int64_t finalC = lookupForB.cForA.back().C;
-            //cout << "finalC: " << finalC << endl;
 
             int64_t cRepeated = finalC;
             int64_t aAfterCRepeats = sqrtMaxA;
-#if 0
-            lookupForB.repetitionsOfC.push_back({cRepeated, 1, aAfterCRepeats});
-            cRepeated--;
-            aAfterCRepeats++;
-#endif
+
+            // This is a bit of a bodge - not sure why it's needed, to be honest XD
             cRepeated--;
             aAfterCRepeats++;
             lookupForB.repetitionsOfC.push_back({cRepeated, 1, aAfterCRepeats});
@@ -180,9 +157,7 @@ vector<LookupForB> computeLookups(int64_t maxB)
             cRepeated--;
             for (int i = sqrtMaxA - 1; i >= 2; i--)
             {
-                //cout << " i: " << i << " aAfterCRepeats:"  << aAfterCRepeats << endl;
                 const int64_t numRepetitions = lookupForB.cForA[i].C - lookupForB.cForA[i + 1].C;
-                //cout << "numRepetitions: " << numRepetitions << endl;
                 assert(numRepetitions >= 1);
                 aAfterCRepeats += numRepetitions;
                 lookupForB.repetitionsOfC.push_back({cRepeated, numRepetitions, aAfterCRepeats});
@@ -190,6 +165,7 @@ vector<LookupForB> computeLookups(int64_t maxB)
             }
         }
 
+        // Calculate cumulativeC/ cumulativeCTimesReps for cForA/ repetitionsOfC.
         {
             ModNum cumulativeCForA;
             for (int A = 2; A <= sqrtMaxA; A++)
@@ -210,33 +186,6 @@ vector<LookupForB> computeLookups(int64_t maxB)
 
     return lookup;
 }
-
-int64_t solveBruteForce(int64_t maxA, int64_t maxB, int64_t maxC)
-{
-    // Looks like minimum of f will be < 0 if (a - 1) * (c - 1) < b * b.
-    int64_t result = 0;
-
-    for (int64_t B = 1; B <= maxB; B++)
-    {
-        for (int64_t A = 1; A <= maxA; A++)
-        {
-            if ((A % 100) == 0)
-                cout << "B: " << B << " A: " << A << endl;
-            for (int64_t C = 1; C <= maxC; C++)
-            {
-                if ((A - 1) * (C - 1) > B * B)
-                {
-                    result = (result + 1) % Mod;
-                }
-
-            }
-        }
-    }
-    
-    return result;
-}
-
-
 
 int64_t solveOptimised(int64_t maxA, int64_t maxB, int64_t maxC, const vector<LookupForB>& lookup)
 {
@@ -328,26 +277,7 @@ int64_t solveOptimised(int64_t maxA, int64_t maxB, int64_t maxC, const vector<Lo
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-
-        for (int t = 0; t < T; t++)
-        {
-            cout << ((rand() % 300) + 1) << " " << ((rand() % 100) + 1) << " " << ((rand() % 300) + 1) << endl;
-        }
-
-        return 0;
-    }
     
-    // TODO - read in testcase.
     const auto T = read<int>();
 
     struct Query
@@ -360,8 +290,6 @@ int main(int argc, char* argv[])
     vector<Query> queries;
 
     int64_t largestMaxB = 0;
-
-
     for (int t = 0; t < T; t++)
     {
         queries.push_back({read<int64_t>(), read<int64_t>(), read<int64_t>()});
@@ -376,23 +304,9 @@ int main(int argc, char* argv[])
         const int64_t maxB = queries[t].maxB;
         const int64_t maxC = queries[t].maxC;
 
-        //const int64_t maxA = read<int64_t>();
-        //const int64_t maxB = read<int64_t>();
-        //const int64_t maxC = read<int64_t>();
-#ifdef BRUTE_FORCE
-        const auto solutionBruteForce = solveBruteForce(maxA, maxB, maxC);
-        cout << "solutionBruteForce: " << solutionBruteForce << endl;
-        const auto solutionOptimised = solveOptimised(maxA, maxB, maxC, lookups);
-        cout << "solutionOptimised:  " << solutionOptimised << endl;
-
-        assert(solutionOptimised == solutionBruteForce);
-#else
         const auto solutionOptimised = solveOptimised(maxA, maxB, maxC, lookups);
         cout << solutionOptimised << endl;
-#endif
     }
 
-    // ∂  f(x) = fxx
-    // ∂x
     assert(cin);
 }
