29550125a1f23353af5dcfee1421d427ac92da6c
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-09 20:03:52 +0100

    Initial attempt at "Chef and Interesting Subsequences".
    https://www.codechef.com/SEPT19B/problems/CHEFINSQ
    
    Brute force and test generator.

---
+++ contests/codechef-september-2019b/chef-and-interesting-subsequences.cpp
@@ -0,0 +1,154 @@
+// Simon St James (ssjgz) - 2019-09-09
+// 
+// Solution to: https://www.codechef.com/SEPT19B/problems/CHEFINSQ
+//
+//#define SUBMISSION
+#define BRUTE_FORCE
+#ifdef SUBMISSION
+#undef BRUTE_FORCE
+#define NDEBUG
+#endif
+#include <iostream>
+#include <vector>
+#include <limits>
+
+#include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
+using namespace std;
+
+template <typename T>
+T read()
+{
+    T toRead;
+    cin >> toRead;
+    assert(cin);
+    return toRead;
+}
+
+#if 1
+int solveBruteForce(int N, int K, const vector<int>& a)
+{
+    int result = 0;
+    int minimumSizeSubset = numeric_limits<int>::max();
+
+    vector<bool> useElement(N, false);
+
+    while (true)
+    {
+        vector<int> subset;
+        for (int i = 0; i < N; i++)
+        {
+            if (useElement[i])
+                subset.push_back(a[i]);
+        }
+        if (subset.size() == K)
+        {
+            int sum = 0;
+            for (const auto x : subset)
+            {
+                sum += x;
+            }
+            if (sum == minimumSizeSubset)
+            {
+                result++;
+            }
+            else if (sum < minimumSizeSubset)
+            {
+                minimumSizeSubset = sum;
+                result = 1;
+            }
+        }
+
+        int index = 0;
+        while (index < N && useElement[index])
+        {
+            useElement[index] = false;
+            index++;
+        }
+        if (index == N)
+            break;
+
+        useElement[index] = true;
+    }
+
+    return result;
+}
+
+#endif
+
+#if 0
+int64_t solveOptimised()
+{
+    int64_t result;
+    
+    return result;
+}
+#endif
+
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+        //const int T = rand() % 100 + 1;
+        const int T = 1;
+
+        cout << T << endl;
+
+        for (int t = 0; t < T; t++)
+        {
+            const int N = rand() % 20 + 1;
+            const int K = rand() % 50;
+            const int maxA = rand() % 100 + 1;
+
+            cout << N << " " << K << endl;
+            for (int i = 0; i < N; i++)
+            {
+                cout << ((rand() % maxA) + 1) << " ";
+            }
+            cout << endl;
+        }
+
+        return 0;
+    }
+    
+    // TODO - read in testcase.
+    const auto T = read<int>();
+
+    for (int t = 0; t < T; t++)
+    {
+        const int N = read<int>();
+        const int K = read<int>();
+
+        vector<int> a(N);
+        for (auto& aElement : a)
+        {
+            aElement = read<int>();
+        }
+
+#ifdef BRUTE_FORCE
+#if 1
+        const auto solutionBruteForce = solveBruteForce(N, K, a);
+        cout << "solutionBruteForce: " << solutionBruteForce << endl;
+#endif
+#if 0
+        const auto solutionOptimised = solveOptimised();
+        cout << "solutionOptimised:  " << solutionOptimised << endl;
+
+        assert(solutionOptimised == solutionBruteForce);
+#endif
+#else
+        const auto solutionOptimised = solveOptimised();
+        cout << solutionOptimised << endl;
+#endif
+    }
+
+    assert(cin);
+}
