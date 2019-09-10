e7ebfe19dcb6828a688a29332460edd6c3bfafac
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-10 13:48:31 +0100

    Begin work on "Doofish Set".
    Just read in a testcase for now - don't have any particularly promising
    ideas for approaching it, yet - even Brute Force is pretty hard!

---
+++ contests/codechef-september-2019b/doofish-set.cpp
@@ -0,0 +1,89 @@
+// Simon St James (ssjgz) - 2019-09-10
+// 
+// Solution to: https://www.codechef.com/SEPT19B/problems/DOOFST
+//
+//#define SUBMISSION
+#define BRUTE_FORCE
+#ifdef SUBMISSION
+#undef BRUTE_FORCE
+#define NDEBUG
+#endif
+#include <iostream>
+#include <vector>
+
+#include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
+using namespace std;
+
+template <typename T>
+T read()
+{
+    T toRead;
+    cin >> toRead;
+    assert(cin);
+    return toRead;
+}
+
+#if 1
+int solveBruteForce(const int N, const int M, const vector<pair<int, int>>& hatefulPairs)
+{
+    int result = 0;
+    
+    return result;
+}
+#endif
+
+#if 0
+int solveOptimised()
+{
+    int result;
+    
+    return result;
+}
+#endif
+
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+
+
+        return 0;
+    }
+
+    const int N = read<int>();
+    const int M = read<int>();
+
+    vector<pair<int, int>> hatefulPairs(M);
+    for (auto& pair : hatefulPairs)
+    {
+        pair.first = read<int>();
+        pair.second = read<int>();
+    }
+
+#ifdef BRUTE_FORCE
+#if 1
+    const auto solutionBruteForce = solveBruteForce(N, M, hatefulPairs);
+    cout << "solutionBruteForce: " << solutionBruteForce << endl;
+#endif
+#if 0
+    const auto solutionOptimised = solveOptimised();
+    cout << "solutionOptimised:  " << solutionOptimised << endl;
+
+    assert(solutionOptimised == solutionBruteForce);
+#endif
+#else
+    const auto solutionOptimised = solveOptimised();
+    cout << solutionOptimised << endl;
+#endif
+
+    assert(cin);
+}
