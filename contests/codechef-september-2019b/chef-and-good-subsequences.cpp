7d8a9fcf0258429ac945222b4b74e46e0c7f4990
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-15 07:45:52 +0100

    Cleanup for Chef and Good Subsequences.

--- contests/codechef-september-2019b/chef-and-good-subsequences.cpp
+++ contests/codechef-september-2019b/chef-and-good-subsequences.cpp
@@ -2,19 +2,11 @@
 // 
 // Solution to: https://www.codechef.com/SEPT19B/problems/GDSUB
 //
-#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -31,11 +23,6 @@ class ModNum
         ModNum& operator+=(const ModNum& other)
         {
             m_n = (m_n + other.m_n) % Mod;
-            return *this;
-        }
-        ModNum& operator-=(const ModNum& other)
-        {
-            m_n = (Mod + m_n - other.m_n) % Mod;
             return *this;
         }
         ModNum& operator*=(const ModNum& other)
@@ -55,23 +42,11 @@ ModNum operator+(const ModNum& lhs, const ModNum& rhs)
     return result;
 }
 
-ModNum operator-(const ModNum& lhs, const ModNum& rhs)
-{
-    ModNum result(lhs);
-    result -= rhs;
-    return result;
-}
-
 ModNum operator*(const ModNum& lhs, const ModNum& rhs)
 {
     ModNum result(lhs);
     result *= rhs;
     return result;
-}
-
-bool operator==(const ModNum& lhs, const ModNum& rhs)
-{
-    return lhs.value() == rhs.value();
 }
 
 ostream& operator<<(ostream& os, const ModNum& toPrint)
@@ -89,72 +64,7 @@ T read()
     return toRead;
 }
 
-vector<int> getPrimesUpTo8000()
-{
-
-    const int largestPrime = 8000;
-    vector<bool> isPrime(largestPrime + 1, true);
-    vector<int> primesUpTo8000;
-    for (int64_t factor = 2; factor <= largestPrime; factor++)
-    {
-        const bool isFactorPrime = isPrime[factor];
-        assert(factor < isPrime.size());
-        if (isFactorPrime)
-        {
-            primesUpTo8000.push_back(factor);
-        }
-        for (int64_t multiple = factor * factor; multiple < isPrime.size(); multiple += factor)
-        {
-            if (!isPrime[multiple] && !isFactorPrime)
-            {
-                // This multiple has already been marked, and since factor is not prime,
-                // all subsequent multiples will already have been marked (by any of the
-                // prime factors of factor!), so we can stop here.
-                break;
-            }
-            isPrime[multiple] = false;
-        }
-    }
-    return primesUpTo8000;
-}
-
-ModNum solveBruteForce(int N, int K, const vector<int>& a)
-{
-    ModNum result;
-
-    vector<bool> useElement(N, false);
-
-    while (true)
-    {
-        vector<int> subset;
-        for (int i = 0; i < N; i++)
-        {
-            if (useElement[i])
-                subset.push_back(a[i]);
-        }
-        sort(subset.begin(), subset.end());
-        const auto isGood = subset.size() <= K && (unique(subset.begin(), subset.end()) == subset.end());
-        if (isGood)
-        {
-            result = result + 1;
-        }
-
-        int index = 0;
-        while (index < N && useElement[index])
-        {
-            useElement[index] = false;
-            index++;
-        }
-        if (index == N)
-            break;
-
-        useElement[index] = true;
-    }
-    
-    return result;
-}
-
-ModNum solveOptimised(const int N, const int K, const vector<int>& aOriginal)
+ModNum findNumGoodSubsequences(const int N, const int K, const vector<int>& aOriginal)
 {
     auto a = aOriginal;
     sort(a.begin(), a.end());
@@ -180,6 +90,9 @@ ModNum solveOptimised(const int N, const int K, const vector<int>& aOriginal)
     }
     const int numDistinctElements = distinctElementInfo.size(); 
 
+    // numWaysToChooseFromFirstDistinct[i][j] is the number of ways of choosing j elements from the first i groups of 
+    // distinct elements.
+    // NB: this crops up throughout - we can't make more choices than there are distinct elements!
     vector<vector<ModNum>> numWaysToChooseFromFirstDistinct(numDistinctElements + 1, vector<ModNum>(numDistinctElements + 1, 0));
     for (int i = 0; i <= numDistinctElements; i++)
     {
@@ -202,59 +115,19 @@ ModNum solveOptimised(const int N, const int K, const vector<int>& aOriginal)
         }
     }
 
-    ModNum result;
+    ModNum numGoodSubsequences;
 
     for (int numChosen = 0; numChosen <= min(K, numDistinctElements); numChosen++)
     {
-        result += numWaysToChooseFromFirstDistinct[numDistinctElements][numChosen];
+        numGoodSubsequences += numWaysToChooseFromFirstDistinct[numDistinctElements][numChosen];
     }
     
-    return result;
+    return numGoodSubsequences;
 }
 
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-#if 0
-        cout << "primesUpTo8000: " << primesUpTo8000.size() << endl;
-        for (int i = 0;i < primesUpTo8000.size(); i++)
-        {
-            cout << " i: " << i << " primesUpTo8000: " << primesUpTo8000[i] << endl;
-        }
-#endif
-
-        const auto primesUpTo8000 = getPrimesUpTo8000();
-        int N = 100'000;
-        const int K = primesUpTo8000.size();
-        //const int maxPrimeIndex = (rand() % (primesUpTo8000.size() - 1) + 1);
-        //const int maxPrimeIndex = 9;
-
-
-        cout << N << " " << K << endl;
-
-        for (int i = 0; i < primesUpTo8000.size(); i++)
-        {
-            cout << primesUpTo8000[i] << " ";
-            N--;
-        }
-        while (N > 0)
-        {
-            cout << primesUpTo8000[rand() % primesUpTo8000.size()] << " ";
-            N--;
-        }
-        cout << endl;
-
-
-
-        return 0;
-    }
-
 
     const int N = read<int>();
     const int K = read<int>();
@@ -265,21 +138,7 @@ int main(int argc, char* argv[])
         aElement = read<int>();
     }
 
-#ifdef BRUTE_FORCE
-#if 1
-    const auto solutionBruteForce = solveBruteForce(N, K, a);
-    cout << "solutionBruteForce: " << solutionBruteForce << endl;
-#endif
-#if 1
-    const auto solutionOptimised = solveOptimised(N, K, a);
-    cout << "solutionOptimised:  " << solutionOptimised << endl;
-
-    assert(solutionOptimised == solutionBruteForce);
-#endif
-#else
-    const auto solutionOptimised = solveOptimised(N, K, a);
-    cout << solutionOptimised << endl;
-#endif
+    cout << findNumGoodSubsequences(N, K, a) << endl;
 
     assert(cin);
 }
