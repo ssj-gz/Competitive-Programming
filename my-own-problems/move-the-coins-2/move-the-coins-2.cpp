b4885a26450bdb52f9395b3eaa529a7215b02735
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-17 12:06:51 +0100

    In "Move the Coins 2", split the TreeGenerator class into declaration and method definitions.
    Looks a bit cleaner, now.

--- my-own-problems/move-the-coins-2/move-the-coins-2.cpp
+++ my-own-problems/move-the-coins-2/move-the-coins-2.cpp
@@ -631,215 +631,25 @@ struct TestEdge
 class TreeGenerator
 {
     public:
-        TestNode* createNode()
-        {
-            m_nodes.emplace_back(new TestNode);
-            auto newNode = m_nodes.back().get();
-            newNode->originalId = m_nodes.size();
-            return newNode;
-        }
-        TestNode* createNode(TestNode* attachedTo)
-        {
-            auto newNode = createNode();
-            addEdge(newNode, attachedTo);
-            return newNode;
-        }
-        TestNode* createNodeWithRandomParent()
-        {
-            const int parentIndex = rand() % m_nodes.size();
-            auto randomParent = m_nodes[parentIndex].get();
-            return createNode(randomParent);
-        }
-        vector<TestNode*> createNodesWithRandomParent(int numNewNodes)
-        {
-            vector<TestNode*> newNodes;
-            for (int i = 0; i < numNewNodes; i++)
-            {
-                newNodes.push_back(createNodeWithRandomParent());
-            }
+        TestNode* createNode();
+        TestNode* createNode(TestNode* attachedTo);
+        TestNode* createNodeWithRandomParent();
+        vector<TestNode*> createNodesWithRandomParent(int numNewNodes);
+        vector<TestNode*> createNodesWithRandomParentPreferringLeafNodes(int numNewNodes, double leafNodePreferencePercent);
+        vector<TestNode*> createNodesWithRandomParentPreferringFromSet(const set<TestNode*>& preferredSet, int numNewNodes, double preferencePercent, std::function<void(TestNode* newNode, TestNode* parent, const bool parentWasPreferred, bool& addNewNodeToSet, bool& removeParentFromSet)> onCreateNode);
 
-            return newNodes;
-        }
-        vector<TestNode*> createNodesWithRandomParentPreferringLeafNodes(int numNewNodes, double leafNodePreferencePercent)
-        {
-            set<TestNode*> leaves;
-            for (auto& node : m_nodes)
-            {
-                if (node->neighbours.size() <= 1)
-                    leaves.insert(node.get());
-            }
-
-            return createNodesWithRandomParentPreferringFromSet(leaves, numNewNodes, leafNodePreferencePercent, [](TestNode* newNode, TestNode* newNodeParent, const bool parentWasPreferred, bool& addNewNodeToSet, bool& removeParentFromSet)
-                    {
-                    addNewNodeToSet = true;
-                    if (newNodeParent->neighbours.size() > 1)
-                    removeParentFromSet = true;
-                    });
-        }
-
-        vector<TestNode*> createNodesWithRandomParentPreferringFromSet(const set<TestNode*>& preferredSet, int numNewNodes, double preferencePercent, std::function<void(TestNode* newNode, TestNode* parent, const bool parentWasPreferred, bool& addNewNodeToSet, bool& removeParentFromSet)> onCreateNode)
-        {
-            vector<TestNode*> newNodes;
-            set<TestNode*> preferredSetCopy(preferredSet);
-            set<TestNode*> nonPreferredSet;
-            for (auto& node : m_nodes)
-            {
-                if (preferredSetCopy.find(node.get()) == preferredSetCopy.end())
-                {
-                    nonPreferredSet.insert(node.get());
-                }
-            }
-
-            auto chooseRandomNodeFromSet = [](set<TestNode*>& nodeSet)
-            {
-                const int randomIndex = rand() % nodeSet.size();
-                //cerr << "nodeSet.size(): " << nodeSet.size() << " randomIndex: " << randomIndex << endl;
-                auto nodeIter = nodeSet.begin();
-                for (int i = 0; i < randomIndex; i++)
-                {
-                    nodeIter++;
-                }
-                return *nodeIter;
-            };
-
-            for (int i = 0; i < numNewNodes; )
-            {
-                cerr << "createNodesWithRandomParentPreferringFromSet: " << (i + 1) << " / " << numNewNodes << endl;
-                const double random = static_cast<double>(rand()) / RAND_MAX * 100;
-                TestNode* newNodeParent = nullptr;
-                bool parentWasPreferred = false;
-                //cerr << "random: " << random << " preferencePercent: " << preferencePercent << endl;
-                if (random <= preferencePercent && !preferredSetCopy.empty())
-                {
-                    // Choose a random element from preferredSetCopy as a parent.
-                    //cerr << " choosing preferred" << endl;
-                    newNodeParent = chooseRandomNodeFromSet(preferredSetCopy); 
-                    parentWasPreferred = true;
-                    cerr << "Chose leaf: " << newNodeParent->neighbours.size() << endl;
-                    assert(newNodeParent->neighbours.size() <= 1);
-                }
-                else if (!nonPreferredSet.empty())
-                {
-                    // Choose a random element from nonPreferredSet as a parent.
-                    cerr << " choosing non-leaf" << endl;
-                    newNodeParent = chooseRandomNodeFromSet(nonPreferredSet); 
-                    assert(newNodeParent->neighbours.size() > 1);
-                }
-                if (newNodeParent)
-                {
-                    auto newNode = createNode(newNodeParent);
-                    newNodes.push_back(newNode);
-                    bool addNewNodeToSet = false;
-                    bool removeParentFromSet = false;
-                    onCreateNode(newNode, newNodeParent, parentWasPreferred, addNewNodeToSet, removeParentFromSet);
-                    if (addNewNodeToSet)
-                        preferredSetCopy.insert(newNode);
-                    else
-                        nonPreferredSet.insert(newNode);
-                    if (removeParentFromSet)
-                    {
-                        preferredSetCopy.erase(newNodeParent);
-                        nonPreferredSet.insert(newNodeParent);
-                    }
-                    i++;
-                }
-            }
-
-            return newNodes;
-        }
-
-        int numNodes() const
-        {
-            return m_nodes.size();
-        };
-        TestNode* firstNode() const
-        {
-            assert(!m_nodes.empty());
-            return m_nodes.front().get();
-        }
-        TestEdge* addEdge(TestNode* nodeA, TestNode* nodeB)
-        {
-            m_edges.emplace_back(new TestEdge);
-            auto newEdge = m_edges.back().get();
-            newEdge->nodeA = nodeA;
-            newEdge->nodeB = nodeB;
-            nodeA->neighbours.push_back(newEdge);
-            nodeB->neighbours.push_back(newEdge);
-
-            return newEdge;
-        }
-        vector<TestNode*> addNodeChain(TestNode* chainStart, int numInChain)
-        {
-            vector<TestNode*> nodeChain;
-
-            auto lastNodeInChain = chainStart;
-            for (int i = 0; i < numInChain; i++)
-            {
-                lastNodeInChain = createNode(lastNodeInChain);
-                nodeChain.push_back(lastNodeInChain);
-            }
-
-            return nodeChain;
-        }
-        void scrambleNodeOrder()
-        {
-            random_shuffle(m_nodes.begin(), m_nodes.end());
-        }
-        void scrambleEdgeOrder()
-        {
-            random_shuffle(m_edges.begin(), m_edges.end());
-            for (auto& edge : m_edges)
-            {
-                if (rand() % 2 == 1)
-                    swap(edge->nodeA, edge->nodeB);
-            }
-        }
+        int numNodes() const;
+        TestNode* firstNode() const;
+        TestEdge* addEdge(TestNode* nodeA, TestNode* nodeB);
+        vector<TestNode*> addNodeChain(TestNode* chainStart, int numInChain);
+        void scrambleNodeOrder();
+        void scrambleEdgeOrder();
         // Scrambles the order of the nodes, then re-ids them, in new order, with the first
         // node in the new order having scrambledId 1, then next 2, etc.
-        void scrambleNodeIdsAndReorder(TestNode* forceAsRootNode)
-        {
-            scrambleNodeOrder();
-            for (int i = 0; i < m_nodes.size(); i++)
-            {
-                m_nodes[i]->scrambledId = (i + 1);
-            }
-            if (forceAsRootNode)
-            {
-                auto forcedRootNodeIter = find_if(m_nodes.begin(), m_nodes.end(), [forceAsRootNode](const auto& nodePtr) { return nodePtr.get() == forceAsRootNode; });
-                assert(forcedRootNodeIter != m_nodes.end());
-                if (forcedRootNodeIter != m_nodes.begin())
-                {
-                    swap((*m_nodes.begin())->scrambledId, forceAsRootNode->scrambledId);
-                    iter_swap(m_nodes.begin(), forcedRootNodeIter);
-                } 
-                assert(forceAsRootNode->scrambledId == 1);
-            }
-        }
-        vector<TestNode*> nodes() const
-        {
-            vector<TestNode*> nodes;
-            for (auto& node : m_nodes)
-            {
-                nodes.push_back(node.get());
-            }
-            return nodes;
-        }
-        vector<TestEdge*> edges() const
-        {
-            vector<TestEdge*> edges;
-            for (auto& edge : m_edges)
-            {
-                edges.push_back(edge.get());
-            }
-            return edges;
-        }
-        void printEdges() const
-        {
-            for (const auto& edge : m_edges)
-            {
-                cout << edge->nodeA->id() << " " << edge->nodeB->id() << endl;
-            }
-        }
+        void scrambleNodeIdsAndReorder(TestNode* forceAsRootNode);
+        vector<TestNode*> nodes() const;
+        vector<TestEdge*> edges() const;
+        void printEdges() const;
     private:
         vector<unique_ptr<TestNode>> m_nodes;
         vector<unique_ptr<TestEdge>> m_edges;
@@ -1416,4 +1226,215 @@ int main(int argc, char** argv)
     cout << endl;
     assert(result == resultBruteForce);
 #endif
+}
+
+
+TestNode* TreeGenerator::createNode()
+{
+    m_nodes.emplace_back(new TestNode);
+    auto newNode = m_nodes.back().get();
+    newNode->originalId = m_nodes.size();
+    return newNode;
+}
+TestNode* TreeGenerator::createNode(TestNode* attachedTo)
+{
+    auto newNode = createNode();
+    addEdge(newNode, attachedTo);
+    return newNode;
+}
+TestNode* TreeGenerator::createNodeWithRandomParent()
+{
+    const int parentIndex = rand() % m_nodes.size();
+    auto randomParent = m_nodes[parentIndex].get();
+    return createNode(randomParent);
+}
+vector<TestNode*> TreeGenerator::createNodesWithRandomParent(int numNewNodes)
+{
+    vector<TestNode*> newNodes;
+    for (int i = 0; i < numNewNodes; i++)
+    {
+        newNodes.push_back(createNodeWithRandomParent());
+    }
+
+    return newNodes;
+}
+vector<TestNode*> TreeGenerator::createNodesWithRandomParentPreferringLeafNodes(int numNewNodes, double leafNodePreferencePercent)
+{
+    set<TestNode*> leaves;
+    for (auto& node : m_nodes)
+    {
+        if (node->neighbours.size() <= 1)
+            leaves.insert(node.get());
+    }
+
+    return createNodesWithRandomParentPreferringFromSet(leaves, numNewNodes, leafNodePreferencePercent, [](TestNode* newNode, TestNode* newNodeParent, const bool parentWasPreferred, bool& addNewNodeToSet, bool& removeParentFromSet)
+            {
+            addNewNodeToSet = true;
+            if (newNodeParent->neighbours.size() > 1)
+            removeParentFromSet = true;
+            });
+}
+
+vector<TestNode*> TreeGenerator::createNodesWithRandomParentPreferringFromSet(const set<TestNode*>& preferredSet, int numNewNodes, double preferencePercent, std::function<void(TestNode* newNode, TestNode* parent, const bool parentWasPreferred, bool& addNewNodeToSet, bool& removeParentFromSet)> onCreateNode)
+{
+    vector<TestNode*> newNodes;
+    set<TestNode*> preferredSetCopy(preferredSet);
+    set<TestNode*> nonPreferredSet;
+    for (auto& node : m_nodes)
+    {
+        if (preferredSetCopy.find(node.get()) == preferredSetCopy.end())
+        {
+            nonPreferredSet.insert(node.get());
+        }
+    }
+
+    auto chooseRandomNodeFromSet = [](set<TestNode*>& nodeSet)
+    {
+        const int randomIndex = rand() % nodeSet.size();
+        //cerr << "nodeSet.size(): " << nodeSet.size() << " randomIndex: " << randomIndex << endl;
+        auto nodeIter = nodeSet.begin();
+        for (int i = 0; i < randomIndex; i++)
+        {
+            nodeIter++;
+        }
+        return *nodeIter;
+    };
+
+    for (int i = 0; i < numNewNodes; )
+    {
+        cerr << "createNodesWithRandomParentPreferringFromSet: " << (i + 1) << " / " << numNewNodes << endl;
+        const double random = static_cast<double>(rand()) / RAND_MAX * 100;
+        TestNode* newNodeParent = nullptr;
+        bool parentWasPreferred = false;
+        //cerr << "random: " << random << " preferencePercent: " << preferencePercent << endl;
+        if (random <= preferencePercent && !preferredSetCopy.empty())
+        {
+            // Choose a random element from preferredSetCopy as a parent.
+            //cerr << " choosing preferred" << endl;
+            newNodeParent = chooseRandomNodeFromSet(preferredSetCopy); 
+            parentWasPreferred = true;
+            cerr << "Chose leaf: " << newNodeParent->neighbours.size() << endl;
+            assert(newNodeParent->neighbours.size() <= 1);
+        }
+        else if (!nonPreferredSet.empty())
+        {
+            // Choose a random element from nonPreferredSet as a parent.
+            cerr << " choosing non-leaf" << endl;
+            newNodeParent = chooseRandomNodeFromSet(nonPreferredSet); 
+            assert(newNodeParent->neighbours.size() > 1);
+        }
+        if (newNodeParent)
+        {
+            auto newNode = createNode(newNodeParent);
+            newNodes.push_back(newNode);
+            bool addNewNodeToSet = false;
+            bool removeParentFromSet = false;
+            onCreateNode(newNode, newNodeParent, parentWasPreferred, addNewNodeToSet, removeParentFromSet);
+            if (addNewNodeToSet)
+                preferredSetCopy.insert(newNode);
+            else
+                nonPreferredSet.insert(newNode);
+            if (removeParentFromSet)
+            {
+                preferredSetCopy.erase(newNodeParent);
+                nonPreferredSet.insert(newNodeParent);
+            }
+            i++;
+        }
+    }
+
+    return newNodes;
+}
+
+int TreeGenerator::numNodes() const
+{
+    return m_nodes.size();
+};
+TestNode* TreeGenerator::firstNode() const
+{
+    assert(!m_nodes.empty());
+    return m_nodes.front().get();
+}
+TestEdge* TreeGenerator::addEdge(TestNode* nodeA, TestNode* nodeB)
+{
+    m_edges.emplace_back(new TestEdge);
+    auto newEdge = m_edges.back().get();
+    newEdge->nodeA = nodeA;
+    newEdge->nodeB = nodeB;
+    nodeA->neighbours.push_back(newEdge);
+    nodeB->neighbours.push_back(newEdge);
+
+    return newEdge;
+}
+vector<TestNode*> TreeGenerator::addNodeChain(TestNode* chainStart, int numInChain)
+{
+    vector<TestNode*> nodeChain;
+
+    auto lastNodeInChain = chainStart;
+    for (int i = 0; i < numInChain; i++)
+    {
+        lastNodeInChain = createNode(lastNodeInChain);
+        nodeChain.push_back(lastNodeInChain);
+    }
+
+    return nodeChain;
+}
+void TreeGenerator::scrambleNodeOrder()
+{
+    random_shuffle(m_nodes.begin(), m_nodes.end());
+}
+void TreeGenerator::scrambleEdgeOrder()
+{
+    random_shuffle(m_edges.begin(), m_edges.end());
+    for (auto& edge : m_edges)
+    {
+        if (rand() % 2 == 1)
+            swap(edge->nodeA, edge->nodeB);
+    }
+}
+// Scrambles the order of the nodes, then re-ids them, in new order, with the first
+// node in the new order having scrambledId 1, then next 2, etc.
+void TreeGenerator::scrambleNodeIdsAndReorder(TestNode* forceAsRootNode)
+{
+    scrambleNodeOrder();
+    for (int i = 0; i < m_nodes.size(); i++)
+    {
+        m_nodes[i]->scrambledId = (i + 1);
+    }
+    if (forceAsRootNode)
+    {
+        auto forcedRootNodeIter = find_if(m_nodes.begin(), m_nodes.end(), [forceAsRootNode](const auto& nodePtr) { return nodePtr.get() == forceAsRootNode; });
+        assert(forcedRootNodeIter != m_nodes.end());
+        if (forcedRootNodeIter != m_nodes.begin())
+        {
+            swap((*m_nodes.begin())->scrambledId, forceAsRootNode->scrambledId);
+            iter_swap(m_nodes.begin(), forcedRootNodeIter);
+        } 
+        assert(forceAsRootNode->scrambledId == 1);
+    }
+}
+vector<TestNode*> TreeGenerator::nodes() const
+{
+    vector<TestNode*> nodes;
+    for (auto& node : m_nodes)
+    {
+        nodes.push_back(node.get());
+    }
+    return nodes;
+}
+vector<TestEdge*> TreeGenerator::edges() const
+{
+    vector<TestEdge*> edges;
+    for (auto& edge : m_edges)
+    {
+        edges.push_back(edge.get());
+    }
+    return edges;
+}
+void TreeGenerator::printEdges() const
+{
+    for (const auto& edge : m_edges)
+    {
+        cout << edge->nodeA->id() << " " << edge->nodeB->id() << endl;
+    }
 }
