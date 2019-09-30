aae51ac3d917d4d29f78df6977d65a072ef78baa
Simon St James <kdedevel@etotheipiplusone.com>
2019-09-30 12:12:23 +0100

    Clumsily splice SuffixTree into Heavy Bits.

--- experiments/heavy-bits.cpp
+++ experiments/heavy-bits.cpp
@@ -11,12 +11,240 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <limits>
 
 #include <cassert>
 
 #include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
+
+
+/**
+ * Simple implementation of Ukkonen's algorithm:
+ *  https://en.wikipedia.org/wiki/Ukkonen's_algorithm
+ * for online construction of suffix trees.
+ * @author Simon St James, Jan 2017.
+ */
+constexpr auto alphabetSize = 2; // 0 and 1.
+class SuffixTree
+{
+    public:
+        SuffixTree()
+        {
+            m_states.reserve(1'000'000);
+
+            m_root = createNewState();
+            m_auxiliaryState = createNewState();
+
+            for (int i = 0; i < alphabetSize; i++)
+            {
+                m_auxiliaryState->transitions.push_back(Transition(m_root, Substring(-(i + 1), -(i + 1)), m_currentString));
+            }
+            m_root->suffixLink = m_auxiliaryState;
+
+            m_s = m_root;
+            m_k = 1;
+        }
+        SuffixTree(const SuffixTree& other) = delete;
+        void appendString(const string& stringToAppend)
+        {
+            for (auto letter : stringToAppend)
+            {
+                appendLetter(letter);
+            }
+        }
+        void finalise()
+        {
+            // Set the correct value for transitions whose ends are openTransitionEnd, and make
+            // startIndex/ endIndex 0-relative.
+            for (auto& state : m_states)
+            {
+                for (auto& transition : state.transitions)
+                {
+                    if (transition.substringFollowed.endIndex == openTransitionEnd)
+                        transition.substringFollowed.endIndex = m_currentString.size();
+
+                    transition.substringFollowed.startIndex--;
+                    transition.substringFollowed.endIndex--;
+                }
+            }
+
+        }
+
+        struct State;
+        State* rootState() const
+        {
+            return m_root;
+        }
+        struct Substring
+        {
+            Substring(int startIndex, int endIndex)
+                : startIndex(startIndex), endIndex(endIndex)
+            {
+            }
+
+            int startIndex = -1;
+            int endIndex = -1;
+        };
+        struct Transition
+        {
+            Transition(State *nextState, const Substring& substringFollowed, const string& currentString)
+                : nextState(nextState), substringFollowed(substringFollowed)
+            {
+            }
+
+            State *nextState = nullptr;
+            Substring substringFollowed;
+        };
+        struct State
+        {
+            vector<Transition> transitions;
+            State* suffixLink = nullptr;
+            State* parent = nullptr;
+        };
+    private:
+        static const int openTransitionEnd = numeric_limits<int>::max();
+
+        string m_currentString;
+
+        vector<State> m_states;
+        State *m_root = nullptr;
+        State *m_auxiliaryState = nullptr;
+
+        // "Persistent" versions of s & k from Algorithm 2 in Ukkonen's paper!
+        State *m_s;
+        int m_k;
+
+        void appendLetter(char letter)
+        {
+            m_currentString += letter;
+            const auto updateResult = update(m_s, m_k, m_currentString.size());
+            m_s = updateResult.first;
+            m_k = updateResult.second;
+            const auto canonizeResult = canonize(m_s, m_k, m_currentString.size());
+            m_s = canonizeResult.first;
+            m_k = canonizeResult.second;
+        }
+
+        std::pair<State*, int> update(State* s, int k, int i)
+        {
+            State* oldr = m_root;
+            const auto testAndSplitResult = testAndSplit(s, k, i - 1, t(i));
+            auto isEndPoint = testAndSplitResult.first;
+            auto r = testAndSplitResult.second;
+            while (!isEndPoint)
+            {
+                auto rPrime = createNewState(r);
+                r->transitions.push_back(Transition(rPrime, Substring(i, openTransitionEnd), m_currentString));
+                if (oldr != m_root)
+                {
+                    oldr->suffixLink = r;
+                }
+                oldr = r;
+                const auto canonizeResult = canonize(s->suffixLink, k, i - 1);
+                s = canonizeResult.first;
+                k = canonizeResult.second;
+
+                const auto testAndSplitResult = testAndSplit(s, k, i - 1, t(i));
+                isEndPoint = testAndSplitResult.first;
+                r = testAndSplitResult.second;
+            }
+
+            if (oldr != m_root)
+            {
+                oldr->suffixLink = s;
+            }
+            return {s, k};
+
+
+        }
+        pair<bool, State*> testAndSplit(State* s, int k, int p, int letterIndex)
+        {
+            assert(s);
+            if (k <= p)
+            {
+                const auto tkTransitionIter = findTransitionIter(s, t(k));
+                auto sPrime = tkTransitionIter->nextState;
+                auto kPrime = tkTransitionIter->substringFollowed.startIndex;
+                auto pPrime = tkTransitionIter->substringFollowed.endIndex;
+                if (letterIndex == t(kPrime + p - k + 1))
+                    return {true, s};
+                else
+                {
+                    s->transitions.erase(tkTransitionIter);
+                    auto r = createNewState(s);
+                    s->transitions.push_back(Transition(r, Substring(kPrime, kPrime + p - k), m_currentString));
+                    r->transitions.push_back(Transition(sPrime, Substring(kPrime + p - k + 1, pPrime), m_currentString));
+                    sPrime->parent = r;
+                    return {false, r};
+                }
+            }
+            else
+            {
+                auto tTransitionIter = findTransitionIter(s, letterIndex, false);
+                if (tTransitionIter == s->transitions.end())
+                    return {false, s};
+                else
+                    return {true, s};
+            }
+        }
+        std::pair<State*, int> canonize(State* s, int k, int p)
+        {
+            assert(s);
+            if (p < k)
+                return {s, k};
+            else
+            {
+                auto tkTransitionIter = findTransitionIter(s, t(k));
+                auto sPrime = tkTransitionIter->nextState;
+                auto kPrime = tkTransitionIter->substringFollowed.startIndex;
+                auto pPrime = tkTransitionIter->substringFollowed.endIndex;
+                while (pPrime - kPrime <= p - k)
+                {
+                    k = k + pPrime - kPrime + 1;
+                    s = sPrime;
+                    if (k <= p)
+                    {
+                        tkTransitionIter = findTransitionIter(s, t(k));
+                        sPrime = tkTransitionIter->nextState;
+                        kPrime = tkTransitionIter->substringFollowed.startIndex;
+                        pPrime = tkTransitionIter->substringFollowed.endIndex;
+                    }
+                }
+            }
+            return {s, k};
+        }
+
+        State *createNewState(State* parent = nullptr)
+        {
+            m_states.push_back(State());
+            State *newState = &(m_states.back());
+            newState->parent = parent;
+            return newState;
+        }
+
+        decltype(State::transitions.begin()) findTransitionIter(State* state, int letterIndex, bool assertFound = true)
+        {
+            for (auto transitionIter = state->transitions.begin(); transitionIter != state->transitions.end(); transitionIter++)
+            {
+                if (transitionIter->substringFollowed.startIndex >= 0 && t(transitionIter->substringFollowed.startIndex) == letterIndex)
+                    return transitionIter;
+
+                if (transitionIter->substringFollowed.startIndex == -letterIndex)
+                    return transitionIter;
+            }
+            if (assertFound)
+                assert(false);
+            return state->transitions.end();
+        };
+
+        int t(int i)
+        {
+            // Ukkonen's algorithm uses 1-indexed strings throughout and alphabet throughout; adjust for this.
+            return m_currentString[i - 1] - 'a' + 1;
+        }
+};
 
 template <typename T>
 T read()
