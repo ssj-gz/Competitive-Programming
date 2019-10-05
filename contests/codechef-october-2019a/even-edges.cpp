3fbbd6699eaf93ed5f53c808183d4961b310ec29
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-05 14:47:41 +0100

    Add empty skeleton for "Even Edges" (EVEDG).

---
+++ contests/codechef-october-2019a/even-edges.cpp
@@ -0,0 +1,92 @@
+// Simon St James (ssjgz) - 2019-10-05
+// 
+// Solution to: https://www.codechef.com/OCT19A/problems/EVEDG
+//
+//#define SUBMISSION
+#define BRUTE_FORCE
+#ifdef SUBMISSION
+#undef BRUTE_FORCE
+#define NDEBUG
+#endif
+#include <iostream>
+#include <vector>
+
+#include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
+using namespace std;
+
+template <typename T>
+T read()
+{
+    T toRead;
+    cin >> toRead;
+    assert(cin);
+    return toRead;
+}
+
+#if 0
+SolutionType solveBruteForce()
+{
+    SolutionType result;
+    
+    return result;
+}
+#endif
+
+#if 0
+SolutionType solveOptimised()
+{
+    SolutionType result;
+    
+    return result;
+}
+#endif
+
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+        //const int T = rand() % 100 + 1;
+        const int T = 1;
+        cout << T << endl;
+
+        for (int t = 0; t < T; t++)
+        {
+        }
+
+        return 0;
+    }
+    
+    // TODO - read in testcase.
+    const auto T = read<int>();
+
+    for (int t = 0; t < T; t++)
+    {
+
+#ifdef BRUTE_FORCE
+#if 0
+        const auto solutionBruteForce = solveBruteForce();
+        cout << "solutionBruteForce: " << solutionBruteForce << endl;
+#endif
+#if 0
+        const auto solutionOptimised = solveOptimised();
+        cout << "solutionOptimised:  " << solutionOptimised << endl;
+
+        assert(solutionOptimised == solutionBruteForce);
+#endif
+#else
+        const auto solutionOptimised = solveOptimised();
+        cout << solutionOptimised << endl;
+#endif
+    }
+
+    assert(cin);
+}
