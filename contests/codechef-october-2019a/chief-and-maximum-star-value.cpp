b688f788fd2be5e8a3f13531aeb7463c657cbc78
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-06 14:38:11 +0100

    Initial attempt at optimised version of MSV.

--- contests/codechef-october-2019a/chief-and-maximum-star-value.cpp
+++ contests/codechef-october-2019a/chief-and-maximum-star-value.cpp
@@ -17,6 +17,8 @@
 
 using namespace std;
 
+constexpr auto maxA = 1'000'000;
+
 template <typename T>
 T read()
 {
@@ -45,14 +47,86 @@ int solveBruteForce(const vector<int>& a)
     return maxStarValue;
 }
 
-#if 0
-SolutionType solveOptimised()
+vector<int> computeFactors(int number, const vector<int>& primesUpToMaxA)
 {
-    SolutionType result;
-    
-    return result;
+    vector<std::pair<int64_t, int64_t>> primeFactorisation;
+    for (const auto prime : primesUpToMaxA)
+    {
+        const auto oldNumber = number;
+        int numOfPrime = 0;
+        while (number % prime == 0)
+        {
+            numOfPrime++;
+            number = number / prime;
+        }
+        if (numOfPrime > 0)
+        {
+            primeFactorisation.push_back({prime, numOfPrime});
+        }
+        if (prime * prime > oldNumber)
+            break;
+    }
+    if (number > 1)
+    {
+        // If we ever exit the loop above with number != 1, number must
+        // be prime.
+        primeFactorisation.push_back({number, 1});
+    }
+
+    // Combine the powers of primes in prime factorisation to produce all factors,
+    // each once and only once.
+    vector<int> factors;
+    vector<int> powerOfPrime(primeFactorisation.size(), 0);
+    while (true)
+    {
+        int factor = 1;
+        for (int i = 0; i < powerOfPrime.size(); i++)
+        {
+            int primeToPower = 1;
+            for (int exponent = 0; exponent < powerOfPrime[i]; exponent++)
+            {
+                primeToPower = primeToPower * primeFactorisation[i].first;
+            }
+            factor *= primeToPower;
+        }
+        factors.push_back(factor);
+
+        int index = 0;
+        while (index < powerOfPrime.size() && powerOfPrime[index] == primeFactorisation[index].second)
+        {
+            powerOfPrime[index] = 0;
+            index++;
+        }
+
+        if (index == powerOfPrime.size())
+            break;
+
+        powerOfPrime[index]++;
+    }
+
+    return factors;
 }
-#endif
+
+
+int solveOptimised(const vector<int>& a, const vector<int>& primesUpToMaxA)
+{
+    vector<int> numWithFactor(maxA + 1, 0);
+
+    int maxStarValue = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        const auto starValue = numWithFactor[a[i]];
+        maxStarValue = max(maxStarValue, starValue);
+
+        const auto factors = computeFactors(a[i], primesUpToMaxA);
+        for (const auto factor : factors)
+        {
+            numWithFactor[factor]++;
+        }
+    }
+
+    return maxStarValue;
+}
 
 
 int main(int argc, char* argv[])
@@ -81,8 +155,47 @@ int main(int argc, char* argv[])
 
         return 0;
     }
-    
-    // TODO - read in testcase.
+
+    const int maxA = 1'000'000;
+
+    // Sieve of Eratosthenes.  To help with factorising numbers, compute a lookup table for
+    // primes up to maxA.
+    vector<int> primesUpToMaxA;
+    vector<bool> isPrime(maxA + 1, true);
+    for (int64_t factor = 2; factor <= maxA; factor++)
+    {
+        const bool isFactorPrime = isPrime[factor];
+        assert(factor < isPrime.size());
+        if (isFactorPrime)
+        {
+            primesUpToMaxA.push_back(factor);
+        }
+        for (int64_t multiple = factor * factor; multiple < isPrime.size(); multiple += factor)
+        {
+            if (!isPrime[multiple] && !isFactorPrime)
+            {
+                // This multiple has already been marked, and since factor is not prime,
+                // all subsequent multiples will already have been marked (by any of the
+                // prime factors of factor!), so we can stop here.
+                break;
+            }
+            isPrime[multiple] = false;
+        }
+    }
+
+
+#if 0
+
+    vector<vector<int>> factorsOf(maxA + 1);
+    for (int factor = 1; factor <= maxA; factor++)
+    {
+        for (int multiple = factor; multiple <= maxA; multiple += factor)
+        {
+            factorsOf[multiple].push_back(factor);
+        }
+    }
+#endif
+
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
@@ -93,16 +206,12 @@ int main(int argc, char* argv[])
             x = read<int>();
 
 #ifdef BRUTE_FORCE
-#if 1
         const auto solutionBruteForce = solveBruteForce(a);
         cout << "solutionBruteForce: " << solutionBruteForce << endl;
-#endif
-#if 0
-        const auto solutionOptimised = solveOptimised();
+        const auto solutionOptimised = solveOptimised(a, primesUpToMaxA);
         cout << "solutionOptimised:  " << solutionOptimised << endl;
 
         assert(solutionOptimised == solutionBruteForce);
-#endif
 #else
         const auto solutionOptimised = solveOptimised();
         cout << solutionOptimised << endl;
