cc6cb3fae45cc60803749ade3b03ef6a9fe5b194
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-14 10:14:54 +0100

    Clean up for DIV1.

--- codechef/chef-and-divisors.cpp
+++ codechef/chef-and-divisors.cpp
@@ -2,27 +2,19 @@
 // 
 // Solution to: https://www.codechef.com/ACD2019/problems/DIV1
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 
 #include <cassert>
 
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
-
 using namespace std;
 
-constexpr int maxA = 1'000'000;
+constexpr int maxFactor = 1'000'000;
 
-vector<int64_t> computeFactors(int64_t number, const vector<int>& primesUpToMaxA)
+vector<int64_t> computeFactors(int64_t number, const vector<int>& primesUpToMaxFactor)
 {
     vector<std::pair<int64_t, int64_t>> primeFactorisation;
-    for (const auto prime : primesUpToMaxA)
+    for (const auto prime : primesUpToMaxFactor)
     {
         const auto oldNumber = number;
         int numOfPrime = 0;
@@ -88,63 +80,34 @@ T read()
     return toRead;
 }
 
-#if 1
-int64_t solveBruteForce(int64_t N, const vector<int>& primesUpToMaxA)
+int64_t findXorOfFactors(int64_t N, const vector<int>& primesUpToMaxFactor)
 {
-    int64_t result = 0;
+    const auto factors = computeFactors(N, primesUpToMaxFactor);
 
-    const auto factors = computeFactors(N, primesUpToMaxA);
-
+    int64_t xorOfFactors = 0;
     for (const auto factor : factors)
     {
-        result = result ^ factor;
+        xorOfFactors = xorOfFactors ^ factor;
     }
     
-    return result;
+    return xorOfFactors;
 }
-#endif
-
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
 
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-        }
-
-        return 0;
-    }
 
     // Sieve of Eratosthenes.  To help with factorising numbers, compute a lookup table for
-    // primes up to maxA.
-    vector<int> primesUpToMaxA;
-    vector<bool> isPrime(maxA + 1, true);
-    for (int64_t factor = 2; factor <= maxA; factor++)
+    // primes up to maxFactor.
+    vector<int> primesUpToMaxFactor;
+    vector<bool> isPrime(maxFactor + 1, true);
+    for (int64_t factor = 2; factor <= maxFactor; factor++)
     {
         const bool isFactorPrime = isPrime[factor];
         assert(factor < isPrime.size());
         if (isFactorPrime)
         {
-            primesUpToMaxA.push_back(factor);
+            primesUpToMaxFactor.push_back(factor);
         }
         for (int64_t multiple = factor * factor; multiple < isPrime.size(); multiple += factor)
         {
@@ -159,30 +122,13 @@ int main(int argc, char* argv[])
         }
     }
 
-
-    
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
     {
         const int64_t N = read<int64_t>();
 
-#ifdef BRUTE_FORCE
-#if 1
-        const auto solutionBruteForce = solveBruteForce(N, primesUpToMaxA);
-        //cout << "solutionBruteForce: " << solutionBruteForce << endl;
-        cout << solutionBruteForce << endl;
-#endif
-#if 0
-        const auto solutionOptimised = solveOptimised();
-        cout << "solutionOptimised:  " << solutionOptimised << endl;
-
-        assert(solutionOptimised == solutionBruteForce);
-#endif
-#else
-        const auto solutionOptimised = solveOptimised();
-        cout << solutionOptimised << endl;
-#endif
+        cout << findXorOfFactors(N, primesUpToMaxFactor) << endl;
     }
 
     assert(cin);
