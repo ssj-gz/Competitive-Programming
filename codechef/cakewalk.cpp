f4808c531f0ab095b18eba4cac69faa26a6affec
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-23 15:27:56 +0100

    Quickly knock up solution for "Cakewalk": https://www.codechef.com/problems/CKWLK
    Successful submission of CKWLK!

---
+++ codechef/cakewalk.cpp
@@ -0,0 +1,113 @@
+// Simon St James (ssjgz) - 2019-10-23
+// 
+// Solution to: https://www.codechef.com/problems/CKWLK
+//
+#define SUBMISSION
+#define BRUTE_FORCE
+#ifdef SUBMISSION
+#undef BRUTE_FORCE
+#define NDEBUG
+#endif
+#include <iostream>
+#include <vector>
+
+#include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
+using namespace std;
+
+template <typename T>
+T read()
+{
+    T toRead;
+    cin >> toRead;
+    assert(cin);
+    return toRead;
+}
+
+#if 0
+SolutionType solveBruteForce()
+{
+    SolutionType result;
+    
+    return result;
+}
+#endif
+
+#if 1
+bool solveOptimised(int64_t N)
+{
+    int numPowersOf5 = 0;
+    while ((N % 5) == 0)
+    {
+        numPowersOf5++;
+        N /= 5;
+    }
+    int numPowersOf2 = 0;
+    while ((N % 2) == 0)
+    {
+        numPowersOf2++;
+        N /= 2;
+    }
+    if (N != 1)
+        return false;
+    const int numCheatCodes = numPowersOf5;
+    for (int numFirstCheatCode = 0; numFirstCheatCode <= numCheatCodes; numFirstCheatCode++)
+    {
+        const int numSecondCheatCode = numCheatCodes - numFirstCheatCode;
+        const int numPowersOf2FromCheatCodes = numFirstCheatCode + 2 * numSecondCheatCode;
+        if (numPowersOf2FromCheatCodes == numPowersOf2)
+            return true;
+    }
+    return false;
+}
+#endif
+
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+        //const int T = rand() % 100 + 1;
+        const int T = 1;
+        cout << T << endl;
+
+        for (int t = 0; t < T; t++)
+        {
+        }
+
+        return 0;
+    }
+    
+    const auto T = read<int>();
+
+    for (int t = 0; t < T; t++)
+    {
+        const int64_t N = read<int64_t>();
+
+
+#ifdef BRUTE_FORCE
+#if 0
+        const auto solutionBruteForce = solveBruteForce();
+        cout << "solutionBruteForce: " << solutionBruteForce << endl;
+#endif
+#if 0
+        const auto solutionOptimised = solveOptimised();
+        cout << "solutionOptimised:  " << solutionOptimised << endl;
+
+        assert(solutionOptimised == solutionBruteForce);
+#endif
+#else
+        const auto solutionOptimised = solveOptimised(N);
+        cout << (solutionOptimised ? "Yes" : "No") << endl;
+#endif
+    }
+
+    assert(cin);
+}
