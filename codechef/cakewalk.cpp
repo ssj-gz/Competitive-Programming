980d9e70a69492205b1f401eb705dbd48661a9b8
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-23 15:39:18 +0100

    Add a bit of documentation for CKWLK.

--- codechef/cakewalk.cpp
+++ codechef/cakewalk.cpp
@@ -32,8 +32,13 @@ bool calcCanMakeAmountFromCheatCodes(int64_t N)
         N /= 2;
     }
     if (N != 1)
+    {
+        // The prime factorisation of N does not consist solely of powers of 2 and powers
+        // of 5, which would be the case if it could be formed by using the cheat codes.
         return false;
+    }
     const int numCheatCodes = numPowersOf5;
+    // Trial and error to split the numCheatCodes cheat codes into numFirstCheatCode & numSecondCheatCode.
     for (int numFirstCheatCode = 0; numFirstCheatCode <= numCheatCodes; numFirstCheatCode++)
     {
         const int numSecondCheatCode = numCheatCodes - numFirstCheatCode;
@@ -46,6 +51,29 @@ bool calcCanMakeAmountFromCheatCodes(int64_t N)
 
 int main(int argc, char* argv[])
 {
+    // Easy one.  Note that applying the First cheat code multiplies the amount by
+    //
+    //    2^1 x 5^1
+    //
+    // and the Second multiplies it by:
+    //
+    //    2^2 x 5^1
+    //
+    // We observe two things:
+    //
+    //    i) if N is formed from the cheat codes, then the number of cheat codes applied is
+    //       the power of 5 in the prime factorisation of N; and
+    //   ii) if N is formed from the cheat codes, then the prime factorisation of N
+    //       consists only of a power of 2 and a power of 5.
+    //
+    // We can easily find numPowersOf5 (== numCheatCodes) by counting how many times we can
+    // divide N by 5.  We then just need to split, by trial and error, the number of cheat
+    // codes into X First and (numCheatCodes - X) second and see if we form the requisite
+    // power of 2 in N.
+    //
+    // Hopefully the code is reasonably clear :)
+
+
     ios::sync_with_stdio(false);
     
     const auto T = read<int>();
