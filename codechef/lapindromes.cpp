f56ea77aac4a542314fd3769f05dfac74a1080a4
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-29 06:31:12 +0000

    Cleanup LAPIN.

--- codechef/lapindromes.cpp
+++ codechef/lapindromes.cpp
@@ -7,8 +7,6 @@
 
 #include <cassert>
 
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
-
 using namespace std;
 
 template <typename T>
@@ -41,49 +39,14 @@ bool isLapindrome(const string& s)
     return letterHistogram(leftHalf) == letterHistogram(rightHalf);
 }
 
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
-
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int len = 1 + rand() % 12;
-            const int maxChar = 1 + rand() % numLetters;
-
-            string s;
-            for (int i = 0; i < len; i++)
-            {
-                s.push_back('a' + rand() % maxChar);
-            }
-            cout << s << endl;
-        }
-
-        return 0;
-    }
     
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
     {
-
         const auto s = read<string>();
         cout << (isLapindrome(s) ? "YES" : "NO") << endl;
     }
