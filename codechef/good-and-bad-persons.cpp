41c35b3762f098377955616b57d560380383fe34
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-31 06:37:05 +0000

    Cleanup GOODBAD.

--- codechef/good-and-bad-persons.cpp
+++ codechef/good-and-bad-persons.cpp
@@ -2,19 +2,10 @@
 // 
 // Solution to: https://www.codechef.com/problems/GOODBAD
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
-#include <algorithm> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -27,8 +18,7 @@ T read()
     return toRead;
 }
 
-
-string solveBruteForce(int N, const string& s, int K)
+string findPossibleSenders(int N, const string& s, int K)
 {
     int numLowerCase = 0;
     int numUpperCase = 0;
@@ -54,53 +44,10 @@ string solveBruteForce(int N, const string& s, int K)
     return "none";
 }
 
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
-
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int N = 1 + rand() % 100;
-            const int K = rand() % (N + 1);
-            cout << N << " " << K << endl;
-            string s;
-            const int numLowercase = rand() % (N + 1);
-            vector<bool> useLowercase(N);
-            for (int i = 0; i < numLowercase; i++)
-                useLowercase[i] = true;
-
-            random_shuffle(useLowercase.begin(), useLowercase.end());
-
-            for (int i = 0; i < N; i++)
-            {
-                s += (useLowercase[i] ? 'a' : 'A') + rand() % 26;
-            }
-            cout << s << endl;
-        }
-
-        return 0;
-    }
     
-    // TODO - read in testcase.
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
@@ -109,21 +56,7 @@ int main(int argc, char* argv[])
         const int K = read<int>();
         const string s = read<string>();
 
-#ifdef BRUTE_FORCE
-#if 1
-        const auto solutionBruteForce = solveBruteForce(N, s, K);
-        cout << solutionBruteForce << endl;
-#endif
-#if 0
-        const auto solutionOptimised = solveOptimised();
-        cout << "solutionOptimised:  " << solutionOptimised << endl;
-
-        assert(solutionOptimised == solutionBruteForce);
-#endif
-#else
-        const auto solutionOptimised = solveOptimised();
-        cout << solutionOptimised << endl;
-#endif
+        cout << findPossibleSenders(N, s, K) << endl;
     }
 
     assert(cin);
