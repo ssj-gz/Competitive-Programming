962bf274a835f05ee26b895ad1180dbee741dae6
Simon St James <kdedevel@etotheipiplusone.com>
2019-10-31 10:13:38 +0000

    Take a snapshot of more sophisticated testcase generator for LAPIN.
    Will revert next commit - just wanted it stored in my history :)

--- codechef/lapindromes.cpp
+++ codechef/lapindromes.cpp
@@ -4,8 +4,11 @@
 //
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 #include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -39,14 +42,68 @@ bool isLapindrome(const string& s)
     return letterHistogram(leftHalf) == letterHistogram(rightHalf);
 }
 
+#if 0
+SolutionType solveOptimised()
+{
+    SolutionType result;
+    
+    return result;
+}
+#endif
+
+
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        //const int T = rand() % 100 + 1;
+        const int T = 1;
+        cout << T << endl;
+
+        for (int t = 0; t < T; t++)
+        {
+            const int len = 1 + rand() % 100;
+            const int maxChar = 1 + rand() % numLetters;
+
+            const bool generateYES = (rand() % 2 == 0);
+
+            string s;
+            if (generateYES)
+            {
+                string halfString;
+                for (int i = 0; i < len / 2; i++)
+                {
+                    halfString.push_back('a' + rand() % maxChar);
+                }
+                s += halfString;
+                if (len % 2 == 1)
+                    s += 'a' + rand() % maxChar;
+                random_shuffle(halfString.begin(), halfString.end());
+                s += halfString;
+            }
+            else
+            {
+
+                for (int i = 0; i < len; i++)
+                {
+                    s.push_back('a' + rand() % maxChar);
+                }
+            }
+            cout << s << endl;
+        }
+
+        return 0;
+    }
     
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
     {
+
         const auto s = read<string>();
         cout << (isLapindrome(s) ? "YES" : "NO") << endl;
     }
