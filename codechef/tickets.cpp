32bc8c945b4bfe31ebb9c7d1bb211de2cd8249ce
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-01 08:44:47 +0000

    Someone asked for help with "Tickets", so doing that :)
    https://www.codechef.com/problems/TICKETS5
    
    Successful submission of "TICKETS5"!

---
+++ codechef/tickets.cpp
@@ -0,0 +1,72 @@
+// Simon St James (ssjgz) - 2019-11-01
+// 
+// Solution to: https://www.codechef.com/problems/TICKETS5
+//
+//#define SUBMISSION
+#define BRUTE_FORCE
+#ifdef SUBMISSION
+#undef BRUTE_FORCE
+#define NDEBUG
+#endif
+#include <iostream>
+#include <vector>
+
+#include <cassert>
+
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
+using namespace std;
+
+template <typename T>
+T read()
+{
+    T toRead;
+    cin >> toRead;
+    assert(cin);
+    return toRead;
+}
+
+bool solveBruteForce(const string& ticket)
+{
+    if (ticket[0] == ticket[1])
+        return false;
+    for (int i = 2; i < ticket.size(); i++)
+    {
+        if (ticket[i] != ticket[i - 2])
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+        //const int T = rand() % 100 + 1;
+        const int T = 1;
+        cout << T << endl;
+
+        for (int t = 0; t < T; t++)
+        {
+        }
+
+        return 0;
+    }
+    
+    const auto T = read<int>();
+
+    for (int t = 0; t < T; t++)
+    {
+        const auto ticket = read<string>();
+
+        const auto solutionBruteForce = solveBruteForce(ticket);
+        cout << (solutionBruteForce ? "YES" : "NO") << endl;
+    }
+
+    assert(cin);
+}
