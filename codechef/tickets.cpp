a43ec2b9d2cf3d158f3de0657d48eeba47b39d17
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-01 08:58:08 +0000

    Revert back to clean TICKET5 now that we have a proper testcase generator in git history.

--- codechef/tickets.cpp
+++ codechef/tickets.cpp
@@ -2,18 +2,9 @@
 // 
 // Solution to: https://www.codechef.com/problems/TICKETS5
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
-#include <vector>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -26,7 +17,7 @@ T read()
     return toRead;
 }
 
-bool solveBruteForce(const string& ticket)
+bool isLuckyTicket(const string& ticket)
 {
     if (ticket[0] == ticket[1])
         return false;
@@ -41,30 +32,6 @@ bool solveBruteForce(const string& ticket)
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int N = 2 + rand() % 10;
-            const int maxChar = 1 + rand() % 26;
-
-            for (int i = 0; i < N; i++)
-            {
-                cout << static_cast<char>('a' + (rand() % maxChar));
-            }
-            cout << endl;
-        }
-
-        return 0;
-    }
     
     const auto T = read<int>();
 
@@ -72,8 +39,7 @@ int main(int argc, char* argv[])
     {
         const auto ticket = read<string>();
 
-        const auto solutionBruteForce = solveBruteForce(ticket);
-        cout << (solutionBruteForce ? "YES" : "NO") << endl;
+        cout << (isLuckyTicket(ticket) ? "YES" : "NO") << endl;
     }
 
     assert(cin);
