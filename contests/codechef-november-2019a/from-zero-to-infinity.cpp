2c036869a6cd5eea018022a189802b96c7e74a88
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-04 11:36:52 +0000

    Brute force implementation of WEIRDO.

--- contests/codechef-november-2019a/from-zero-to-infinity.cpp
+++ contests/codechef-november-2019a/from-zero-to-infinity.cpp
@@ -29,12 +29,114 @@ T read()
     return toRead;
 }
 
-#if 0
-SolutionType solveBruteForce()
+bool isVowel(char letter)
 {
-    SolutionType result;
+    switch (letter)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        return true;
+    }
+    return false;
+}
+
+bool isAlicesRecipeBruteForce(const string& recipe)
+{
+    for (int startPos = 0; startPos < recipe.size(); startPos++)
+    {
+        for (int length = 2; startPos + length <= recipe.size(); length++)
+        {
+            const string substring = recipe.substr(startPos, length);
+            int numVowels = 0;
+            int numConsonants = 0;
+            for (const auto letter : substring)
+            {
+                if (isVowel(letter))
+                    numVowels++;
+                else 
+                    numConsonants++;
+            }
+            if (numVowels < numConsonants)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+long double calcScoreBruteForce(const vector<string>& recipes)
+{
+    cout << "calcScoreBruteForce: " << endl;
+    long double score = 1;
+    for (char letter = 'a'; letter <= 'z'; letter++)
+    {
+        int numRecipesWithLetter = 0;
+        int numOccurencesOfLetter = 0;
+        for (const auto& recipe : recipes)
+        {
+            const int numOfLetterInRecipe = std::count(recipe.begin(), recipe.end(), letter);
+            if (numOfLetterInRecipe > 0)
+                numRecipesWithLetter++;
+
+            numOccurencesOfLetter += numOfLetterInRecipe;
+        }
+
+        //cout << "Letter: " << letter << " numOccurencesOfLetter: " << numOccurencesOfLetter << " numRecipesWithLetter: " << numRecipesWithLetter << endl;
+
+
+        if (numOccurencesOfLetter > 0)
+        {
+            assert(numRecipesWithLetter > 0);
+            score *= numRecipesWithLetter;
+            for (int i = 0; i < recipes.size(); i++)
+            {
+                score /= numOccurencesOfLetter;
+            }
+        }
+    }
+
+    cout << "score: " << score << endl;
+
+    return score;
+}
+
+#if 1
+long double solveBruteForce(const int numRecipes, const vector<string>& recipes)
+{
+    long double result = 0;
+
+    vector<string> alicesRecipes;
+    vector<string> bobsRecipes;
+
+    for (const auto& recipe : recipes)
+    {
+        if (isAlicesRecipeBruteForce(recipe))
+            alicesRecipes.push_back(recipe);
+        else
+            bobsRecipes.push_back(recipe);
+    }
+
+    cout << "Alice's recipes: " << endl;
+    for (const auto recipe : alicesRecipes)
+    {
+        cout << " " << recipe << endl;
+    }
+    cout << endl;
+    cout << "Bob's recipes: " << endl;
+    for (const auto recipe : bobsRecipes)
+    {
+        cout << " " << recipe << endl;
+    }
+    cout << endl;
+
+    const auto aliceScore = calcScoreBruteForce(alicesRecipes);
+    const auto bobScore = calcScoreBruteForce(bobsRecipes);
+
     
-    return result;
+    return aliceScore/ bobScore;
 }
 #endif
 
@@ -78,6 +180,7 @@ int main(int argc, char* argv[])
         return result;
     };
 
+#if 0
     {
         cout <<  std::setprecision (std::numeric_limits<long double>::digits10 + 1);
         cout << "log 5: " << logl(5) << endl;
@@ -87,17 +190,26 @@ int main(int argc, char* argv[])
 
         return 0;
     }
+#endif
 
     
-    // TODO - read in testcase.
     const auto T = read<int>();
+
+    cout <<  std::setprecision (std::numeric_limits<long double>::digits10 + 1);
 
     for (int t = 0; t < T; t++)
     {
+        const int numRecipes = read<int>();
+        vector<string> recipes(numRecipes);
+
+        for (auto& recipe : recipes)
+            recipe = read<string>();
+
+
 
 #ifdef BRUTE_FORCE
-#if 0
-        const auto solutionBruteForce = solveBruteForce();
+#if 1
+        const auto solutionBruteForce = solveBruteForce(numRecipes, recipes);
         cout << "solutionBruteForce: " << solutionBruteForce << endl;
 #endif
 #if 0
