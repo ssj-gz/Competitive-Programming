03ee4fd987796d4da3f1181f6e06f93a267df5a3
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-06 17:56:19 +0000

    Some documentation for PHCUL.

--- contests/codechef-november-2019a/physical-exercise.cpp
+++ contests/codechef-november-2019a/physical-exercise.cpp
@@ -27,8 +27,13 @@ struct Coord
     int64_t y = -1;
 };
 
-long double findMinDistance(const int64_t x, const int64_t y, const vector<Coord>& phase2Coords, const vector<Coord>& phase3Coords, const vector<Coord>& phase4Coords)
+long double findMinDistance(const int64_t x, const int64_t y, const vector<Coord>& phase1Coords, const vector<Coord>& phase2Coords, const vector<Coord>& phase3Coords)
 {
+
+    // [The beginning!] (x,y) -> phase1Coords -> phase2Coords -> phase3Coords [the end!]
+    //                    └─────┬─────┘└───────┬──────┘└───────┬──────┘
+    //                       Phase 1        Phase 2           Phase 3
+
     // Simple bit of dynamic programming; work backwards from the end to the beginning point (x, y).
     const int numPhases = 3;
     struct CoordAndDistToEnd
@@ -39,12 +44,12 @@ long double findMinDistance(const int64_t x, const int64_t y, const vector<Coord
 
     vector<vector<CoordAndDistToEnd>> distToEndForPhaseInfo(numPhases + 1);
     distToEndForPhaseInfo[0].push_back({{x, y}, -1});
+    for (int i = 0; i < phase1Coords.size(); i++)
+        distToEndForPhaseInfo[1].push_back({phase1Coords[i], -1});
     for (int i = 0; i < phase2Coords.size(); i++)
-        distToEndForPhaseInfo[1].push_back({phase2Coords[i], -1});
+        distToEndForPhaseInfo[2].push_back({phase2Coords[i], -1});
     for (int i = 0; i < phase3Coords.size(); i++)
-        distToEndForPhaseInfo[2].push_back({phase3Coords[i], -1});
-    for (int i = 0; i < phase4Coords.size(); i++)
-        distToEndForPhaseInfo[3].push_back({phase4Coords[i], 0}); // Already at the end, so "0".
+        distToEndForPhaseInfo[3].push_back({phase3Coords[i], 0}); // Already at the end, so "0".
 
     for (int phase = numPhases; phase >=1 ; phase--)
     {
@@ -103,9 +108,9 @@ int main(int argc, char* argv[])
         const vector<Coord> ef = readCoordVector(K);
         
         cout << min(
-                    // Visit the ab before the cd one.
+                    // Visit the ab coord before the cd one.
                     findMinDistance(x, y, ab, cd, ef), 
-                    // Visit the cd before the ab one.
+                    // Visit the cd coord before the ab one.
                     findMinDistance(x, y, cd, ab, ef)
                    ) << endl;
 
