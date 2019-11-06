50ee6fcee5fa04c46c6c77b3953d9ff0352eab92
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-06 16:24:57 +0000

    Clean up PHCUL a little.

--- contests/codechef-november-2019a/physical-exercise.cpp
+++ contests/codechef-november-2019a/physical-exercise.cpp
@@ -2,12 +2,6 @@
 // 
 // Solution to: https://www.codechef.com/NOV19A/problems/PHCUL
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -15,8 +9,6 @@
 #include <limits>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -80,25 +72,8 @@ long double findMinDistance(const int64_t x, const int64_t y, const vector<Coord
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-        }
-
-        return 0;
-    }
 
     cout << std::setprecision (std::numeric_limits<long double>::digits10 + 1);
-
     
     const auto T = read<int>();
 
