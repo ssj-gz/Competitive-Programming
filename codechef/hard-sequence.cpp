b6e45ad2d278cfe2720c9d823173a5bd7c8df9e8
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-11 14:29:02 +0000

    Tidy up HRDSEQ.

--- codechef/hard-sequence.cpp
+++ codechef/hard-sequence.cpp
@@ -2,19 +2,11 @@
 // 
 // Solution to: https://www.codechef.com/problems/HRDSEQ
 //
-#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -29,25 +21,12 @@ T read()
 
 int solveBruteForce(int N)
 {
-    int result = 0;
-
     vector<int> elements;
     elements.push_back(0);
 
     for (int i = 0; i < N - 1; i++)
     {
-#if 0
-        cout << "i: " << i << endl;
-
-        cout << "elements: " << endl;
-        for (const auto x : elements)
-        {
-            cout << x << " ";
-        }
-        cout << endl;
-#endif
         const auto lastElement = elements.back();
-        //cout << "i: " << i << " lastElement: " << lastElement << endl;
         int lastOccurence = -1;
 
         if (elements.size() > 1)
@@ -62,8 +41,6 @@ int solveBruteForce(int N)
             }
         }
 
-        //cout << "lastOccurence: " << lastOccurence << " elements.size(): " << elements.size() << endl;
-
         if (lastOccurence != -1)
         {
             elements.push_back(elements.size() - 1 - lastOccurence);
@@ -74,43 +51,14 @@ int solveBruteForce(int N)
         }
     }
 
-#if 0
-    cout << "elements final: " << endl;
-    for (const auto x : elements)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-#endif
-
-
     return count(elements.begin(), elements.end(), elements.back());
 }
 
 int main(int argc, char* argv[])
 {
+    // Easy one - the constraints are sufficiently small that a naive
+    // brute-force approach will work.
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        //const int T = 1;
-        //cout << T << endl;
-
-        for (int N = 1; N <= 128; N++)
-        {
-            cout << "Q: 2 lines" << endl;
-            cout << 1 << endl;
-            cout << N << endl;
-            cout << "A: 1 lines" << endl;
-            cout << solveBruteForce(N) << endl;
-        }
-
-        return 0;
-    }
     
     const auto T = read<int>();
 
@@ -118,20 +66,7 @@ int main(int argc, char* argv[])
     {
         const int N = read<int>();
 
-#ifdef BRUTE_FORCE
-#if 1
-        const auto solutionBruteForce = solveBruteForce(N);
-        cout << "solutionBruteForce: " << solutionBruteForce << endl;
-#endif
-#if 0
-        const auto solutionOptimised = solveOptimised();
-        cout << "solutionOptimised:  " << solutionOptimised << endl;
-
-        assert(solutionOptimised == solutionBruteForce);
-#endif
-#else
         cout << solveBruteForce(N) << endl;
-#endif
     }
 
     assert(cin);
