4a90fdb16746ca4b83a314c9d95d11071f779b42
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-16 13:02:15 +0000

    Cleanup PROXYC.

--- codechef/chef-and-proxy.cpp
+++ codechef/chef-and-proxy.cpp
@@ -2,19 +2,11 @@
 // 
 // Solution to: https://www.codechef.com/problems/PROXYC
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -42,28 +34,28 @@ bool canBeMarkedAsProxy(const int dayIndex, const string& attendance)
     return false;
 }
 
-#if 1
-int solveBruteForce(int D, const string& attendance)
+int integerCeilingDivide(int numerator, int denominator)
 {
-    const int numAttendancesFor75 = (D % 4 == 0) ? (3 * D / 4)  : (3 * D / 4 + 1);
+    if (numerator % denominator == 0)
+        return numerator / denominator;
+    else
+        return numerator / denominator + 1;
+
+};
+
+int findMinProxyAttendances(int D, const string& attendance)
+{
+    const int numAttendancesFor75 = integerCeilingDivide(3 * D, 4);
     const int numActualAttendances = count(attendance.begin(), attendance.end(), 'P');
 
     int numPossibleProxyAttendances = 0;
     for (int dayIndex = 0; dayIndex < D; dayIndex++)
     {
-        //cout << "dayIndex: " << dayIndex << " ";
         if (canBeMarkedAsProxy(dayIndex, attendance))
         {
-            //cout << "canBeMarkedAsProxy" << endl;
             numPossibleProxyAttendances++;
         }
-        else
-        {
-            //cout << " can not be marked as proxy" << endl;
-        }
     }
-    //cout << "numActualAttendances: " << numActualAttendances << " numAttendancesFor75: " << numAttendancesFor75 << " numPossibleProxyAttendances: " << numPossibleProxyAttendances << endl;
-
 
     if (numActualAttendances < numAttendancesFor75)
     {
@@ -79,53 +71,11 @@ int solveBruteForce(int D, const string& attendance)
     
     return 0;
 }
-#endif
-
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
 
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int D = rand() % 1000 + 1;
-            cout << D << endl;
-            const int numPresent = rand() % (D + 1);
-            string attendance;
-            for (int i = 0; i < D; i++)
-            {
-                if (i < numPresent)
-                    attendance += "P";
-                else
-                    attendance += "A";
-            }
-            random_shuffle(attendance.begin(), attendance.end());
-            cout << attendance << endl;
-            assert(attendance.size() == D && count(attendance.begin(), attendance.end(), 'P') == numPresent);
-        }
-
-        return 0;
-    }
     
-    // TODO - read in testcase.
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
@@ -134,9 +84,7 @@ int main(int argc, char* argv[])
         const string S = read<string>();
         assert(S.length() == D);
 
-
-        const auto solutionBruteForce = solveBruteForce(D, S);
-        cout << solutionBruteForce << endl;
+        cout << findMinProxyAttendances(D, S) << endl;
     }
 
     assert(cin);
