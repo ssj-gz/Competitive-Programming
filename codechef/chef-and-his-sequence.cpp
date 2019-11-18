2357514233dbf54e5540263d10f1c782ee31f45a
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-18 18:38:04 +0000

    Clean up CHEFSQ and fix some typos, etc.

--- codechef/chef-and-his-sequence.cpp
+++ codechef/chef-and-his-sequence.cpp
@@ -2,18 +2,10 @@
 // 
 // Solution to: https://www.codechef.com/problems/CHEFSQ
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -26,80 +18,32 @@ T read()
     return toRead;
 }
 
-#if 1
-bool containsAsSubsequence(const vector<int>& sequence, const vector<int>& candiateSubsequence)
+bool containsAsSubsequence(const vector<int>& sequence, const vector<int>& candidateSubsequence)
 {
     int numOfCandidateSubsequenceFound = 0;
     for (const auto sequenceValue : sequence)
     {
-        if (sequenceValue == candiateSubsequence[numOfCandidateSubsequenceFound])
+        if (sequenceValue == candidateSubsequence[numOfCandidateSubsequenceFound])
         {
             numOfCandidateSubsequenceFound++;
-            if (numOfCandidateSubsequenceFound == candiateSubsequence.size())
+            if (numOfCandidateSubsequenceFound == candidateSubsequence.size())
                 return true;
         }
     }
     
     return false;
 }
-#endif
-
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
 
 int main(int argc, char* argv[])
 {
-    // Trivial - the answer difficult part is that the Problem Statement is 
+    // Trivial - the only difficult part is that the Problem Statement is 
     // so sloppy that it's hard to tell exactly what it wants
-    // (it basically wants to know if what I've called "candiateSubsequence" occurs
-    // as a subsequence of sequence).
+    // (it basically wants to know whether what I've called "candidateSubsequence" occurs
+    // as a sub*sequence* of sequence - not a sub*string*, as stated in the Problem Statement
+    // Sigh).
     //
     // This is easily-solved with the greedy algorithm above.
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = rand() % 10 + 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int maxValue = rand() % 50 + 1;
-
-            const int sequenceLength = rand() % 20 + 1;
-            cout << sequenceLength << endl;
-            for (int i = 0; i < sequenceLength; i++)
-            {
-                cout << ((rand() % maxValue + 1));
-                if (i != sequenceLength - 1)
-                    cout << " ";
-            }
-            cout << endl;
-
-            const int candiateSubsequenceLength = rand() % 20 + 1;
-            cout << candiateSubsequenceLength << endl;
-            for (int i = 0; i < candiateSubsequenceLength; i++)
-            {
-                cout << ((rand() % maxValue + 1));
-                if (i != candiateSubsequenceLength - 1)
-                    cout << " ";
-            }
-            cout << endl;
-        }
-
-        return 0;
-    }
     
     const auto T = read<int>();
 
@@ -111,11 +55,11 @@ int main(int argc, char* argv[])
             value = read<int>();
 
         const int M = read<int>();
-        vector<int> candiateSubsequence(M);
-        for (auto& value : candiateSubsequence)
+        vector<int> candidateSubsequence(M);
+        for (auto& value : candidateSubsequence)
             value = read<int>();
 
-        cout << (containsAsSubsequence(sequence, candiateSubsequence) ? "Yes" : "No") << endl;
+        cout << (containsAsSubsequence(sequence, candidateSubsequence) ? "Yes" : "No") << endl;
     }
 
     assert(cin);
