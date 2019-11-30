87156e63af821eb59b85aa27cde014d4310672a6
Simon St James <kdedevel@etotheipiplusone.com>
2019-11-30 15:15:39 +0000

    Testcase generator (and a little bit of documentation) for NNN.

--- codechef/no-nut-november.cpp
+++ codechef/no-nut-november.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 
 #include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+#include <algorithm> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -23,6 +24,12 @@ T read()
 
 int main(int argc, char* argv[])
 {
+    // Easy one, though with an abysmal Problem Statement: rephrased,
+    //   i) Each squirrel can carry at most one nut;
+    //  ii) No squirrel can carry a nut that is more than K units away.
+    //
+    // The strategy is to sweep from left to right and have each squirrel
+    // carry the leftmost remaining nut that is <= K units away from it.
     ios::sync_with_stdio(false);
     if (argc == 2 && string(argv[1]) == "--test")
     {
@@ -36,6 +43,31 @@ int main(int argc, char* argv[])
 
         for (int t = 0; t < T; t++)
         {
+            const int numSquirrels = rand() % 20 + 1;
+            const int numNuts = rand() % 20 + 1;
+            const int N = numNuts + numSquirrels;
+
+            string nutsAndSquirrels;
+            for (int i = 0; i < numNuts; i++)
+            {
+                nutsAndSquirrels.push_back('N');
+            }
+            for (int i = 0; i < numSquirrels; i++)
+            {
+                nutsAndSquirrels.push_back('S');
+            }
+            random_shuffle(nutsAndSquirrels.begin(), nutsAndSquirrels.end());
+            const int K = 1 + rand() % (N - 1);
+            assert(1 <= K && K < N);
+
+            cout << K << " " << N << endl;
+            for (int i = 0; i < N; i++)
+            {
+                cout << nutsAndSquirrels[i];
+                if (i != N - 1)
+                    cout << " ";
+            }
+            cout << endl;
         }
 
         return 0;
