88c69932ed0c3cb820181c6206fc1705aaa498f0
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-03 10:44:24 +0000

    Generate the first deterministic, testlib.h-based testcase for "Counters on a Tree"!

--- my-own-problems/counters-on-a-tree/testcase-generator/counters-on-a-tree-testcase-generator.cpp
+++ my-own-problems/counters-on-a-tree/testcase-generator/counters-on-a-tree-testcase-generator.cpp
@@ -1,5 +1,32 @@
 #include <tree-generator.h>
+#include <testlib.h>
+
+#include <iostream>
+
+struct NodeData
+{
+    int numCounters = -1;
+};
+
+constexpr int maxNodes = 100'000;
+constexpr int maxCounters = 16;
 
 int main()
 {
+    rnd.setSeed(17666);
+    TreeGenerator<NodeData> treeGenerator;
+    auto rootNode = treeGenerator.createNode();
+    treeGenerator.createNodesWithRandomParentPreferringLeafNodes(99'999, 2.0);
+    for (auto& node : treeGenerator.nodes())
+    {
+        node->data.numCounters = rnd.next(maxCounters + 1);
+    }
+    treeGenerator.scrambleNodeIdsAndReorder(nullptr);
+    treeGenerator.scrambleEdgeOrder();
+    std::cout << treeGenerator.numNodes() << std::endl;
+    for (const auto& node : treeGenerator.nodes())
+    {
+        std::cout << node->data.numCounters << std::endl;
+    }
+    treeGenerator.printEdges();
 }
