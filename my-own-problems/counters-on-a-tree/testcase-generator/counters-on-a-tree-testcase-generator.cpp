14d747fc70ed36475616206e717f39b26374841c
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-04 10:38:23 +0000

    Add "addCounters" utility for ensuring a given percentage of nodes have counters.
    Using this means that we have to alter the seed for the current (only)
    testcase, and a completely different testcase is now generated.

--- my-own-problems/counters-on-a-tree/testcase-generator/counters-on-a-tree-testcase-generator.cpp
+++ my-own-problems/counters-on-a-tree/testcase-generator/counters-on-a-tree-testcase-generator.cpp
@@ -43,28 +43,62 @@ void scrambleAndwriteTestcase(TreeGenerator<NodeData>& treeGenerator, Testcase<S
     std::cout << destTestcase.contents() << std::endl;
 }
 
+/**
+ * Add a random number of counters to each node (0 <= c <= maxCounters), ensuring that the
+ * given percentage of nodes have an *odd* number of counters.
+ */
+void addCounters(TreeGenerator<NodeData>& treeGenerator, double percentageWithCounters)
+{
+    const int numWithCounters = (percentageWithCounters / 100.0) * treeGenerator.numNodes();
+    std::vector<bool> nodeHasCounter(treeGenerator.numNodes(), false);
+    for (int i = 0; i < numWithCounters; i++)
+    {
+        nodeHasCounter[i] = true;
+    }
+    shuffle(nodeHasCounter.begin(), nodeHasCounter.end());
+    int nodeIndex = 0;
+    for (auto node : treeGenerator.nodes())
+    {
+        const int randomNumCounters = rnd.next(maxCounters + 1);
+        int numCounters = randomNumCounters;
+        if (nodeHasCounter[nodeIndex])
+        {
+            if (numCounters % 2 == 0)
+                numCounters = (numCounters == 0 ? 1 : numCounters - 1);
+            assert(numCounters % 2 == 1);
+        }
+        else
+        {
+            if (numCounters % 2 == 1)
+                numCounters = numCounters - 1;
+            assert(numCounters % 2 == 0);
+        }
+        node->data.numCounters = numCounters;
+
+        nodeIndex++;
+    }
+
+}
+
 bool verifyTestFile(std::istream& testFileStream, const SubtaskInfo& containingSubtask)
 {
     return true;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     TestSuite<SubtaskInfo> testsuite;
     testsuite.setTestFileVerifier(&verifyTestFile);
     {
         auto& testFile = testsuite.newTestFile(TestFileInfo<SubtaskInfo>().belongingToSubtask(subtask1)
-                                                                          .withSeed(17666));
+                                                                          .withSeed(122));
 
-        auto& testcase = testFile.newTestcase(TestcaseInfo<SubtaskInfo>().withDescription("max nodes - randomly generated with 2 percent preference for leaves"));
+        auto& testcase = testFile.newTestcase(TestcaseInfo<SubtaskInfo>().withDescription("max nodes - randomly generated with 2 percent preference for leaves; 80% with counter; 10944 Bob wins"));
 
         TreeGenerator<NodeData> treeGenerator;
         auto rootNode = treeGenerator.createNode();
         treeGenerator.createNodesWithRandomParentPreferringLeafNodes(99'999, 2.0);
-        for (auto& node : treeGenerator.nodes())
-        {
-            node->data.numCounters = rnd.next(maxCounters + 1);
-        }
+        addCounters(treeGenerator, 80.0);
         scrambleAndwriteTestcase(treeGenerator, testcase);
     }
 }
