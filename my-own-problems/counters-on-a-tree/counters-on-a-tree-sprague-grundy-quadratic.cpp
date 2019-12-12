3be1e42759b74e0e72499edceb5c3e6fb0a9a15d
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-12 20:10:13 +0000

    Add a quick O(N^2), Sprague-Grundy-based implementation.

---
+++ my-own-problems/counters-on-a-tree/counters-on-a-tree-sprague-grundy-quadratic.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cassert>
+
+using namespace std;
+
+struct Node
+{
+    int index = -1;
+    int numCoins = -1;
+    vector<Node*> neighbours;
+
+    int grundyNumberIfRoot = 0;
+};
+
+int grundyContributionFromSubtree(Node* currentNode, Node* parentNode, int depth)
+{
+    int grundyContribution = 0;
+    if (currentNode->numCoins % 2 == 1)
+        grundyContribution ^= depth;
+
+    for (const auto child : currentNode->neighbours)
+    {
+        if (child == parentNode)
+            continue;
+
+        grundyContribution ^= grundyContributionFromSubtree(child, currentNode, depth + 1);
+    }
+
+    return grundyContribution;
+}
+
+int main()
+{
+    auto readInt = [](){ int x; cin >> x; return x; };
+
+    const auto numTestcases = readInt();
+    for (int t = 0; t < numTestcases; t++)
+    {
+        const auto numNodes = readInt();
+        vector<Node> nodes(numNodes);
+
+        int nodeIndex = 0;
+        for (auto& node : nodes)
+        {
+            node.index = nodeIndex;
+            node.numCoins = readInt();
+
+            nodeIndex++;
+        }
+
+        for (auto edgeNum = 0; edgeNum < numNodes - 1; edgeNum++)
+        {
+            const auto node1Index = readInt() - 1;
+            const auto node2Index = readInt() - 1;
+
+            nodes[node1Index].neighbours.push_back(&(nodes[node2Index]));
+            nodes[node2Index].neighbours.push_back(&(nodes[node1Index]));
+        }
+
+        int64_t MOD = 1'000'000'007;
+        int64_t result = 0;
+        int64_t powerOf2 = 1;
+        for (auto& rootNode : nodes)
+        {
+            const bool bobWins = (grundyContributionFromSubtree(&rootNode, nullptr, 0) == 0);
+            if (bobWins)
+            {
+                result = (result + powerOf2) % MOD;
+            }
+            powerOf2 = (powerOf2 * 2) % MOD;
+        }
+        cout << result << endl;
+    }
+
+}
