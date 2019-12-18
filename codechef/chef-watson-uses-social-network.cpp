7af9e529366cc331dde92d6b1a06b71a9e21569b
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-18 10:07:44 +0000

    REFACTOR,BOOKCHEF: Cleanup.

--- codechef/chef-watson-uses-social-network.cpp
+++ codechef/chef-watson-uses-social-network.cpp
@@ -2,20 +2,12 @@
 // 
 // Solution to: https://www.codechef.com/problems/BOOKCHEF
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <set>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -28,59 +20,13 @@ T read()
     return toRead;
 }
 
-
-
 int main(int argc, char* argv[])
 {
+    // Easy one - just do what the question says!
+    // This version should work even if Posts can have the 
+    // same popularity (the question itself guarantees the
+    // easier situation where they do not).
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-        const int numSpecialFriends = 1 + rand() % 100;
-        const int numPosts = 1 + rand() % 100;
-
-        const int maxPopularity = numPosts + rand() % 100;
-        const int maxSpecialFriendId = numSpecialFriends + rand() % 100;
-        const int maxPostFriendId = 1 + rand() % 100;
-
-        cout << numSpecialFriends << " " << numPosts << endl;
-        vector<int> specialFriendIds;
-        for (int i = 1; i <= maxSpecialFriendId; i++)
-        {
-            specialFriendIds.push_back(i);
-        }
-        random_shuffle(specialFriendIds.begin(), specialFriendIds.end());
-        for (int i = 0; i < numSpecialFriends; i++)
-        {
-            cout << specialFriendIds[i];
-            if (i != numSpecialFriends - 1)
-            {
-                cout << " ";
-            }
-        }
-        cout << endl;
-        vector<int> postPopularities;
-        for (int i = 1; i <= maxPopularity; i++)
-        {
-            postPopularities.push_back(i);
-        }
-        random_shuffle(postPopularities.begin(), postPopularities.end());
-        for (int i = 0; i < numPosts; i++)
-        {
-            string s;
-            const int strLen = 1 + rand() % 10;
-            for (int j = 0; j < strLen; j++)
-            {
-                s += 'a' + rand() % 26;
-            }
-            cout << (1 + rand() % maxPostFriendId) << " " << postPopularities[i] << s << endl;
-        }
-
-        return 0;
-    }
 
     struct Post
     {
@@ -110,9 +56,9 @@ int main(int argc, char* argv[])
 
     sort(posts.begin(), posts.end(), [](const auto& lhsPost, const auto& rhsPost)
             {
-            if (lhsPost.isSpecialFriend == rhsPost.isSpecialFriend)
-            return lhsPost.popularity > rhsPost.popularity;
-            return (lhsPost.isSpecialFriend);
+                if (lhsPost.isSpecialFriend == rhsPost.isSpecialFriend)
+                    return lhsPost.popularity > rhsPost.popularity;
+                return (lhsPost.isSpecialFriend);
             });
 
     for (const auto& post : posts)
