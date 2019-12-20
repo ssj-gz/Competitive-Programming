da220939c9d256666aa98fca67c45965ff500615
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-20 13:58:09 +0000

    REFACTOR, CMPRSS: Clean up.

--- codechef/compress-the-list.cpp
+++ codechef/compress-the-list.cpp
@@ -2,19 +2,11 @@
 // 
 // Solution to: https://www.codechef.com/problems/CMPRSS/
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -33,7 +25,7 @@ string compress(const vector<int>& a)
     for (int rangeBeginIndex = 0; rangeBeginIndex < a.size();)
     {
         // As with STL iterators, rangeEndIndex is the index *one past* the range
-        // of consecutive numbers beginning at rangeBeginIndex
+        // of consecutive numbers beginning at index rangeBeginIndex.
         int rangeEndIndex = rangeBeginIndex + 1;
         for (; rangeEndIndex < a.size(); rangeEndIndex++)
         {
@@ -63,46 +55,11 @@ string compress(const vector<int>& a)
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int N = 1 + rand() % 20;
-            const int maxA = N + rand() % 30;
-            cout << N << endl;
-            vector<int> nums;
-            for (int i = 1; i <= maxA; i++)
-            {
-                nums.push_back(i);
-            }
-            random_shuffle(nums.begin(), nums.end());
-            vector<int> a(nums.begin(), nums.begin() + N);
-            sort(a.begin(), a.end());
-            for (int i = 0; i < N; i++)
-            {
-                cout << a[i];
-                if (i != N - 1)
-                    cout << " ";
-            }
-            cout << endl;
-        }
-
-        return 0;
-    }
     
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
     {
-
         const int N = read<int>();
         vector<int> a(N);
         for (auto& x : a)
