f57d3a743228ba8fab8e9aceb7361936d052518b
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-27 13:23:07 +0000

    Add testcase generator for RECIPE.
    Will revert to pristine state next commit - just wanted this in my git
    history :)

--- codechef/cutting-recipes.cpp
+++ codechef/cutting-recipes.cpp
@@ -7,6 +7,8 @@
 
 #include <cassert>
 
+#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
+
 using namespace std;
 
 template <typename T>
@@ -40,6 +42,39 @@ int gcd(int a, int b)
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
+    if (argc == 2 && string(argv[1]) == "--test")
+    {
+        struct timeval time;
+        gettimeofday(&time,NULL);
+        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+        // TODO - generate randomised test.
+        //const int T = rand() % 100 + 1;
+        const int T = 1;
+        cout << T << endl;
+
+        for (int t = 0; t < T; t++)
+        {
+            const int N = 1 + rand() % 10;
+            const int maxA = 1 + rand() % 1000;
+            const int multiplier = 1 + rand() % min(10, maxA);
+            assert(maxA / multiplier > 0);
+
+            cout << N << endl;
+            for (int i = 0; i < N; i++)
+            {
+                const int value = multiplier * (1 + rand() % (maxA / multiplier));
+                assert(value > 0);
+                cout << value;
+                if (i != N - 1)
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+
+        return 0;
+    }
 
     const auto T = read<int>();
 
