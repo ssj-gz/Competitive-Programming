eea25784c2841ce57563f398fb5076a5c8acae00
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-27 16:42:11 +0000

    REFACTOR,SAD:Cleanup.

--- codechef/gift-rift.cpp
+++ codechef/gift-rift.cpp
@@ -2,20 +2,12 @@
 // 
 // Solution to: https://www.codechef.com/problems/SAD
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <limits>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -31,30 +23,6 @@ T read()
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int numRows = 1 + rand() % 10;
-        const int numCols = 1 + rand() % 10;
-
-        cout << numRows << " " << numCols << endl;
-        for (int i = 0; i < numRows; i++)
-        {
-            for (int j = 0; j < numCols; j++)
-            {
-                cout << 1 + rand() % 100;
-                if (j != numCols - 1)
-                    cout << " ";
-            }
-            cout << endl;
-        }
-
-        return 0;
-    }
 
     const int numRows = read<int>();
     const int numCols = read<int>();
@@ -90,11 +58,12 @@ int main(int argc, char* argv[])
     {
         for (int colNum = 0; colNum < numCols; colNum++)
         {
-            if (grid[rowNum][colNum] == smallestInRow[rowNum] &&
-                grid[rowNum][colNum] == largestInCol[colNum])
+            const int cellValue = grid[rowNum][colNum];
+            if (cellValue == smallestInRow[rowNum] &&
+                cellValue == largestInCol[colNum])
             {
-                assert(answer == -1 || answer == grid[rowNum][colNum]);
-                answer = grid[rowNum][colNum];
+                assert(answer == -1 || answer == cellValue); // The answer should be unique.
+                answer = cellValue;
             }
 
         }
