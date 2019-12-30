8f188e8b8c53568c4b682d16ce2b69bda7ca6dc6
Simon St James <kdedevel@etotheipiplusone.com>
2019-12-30 18:19:01 +0000

    REFACTOR,PD33: Cleanup.

--- codechef/to-and-fro.cpp
+++ codechef/to-and-fro.cpp
@@ -2,18 +2,10 @@
 // 
 // Solution to: https://www.codechef.com/problems/PD33
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -28,11 +20,11 @@ T read()
 
 string decrypt(const int numColumns, const string& encrypted)
 {
-
     const int numRows = encrypted.length() / numColumns;
 
     vector<string> decryptedCells(numRows, string(numColumns, '.'));
 
+    // Read into decryptedCells, go back and forth for each row.
     int index = 0;
     int direction = +1;
     for (int rowIndex = 0; rowIndex < numRows; rowIndex++)
@@ -46,6 +38,7 @@ string decrypt(const int numColumns, const string& encrypted)
         direction = -direction;
     }
 
+    // Read decryptedCells into decrypted, column-by-column.
     string decrypted;
     for (int colIndex = 0; colIndex < numColumns; colIndex++)
     {
@@ -61,34 +54,6 @@ string decrypt(const int numColumns, const string& encrypted)
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-        const int T = 1 + rand() % 10;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int numColumns = 2 + rand() % 19;
-            const int numRows = 1 + rand() % (20 / numColumns);
-            const int maxLetter = 1 + rand() % 26;
-
-
-            string s;
-            for (int i = 0; i < numRows * numColumns; i++)
-            {
-                s += 'a' + rand() % maxLetter;
-            }
-
-            cout << numColumns << endl;
-            cout << s << endl;
-        }
-        cout << 0 << endl;
-
-        return 0;
-    }
 
     while (true)
     {
