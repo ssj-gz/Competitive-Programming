8d5e9d7e5418c8cddd85672397f4b99e5c68db5e
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-08 18:06:53 +0000

    REFACTOR: Spread generate-output.sh across multiple lines for readability.

--- my-own-problems/shared/generate-output.sh
+++ my-own-problems/shared/generate-output.sh
@@ -5,5 +5,9 @@ if [ "$#" -eq 1 ]; then
     EXECUTABLE=$1
 fi
 
-time -p for i in testcase-generator/testfile*.in; do echo $i; outfilename=${i//.in/.out} ; cat $i | ${EXECUTABLE} > $outfilename ; done
+time -p for i in testcase-generator/testfile*.in; do 
+    echo $i
+    outfilename=${i//.in/.out}
+    cat $i | ${EXECUTABLE} > $outfilename 
+done
 
