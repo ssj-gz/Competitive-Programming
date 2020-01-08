8bc40aaa3822e366b12ad64ec58598a23a4dfb5c
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-08 17:54:36 +0000

    Also print the time taken to run process the test input.

--- my-own-problems/shared/check-output.sh
+++ my-own-problems/shared/check-output.sh
@@ -6,8 +6,10 @@ if [ "$#" -eq 1 ]; then
 fi
 
 time -p for i in testcase-generator/testfile*.in; do 
-    echo $i 
-    cat $i | ${EXECUTABLE} > last-output 
+    echo -n $i 
+    cat $i | /usr/bin/time -f %e -o last-testcase-time.txt ${EXECUTABLE} > last-output 
+    last_testcase_time="$(cat last-testcase-time.txt)"
+    echo " (${last_testcase_time} seconds)"
     diff ${i//.in/.out} last-output 
     if [ $? -eq "0" ]; then 
         echo -e "[\033[0;32mOK\033[0m]"
