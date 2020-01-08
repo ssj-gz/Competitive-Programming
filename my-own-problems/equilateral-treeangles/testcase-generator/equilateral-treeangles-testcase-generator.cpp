b3f3b7b2de290f7aae42b3af22e40ac332f93c59
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-08 10:53:22 +0000

    Add beginnings of EQUITREE testcase generator.
    Just contains that one "cheat-breaker" testcase for now.

---
+++ my-own-problems/equilateral-treeangles/testcase-generator/equilateral-treeangles-testcase-generator.cpp
@@ -0,0 +1,286 @@
+#include <tree-generator.h>
+#include <testlib.h>
+#include "testcase-generator-lib.h"
+#include "random-utilities.h"
+
+#include <iostream>
+#include <set>
+
+constexpr int maxNodes = 200'000;
+constexpr int maxNumTestcases = 1000;
+
+struct SubtaskInfo
+{
+    int subtaskId = -1;
+    int maxNodesPerTestcase = -1;
+    int maxNodesOverAllTestcases = -1;
+    int maxNumTestcases = -1;
+
+    static SubtaskInfo create()
+    {
+        return SubtaskInfo();
+    }
+    SubtaskInfo& withSubtaskId(int subtaskId)
+    {
+        this->subtaskId = subtaskId;
+        return *this;
+    }
+    SubtaskInfo& withMaxNodesPerTestcase(int maxNodesPerTestcase)
+    {
+        this->maxNodesPerTestcase = maxNodesPerTestcase;
+        return *this;
+    }
+    SubtaskInfo& withMaxNodesOverAllTestcases(int maxNodesOverAllTestcases)
+    {
+        this->maxNodesOverAllTestcases = maxNodesOverAllTestcases;
+        return *this;
+    }
+    SubtaskInfo& withMaxNumTestcases(int maxNumTestcases)
+    {
+        this->maxNumTestcases = maxNumTestcases;
+        return *this;
+    }
+};
+
+const int NoExplicitLimit = std::numeric_limits<int>::max();
+
+SubtaskInfo subtask1 = SubtaskInfo::create().withSubtaskId(1)
+                                            .withMaxNodesPerTestcase(15)
+                                            .withMaxNodesOverAllTestcases(NoExplicitLimit)
+                                            .withMaxNumTestcases(10);
+                                            
+SubtaskInfo subtask2 = SubtaskInfo::create().withSubtaskId(2)
+                                            .withMaxNodesPerTestcase(1000)
+                                            .withMaxNodesOverAllTestcases(NoExplicitLimit)
+                                            .withMaxNumTestcases(100);
+
+SubtaskInfo subtask3 = SubtaskInfo::create().withSubtaskId(3)
+                                            .withMaxNodesPerTestcase(maxNodes)
+                                            .withMaxNodesOverAllTestcases(maxNodes)
+                                            .withMaxNumTestcases(maxNumTestcases);
+                                        
+std::ostream& operator<<(std::ostream& outStream, const SubtaskInfo& subtaskInfo)
+{
+    outStream << "Constraints: " << std::endl;
+    outStream << " Maximum num nodes per testcase (i.e. N): " << subtaskInfo.maxNodesPerTestcase << std::endl;
+    if (subtaskInfo.maxNodesOverAllTestcases != NoExplicitLimit)
+        outStream << " Maximum sum of N over all testcases: " << subtaskInfo.maxNodesOverAllTestcases << std::endl;
+    outStream << " Max num testcases (i.e. T): " << subtaskInfo.maxNumTestcases << std::endl;
+
+    return outStream;
+};
+
+struct NodeData
+{
+    bool isSuitable = false;
+};
+
+using EQTTestCaseInfo = TestcaseInfo<SubtaskInfo>;
+using EQTTestFileInfo = TestFileInfo<SubtaskInfo>;
+
+void scrambleAndwriteTestcase(TreeGenerator<NodeData>& treeGenerator, Testcase<SubtaskInfo>& destTestcase)
+{
+    treeGenerator.scrambleNodeIdsAndReorder(nullptr);
+    treeGenerator.scrambleEdgeOrder();
+    destTestcase.writeLine(treeGenerator.numNodes());
+
+    for (const auto& edge : treeGenerator.edges())
+    {
+        destTestcase.writeLine(edge->nodeA->id(), edge->nodeB->id());
+    }
+
+    std::vector<int> nodeIsSuitable;
+    for (const auto& node : treeGenerator.nodes())
+    {
+        nodeIsSuitable.push_back(node->data.isSuitable ? 1 : 0);
+    }
+    destTestcase.writeObjectsAsLine(nodeIsSuitable.begin(), nodeIsSuitable.end());
+}
+
+/**
+ * Set isSuitable for random nodes, ensuring that the given percentage of nodes have isSuitable == true.
+ */
+void setRandomSuitable(TreeGenerator<NodeData>& treeGenerator, double percentageSuitable)
+{
+    const int numSuitable = (percentageSuitable / 100.0) * treeGenerator.numNodes();
+    std::vector<bool> nodeIsSuitable(treeGenerator.numNodes(), false);
+    for (int i = 0; i < numSuitable; i++)
+    {
+        nodeIsSuitable[i] = true;
+    }
+    shuffle(nodeIsSuitable.begin(), nodeIsSuitable.end());
+    int nodeIndex = 0;
+    for (auto node : treeGenerator.nodes())
+    {
+        node->data.isSuitable = nodeIsSuitable[nodeIndex];
+        nodeIndex++;
+    }
+}
+
+bool verifyTestFile(TestFileReader& testFileReader, const SubtaskInfo& containingSubtask);
+
+int main(int argc, char* argv[])
+{
+    TestSuite<SubtaskInfo> testsuite;
+    testsuite.setTestFileVerifier(&verifyTestFile);
+
+    // SUBTASK 1
+    {
+    }
+    
+    // SUBTASK 2
+    {
+    }
+    
+    // SUBTASK 3
+    {
+        {
+            auto& testFile = testsuite.newTestFile(EQTTestFileInfo().belongingToSubtask(subtask3)
+                    .withSeed(122));
+
+            {
+                auto& testcase = testFile.newTestcase(EQTTestCaseInfo().withDescription("Squat graph where all nodes have degree at least 3.  Some central nodes have high degree"));
+                const int numNodes = 200'000;
+                TreeGenerator<NodeData> treeGenerator;
+                auto rootNode = treeGenerator.createNode();
+
+                //treeGenerator.createNodesWithRandomParentPreferringLeafNodes((numNodes - treeGenerator.numNodes()) / 2, 90);
+                //treeGenerator.createNodesWithRandomParentPreferringLeafNodes((numNodes - treeGenerator.numNodes()) / 2, 1.0);
+                //treeGenerator.createNodesWithRandomParentPreferringLeafNodes(numNodes - treeGenerator.numNodes(), 98);
+
+                std::vector<TestNode<NodeData>*> leafNodes;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    leafNodes.push_back(treeGenerator.createNode(rootNode));
+                }
+
+                for (int i = 0; i < 50'000; i++)
+                {
+                    treeGenerator.createNode(rootNode);
+                }
+
+                while (treeGenerator.numNodes() < (numNodes - 1) / 2)
+                {
+                    //cerr << " # leaf nodes: " << leafNodes.size() << endl;
+                    std::vector<TestNode<NodeData>*> nextLeafNodes;
+                    for (auto leafNode : leafNodes)
+                    {
+                        auto newLeafNode = treeGenerator.createNode(leafNode);
+                        nextLeafNodes.push_back(newLeafNode);
+                        newLeafNode = treeGenerator.createNode(leafNode);
+                        nextLeafNodes.push_back(newLeafNode);
+
+                        if (treeGenerator.numNodes() >= (numNodes - 1) / 2)
+                            break;
+                    }
+
+                    leafNodes = nextLeafNodes;
+                }
+
+                treeGenerator.createNodesWithRandomParentPreferringLeafNodes((numNodes - treeGenerator.numNodes()) / 2, 90);
+                treeGenerator.createNodesWithRandomParentPreferringLeafNodes((numNodes - treeGenerator.numNodes()) / 2, 1.0);
+                treeGenerator.createNodesWithRandomParentPreferringLeafNodes(numNodes - treeGenerator.numNodes(), 98);
+
+
+
+                setRandomSuitable(treeGenerator, 80.0);
+                scrambleAndwriteTestcase(treeGenerator, testcase);
+            }
+        }
+    }
+    const bool validatedAndWrittenSuccessfully = testsuite.writeTestFiles();
+    if (!validatedAndWrittenSuccessfully)
+        return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
+}
+
+struct Node
+{
+    std::vector<Node*> neighbours;
+    int nodeId = -1;
+};
+
+bool verifyTestFile(TestFileReader& testFileReader, const SubtaskInfo& containingSubtask)
+{
+    using std::cout;
+    using std::endl;
+
+    int64_t totalNumNodes = 0;
+
+    const auto& [numTestCases] = testFileReader.readLine<int>();
+    for (int t = 0; t < numTestCases; t++)
+    {
+        const auto& [numNodes] = testFileReader.readLine<int>();
+        testFileReader.addErrorUnless(numNodes >= 1, "numNodes must be greater than or equal to 1, not " + std::to_string(numNodes));
+        testFileReader.addErrorUnless(numNodes <= containingSubtask.maxNodesPerTestcase, "numNodes must be greater than or equal to maxNodesPerTestcase, not " + std::to_string(numNodes));
+
+        totalNumNodes += numNodes;
+
+        std::vector<Node> nodes(numNodes);
+        for (int i = 0; i < numNodes; i++)
+        {
+            nodes[i].nodeId = (i + 1);
+        }
+        
+        for (int i = 0; i < numNodes - 1; i++)
+        {
+            const auto& [edgeNodeAId, edgeNodeBId] = testFileReader.readLine<int, int>();
+            testFileReader.addErrorUnless(0 <= edgeNodeAId && edgeNodeAId <= numNodes, "Invalid node id " + std::to_string(edgeNodeAId));
+            testFileReader.addErrorUnless(0 <= edgeNodeBId && edgeNodeBId <= numNodes, "Invalid node id " + std::to_string(edgeNodeBId));
+            
+            nodes[edgeNodeAId - 1].neighbours.push_back(&(nodes[edgeNodeBId - 1]));
+            nodes[edgeNodeBId - 1].neighbours.push_back(&(nodes[edgeNodeAId - 1]));
+        }
+
+        const auto nodeIsSuitable = testFileReader.readLineOfValues<int>(numNodes);
+        testFileReader.addErrorUnless(static_cast<int>(nodeIsSuitable.size()) == numNodes, "Line of nodeIsSuitable values should contain " + std::to_string(numNodes) + " values");
+        for (const auto isSuitableValue : nodeIsSuitable)
+        {
+            testFileReader.addErrorUnless(isSuitableValue == 0 || isSuitableValue == 1, "nodeIsSuitable value should be 0 or 1, not " + std::to_string(isSuitableValue));
+        }
+
+        int nodeId = 1;
+        for (const auto& node : nodes)
+        {
+            std::set<Node*> distinctNeighbours;
+            for (const auto neighbour : node.neighbours)
+            {
+                testFileReader.addErrorUnless(neighbour != &node, "Node " + std::to_string(node.nodeId) + " is connected to itself!");
+                testFileReader.addErrorUnless(distinctNeighbours.find(neighbour) == distinctNeighbours.end(), "Node " + std::to_string(nodeId) + " is connected to node " + std::to_string(neighbour->nodeId) + " more than once!");
+
+                distinctNeighbours.insert(neighbour);
+            }
+            nodeId++;
+        }
+
+        auto rootNode = &(nodes.front());
+        std::vector<Node*> toExplore = { rootNode };
+        std::set<Node*> visited = { rootNode };
+        while (!toExplore.empty())
+        {
+            std::vector<Node*> nextToExplore;
+            for (const auto& node : toExplore)
+            {
+                for (const auto neighbour : node->neighbours)
+                {
+                    if (visited.find(neighbour) == visited.end())
+                    {
+                        nextToExplore.push_back(neighbour);
+                        visited.insert(neighbour);
+                    }
+                }
+            }
+            toExplore = nextToExplore;
+        }
+
+        testFileReader.addErrorUnless(static_cast<int>(visited.size()) == numNodes, "Tree is not connected!");
+
+        testFileReader.markTestcaseAsValidated();
+    }
+    testFileReader.addErrorUnless(totalNumNodes <= containingSubtask.maxNodesOverAllTestcases, "Total numNodes must be less than or equal to maxNodesOverAllTestcases, not " + std::to_string(totalNumNodes));
+    testFileReader.markTestFileAsValidated();
+
+    return true;
+}
