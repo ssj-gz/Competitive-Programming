2c78e93207c4e1a7a4a0c4ce70f22afde374e345
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-08 18:08:00 +0000

    Print the time taken to generate the output.

--- my-own-problems/shared/generate-output.sh
+++ my-own-problems/shared/generate-output.sh
@@ -6,8 +6,10 @@ if [ "$#" -eq 1 ]; then
 fi
 
 time -p for i in testcase-generator/testfile*.in; do 
-    echo $i
+    echo -n $i
     outfilename=${i//.in/.out}
-    cat $i | ${EXECUTABLE} > $outfilename 
+    cat $i | /usr/bin/time -f %e -o last-testcase-time.txt ${EXECUTABLE} > $outfilename 
+    last_testcase_time="$(cat last-testcase-time.txt)"
+    echo " (${last_testcase_time} seconds)"
 done
 
