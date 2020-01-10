557afc1530cef284c1ef6886a9dbc6d0da49f9eb
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-10 14:26:48 +0000

    REFACTOR,CDUTSV01: More cleanup.

--- codechef/bank-cooperation.cpp
+++ codechef/bank-cooperation.cpp
@@ -2,19 +2,11 @@
 // 
 // Solution to: https://www.codechef.com/HEST2020/problems/CDUTSV01
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -69,44 +61,17 @@ int64_t solveBruteForce(const vector<int>& amountStolenFromBank, const vector<ve
 
 int main(int argc, char* argv[])
 {
+    // Easily solvable via Brute Force.
+    // I got a string of WAs on this for an embarrassing reason: the Problem says:
+    //
+    //    "Everything is guaranteed to easily fit in 32 bit integer type."
+    // 
+    // and, by "everything", I assumed it meant the output, too XD
+    //
+    // Turns out that, no, there are inputs whose output is too big for an int32. 
+    //
+    // Sigh.
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-
-        const int maxStolen = 1 + rand() % 100;
-        for (int i = 0; i < numBanks; i++)
-        {
-            cout << (1 + rand() % maxStolen);
-            if ( i != numBanks - 1)
-                cout << " ";
-        }
-        cout << endl;
-
-        vector<pair<int, int>> allBankPairs;
-
-        for (int i = 0; i < numBanks; i++)
-        {
-            for (int j = 0; j < numBanks; j++)
-            {
-                if (i != j)
-                    allBankPairs.push_back({i + 1, j + 1});
-            }
-        }
-
-        const int numPairsCooperatingBanks = rand() % (allBankPairs.size() + 1);
-        cout << numPairsCooperatingBanks << endl;
-
-        for (int i = 0; i < numPairsCooperatingBanks; i++)
-        {
-            const int pairIndex = rand() % numPairsCooperatingBanks;
-            cout << allBankPairs[pairIndex].first << " " << allBankPairs[pairIndex].second << endl;
-        }
-
-        return 0;
-    }
     
     vector<int> amountStolenFromBank(numBanks);
     for (auto& amount : amountStolenFromBank)
