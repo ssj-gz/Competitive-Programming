3cf0c8d331467e3c6f648a05afb769230c489321
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-13 20:28:02 +0000

    REFACTOR: Misc renamings in generate-output.sh.

--- my-own-problems/shared/generate-output.sh
+++ my-own-problems/shared/generate-output.sh
@@ -5,11 +5,11 @@ if [ "$#" -eq 1 ]; then
     EXECUTABLE=$1
 fi
 
-time -p for i in testcase-generator/testfile*.in; do 
-    echo -n $i
-    outfilename=${i//.in/.out}
-    cat $i | /usr/bin/time -f %e -o last-testcase-time.txt ${EXECUTABLE} > $outfilename 
-    last_testcase_time="$(cat last-testcase-time.txt)"
-    echo " (${last_testcase_time} seconds)"
+time -p for testfile_name in testcase-generator/testfile*.in; do 
+    echo -n $testfile_name
+    outfilename=${testfile_name//.in/.out}
+    cat $testfile_name | /usr/bin/time -f %e -o last_testfile_time.txt ${EXECUTABLE} > $outfilename 
+    last_testfile_time="$(cat last_testfile_time.txt)"
+    echo " (${last_testfile_time} seconds)"
 done
 
