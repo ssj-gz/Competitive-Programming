5f05fb09e65c8710725c9f0befbe671791f46fc4
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-14 10:50:28 +0000

    Use named constants for changing output colour in check-output.sh
    Magic bash codes are indecipherable!

--- my-own-problems/shared/check-output.sh
+++ my-own-problems/shared/check-output.sh
@@ -5,16 +5,21 @@ if [ "$#" -eq 1 ]; then
     EXECUTABLE=$1
 fi
 
+CHANGE_TO_GREEN="\033[0;32m"
+CHANGE_TO_WHITE="\033[0m"
+CHANGE_TO_RED="\033[0;31m"
+
 time -p for testfile_name in testcase-generator/testfile*.in; do 
     echo -n $testfile_name 
     cat $testfile_name | /usr/bin/time -f %e -o last-testfile-time.txt ${EXECUTABLE} > last-output 
     last_testcase_time="$(cat last-testfile-time.txt)"
     echo " (${last_testcase_time} seconds)"
     diff ${testfile_name//.in/.out} last-output 
+
     if [ $? -eq "0" ]; then 
-        echo -e "[\033[0;32mCORRECT\033[0m]"
+        echo -e "[${CHANGE_TO_GREEN}CORRECT${CHANGE_TO_WHITE}]"
     else  
-        echo -e "[\033[0;31mWRONG ANSWER\033[0m]"
+        echo -e "[${CHANGE_TO_RED}WRONG ANSWER${CHANGE_TO_WHITE}]"
     fi
 done
 
