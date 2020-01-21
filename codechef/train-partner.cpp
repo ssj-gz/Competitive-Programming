fa1e101719d1e06b25abd13a9b69c4dcdd4b2361
Simon St James <kdedevel@etotheipiplusone.com>
2020-01-21 17:42:43 +0000

    REFACTOR,ANKTRAIN: Clean up.

--- codechef/train-partner.cpp
+++ codechef/train-partner.cpp
@@ -2,18 +2,10 @@
 // 
 // Solution to: https://www.codechef.com/problems/ANKTRAIN
 //
-//#define SUBMISSION
-#define BRUTE_FORCE
-#ifdef SUBMISSION
-#undef BRUTE_FORCE
-#define NDEBUG
-#endif
 #include <iostream>
 #include <vector>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -26,11 +18,8 @@ T read()
     return toRead;
 }
 
-#if 1
-string solveBruteForce(int berthNumber)
+string findBerthPartner(int berthNumber)
 {
-    string result;
-
     static const string berthNames[] = 
     {
         "LB", // 1
@@ -63,62 +52,17 @@ string solveBruteForce(int berthNumber)
     
     return to_string(berthPartnerNumber) + berthPartnerName;
 }
-#endif
-
-#if 0
-SolutionType solveOptimised()
-{
-    SolutionType result;
-    
-    return result;
-}
-#endif
-
 
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        // TODO - generate randomised test.
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int n = 1 + rand() % 500;
-            cout << n << endl;
-        }
-
-        return 0;
-    }
     
-    // TODO - read in testcase.
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
     {
         const int berthNumber = read<int>();
-
-#ifdef BRUTE_FORCE
-#if 1
-        const auto solutionBruteForce = solveBruteForce(berthNumber);
-        cout << solutionBruteForce << endl;
-#endif
-#if 0
-        const auto solutionOptimised = solveOptimised();
-        cout << "solutionOptimised:  " << solutionOptimised << endl;
-
-        assert(solutionOptimised == solutionBruteForce);
-#endif
-#else
-        const auto solutionOptimised = solveOptimised();
-        cout << solutionOptimised << endl;
-#endif
+        cout << findBerthPartner(berthNumber) << endl;
     }
 
     assert(cin);
