73fe41603dde769bb2971252a7cfddd68506c8fe
Simon St James <kdedevel@etotheipiplusone.com>
2020-02-13 07:28:56 +0000

    Add option to give the EXECUTABLE an arg in check-output.sh
    Useful for java e.g.
    
        ./check-output.sh java -a NameOfTheClassFile

--- my-own-problems/shared/check-output.sh
+++ my-own-problems/shared/check-output.sh
@@ -4,7 +4,7 @@ usage() {
     echo "Usage: TODO"
 }
 
-params="$(getopt -o n,a -l no-diff:,exe-arg --name "$cmdname" -- "$@")"
+params="$(getopt -o n,a: -l no-diff,exe-arg: --name "$cmdname" -- "$@")"
 
 if [ $? -ne 0 ]
 then
@@ -17,6 +17,7 @@ unset params
   
 EXECUTABLE=./editorial
 NO_DIFF=false
+EXECUTABLE_ARG=
 
 while true
 do
@@ -24,6 +25,10 @@ do
         -n|--no-diff)
             NO_DIFF=true
             shift
+            ;;
+        -a|--exe_arg)
+            EXECUTABLE_ARG=${2-}
+            shift 2
             ;;
         -h|--help)
             usage
@@ -47,11 +52,11 @@ CHANGE_TO_GREEN="\033[0;32m"
 CHANGE_TO_WHITE="\033[0m"
 CHANGE_TO_RED="\033[0;31m"
 
-echo "Executable name: $EXECUTABLE NO_DIFF: ${NO_DIFF}"
+echo "Executable name: $EXECUTABLE NO_DIFF: ${NO_DIFF} EXECUTABLE_ARG ${EXECUTABLE_ARG}"
 
 time -p for testfile_name in testcase-generator/testfile*.in; do 
     echo -n $testfile_name 
-    cat $testfile_name | /usr/bin/time -f %e -o last-testfile-time.txt ${EXECUTABLE} > last-output 
+    cat $testfile_name | /usr/bin/time -f %e -o last-testfile-time.txt ${EXECUTABLE} ${EXECUTABLE_ARG} > last-output 
     last_testcase_time="$(cat last-testfile-time.txt)"
     echo " (${last_testcase_time} seconds)"
 
