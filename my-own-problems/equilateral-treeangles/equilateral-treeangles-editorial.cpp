de6e6743a1edc089b02ae0679234f39c56713ab0
Simon St James <kdedevel@etotheipiplusone.com>
2020-02-16 08:15:20 +0000

    EQUITREE,EDC: Adapt the Brief Description for the high-level overview
    Needs a lot of proof-reading etc.  I've added a note at the beginning
    stressing that this is just the first solution that occurred to me, and
    that the official Editorial will doubtless be better.

--- my-own-problems/equilateral-treeangles/equilateral-treeangles-editorial.cpp
+++ my-own-problems/equilateral-treeangles/equilateral-treeangles-editorial.cpp
@@ -322,6 +322,7 @@ map<int, HeightInfo> buildDescendantHeightInfo(Node* currentNode, Node* parentNo
             const auto earlierChildHasThisHeight = (numKnownDescendantsWithHeight > 0);
             if (earlierChildHasThisHeight)
             {
+                // Incorporate any un-incorporated HeightInfo into this Node's descendantWithHeightInfo.
                 auto& descendantHeightInfo = currentNode->descendantWithHeightInfo[descendantHeight];
                 auto& numPairsWithHeightWithNodeAsLCA = descendantHeightInfo.numPairsWithHeightWithNodeAsLCA;
                 auto& numberDescendantsWithThisHeight = descendantHeightInfo.number;
@@ -393,6 +394,158 @@ T read()
 
 int main(int argc, char* argv[])
 {
+    // This solution is the first one that occurred to me (well, ish - the first one
+    // didn't use Centroid Decomposition but a technique with similar capabilities
+    // based around Heavy-Light Decomposition which seems a little faster in practice,
+    // and was a little simpler for this Problem).  Based on the solutions of the Tester
+    // (and basically everyone who got 100pts on this Problem during the Contest :)),
+    // it's become clear that this is an unsually clunky and fiddly way of doing things,
+    // so I'm only going to give a brief overview of it, here - the official Editorial 
+    // solution will doubtless be better to learn from :) This explanation is adapted from
+    // the "Brief (ha!) Description" that is required when submitting a new Problem Idea.
+    //
+    // Anyway, if you're still reading (which you shouldn't be :p), here goes:
+    //
+    // Imagine we pick some arbitrary node (I pick node number 1) as the root R of the tree
+    // and do a DFS from there.  Then it can be shown that any triple (p, q, r) will be
+    // of one of the following two types:
+    //
+    //
+    // Type A - p, q, r have the same lca, from which they are equidistant e.g.
+    //
+    //                        R
+    //                       /|\
+    //                        ...
+    //                           X = lca(p, q, r)
+    //                         / | \
+    //                        .......
+    //                       /   |   \
+    //                      p    q    r   <-- dist(p, x) = dist(q, x) = dist(r, x); 
+    //                                        p, q and r all have same distance from R 
+    //                                        (aka "height").
+    //
+    // Type B - the two "lowest" nodes - say q and r wlog, are equidistant from their lca = X.  
+    // p is *not* a descendent of X, but dist(p, X) = dist(q, X) (= dist(r, X)) e.g.
+    //
+    //                        R
+    //                       /|\
+    //                        ...
+    //                       /\
+    //                      ... ...
+    //                     /    \
+    //                    p     ...
+    //                           |
+    //                           X = lca(q, r)
+    //                         /   \
+    //                        .......
+    //                       /       \
+    //                      q         r   <-- dist(p, X) = dist(r, X); p, q have the same distance from R.
+    //
+    // Type A is the easiest to compute; Type B is harder and is computed in two phases: the 
+    // first phase is shared with the computation of the number of Type A triplets; the 
+    // second is separate and uses Centroid Decomposition.
+    // 
+    // There's a reasonably well known algorithm for calculating, for each node v, the set of 
+    // all descendents of v in O(N log N):
+    // 
+    //    findDescendents(root)
+    //        set_of_descendents = empty-set
+    //        for each child c of root:
+    //            set_of_descendents_of_child = findDescendents(c)
+    //            if |set_of_descendents_of_child| > |set_of_descendents|:
+    //                swap set_of_descendents with set_of_descendents_of_child (O(1)).
+    //    
+    //            for each node in set_of_descendents_of_child:
+    //                add node to set_of_descendents
+    //    
+    //        return set_of_descendents
+    // 
+    // The algorithm looks like it's O(N^2) worst case, but the fact that we always "copy" 
+    // the smaller set into the larger actually makes it asymptotically better (O(N log N)), 
+    // for the same reason as disjoint-union-by-size is O(N log N): 
+    // https://en.wikipedia.org/wiki/Disjoint-set_data_structure#by_size
+    // I've also seen this technique referred to as the "Small-To-Large Trick".
+    // 
+    // For a node v, let height(v) = dist(R, v).  We adapt the algorithm to compute not the 
+    // set of descendents of each node v, but a count (map) of *heights of descendents of 
+    // suitable nodes of v* i.e. a map where the keys are heights and the values are the 
+    // number of descendents of v which are suitable and have that height; it can be shown 
+    // that this also is achievable in worst case O(N log N).
+    // 
+    // With some more book-keeping, it can be shown that we can extract, in O(N log N) time 
+    // and space, for all nodes v, for all heights h [NB: obviously such heights are stored 
+    // sparsely, else the space requirements would be O(N^2)]:
+    // 
+    //     the number of pairs of descendents (u', v') of v such that:
+    //         * u' and v' are both suitable;
+    //         * lca(u', v') = v; and
+    //         * height(u') = height(v') = h (and so: dist(u', v) = dist(v', v))
+    // 
+    // and also
+    // 
+    //     the number of triples (u', v', w') of v such that
+    //         * u', v' and w' are all suitable;
+    //         * lca(u', v') = lca(u', w') = lca(v', w') = v; and
+    //         * height(u') = height(v') = height(w') = h (and so: 
+    //           dist(u', v) = dist(v', v) = dist(w', v))
+    // 
+    // This completes Phase one of two.
+    // 
+    // We see that the latter count of triples is the number of triples of Type A (more 
+    // precisely - it is the number of such triples divided by the number of permutations 
+    // of a triple, 3! - see later).
+    // 
+    // The former is a step towards the computation of the number of Type B triples - 
+    // referring to the definition of Type B, we've found, for each X, the number of q's 
+    // and r's with a given height h (which equates to a distance d from X: height(q) - height(x))
+    // an lca X, and now we just need to find for each X and h the number of p's such that 
+    // dist(p, X) = height(q) - height(X).  We do this latter step via Centroid Decomposition.
+    // 
+    // More precisely, Phase one gives us, for each node v, a map from heights to number 
+    // of pairs of descendents called v.numPairsWithHeightWithNodeAsLCA - all in O(N log N) 
+    // time and space.
+    // 
+    // We then perform Centroid Decomposition with an efficient DistTracker class that 
+    // implements the following API:
+    // 
+    //     * addDistance(newDistance) - adds the distance to the list of tracked distances
+    //                                  in O(1).
+    //     * addToAllDistances(distanceIncrease) - adds distanceIncrease to each of the 
+    //                                             tracked distances in O(1).
+    //     * getNumWithDistance(distance) - return the number of tracked distances whos 
+    //                                      current value is precisely distance in O(1).
+    // 
+    // A simple combination of CD and DistTracker allows us to finally "complete" the "q's" 
+    // and "r's" found in Phase one with the "p's" necessary to form a complete, Type B-triplet 
+    // in O(N x (log N) x (log N)).
+    // 
+    // It can be further shown that if a triplet (p, q, r) of either type (i) or B) is 
+    // counted by the algorithm, then none of its (3! - 1) other permutations - (p, r, q); 
+    // (r, p, q) etc - will be counted, so simply multiplying the count of Type A and 
+    // Type B triplets by 3! gives us the final result.
+    // 
+    // Well - not quite :) The Centroid Decomposition step doesn't know about parent and 
+    // children in our original DFS from R, so it will overcount triples.  For example, 
+    // consider the simple example:
+    //
+    //                     R
+    //                     |
+    //                     X
+    //                    / \
+    //                   A   B
+    //
+    // Imagine that A and B are the only suitable nodes, so there are no valid triples in 
+    // this example.  
+    //
+    // Note that X's descendantWithHeightInfo[2].numPairsWithHeightWithNodeAsLCA will be 1.  
+    // The Centroid Decomposition step will then, unfortunately, treat A as a "completer" 
+    // of this pair (A, B) (and will treat B the same way), resulting in it reporting *2*
+    // triples, instead of the correct answer of 0.  Luckily, it's easy (though clunky) to 
+    // remove this overcount: see DescendantWithHeightInfo::number for more information.  
+    // The proper Editorial solution, and the original HLD-based solution, don't have this 
+    // clunky "overcount correction" step, which is another reason why they are superior :)
+    //
+    // The whole algorithm runs in O(N x (log N) x (log N)) time with O(N log N) space.
     ios::sync_with_stdio(false);
 
     const auto numTestcases = read<int>();
