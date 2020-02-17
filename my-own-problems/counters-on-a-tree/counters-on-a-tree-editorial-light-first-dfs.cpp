1d81323010bf903b0f48785adf7c8d120aef2394
Simon St James <kdedevel@etotheipiplusone.com>
2020-02-17 16:43:36 +0000

    COUNTREE,EDLF: Begin porting to use the optimised DistTracker from CD-version

--- my-own-problems/counters-on-a-tree/counters-on-a-tree-editorial-light-first-dfs.cpp
+++ my-own-problems/counters-on-a-tree/counters-on-a-tree-editorial-light-first-dfs.cpp
@@ -79,30 +79,36 @@ void doHeavyLightDecomposition(Node* node, bool followedHeavyEdge)
 class DistTracker
 {
     public:
-        DistTracker()
+        DistTracker(int maxDist)
         {
             auto powerOf2 = 2;
+            m_numBits = -1;
             for (auto binaryDigitNum = 0; binaryDigitNum <= maxBinaryDigits; binaryDigitNum++)
             {
-                m_distsModPowerOf2[binaryDigitNum] = vector<VersionedValue>(powerOf2);
+                m_distsModPowerOf2[binaryDigitNum] = vector<int>(powerOf2);
+                m_makesDigitOneBegin[binaryDigitNum] = powerOf2 >> 1;
+                m_makesDigitOneEnd[binaryDigitNum] = powerOf2 - 1;
+                if (powerOf2 / 2 > maxDist)
+                    break; // i.e. even if we scrolled this powerOf2 maxDist units, we'd never enter the "make digit 1" zone.
                 powerOf2 <<= 1;
+                m_numBits++;
             }
-            clear();
         }
         void insertDist(const int newDist)
         {
+            const int numBits = m_numBits;
             doPendingDistAdjustments();
-            const auto newDistAdjusted = newDist 
+            const auto newDistAdjusted = newDist
                 // The m_makesDigitOneBegin/End will have been shifted by a total of m_cumulativeDistAdjustment, so counteract that.
                 - m_cumulativeDistAdjustment
                 // Add a number guarantees that newDistAdjusted >= 0, but does not affect its value modulo the powers of 2 we care about.
-                + (1 << (maxBinaryDigits + 1)); 
+                + (1 << (maxBinaryDigits + 1));
             assert(newDistAdjusted >= 0);
             auto powerOf2 = 2;
-            for (auto binaryDigitNum = 0; binaryDigitNum <= maxBinaryDigits; binaryDigitNum++)
+            for (auto binaryDigitNum = 0; binaryDigitNum <= numBits; binaryDigitNum++)
             {
                 const auto distModPowerOf2 = newDistAdjusted & (powerOf2 - 1); // "& (powerOf2 - 1)" is a faster "% powerOf2".
-                numDistsModPowerOf2(binaryDigitNum, distModPowerOf2)++;
+                m_distsModPowerOf2[binaryDigitNum][distModPowerOf2]++;
                 if (m_makesDigitOneBegin[binaryDigitNum] <= m_makesDigitOneEnd[binaryDigitNum])
                 {
                     if (distModPowerOf2 >= m_makesDigitOneBegin[binaryDigitNum] && distModPowerOf2 <= m_makesDigitOneEnd[binaryDigitNum])
@@ -133,62 +139,55 @@ class DistTracker
             m_pendingDistAdjustment = 0;
             m_cumulativeDistAdjustment += distDiff;
 
+            const int numBits = m_numBits;
             auto powerOf2 = 2;
             if (distDiff > 0)
             {
-                for (auto binaryDigitNum = 0; binaryDigitNum <= maxBinaryDigits; binaryDigitNum++)
+                for (auto binaryDigitNum = 0; binaryDigitNum <= numBits; binaryDigitNum++)
                 {
                     // Scroll the begin/ end of the "makes digit one" zone to the left, updating m_grundyNumber
                     // on-the-fly.
+                    const auto reducedDistDiff = distDiff & (powerOf2 - 1); // Scrolling powerOf2 units is the same as not scrolling at all!
                     auto parityChangeToNumberOfDistsThatMakeDigitsOne = 0;
-                    for (auto i = 0; i < distDiff; i++)
+                    for (auto i = 0; i < reducedDistDiff; i++)
                     {
-                        parityChangeToNumberOfDistsThatMakeDigitsOne += numDistsModPowerOf2(binaryDigitNum, m_makesDigitOneEnd[binaryDigitNum]);
+                        parityChangeToNumberOfDistsThatMakeDigitsOne += m_distsModPowerOf2[binaryDigitNum][m_makesDigitOneEnd[binaryDigitNum]];
                         m_makesDigitOneEnd[binaryDigitNum] = (powerOf2 + m_makesDigitOneEnd[binaryDigitNum] - 1) & (powerOf2 - 1);
 
                         m_makesDigitOneBegin[binaryDigitNum] = (powerOf2 + m_makesDigitOneBegin[binaryDigitNum] - 1) & (powerOf2 - 1);
-                        parityChangeToNumberOfDistsThatMakeDigitsOne += numDistsModPowerOf2(binaryDigitNum, m_makesDigitOneBegin[binaryDigitNum]);
+                        parityChangeToNumberOfDistsThatMakeDigitsOne += m_distsModPowerOf2[binaryDigitNum][m_makesDigitOneBegin[binaryDigitNum]];
                     }
 
                     if ((parityChangeToNumberOfDistsThatMakeDigitsOne & 1) == 1)
                         m_grundyNumber ^= (powerOf2 >> 1); // This binary digit in the grundy number has flipped; update grundyNumber.
 
                     powerOf2 <<= 1;
-                } 
+                }
             }
             else
             {
                 distDiff = -distDiff;
                 assert(distDiff > 0);
-                for (auto binaryDigitNum = 0; binaryDigitNum <= maxBinaryDigits; binaryDigitNum++)
+                for (auto binaryDigitNum = 0; binaryDigitNum <= numBits; binaryDigitNum++)
                 {
                     // As above, but scroll the "makes digit one" zone to the right.
                     auto parityChangeToNumberOfDistsThatMakeDigitsOne = 0;
-                    for (auto i = 0; i < distDiff; i++)
+                    const auto reducedDistDiff = distDiff & (powerOf2 - 1); // Scrolling powerOf2 units is the same as not scrolling at all!
+                    for (auto i = 0; i < reducedDistDiff; i++)
                     {
-                        parityChangeToNumberOfDistsThatMakeDigitsOne += numDistsModPowerOf2(binaryDigitNum, m_makesDigitOneBegin[binaryDigitNum]);
+                        parityChangeToNumberOfDistsThatMakeDigitsOne += m_distsModPowerOf2[binaryDigitNum][m_makesDigitOneBegin[binaryDigitNum]];
                         m_makesDigitOneBegin[binaryDigitNum] = (m_makesDigitOneBegin[binaryDigitNum] + 1) & (powerOf2 - 1);
 
                         m_makesDigitOneEnd[binaryDigitNum] = (m_makesDigitOneEnd[binaryDigitNum] + 1) & (powerOf2 - 1);
-                        parityChangeToNumberOfDistsThatMakeDigitsOne += numDistsModPowerOf2(binaryDigitNum, m_makesDigitOneEnd[binaryDigitNum]);
+                        parityChangeToNumberOfDistsThatMakeDigitsOne += m_distsModPowerOf2[binaryDigitNum][m_makesDigitOneEnd[binaryDigitNum]];
                     }
 
                     if ((parityChangeToNumberOfDistsThatMakeDigitsOne & 1) == 1)
                         m_grundyNumber ^= (powerOf2 >> 1);
 
                     powerOf2 <<= 1;
-                } 
+                }
             }
-        }
-        int& numDistsModPowerOf2(int binaryDigitNum, int modPowerOf2)
-        {
-            auto& numDists = m_distsModPowerOf2[binaryDigitNum][modPowerOf2];
-            if (numDists.versionNumber != m_versionNumber)
-            {
-                numDists.value = 0;
-                numDists.versionNumber = m_versionNumber;
-            }
-            return numDists.value;
         }
         int grundyNumber()
         {
@@ -198,35 +197,14 @@ class DistTracker
             }
             return m_grundyNumber;
         }
-        void clear()
-        {
-            // Lazily "clear" in O(log2 N) by updating the version number.
-            m_versionNumber++;
-            auto powerOf2 = 2;
-            for (auto binaryDigitNum = 0; binaryDigitNum <= maxBinaryDigits; binaryDigitNum++)
-            {
-                m_makesDigitOneBegin[binaryDigitNum] = powerOf2 >> 1;
-                m_makesDigitOneEnd[binaryDigitNum] = powerOf2 - 1;
-
-                powerOf2 <<= 1;
-            }
-            m_grundyNumber = 0;
-            m_cumulativeDistAdjustment = 0;
-        }
     private:
-        struct VersionedValue
-        {
-            int value = 0;
-            int versionNumber = -1;
-        };
-        vector<VersionedValue> m_distsModPowerOf2[maxBinaryDigits + 1];
+        int m_numBits = 0;
+        vector<int> m_distsModPowerOf2[maxBinaryDigits + 1];
         int m_makesDigitOneBegin[maxBinaryDigits + 1];
         int m_makesDigitOneEnd[maxBinaryDigits + 1];
 
         int m_cumulativeDistAdjustment = 0;
         int m_grundyNumber = 0;
-
-        int m_versionNumber = 0;
 
         int m_pendingDistAdjustment = 0;
 };
@@ -238,7 +216,7 @@ void doDfs(Node* node, int depth, DistTracker& distTracker, DistTrackerAdjustmen
     if (distTrackerAdjustment == AdjustWithDepth)
         distTracker.adjustAllDists(1);
 
-    processNode(node, depth);
+    processNode(node, depth, distTracker);
 
     for (auto child : node->children)
         doDfs(child, depth + 1, distTracker, distTrackerAdjustment, processNode);
@@ -249,13 +227,12 @@ void doDfs(Node* node, int depth, DistTracker& distTracker, DistTrackerAdjustmen
 
 void computeGrundyNumberIfRootForAllNodes(vector<Node>& nodes)
 {
-    DistTracker distTracker;
-    auto collectDists = [&distTracker](Node* node, int depth)
+    auto collectDists = [](Node* node, int depth, DistTracker& distTracker)
                         {
                             if (node->hasCoin)
                                 distTracker.insertDist(depth);
                         };
-    auto propagateDists = [&distTracker](Node* node, int depth)
+    auto propagateDists = [](Node* node, int depth, DistTracker& distTracker)
                         {
                             node->grundyNumberIfRoot ^= distTracker.grundyNumber();
                         };
@@ -263,7 +240,7 @@ void computeGrundyNumberIfRootForAllNodes(vector<Node>& nodes)
     {
         for (auto pass = 1; pass <= 2; pass++)
         {
-            distTracker.clear();
+            DistTracker distTracker(maxDist); // TODO - optimise this - maxDist is way too big!
             // Crawl along chain, collecting from one node and propagating to the next.
             for (auto node : chain)
             {
