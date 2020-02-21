8673a595e0890dfeeea75aedf226828c29794ea1
Simon St James <kdedevel@etotheipiplusone.com>
2020-02-21 10:27:11 +0000

    MOVCOINS2: Add simplified brute-force (Sprague-Grundy) implementation
    Could use a bit more simplification and cleanup!

---
+++ my-own-problems/move-the-coins-2/move-the-coins-2-sprague-grundy-quadratic.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+struct Node
+{
+    vector<Node*> children;
+    int numCoins = -1;
+    Node* parent = nullptr;
+    int nodeId = -1;
+    bool usable = true;
+    int height = -1;
+
+    int grundyNumber = -1;
+};
+
+struct Query
+{
+    Node* nodeToMove = nullptr;
+    Node* newParent = nullptr;
+};
+
+void grundyNumberForTreeBruteForce(Node* node, const int depth, int& grundyNumber, int& numNodes)
+{
+    numNodes++;
+    for (int i = 0; i < node->numCoins; i++)
+    {
+        grundyNumber ^= depth;
+    }
+    for (Node* child : node->children)
+    {
+        grundyNumberForTreeBruteForce(child, depth + 1, grundyNumber, numNodes);
+    }
+}
+
+int grundyNumberForTreeBruteForce(Node* node, int& numNodes)
+{
+    int grundyNumber = 0;
+    grundyNumberForTreeBruteForce(node, 0, grundyNumber, numNodes);
+    return grundyNumber;
+}
+
+void reparentNode(Node* nodeToMove, Node* newParent)
+{
+    auto oldParent = nodeToMove->parent;
+    if (oldParent)
+    {
+        assert(find(oldParent->children.begin(), oldParent->children.end(), nodeToMove) != oldParent->children.end());
+        oldParent->children.erase(remove(oldParent->children.begin(), oldParent->children.end(), nodeToMove), oldParent->children.end());
+        assert(find(oldParent->children.begin(), oldParent->children.end(), nodeToMove) == oldParent->children.end());
+    }
+    nodeToMove->parent = newParent;
+    if (newParent)
+    {
+        assert(find(newParent->children.begin(), newParent->children.end(), nodeToMove) == newParent->children.end());
+        newParent->children.push_back(nodeToMove);
+    }
+}
+
+void fixParentChildAndHeights(Node* node, Node* parent = nullptr, int height = 0)
+{
+    node->height = height;
+    node->parent = parent;
+
+    node->children.erase(remove(node->children.begin(), node->children.end(), parent), node->children.end());
+
+    for (auto child : node->children)
+    {
+        fixParentChildAndHeights(child, node, height + 1);
+    }
+}
+
+vector<int> grundyNumbersForQueriesBruteForce(vector<Node>& nodes, const vector<Query>& queries)
+{
+    auto rootNode = &(nodes.front());
+    vector<int> grundyNumbersForQueries;
+    int queryNum = 0;
+    int unused = 0;
+    const auto originalTreeGrundyNumber = grundyNumberForTreeBruteForce(rootNode, unused);
+    for (const auto& query : queries)
+    {
+        auto originalParent = query.nodeToMove->parent;
+        reparentNode(query.nodeToMove, query.newParent);
+        int numNodesInModifiedTree = 0;
+        grundyNumbersForQueries.push_back(grundyNumberForTreeBruteForce(rootNode, numNodesInModifiedTree));
+        assert(numNodesInModifiedTree == nodes.size());
+        reparentNode(query.nodeToMove, originalParent);
+        queryNum++;
+        if (queryNum % 100 == 0)
+            cerr << "Brute force queryNum: " << queryNum << " out of " << queries.size() << endl;
+    }
+    return grundyNumbersForQueries;
+}
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+
+    int numNodes;
+    cin >> numNodes;
+
+    vector<Node> nodes(numNodes);
+    for (int i = 0; i < numNodes; i++)
+    {
+        nodes[i].nodeId = i;
+        cin >> nodes[i].numCoins;
+    }
+    for (int i = 0; i < numNodes - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+
+        nodes[a].children.push_back(&nodes[b]);
+        nodes[b].children.push_back(&nodes[a]);
+    }
+
+    int numQueries;
+    cin >> numQueries;
+
+    vector<Query> queries(numQueries);
+
+    for (int i = 0; i < numQueries; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+
+        queries[i].nodeToMove = &(nodes[u]);
+        queries[i].newParent = &(nodes[v]);
+    }
+
+    auto rootNode = &(nodes.front());
+    fixParentChildAndHeights(rootNode);
+
+    const auto resultBruteForce = grundyNumbersForQueriesBruteForce(nodes, queries);
+    vector<int> queriesWhereBobWins;
+    int queryNum = 1;
+    for (const auto queryResult : resultBruteForce)
+    {
+        if (queryResult == 0)
+            queriesWhereBobWins.push_back(queryNum);
+        queryNum++;
+    }
+    cout << queriesWhereBobWins.size() << endl;
+    for (const auto queryNum : queriesWhereBobWins)
+    {
+        cout << queryNum << endl;
+    }
+}
