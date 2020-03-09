053ef5edf2b02a8ada3bec4ce85861b4d2d94002
Simon St James <kdedevel@etotheipiplusone.com>
2020-03-09 10:42:53 +0000

    MOVCOINS2,tgc: Delete utils.cpp, and move implementations into utils.h
    As mentioned in the previous commit, test-lib.h is not designed to be #include'able into
    multiple .cpp files - we'll have to bung everything into utils.h.

--- my-own-problems/move-the-coins-2/testcase-generator/utils.h
+++ my-own-problems/move-the-coins-2/testcase-generator/utils.h
@@ -16,7 +16,11 @@ struct NodeData
  * of height h, with p not a descendent of v, such that re-parenting v to p
  * gives a game board in which Bob wins.
  */
-std::map<TestNode<NodeData>*, std::vector<int>> findBobWinningRelocatedHeightsForNodes(const TreeGenerator<NodeData>& treeGenerator);
+std::map<TestNode<NodeData>*, std::vector<int>> findBobWinningRelocatedHeightsForNodes(const TreeGenerator<NodeData>& treeGenerator)
+{
+    std::map<TestNode<NodeData>*, std::vector<int>> result;
+    return result;
+}
 
 
 #endif
