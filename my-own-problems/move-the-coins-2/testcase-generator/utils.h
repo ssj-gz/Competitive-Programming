3911ca17ac9471716aa6fbd401394877a7ca2900
Simon St James <kdedevel@etotheipiplusone.com>
2020-03-09 10:52:46 +0000

    MOVCOINS2,TGC: Initial implementation of findMaxHeightOfNonDescendent
    Needs testing!

--- my-own-problems/move-the-coins-2/testcase-generator/utils.h
+++ my-own-problems/move-the-coins-2/testcase-generator/utils.h
@@ -11,6 +11,24 @@ struct NodeData
     int numCounters = -1;
 };
 
+int findMaxHeightOfNonDescendent(TestNode<NodeData>* currentNode, TestNode<NodeData>* parent, int height, TestNode<NodeData>* nodeToIgnore)
+{
+    if (currentNode == nodeToIgnore)
+        return -1;
+
+    int maxHeightOfDescendent = height; // Current node.
+
+    for (auto edge : currentNode->neighbours)
+    {
+        auto childNode = edge->otherNode(currentNode);
+        if (childNode == parent)
+            continue;
+
+        maxHeightOfDescendent = std::max(maxHeightOfDescendent, findMaxHeightOfNonDescendent(childNode, parent, height + 1, nodeToIgnore));
+    }
+
+    return maxHeightOfDescendent;
+}
 /**
  * For each node v in treeGenerator, find the heights h such that there is a node p
  * of height h, with p not a descendent of v, such that re-parenting v to p
@@ -19,6 +37,14 @@ struct NodeData
 std::map<TestNode<NodeData>*, std::vector<int>> findBobWinningRelocatedHeightsForNodes(const TreeGenerator<NodeData>& treeGenerator)
 {
     std::map<TestNode<NodeData>*, std::vector<int>> result;
+    auto rootNode = treeGenerator.nodes().front();
+
+    for (auto nodeToReparent : treeGenerator.nodes())
+    {
+        const int maxHeightOfNonDescendent = findMaxHeightOfNonDescendent(rootNode, nullptr, 0, nodeToReparent);
+        if (maxHeightOfNonDescendent == -1)
+            continue;
+    }
     return result;
 }
 
