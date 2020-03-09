d5592be127137655060c1d4f3a8bd67c07731f85
Simon St James <kdedevel@etotheipiplusone.com>
2020-03-09 11:58:39 +0000

    MOVCOINS2,TGC: Initial draft of "calcTreeHash"
    Gives a (hopefully "unique" "in practice"!) hash of the given tree.
    
    This will be used for caching the results of the (expensive)
    findBobWinningRelocatedHeightsForNodes method.

--- my-own-problems/move-the-coins-2/testcase-generator/utils.h
+++ my-own-problems/move-the-coins-2/testcase-generator/utils.h
@@ -96,6 +96,34 @@ int findGrundyNumberIfRelocatedNode(TestNode<NodeData>* rootNode, int height, Te
 
     return grundyNumber;
 }
+
+void calcTreeHash(TestNode<NodeData>* currentNode, TestNode<NodeData>* parent, int& dfsIndex, std::size_t& treeHash)
+{
+    // Largely arbitrary "hash combine" function, taken from 
+    //  
+    //   https://stackoverflow.com/questions/2590677/how-do-i-combine-hash-values-in-c0x
+    //
+    // Incorporating a node's dfsIndex, id, and numCounters seems like it would give a reasonably good hash.  The magic numbers are
+    // arbitrary.
+    treeHash ^= std::hash<int>()(dfsIndex * (currentNode->id() + currentNode->data.numCounters + 876474)) + 0x9e3779b9 + (treeHash<<6) + (treeHash>>2);
+
+    for (auto edge : currentNode->neighbours)
+    {
+        auto childNode = edge->otherNode(currentNode);
+        if (childNode == parent)
+            continue;
+
+        calcTreeHash(childNode, currentNode, dfsIndex, treeHash);
+    }
+}
+
+std::size_t calcTreeHash(TestNode<NodeData>* rootNode)
+{
+    std::size_t treeHash = 0;
+    int dfsIndex = 0;
+    calcTreeHash(rootNode, nullptr, dfsIndex, treeHash);
+    return treeHash;
+}
 /**
  * For each node v in treeGenerator, find the heights h such that there is a node p
  * of height h, with p not a descendent of v, such that re-parenting v to p
@@ -116,6 +144,29 @@ std::map<TestNode<NodeData>*, std::vector<int>> findBobWinningRelocatedHeightsFo
         const int maxHeightOfNonDescendent = findMaxHeightOfNonDescendent(rootNode, nullptr, 0, nodeToReparent);
         if (maxHeightOfNonDescendent == -1)
             continue;
+
+        for (int newParentHeight = 0; newParentHeight <= maxHeightOfNonDescendent; newParentHeight++)
+        {
+            bool foundNewParentAtHeight = false;
+            for (auto newParentAtHeight : nodesAtHeight[newParentHeight])
+            {
+                const bool isDescendentOfNodeToReparent = (newParentAtHeight->data.dfsVisitBegin > nodeToReparent->data.dfsVisitBegin &&
+                                                           newParentAtHeight->data.dfsVisitEnd < nodeToReparent->data.dfsVisitEnd);
+                if (isDescendentOfNodeToReparent)
+                    continue;
+
+                foundNewParentAtHeight = true;
+                const int grundyNumberIfRelocated = findGrundyNumberIfRelocatedNode(rootNode, -1, nodeToReparent, nodeToReparent->data.parent, newParentAtHeight);
+                if (grundyNumberIfRelocated == 0)
+                {
+                    result[nodeToReparent].push_back(newParentHeight);
+                }
+                // Only need to check one newParentAtHeight: re-parenting nodeToReparent to any of them will give
+                // the same grundy number.
+                break;
+            }
+            assert(foundNewParentAtHeight);
+        }
     }
     return result;
 }
