e1b67f3af428bd69b7ae80d6be4399e044733f80
Simon St James <kdedevel@etotheipiplusone.com>
2020-03-09 11:12:31 +0000

    MOVCOINS2,TGC: Initial implementation of findGrundyNumberIfRelocatedNode
    Needs testing!
    
    This is the brute-force version (takes O(N)): using this would make the
    problem of finding, for each node, the heights that it can be
    re-parented to to give a Bob Win O(N^3), which would be appallingly
    slow.
    
    It will eventually be replaced with an implementation based on "Counters
    on a Tree"'s DistTracker class.

--- my-own-problems/move-the-coins-2/testcase-generator/utils.h
+++ my-own-problems/move-the-coins-2/testcase-generator/utils.h
@@ -29,6 +29,43 @@ int findMaxHeightOfNonDescendent(TestNode<NodeData>* currentNode, TestNode<NodeD
 
     return maxHeightOfDescendent;
 }
+
+void findGrundyNumberIfRelocatedNodeAux(TestNode<NodeData>* currentNode, TestNode<NodeData>* parent, int height, TestNode<NodeData>* nodeToReparent, TestNode<NodeData>* oldParent, TestNode<NodeData>* newParent, int& grundyNumber)
+{
+    if (currentNode->data.numCounters % 2 == 1)
+        grundyNumber ^= height;
+
+    std::vector<TestNode<NodeData>*> childNodes;
+
+    for (auto edge : currentNode->neighbours)
+    {
+        auto childNode = edge->otherNode(currentNode);
+        if (childNode == parent)
+            continue;
+        if (childNode == nodeToReparent)
+        {
+            assert(currentNode == oldParent);
+            continue;
+        }
+
+        childNodes.push_back(childNode);
+    }
+    if (currentNode == newParent)
+        childNodes.push_back(nodeToReparent);
+
+    for (auto childNode : childNodes)
+    {
+        findGrundyNumberIfRelocatedNodeAux(childNode, currentNode, height + 1, nodeToReparent, oldParent, newParent, grundyNumber);
+    }
+}
+
+int findGrundyNumberIfRelocatedNode(TestNode<NodeData>* rootNode, int height, TestNode<NodeData>* nodeToReparent, TestNode<NodeData>* oldParent, TestNode<NodeData>* newParent)
+{
+    int grundyNumber = 0;
+    findGrundyNumberIfRelocatedNodeAux(rootNode, nullptr, 0, nodeToReparent, oldParent, newParent, grundyNumber);
+
+    return grundyNumber;
+}
 /**
  * For each node v in treeGenerator, find the heights h such that there is a node p
  * of height h, with p not a descendent of v, such that re-parenting v to p
