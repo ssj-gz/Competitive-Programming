6441b4aadd05cf665ef72bf94aa90ccd7b244849
Simon St James <kdedevel@etotheipiplusone.com>
2020-03-12 20:11:14 +0000

    Add "DIAGNOSTICS" mode to TLG

--- codechef/the-lead-game.cpp
+++ codechef/the-lead-game.cpp
@@ -4,6 +4,8 @@
 
 #include <cassert>
 
+//#define DIAGNOSTICS
+
 using namespace std;
 
 template <typename T>
@@ -35,6 +37,12 @@ int main(int argc, char* argv[])
         player1CumulativeScore += player1RoundScore;
         player2CumulativeScore += player2RoundScore;
 
+#ifdef DIAGNOSTICS
+        cout << "In round " << (i + 1) << " player 1 scored: " << player1RoundScore << " and player 2 scored: " << player2RoundScore << endl;
+        cout << "The cumulative scores are now - player 1: " << player1CumulativeScore << "; player 2: " << player2CumulativeScore << endl;
+        cout << endl;
+#endif
+
         const int64_t lead = abs(player1CumulativeScore - player2CumulativeScore);
 
         if (lead > greatestLead)
@@ -42,13 +50,26 @@ int main(int argc, char* argv[])
             greatestLead = lead;
 
             if (player1CumulativeScore < player2CumulativeScore)
+            {
+#ifdef DIAGNOSTICS
+                cout << "New best lead: player 2 has a lead of " << greatestLead << " over player 1" << endl << endl;
+#endif
                 winnerIsPlayer1 = false;
+            }
             else
+            {
+#ifdef DIAGNOSTICS
+                cout << "New best lead: player 2 has a lead of " << greatestLead << " over player 1" << endl << endl;
+#endif
                 winnerIsPlayer1 = true;
+            }
         }
         assert(greatestLead != 0);
     }
 
+#ifdef DIAGNOSTICS
+    cout << "Final winner/ lead: " << endl;
+#endif
     cout << (winnerIsPlayer1 ? 1 : 2) << " " << greatestLead << endl;
 
     assert(cin);
