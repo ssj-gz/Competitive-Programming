01f3a51b5ae56dc3e00dc84a1761d2bf3a976a78
Simon St James <kdedevel@etotheipiplusone.com>
2020-07-09 16:24:15 +0100

    SIMKDOWN: Fix a LaTeX error in DIAGNOSTICS mode

--- my-own-problems/simple-markdown-editor/simple-markdown-editor.cpp
+++ my-own-problems/simple-markdown-editor/simple-markdown-editor.cpp
@@ -390,7 +390,7 @@ int64_t solveBruteForce(const vector<Query>& queries, vector<string>& bruteForce
                     const int insertionPos = (query.encryptedArgument ^ decryptionKey) - 1;
                     const int numToInsert = query.encryptedArgument2 ^ decryptionKey;
 #ifdef DIAGNOSTICS
-                    cout << "Need to insert " << "$" << decryptionKey << " \\oplus " << query.encryptedArgument2 << " = " << numToInsert << " non-formatting chars at position $" << decryptionKey << "\\oplus " << query.encryptedArgument2 << " = " << (insertionPos + 1) << "$" << endl;
+                    cout << "Need to insert " << "$" << decryptionKey << " \\oplus " << query.encryptedArgument2 << " = " << numToInsert << "$ non-formatting chars at position $" << decryptionKey << "\\oplus " << query.encryptedArgument2 << " = " << (insertionPos + 1) << "$" << endl;
                     cout << "```" << endl;
                     showStatus(true, false, false);
                     cout << indentationSpaces << repeatedString(" ", insertionPos) << "^" << " insert " << numToInsert << " non-formatting chars here" << endl;
