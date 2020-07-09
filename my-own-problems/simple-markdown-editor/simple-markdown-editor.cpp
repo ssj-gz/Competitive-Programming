4bfaa5dab9b04d004ab325636201cffe43998092
Simon St James <kdedevel@etotheipiplusone.com>
2020-07-09 16:40:19 +0100

    SIMKDOWN: Fix DIAGNOSTICS decryptionKey update output when queryAnswer = -1
    The generated decryptionKey update LaTeX would have queryAnswer == -1 in
    the formula, instead of 3'141'592.

--- my-own-problems/simple-markdown-editor/simple-markdown-editor.cpp
+++ my-own-problems/simple-markdown-editor/simple-markdown-editor.cpp
@@ -460,12 +460,12 @@ int64_t solveBruteForce(const vector<Query>& queries, vector<string>& bruteForce
                         cout << "The range of non-formatted chars around the query point is non-formatted; the answer to query #" << queryNum << " is $3141592$." << endl;
                     else
                         cout << "The range of non-formatted chars around the query point is formatted; and the number is this range is " << queryAnswer << "; the answer to query #" << queryNum << " query is $" << queryAnswer << "$." << endl;
+                    if (queryAnswer == -1)
+                        queryAnswer = 3'141'592;
                     cout << "\n\nUpdating $decryptionKey$: \n" << endl;
                     cout << "$$\ndecryptionKey = decryptionKey + " << queryAnswer << "\\times  2^" << queryNum << "  = " << decryptionKey << " + " << queryAnswer << "\\times " << ((1 << queryNum))  << " = " << decryptionKey + ((1 << queryNum)) * queryAnswer << " \\mod 10^9+7 = " << ((decryptionKey + ((1 << queryNum)) * queryAnswer) % Mod) << "\n$$" << endl;
 
 #endif
-                    if (queryAnswer == -1)
-                        queryAnswer = 3'141'592;
                     decryptionKey = (decryptionKey + ((queryAnswer % Mod) * powerOf2) % Mod) % Mod;
 
                     queryResults.push_back(queryAnswer);
