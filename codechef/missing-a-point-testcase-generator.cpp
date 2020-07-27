aa0cefcbeb7c06507361326269e34e89395b2f1b
Simon St James <kdedevel@etotheipiplusone.com>
2020-07-27 07:58:57 +0100

    Add hostile testcase generator for "Missing a Point" Problem
    https://www.codechef.com/problems/PTMSSNG
    
    Inspired by the forum post:
    
    https://discuss.codechef.com/t/problem-with-hashmaps-time-complexity-o-n-fails-while-o-n-2-works/73242/4
    
    Using bucket-hacking to cause worse-case performance in solutions that
    use unordered_map to store the vertices.

---
+++ codechef/missing-a-point-testcase-generator.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <unordered_map>
+#include <set>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+const int N = 2e5;
+
+/*
+ * Generates a testcase for the "Missing a Point" Problem:
+ *
+ *    https://www.codechef.com/problems/PTMSSNG
+ *
+ * that should cause solutions using an unordered_map to time out,
+ * using the "Blowing Up Unordered Map" technique described here:
+ *
+ *    https://codeforces.com/blog/entry/62393
+ *
+ * Essentially, generates a constraints-conformant testcase where all 
+ * Rectangle coords are the multiples of a known unordered_map bucket_count,
+ * so they all get mapped to the same bucket, causing worse-case performance.
+ *
+ * Usually, we would pick p >= 100'000; however, the requirement that
+ * coords have absolute value <= 1e9 makes this a little harder - the 
+ * "maxCoordMultiplier" is quite small for such large p, and so we get
+ * fewer *distinct* coordinates hashing to the same value.
+ *
+ * A smaller, but still reasonably large, value of p like 26267 is a good compromise.
+ */
+
+
+int main() {
+    // On my system, an unordered_map will eventually grow to have p buckets.
+    const auto p = 26267;
+    const auto maxCoordVal = 1'000'000'000;
+    const auto maxCoordMultiplier = maxCoordVal / p;
+
+    struct Vertex
+    {
+        int x = -1;
+        int y = -1;
+        bool operator<(const Vertex& other) const
+        {
+            if (x != other.x)
+                return x < other.x;
+            return y < other.y;
+        }
+    };
+    struct Rectangle
+    {
+        Vertex vertices[4];
+    };
+    vector<Rectangle> rectangles;
+    set<Vertex> vertices;
+
+    // Should generate valid rectangles with distinct vertices, where each vertex coord in a multiple of
+    // p, and does not exceed maxCoordVal.
+    // Since all coordinates are multiples of p, when the unordered_map grows to have p buckets, 
+    // the coordinates will all end up in the same bucket.
+    for (int i = 0 ; i < maxCoordMultiplier / 2; i += 2)
+    {
+        for (int j = 0; j < maxCoordMultiplier / 2; j += 2)
+        {
+            // Should be a non-degenerate rectangle.
+            const Vertex topLeft = {(i * 2) * p, (j * 2) * p};
+            const Vertex topRight = {(i * 2 + 1) * p, (j * 2) * p};
+            const Vertex bottomLeft = {(i * 2) * p, (j * 2 + 1) * p};
+            const Vertex bottomRight = {(i * 2 + 1) * p, (j * 2 + 1) * p};
+
+            Rectangle rectangle { topLeft, topRight, bottomLeft, bottomRight };
+            rectangles.push_back(rectangle);
+
+        }
+        if (rectangles.size() > 2e5)
+            break;
+    }
+
+    while (rectangles.size() > 2e5)
+        rectangles.pop_back();
+
+    const int numRectangles = rectangles.size();
+    // Verify coords are unique.
+    for (const auto& rectangle : rectangles)
+    {
+        for (const auto& vertex : rectangle.vertices)
+        {
+            assert(vertices.find(vertex) == vertices.end());
+            vertices.insert(vertex);
+        }
+    }
+    // Mark one random vertex as not-to-be-printed.
+    auto& vertexToOmit = rectangles[rand() % rectangles.size()].vertices[rand() % 4];
+    cerr << "Will omit the vertex: " << vertexToOmit.x << " " << vertexToOmit.y << endl;
+    vertexToOmit.x = -1;
+    vertexToOmit.y = -1;
+
+    cout << 1 << endl;
+    cout << numRectangles << endl;
+    for (const auto& rectangle : rectangles)
+    {
+        for (const auto& vertex : rectangle.vertices)
+        {
+            if (vertex.x == -1 && vertex.y == -1)
+                continue;
+            cout << vertex.x << " " << vertex.y << endl;
+        }
+    }
+}
+
