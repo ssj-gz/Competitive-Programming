7448f7639685c05818405def6485d0b8a7b7359d
Simon St James <kdedevel@etotheipiplusone.com>
2020-07-31 18:30:00 +0100

    ENTEXAM: Cleanup

--- codechef/entrance-exam.cpp
+++ codechef/entrance-exam.cpp
@@ -2,21 +2,11 @@
 // 
 // Solution to: https://www.codechef.com/problems/ENTEXAM
 //
-//#define SUBMISSION
-#ifdef SUBMISSION
-#define NDEBUG
-#else
-#define _GLIBCXX_DEBUG       // Iterator safety; out-of-bounds access for Containers, etc.
-#pragma GCC optimize "trapv" // abort() on (signed) integer overflow.
-#define BRUTE_FORCE
-#endif
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 #include <cassert>
-
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
 
 using namespace std;
 
@@ -32,48 +22,7 @@ T read()
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        const int T = rand() % 10 + 1;
-        cout << T << endl;
 
-        for (int t = 0; t < T; t++)
-        {
-            const auto numStudents = 2 + rand() % 100;
-            const auto maxEnrollableStudents = 1 + rand() % (numStudents - 1);
-            assert(maxEnrollableStudents < numStudents);
-            const auto numExams = 1 + rand() % 4;
-            const auto maxExamScore = 1 + rand() % 100;
-
-            cout << numStudents << " " << maxEnrollableStudents << " " << numExams << " " << maxExamScore << endl;
-
-            for (int otherStudentIndex = 0; otherStudentIndex < numStudents - 1; otherStudentIndex++)
-            {
-                for (int examIndex = 0; examIndex < numExams; examIndex++)
-                {
-                    const auto score = rand() % (maxExamScore + 1);
-                    cout << score;
-                    if (examIndex != numExams - 1)
-                        cout << " ";
-                }
-                cout << endl;
-            }
-            for (int examIndex = 0; examIndex < numExams - 1; examIndex++)
-            {
-                const auto score = rand() % (maxExamScore + 1);
-                cout << score;
-                if (examIndex != numExams - 1)
-                    cout << " ";
-            }
-
-        }
-
-        return 0;
-    }
-    
     const auto T = read<int>();
 
     for (int t = 0; t < T; t++)
@@ -106,12 +55,13 @@ int main(int argc, char* argv[])
 
         const int64_t requiredTotalScoreForEnrollment = otherStudentScoresSorted[numStudents - maxEnrollableStudents - 1] + 1;
 #ifdef DIAGNOSTICS
-        cout << "requiredTotalScoreForEnrollment: " << requiredTotalScoreForEnrollment << endl;
+        cout << "otherStudentScoresSorted: " << endl;
         for (const auto x  : otherStudentScoresSorted)
         {
             cout << x << " ";
         }
         cout << endl;
+        cout << "requiredTotalScoreForEnrollment: " << requiredTotalScoreForEnrollment << endl;
         cout << "sergeysScore: " << sergeysScore << endl;
 #endif
         if (requiredTotalScoreForEnrollment > sergeysScore + maxExamScore)
