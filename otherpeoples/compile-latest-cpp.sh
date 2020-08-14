5824460efdfba56971ee821396e6750f70d65005
Simon St James <kdedevel@etotheipiplusone.com>
2020-08-14 16:25:07 +0100

    Add my "compile-latest-cpp.sh" script
    This is for compiling a "hardened" version of the latest .cpp file in
    the current directory (extra debug flags, etc).  Can choose between gcc
    (default) and clang (by adding
    
    clang
    
    as an argument).
    
    Very useful for debugging other people's code in conjunction with codechef-download-solution.rb!

---
+++ otherpeoples/compile-latest-cpp.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ "$1" == "clang" ]; then
+    echo "clang"
+    export USE_CLANG=true
+fi
+
+export LATESTCPP=$(ls -1at *.cpp | head -n 1)
+echo "Compiling $LATESTCPP" 
+if [ -z "${USE_CLANG}" ]; then
+    set -x
+    g++ -std=c++14 $LATESTCPP -O3 -g3 -Wall -Wextra -Wconversion -DONLINE_JUDGE -D_GLIBCXX_DEBUG    -fsanitize=undefined -ftrapv
+    set +x
+else
+    set -x
+    clang++ -std=c++14 -stdlib=libc++ $LATESTCPP -O3 -g3 -Wall -Wextra -Wconversion -DONLINE_JUDGE -D_LIBCPP_DEBUG=1 -L/home/simon/tmp/DONTSYNC/clang-git/install/lib/ -fsanitize=undefined,bounds,integer -ftrapv
+    set +x
+fi
+
+if [ "$?" -eq "0" ]; then
+    echo "Successful"
+else
+    echo "Failed!!!!!"
+fi
