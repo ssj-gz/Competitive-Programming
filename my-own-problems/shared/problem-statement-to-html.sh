66ac9a392b410f4919d54cbcf5578331a85a8652
Simon St James <kdedevel@etotheipiplusone.com>
2020-08-15 08:07:29 +0100

    Change the name of the exported .html to "problem-statement.html"
    "simple.html" is a stupid name :)

--- my-own-problems/shared/problem-statement-to-html.sh
+++ my-own-problems/shared/problem-statement-to-html.sh
@@ -2,5 +2,4 @@
 
 sed 's/http:\/\/campus\.codechef\.com\/SITJMADM\/content\//images\//' problem-statement.md > problem-statement-with-local-image-urls.md
 
-pandoc  -t html5  -o simple.html problem-statement-with-local-image-urls.md --mathml --self-contained
-
+pandoc  -t html5  -o problem-statement.html problem-statement-with-local-image-urls.md --mathml --self-contained
