aafba9492d71d072ecb0dbf85833a28dac60d1fc
Simon St James <kdedevel@etotheipiplusone.com>
2020-08-16 18:00:01 +0100

    COUNTREE,PS: Add script for converting .odg to .png

---
+++ my-own-problems/counters-on-a-tree/markdown/images/convert-odg-to-png.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+ODG_FILENAME=$1
+PERCENTAGE_SIZE=24
+FILE_BASENAME=$(basename ${ODG_FILENAME} .odg)
+
+soffice --headless --convert-to pdf ${ODG_FILENAME}
+convert -density 400 ${FILE_BASENAME}.pdf -resize ${PERCENTAGE_SIZE}% ${FILE_BASENAME}.png
+rm ${FILE_BASENAME}.pdf
+
