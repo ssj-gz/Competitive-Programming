c023202cceb8d920574fb628657acea530402708
Simon St James <kdedevel@etotheipiplusone.com>
2020-08-20 09:19:55 +0100

    Make the "editorial to html&pdf" convert [details=XXX] to proper markdown
    Markdown does support the "expandable/ collapsible details", but the
    correct syntax is:
    
        <details>
        <summary>Label for the Expandable Section</summary>
        * Hidden markdown here
        </details>
    
    instead of the:
    
        [details="Label for the Expandable Section"]
        * Hidden markdown here
        [/details]
    
    used by Discourse.

--- my-own-problems/shared/editorial-to-html-and-pdf.sh
+++ my-own-problems/shared/editorial-to-html-and-pdf.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 sed 's/http:\/\/campus\.codechef\.com\/SITJMADM\/content\//images\//' editorial.md > editorial-with-local-image-urls.md
+sed -iE 's/\[details="\([^"]*\)"\]/<details><summary>\1<\/summary>/' editorial-with-local-image-urls.md
+sed -iE 's/\[\/details\]/<\/details>/' editorial-with-local-image-urls.md
 
 pandoc  -t html5  -o editorial.html editorial-with-local-image-urls.md --mathml --self-contained
 
