1738e8e5d8ff7446588f062bec2df4e7772158bf
Simon St James <kdedevel@etotheipiplusone.com>
2020-09-03 09:49:05 +0100

    Add lists_without_preceding_blankline flag when converting Problem Statements
    The Codechef Problem Statement markdown parser relies on this.  Without
    it:
    
    Subtasks:
    - blah
    - blah2
    
    would not render as a list when converted with pandoc, whereas it would
    with Codechef's markdown parser.
    
    Found this flag via:
    
        https://github.com/jgm/pandoc/wiki/Tabular-overview:-Pandoc%27s-supported-Markdown-dialects-and-extensions-enabled-by-default

--- my-own-problems/shared/problem-statement-to-html-and-pdf.sh
+++ my-own-problems/shared/problem-statement-to-html-and-pdf.sh
@@ -3,6 +3,6 @@
 sed 's/http:\/\/campus\.codechef\.com\/SITJMADM\/content\//images\//' problem-statement.md > problem-statement-with-local-image-urls.md
 sed -i -r 's/\(https:\/\/codechef_shared.s3.amazonaws.com\/download\/Images.*\/([^/]+)\)/(images\/\1)/' problem-statement-with-local-image-urls.md
 
-pandoc  -t html5  -o problem-statement.html problem-statement-with-local-image-urls.md --mathml --self-contained
+pandoc --read=markdown+lists_without_preceding_blankline  -t html5  -o problem-statement.html problem-statement-with-local-image-urls.md --mathml --self-contained
 
 wkhtmltopdf problem-statement.html problem-statement.pdf
