dfe1e27b8e673c8739a3724e887b7422ac8526a1
Simon St James <kdedevel@etotheipiplusone.com>
2020-09-04 16:43:30 +0100

    SIMKDOWN,DIAGNOSTIC: Simplify "update d:" output
    We don't need to spoon-feed people each step of the equation.

--- my-own-problems/simple-markdown-editor/simple-markdown-editor.cpp
+++ my-own-problems/simple-markdown-editor/simple-markdown-editor.cpp
@@ -504,7 +504,7 @@ int64_t solveBruteForce(const vector<Query>& queries, vector<string>& bruteForce
                     }
                     else
                         cout << "The queried position is part of a formatted range and the number of non-formatting characters is this range is " << queryAnswer << "; the answer to query #" << queryNum << " is $" << queryAnswer << "$." << endl;
-                    cout << "\nUpdate $d$: $d = d + " << queryAnswer << "\\times  2^{" << queryNum << "}" << "  = " << decryptionKey << " + " << queryAnswer << "\\times " << ((1 << queryNum))  << " = " << decryptionKey + ((1 << queryNum)) * queryAnswer << " \\mod 10^9+7 = " << ((decryptionKey + ((1 << queryNum)) * queryAnswer) % Mod) << "$.\n\n" << endl;
+                    cout << "\nUpdate $d$: $d = " << decryptionKey << " + " << queryAnswer << "\\times  2^{" << queryNum << "} \\mod 10^9+7 " << " = " << ((decryptionKey + ((1 << queryNum)) * queryAnswer) % Mod) << "$.\n\n" << endl;
 
 #endif
                     if (queryAnswer == -1)
