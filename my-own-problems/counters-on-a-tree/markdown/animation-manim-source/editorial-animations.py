25f49aa2d9e031551f45e6ac6d5b8a57272b77cc
Simon St James <kdedevel@etotheipiplusone.com>
2020-09-10 18:52:31 +0100

    COUNTREE,EDA: Preliminary work on right-to-left anim
    For now, just factor all the code out into
    
        do_collect_and_propagate_along_node_chain_naive
    
    helper.

--- my-own-problems/counters-on-a-tree/markdown/animation-manim-source/editorial-animations.py
+++ my-own-problems/counters-on-a-tree/markdown/animation-manim-source/editorial-animations.py
@@ -22,14 +22,11 @@ class DistTracker():
             result = result ^ dist
         return result
 
-class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right_naive(SSJGZScene):
-    def construct(self):
-        super().construct()
-
+def do_collect_and_propagate_along_node_chain_naive(scene, right_to_left = False):
         # Graph, coins, and the Grundy numbers beneath the coins.
         num_nodes = 8
-        frame_width = self.camera.get_frame_width()
-        frame_height = self.camera.get_frame_height()
+        frame_width = scene.camera.get_frame_width()
+        frame_height = scene.camera.get_frame_height()
 
         node_diameter_to_gap_ratio = 1.3
         proportion_of_frame_width_to_use = 0.9
@@ -42,7 +39,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
         arrow_dist_above_node = node_diameter / 5
 
         node_left = -frame_width * proportion_of_frame_width_to_use  / 2
-        g = Graph(self, globals()["Node"], globals()["NodeMObject"])
+        g = Graph(scene, globals()["Node"], globals()["NodeMObject"])
         previous_node = None
         nodes = []
         for i in range(0, num_nodes):
@@ -55,7 +52,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
             previous_node = newNode
             nodes.append(newNode)
 
-        self.play(g.create_animation(run_time = 2))
+        scene.play(g.create_animation(run_time = 2))
 
         def arrow_tip_position_for_node(node):
             arrow_tip = [node.config['center_x'], node.config['center_y'], 0] + (node_diameter * 2 / 3) * UP
@@ -68,7 +65,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
                 arrow_tip_base_width = node_diameter * 2 / 3,
                 arrow_tip_length = node_diameter / 2
                 )
-        self.play(GrowFromCenter(arrow))
+        scene.play(GrowFromCenter(arrow))
 
         coin_radius = node_radius / 2
 
@@ -83,7 +80,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
         coin_mobjects.append(create_coin_for_node(nodes[0], RED))
         coin_mobjects.append(create_coin_for_node(nodes[1], GREEN))
         coin_mobjects.append(create_coin_for_node(nodes[4], BLUE))
-        self.play(GrowFromCenter(coin_mobjects[0]),
+        scene.play(GrowFromCenter(coin_mobjects[0]),
                   GrowFromCenter(coin_mobjects[1]),
                   GrowFromCenter(coin_mobjects[2]))
 
@@ -99,7 +96,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
 
         grundy_tex_mobjects_appear_anims = list(map(lambda n: GrowFromCenter(n), grundy_tex_mobjects))
 
-        self.play(*grundy_tex_mobjects_appear_anims)
+        scene.play(*grundy_tex_mobjects_appear_anims)
 
         grundy_tex_mobjects_change_to_0_anims = []
         for grundy_tex in grundy_tex_mobjects:
@@ -107,7 +104,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
             target_0_grundy_tex.move_to(grundy_tex)
             grundy_tex_mobjects_change_to_0_anims.append(Transform(grundy_tex, target_0_grundy_tex))
 
-        self.play(*grundy_tex_mobjects_change_to_0_anims)
+        scene.play(*grundy_tex_mobjects_change_to_0_anims)
 
         # DistTracker display.
         disttracker_text_scale = 1.5
@@ -130,7 +127,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
         grundy_value_mobject.scale(disttracker_text_scale)
         grundy_value_mobject.next_to(grundy_number_label, RIGHT)
 
-        self.play(AnimationGroup(Write(disttracker_title_display), Write(grundy_number_label), Write(grundy_value_mobject)))
+        scene.play(AnimationGroup(Write(disttracker_title_display), Write(grundy_number_label), Write(grundy_value_mobject)))
 
         # Ok - move through the node chain!
         distTracker = DistTracker()
@@ -141,7 +138,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
             propagate_text.scale(disttracker_text_scale)
             propagate_text.align_on_border(RIGHT)
             propagate_text.set_y(disttracker_title_display.get_y())
-            self.play(FadeInFrom(propagate_text, DOWN))
+            scene.play(FadeInFrom(propagate_text, DOWN))
 
             grundy_value_to_move = grundy_value_mobject.copy()
             node_grundy = node.config['grundy_text']
@@ -154,7 +151,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
             equal_symbol = TexMobject(r'=', colour = BLACK, fill_opacity = 1, fill_color = BLACK)
             equal_symbol.next_to(grundy_value_destination)
 
-            self.play(
+            scene.play(
                       Transform(node_grundy, node_grundy_target),
                       Transform(grundy_value_to_move, grundy_value_destination),
                       FadeIn(xor_symbol),
@@ -162,15 +159,15 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
 
             new_grundy_tex = TexMobject(str(node.config['grundy_number'] ^ distTracker.grundyNumber()), colour = BLACK, fill_opacity = 1, fill_color = grundy_node_tex_colour)
             new_grundy_tex.next_to(equal_symbol, RIGHT)
-            self.play(FadeIn(new_grundy_tex))
+            scene.play(FadeIn(new_grundy_tex))
 
             node_grundy_to_fade_out = node_grundy.copy()
             node_grundy.become(new_grundy_tex)
-            self.remove(new_grundy_tex)
+            scene.remove(new_grundy_tex)
             node_grundy_target = node_grundy.copy()
             node_grundy_target.next_to(g.mobject_for_node[node], DOWN)
 
-            self.play(FadeOutAndShift(propagate_text, UP),
+            scene.play(FadeOutAndShift(propagate_text, UP),
                       FadeOut(node_grundy_to_fade_out),
                       FadeOut(grundy_value_to_move),
                       FadeOut(xor_symbol),
@@ -182,13 +179,13 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
             collect_text.scale(disttracker_text_scale)
             collect_text.align_on_border(RIGHT)
             collect_text.set_y(disttracker_title_display.get_y())
-            self.play(FadeInFrom(collect_text, DOWN))
+            scene.play(FadeInFrom(collect_text, DOWN))
 
             coin_mobject_for_node = node.config['coin_mobject']
             if not coin_mobject_for_node:
                 cross_out = Cross(collect_text)
-                self.play(Write(cross_out))
-                self.play(FadeOutAndShift(collect_text, UP),
+                scene.play(Write(cross_out))
+                scene.play(FadeOutAndShift(collect_text, UP),
                           FadeOutAndShift(cross_out, UP))
             else:
                 shift_elements_to_right_by = 0
@@ -237,10 +234,10 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
                 for shift_pair in shift_pairs:
                     animations.append(Transform(shift_pair[0], shift_pair[1]))
 
-                self.play(*animations)
+                scene.play(*animations)
 
-                self.remove(coin_copy)
-                self.add(new_tracked_distance_mobject)
+                scene.remove(coin_copy)
+                scene.add(new_tracked_distance_mobject)
                 tracked_distance_mobjects.append(new_tracked_distance_mobject)
 
                 distTracker.insertDist(0)
@@ -259,7 +256,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
                     plusOne.scale(disttracker_text_scale / 3)
                     plusOne.next_to(digitMObject, TOP / 5)
                     plusOne.shift((digitMObject.get_width() / 2, 0, 0))
-                    self.add(plusOne)
+                    scene.add(plusOne)
                     return plusOne
 
                 addPlusOneAnims = []
@@ -275,7 +272,7 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
 
                     fadePlusOneAnims.append(Transform(plusOne, fadedMovedPlusOne))
 
-                self.play(FadeInFrom(adjust_dists_text, DOWN),
+                scene.play(FadeInFrom(adjust_dists_text, DOWN),
                           LaggedStart(*addPlusOneAnims))
 
                 current_grundy_value = distTracker.grundyNumber()
@@ -296,7 +293,13 @@ class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right
                 for digit_mobject in tracked_distance_mobjects:
                     digit_mobject.digit = digit_mobject.digit + 1
                 
-                self.play(LaggedStart(*animations),
+                scene.play(LaggedStart(*animations),
                           LaggedStart(*fadePlusOneAnims))
 
+class MoveCoins2Editorial_1_collect_and_propagate_along_node_chain_left_to_right_naive(SSJGZScene):
+
+    def construct(self):
+        super().construct()
+        do_collect_and_propagate_along_node_chain_naive(self, right_to_left = False)
+        
 
