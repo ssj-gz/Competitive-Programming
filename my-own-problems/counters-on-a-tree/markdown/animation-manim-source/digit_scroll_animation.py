a537011f2a798377a0596880e79d2cd0e54118b8
Simon St James <kdedevel@etotheipiplusone.com>
2020-09-16 10:48:48 +0100

    COUNTREE,ED: Allow a digit to be moved horizontally while scrolling

--- my-own-problems/counters-on-a-tree/markdown/animation-manim-source/digit_scroll_animation.py
+++ my-own-problems/counters-on-a-tree/markdown/animation-manim-source/digit_scroll_animation.py
@@ -27,7 +27,7 @@ class DigitVGroup(VGroup):
 
 
 class DigitVGroupMoveAnim(Animation):
-    def __init__(self, digitVGroup, start_pos, end_pos, actual_center, dist_from_center_before_invisible, original_digitMobject, replacement_digitMobject, *args):
+    def __init__(self, digitVGroup, start_pos, end_pos, actual_center, dist_from_center_before_invisible, original_digitMobject, replacement_digitMobject, x_move_amount, *args):
         self.digitVGroup = digitVGroup
         self.start_pos = start_pos
         self.end_pos = end_pos
@@ -39,6 +39,8 @@ class DigitVGroupMoveAnim(Animation):
                         *args
                        )
         self.remover = True
+        self.x_move_amount = x_move_amount
+        print("x_move_amount:", x_move_amount)
 
     def interpolate_mobject(self, alpha):
        if alpha == 0:
@@ -47,7 +49,7 @@ class DigitVGroupMoveAnim(Animation):
             self.original_digitMobject.become(self.replacement_digitMobject)
             self.original_digitMobject.set_opacity(1)
 
-       self.digitVGroup.move_to(self.start_pos + (self.end_pos - self.start_pos) * (alpha))
+       self.digitVGroup.move_to(self.start_pos + (self.end_pos - self.start_pos) * (alpha) + self.x_move_amount * alpha * RIGHT)
        for digit,digitMObject in enumerate(self.digitVGroup.digits):
            digit_position = self.digitVGroup.get_center() + self.digitVGroup.initial_digit_pos[digit]
            dist_from_actual_center = abs(digit_position[1] - self.actual_center[1])
@@ -60,7 +62,7 @@ class DigitVGroupMoveAnim(Animation):
 
 # We can't detect the amount the initial_digitMObject is scaled by nor which digit it represents, so these need
 # to be passed in as digitMObjectScale and initial_digit, respectively.
-def create_scroll_digit_to_animation(initial_digitMObject, initial_digit, digit_to_scroll_to, digitMObjectScale = 1):
+def create_scroll_digit_to_animation(initial_digitMObject, initial_digit, digit_to_scroll_to, digitMObjectScale = 1, x_move_amount = 0, y_move_amount = 0):
     print("initial_digit:", initial_digit)
 
     digitVGroup = DigitVGroup(color = initial_digitMObject.get_color(), scale = digitMObjectScale)
@@ -72,12 +74,12 @@ def create_scroll_digit_to_animation(initial_digitMObject, initial_digit, digit_
     replacement_DigitMObject = TexMobject(str(digit_to_scroll_to))
     replacement_DigitMObject.scale(digitMObjectScale)
     replacement_DigitMObject.set_color(initial_digitMObject.get_color())
-    replacement_DigitMObject.move_to(initial_digitMObject.get_center())
+    replacement_DigitMObject.move_to(initial_digitMObject.get_center() + x_move_amount * RIGHT)
     replacement_DigitMObject.set_opacity(0)
 
     amount_to_scroll_by = (digitVGroup.digits[initial_digit].get_center() - digitVGroup.digits[digit_to_scroll_to].get_center())
     fully_opaque_at_pos = digitVGroup.get_center() + digitVGroup.initial_digit_pos[initial_digit]
     dist_from_center_before_invisible = digitVGroup.digits[digit_to_scroll_to].get_height()
-    return DigitVGroupMoveAnim(digitVGroup, digitVGroup.get_center(), digitVGroup.get_center() + amount_to_scroll_by, fully_opaque_at_pos, dist_from_center_before_invisible, initial_digitMObject, replacement_DigitMObject)
+    return DigitVGroupMoveAnim(digitVGroup, digitVGroup.get_center(), digitVGroup.get_center() + amount_to_scroll_by, fully_opaque_at_pos, dist_from_center_before_invisible, initial_digitMObject, replacement_DigitMObject, x_move_amount)
 
 
