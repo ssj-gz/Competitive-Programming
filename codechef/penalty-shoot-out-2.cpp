78b73d93d6e465f1b0eaab562a5568794cc29527
Simon St James <kdedevel@etotheipiplusone.com>
2021-04-25 18:21:03 +0100

    Clean up "PSHOT"

--- codechef/penalty-shoot-out-2.cpp
+++ codechef/penalty-shoot-out-2.cpp
@@ -5,8 +5,6 @@
 #include <iostream>
 #include <cassert>
 
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
-
 using namespace std;
 
 template <typename T>
@@ -61,30 +59,6 @@ int findFirstShotWhereResultIsKnown(int numShots, const string& shotResults)
 int main(int argc, char* argv[])
 {
     ios::sync_with_stdio(false);
-    if (argc == 2 && string(argv[1]) == "--test")
-    {
-        struct timeval time;
-        gettimeofday(&time,NULL);
-        srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
-        //const int T = rand() % 100 + 1;
-        const int T = 1;
-        cout << T << endl;
-
-        for (int t = 0; t < T; t++)
-        {
-            const int n = 1 + rand() % 20;
-            cout << n << endl;
-            string shotResults;
-            for (int i = 0; i < 2 * n; i++)
-            {
-                shotResults += (rand() % 2 == 0) ? '1' : '0';
-            }
-            cout << shotResults << endl;
-        }
-
-        return 0;
-    }
-
 
     const auto T = read<int>();
 
