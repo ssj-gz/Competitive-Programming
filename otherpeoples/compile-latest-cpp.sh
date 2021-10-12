ae5591c07b4e6fa72e98b10f4c2ff029eeb43489
Simon St James <kdedevel@etotheipiplusone.com>
2021-10-12 09:12:54 +0100

    compile-latest-cpp.sh: Fix bug (compile errors reported as Successful!)
    The call to:
    
        set +x
    
    clobbered the exit code value ($?) for the actual compilation and always
    set it to 0 ("Successful").
    
    Use a different approach from using "set +/-x": just form the command to
    run as a string; print that string; then execute it; no calls to "set
    +/-x" required.
    
    This fixes that bug and makes the output less noisy, too.

--- otherpeoples/compile-latest-cpp.sh
+++ otherpeoples/compile-latest-cpp.sh
@@ -8,16 +8,16 @@ fi
 export LATESTCPP=$(ls -1at *.cpp | head -n 1)
 echo "Compiling $LATESTCPP" 
 if [ -z "${USE_CLANG}" ]; then
-    set -x
-    g++ -std=c++17 $LATESTCPP -O3 -g3 -Wall -Wextra -Wconversion -DONLINE_JUDGE -D_GLIBCXX_DEBUG    -fsanitize=undefined -ftrapv 
-    set +x
+    COMMAND="g++ -std=c++17 $LATESTCPP -O3 -g3 -Wall -Wextra -Wconversion -DONLINE_JUDGE -D_GLIBCXX_DEBUG    -fsanitize=undefined -ftrapv"
 else
-    set -x
-    clang++ -std=c++17 -stdlib=libc++ $LATESTCPP -O3 -g3 -Wall -Wextra -Wconversion -DONLINE_JUDGE -D_LIBCPP_DEBUG=1 -L/home/simon/tmp/DONTSYNC/clang-git/install/lib/ -fsanitize=undefined,bounds,integer -ftrapv
-    set +x
+    COMMAND="clang++ -std=c++17 -stdlib=libc++ $LATESTCPP -O3 -g3 -Wall -Wextra -Wconversion -DONLINE_JUDGE -D_LIBCPP_DEBUG=1 -L/home/simon/tmp/DONTSYNC/clang-git/install/lib/ -fsanitize=undefined,bounds,integer -ftrapv"
 fi
 
-if [ "$?" -eq "0" ]; then
+echo -e "Executing command:\n  ${COMMAND}"
+$($COMMAND)
+export COMPILATION_STATUS=$?
+
+if [ "${COMPILATION_STATUS}" -eq "0" ]; then
     echo "Successful"
 else
     echo "Failed!!!!!"
