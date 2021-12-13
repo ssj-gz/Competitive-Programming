e72b4ec78052fe3cd0b76f83319c26025d14476b
Simon St James <kdedevel@etotheipiplusone.com>
2021-12-13 10:48:07 +0000

    Clean up AoC 2021 Day 13 Part 2

--- adventofcode/2021/day13/day13_part2_solution.cpp
+++ adventofcode/2021/day13/day13_part2_solution.cpp
@@ -17,7 +17,6 @@ vector<vector<char>> getFolded(const vector<vector<char>>& originalPaper,  FoldI
     vector<vector<char>> result;
     if (foldInstruction.foldDirection == FoldInstruction::RightToLeft)
     {
-        cout << "originalWidth: " << originalWidth << " foldAtCoord: " << foldInstruction.foldAtCoord << endl;
         assert(foldInstruction.foldAtCoord * 2 + 1 == originalWidth);
         const int foldedWidth = originalWidth / 2;
         result = vector<vector<char>>(foldedWidth, vector<char>(originalHeight, '.'));
@@ -25,7 +24,6 @@ vector<vector<char>> getFolded(const vector<vector<char>>& originalPaper,  FoldI
         {
             for (int y = 0; y < originalHeight; y++)
             {
-                cout << "x: " << x << y << " y: " << y << " originalWidth - x: " << (originalWidth - x - 1) << " originalWidth: " << originalWidth << endl;
                 if (originalPaper[x][y] == '#' || originalPaper[originalWidth - x - 1][y] == '#')
                 {
                     result[x][y] = '#';
@@ -36,7 +34,6 @@ vector<vector<char>> getFolded(const vector<vector<char>>& originalPaper,  FoldI
     }
     else if (foldInstruction.foldDirection == FoldInstruction::BottomToTop)
     {
-        cout << "originalHeight: " << originalHeight << " foldAtCoord: " << foldInstruction.foldAtCoord << endl;
         assert(foldInstruction.foldAtCoord * 2 + 1 == originalHeight);
         const int foldedHeight = originalHeight / 2;
         result = vector<vector<char>>(originalWidth, vector<char>(foldedHeight, '.'));
@@ -44,7 +41,6 @@ vector<vector<char>> getFolded(const vector<vector<char>>& originalPaper,  FoldI
         {
             for (int y = 0; y < foldedHeight; y++)
             {
-                cout << "x: " << x << y << " y: " << y << " originalHeight - y: " << (originalHeight - y) << " originalHeight: " << originalHeight << endl;
                 if (originalPaper[x][y] == '#' || originalPaper[x][originalHeight - y - 1] == '#')
                 {
                     result[x][y] = '#';
@@ -57,7 +53,6 @@ vector<vector<char>> getFolded(const vector<vector<char>>& originalPaper,  FoldI
 
 int main()
 {
-    string dotCoord;
     struct Coord
     {
         int x = -1;
@@ -66,15 +61,15 @@ int main()
     vector<Coord> dotCoords;
     int width = 0;
     int height = 0;
-    while (getline(cin, dotCoord))
+    string dotCoordLine;
+    while (getline(cin, dotCoordLine))
     {
-        if (dotCoord.empty())
+        if (dotCoordLine.empty())
             break;
 
         static regex dotCoordRegex(R"((\d+),(\d+)\s*)");
         std::smatch dotCoordMatch;
-        cout << "Line: " << dotCoord << endl;
-        if (regex_match(dotCoord, dotCoordMatch, dotCoordRegex))
+        if (regex_match(dotCoordLine, dotCoordMatch, dotCoordRegex))
         {
             const int x = stoi(dotCoordMatch[1]);
             const int y = stoi(dotCoordMatch[2]);
@@ -83,22 +78,20 @@ int main()
 
             width = max(width, x + 1);
             height = max(height, y + 1);
-
         }
         else
         {
             assert(false);
         }
     }
-    cout << "Dots: " << endl;
+
     vector<FoldInstruction> foldInstructions;
-    string foldInstruction;
-    while (getline(cin, foldInstruction))
+    string foldInstructionLine;
+    while (getline(cin, foldInstructionLine))
     {
         static regex foldInstructionRegex(R"(fold along (.)=(\d+)\s*)");
         std::smatch foldInstructionMatch;
-        cout << "Line: " << foldInstruction << endl;
-        if (regex_match(foldInstruction, foldInstructionMatch, foldInstructionRegex))
+        if (regex_match(foldInstructionLine, foldInstructionMatch, foldInstructionRegex))
         {
             foldInstructions.push_back({foldInstructionMatch[1] == "x"s ? FoldInstruction::RightToLeft : FoldInstruction::BottomToTop, stoi(foldInstructionMatch[2])});
         }
@@ -108,7 +101,6 @@ int main()
         }
     }
 
-    cout << "width: " << width << " height: " << height << endl;
     vector<vector<char>> paper(width, vector<char>(height, '.'));
     auto printPaper = [](const vector<vector<char>>& paper)
     {
@@ -126,7 +118,6 @@ int main()
     
     for (const auto dotCoord : dotCoords)
     {
-        cout << dotCoord.x << ", " << dotCoord.y << endl;
         paper[dotCoord.x][dotCoord.y] = '#';
     }
     printPaper(paper);
