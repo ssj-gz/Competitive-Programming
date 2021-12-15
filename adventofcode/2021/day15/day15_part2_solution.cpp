31497beb8c87736deb418710155103cfc91d59f6
Simon St James <kdedevel@etotheipiplusone.com>
2021-12-15 12:45:42 +0000

    Clean up AoC 2021 Day 15 Part 2

--- adventofcode/2021/day15/day15_part2_solution.cpp
+++ adventofcode/2021/day15/day15_part2_solution.cpp
@@ -19,7 +19,6 @@ int main()
 
     const int tileWidth = static_cast<int>(riskGridTile.front().size());
     const int tileHeight = static_cast<int>(riskGridTile.size());
-    cout << "tileWidth: " << tileWidth << " tileHeight: " << tileHeight << endl;
 
     const int tilesAcross = 5;
     const int tilesDown = 5;
@@ -33,14 +32,14 @@ int main()
         for (int tileCol = 0; tileCol < tilesAcross; tileCol++)
         {
             const int riskIncrease = tileRow + tileCol;
-            for (int row = 0; row < tileWidth; row++)
+            for (int rowInTile = 0; rowInTile < tileWidth; rowInTile++)
             {
-                for (int col = 0; col < tileHeight; col++)
+                for (int colInTile = 0; colInTile < tileHeight; colInTile++)
                 {
-                    int cellRisk = riskGridTile[row][col] + riskIncrease;
+                    int cellRisk = riskGridTile[rowInTile][colInTile] + riskIncrease;
                     while (cellRisk > 9)
                         cellRisk -= 9;
-                    riskGrid[tileRow * tileHeight + row][tileCol * tileWidth + col] = cellRisk;
+                    riskGrid[tileRow * tileHeight + rowInTile][tileCol * tileWidth + colInTile] = cellRisk;
                 }
             }
         }
@@ -57,33 +56,27 @@ int main()
     }
 
 
-    struct Coord
+    struct CoordInfo
     {
         int row = -1;
         int col = -1;
-    };
-    struct CoordInfo
-    {
-        Coord coord;
         int shortestTo = std::numeric_limits<int>::max();
     };
 
-    Coord start{0, 0};
-    Coord end{height - 1, width - 1};
+    CoordInfo start{0, 0, 0};
 
     vector<vector<int>> shortestPathLenTo(height, vector<int>(width, std::numeric_limits<int>::max()));
 
-    queue<CoordInfo> toExplore ( {{start, 0}} );
+    queue<CoordInfo> toExplore ( {start} );
 
     while (!toExplore.empty())
     {
         const auto coordInfo = toExplore.front();
         toExplore.pop();
 
-        if (shortestPathLenTo[coordInfo.coord.row][coordInfo.coord.col] > coordInfo.shortestTo)
+        if (shortestPathLenTo[coordInfo.row][coordInfo.col] > coordInfo.shortestTo)
         {
-            shortestPathLenTo[coordInfo.coord.row][coordInfo.coord.col] = coordInfo.shortestTo;
-            //cout << "Found new shortest path to: (" << coordInfo.coord.row << ", " << coordInfo.coord.col << "): " << coordInfo.shortestTo << endl;
+            shortestPathLenTo[coordInfo.row][coordInfo.col] = coordInfo.shortestTo;
             const pair<int, int> dxy[] = {
                 {-1, 0},
                 {0, -1},
@@ -92,16 +85,16 @@ int main()
             };
             for (const auto [dx, dy] : dxy)
             {
-                const int neighbourRow = coordInfo.coord.row - dy;
-                const int neighbourCol = coordInfo.coord.col - dx;
+                const int neighbourRow = coordInfo.row - dy;
+                const int neighbourCol = coordInfo.col - dx;
                 if (neighbourRow >= 0 && neighbourRow < height && neighbourCol >= 0 && neighbourCol < width)
                 {
-                    toExplore.push({{neighbourRow, neighbourCol}, coordInfo.shortestTo + riskGrid[neighbourRow][neighbourCol]});
+                    toExplore.push({neighbourRow, neighbourCol, coordInfo.shortestTo + riskGrid[neighbourRow][neighbourCol]});
                 }
             }
         }
     }
 
-    cout << shortestPathLenTo[end.row][end.col] << endl;
+    cout << shortestPathLenTo.back().back() << endl;
 
 }
