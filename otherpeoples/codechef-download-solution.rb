03df52fa14e3d1ed8c14325620e63399bc6f8ae2
Simon St James <kdedevel@etotheipiplusone.com>
2022-08-25 19:56:51 +0100

    codechef-download-solution.rb: Adapt to Codechef API changes
    Should work just as before, now.

--- otherpeoples/codechef-download-solution.rb
+++ otherpeoples/codechef-download-solution.rb
@@ -13,47 +13,57 @@ if ARGV.length != 1 && ARGV.length != 2
     usage_and_die()
 end
 
-SOLUTION_URL=ARGV[0]
+SOLUTION_URL_ARG=ARGV[0]
 SUFFIX= if ARGV.length > 1
             "-" + ARGV[1]
         else
             ""
         end
 
-if ! (SOLUTION_URL =~ /https:\/\/www.codechef.com\/viewsolution\/\d+$/)
+if ! (SOLUTION_URL_ARG =~ /https:\/\/www.codechef.com\/viewsolution\/(\d+$)/)
     usage_and_die()
 end
+SOLUTION_ID=$1.to_s
 
 STDOUT.sync = true
 
-html = Faraday.new(SOLUTION_URL, headers: { 'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'}).get.body
+# Get the source code, and the the language it was written in.
 
-html.each_line do |line| 
-    if (line =~ /var meta_info = (\{.*\});\s*$/)
-        meta_info = JSON.parse($1)
-        data = meta_info['data']
+SOLUTION_CODE_URL="https://www.codechef.com/api/submission-code/#{SOLUTION_ID}"
 
-        code = data['plaintext']
-        problemCode = data['problemCode']
-        userName = data['solutionOwnerHandle']
-        language = data['languageShortName']
-        languageExtension = data['languageExtension']
+code_json = Faraday.new(SOLUTION_CODE_URL, headers: { 'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'}).get.body
 
-        languageExtension = "cpp" if language == 'C' # Rename "*.c" to "*.cpp".
+code_info = JSON.parse(code_json)
+data = code_info['data']
+code = data['code']
+code.gsub!("\r", '')
 
-        filename = userName + "-" + problemCode + SUFFIX + "." + languageExtension
+language_info = data['language']
+languageExtension = language_info['extension']
+language = language_info['short_name']
+languageExtension = "cpp" if language == 'C' # Rename "*.c" to "*.cpp".
 
-        if language == 'PYTH 3.6'
-            File.open(filename, "w") { |file| file.print "#! /usr/bin/python3\n" + code }
-            FileUtils.chmod 0755, filename
-        else
-            File.open(filename, "w") { |file| file.print code }
-        end
+# Get the problem code (i.e. SHORTNAME) and the username of the submitter.
 
-        print "Solution written to: #{filename}\n"
+SUBMISSION_INFO_URL="https://www.codechef.com/api/submission-details/#{SOLUTION_ID}"
 
-        break
-    end
+submission_info_json = Faraday.new(SUBMISSION_INFO_URL, headers: { 'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'}).get.body
+
+submission_info = JSON.parse(submission_info_json)['data']['other_details']
+problemCode = submission_info['problemCode']
+userName = submission_info['submissionOwnerHandle']
+
+# Write source code to appropriately-named file.
+
+filename = userName + "-" + problemCode + SUFFIX + "." + languageExtension
+if language == 'PYTH 3'
+    File.open(filename, "wb") { |file| file.print "#! /usr/bin/python3\n" + code }
+    FileUtils.chmod 0755, filename
+else
+    File.open(filename, "wb") { |file| file.print code }
 end
+
+print "Solution written to: #{filename}\n"
+
 
 
