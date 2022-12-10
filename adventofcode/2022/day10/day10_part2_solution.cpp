157a9b3703f6a729701363faee0a6d56ef8b922c
Simon St James <kdedevel@etotheipiplusone.com>
2022-12-10 18:06:09 +0000

    AoC22D10P2 - REFACTOR - introduce CycleSimulator class and port to it
    This pulls all the "cycle"/ "register" logic into a separate class,
    which can ultimately be shared with Part 1, too.

--- adventofcode/2022/day10/day10_part2_solution.cpp
+++ adventofcode/2022/day10/day10_part2_solution.cpp
@@ -4,27 +4,79 @@
 
 using namespace std;
 
-int main()
+class CycleSimulator
 {
-    int64_t x = 1;
-    int currentCycleNum = 1;
-    bool isMidwayThroughAdd = false;
-    int64_t numberToAdd = 0;
-    int64_t totalSignalStrength = 0;
-
-    string currentCrtRowPixels;
-    int currentCrlCol = 0; // 0-relative.
-    while (cin || isMidwayThroughAdd)
-    {
-        const bool wasMidwayThroughAdd = isMidwayThroughAdd;
-        if (isMidwayThroughAdd)
+    public:
+        CycleSimulator(istream& instructionsStream)
+            : m_instructionsStream(instructionsStream)
         {
-            isMidwayThroughAdd = false;
+            readAndProcessNextInstruction();
         }
-        else
+
+        int currentCycleNum() const
+        {
+            return m_currentCycleNum;
+        }
+
+        int64_t registerValueDuringCurrentCycle() const
+        {
+            return m_registerValue;
+        }
+
+        bool isFinished() const
+        {
+            return !(m_instructionsStream || m_isProcessingAdd);
+        }
+
+        void computeNextCycle()
+        {
+            if (isFinished())
+            {
+                m_currentCycleNum++;
+                return;
+            }
+
+            // Finish off previous cycle.
+            bool isReadyForNextInstruction = true;
+            if (m_isProcessingAdd)
+            {
+                if ( m_currentCycleNum == m_cycleNumAfterWhichAddFinished )
+                {
+                    m_registerValue += m_numberToAdd;
+                    std::cout << "added " << m_numberToAdd << " to register after cycle: " << m_currentCycleNum << " to give: " << m_registerValue << std::endl;
+
+                    m_isProcessingAdd = false;
+                    m_cycleNumAfterWhichAddFinished = -1;
+                    m_numberToAdd = -1;
+
+                }
+                else
+                {
+                    isReadyForNextInstruction = false;
+                }
+            }
+
+            m_currentCycleNum++;
+            if (isReadyForNextInstruction)
+                readAndProcessNextInstruction();
+        }
+
+
+    private:
+        istream& m_instructionsStream;
+
+        int64_t m_registerValue = 1;
+
+        int m_currentCycleNum = 1;
+
+        bool m_isProcessingAdd = false;
+        int m_cycleNumAfterWhichAddFinished = -1;
+        int64_t m_numberToAdd = 0;
+
+        void readAndProcessNextInstruction()
         {
             string instructionLine;
-            getline(cin, instructionLine);
+            getline(m_instructionsStream, instructionLine);
             if (!instructionLine.empty())
             {
                 istringstream instructionLineStream(instructionLine);
@@ -38,45 +90,45 @@ int main()
                 }
                 else if (command == "addx")
                 {
-                    isMidwayThroughAdd = true;
-                    instructionLineStream >> numberToAdd;
-                    cout << "Need to add " << numberToAdd << std::endl;
+                    m_isProcessingAdd = true;
+                    instructionLineStream >> m_numberToAdd;
+                    cout << "Need to add " << m_numberToAdd << std::endl;
                     assert(instructionLineStream);
+                    m_cycleNumAfterWhichAddFinished = m_currentCycleNum + 1;
                 }
                 else
                 {
                     assert(false);
                 }
             }
+            std::cout << "instructionLine: " << instructionLine << " m_instructionsStream: " << static_cast<bool>(m_instructionsStream) << std::endl;
         }
 
-        
-        // About to finish cycle.
-        std::cout << "currentCrlCol: " << currentCrlCol << " x: " << x << endl;
-        if (abs(currentCrlCol - x) <= 1)
+};
+
+int main()
+{
+
+    string currentCrtRowPixels;
+    int currentCrlCol = 0; // 0-relative.
+
+    CycleSimulator cycleSimulator(cin);
+
+    while (!cycleSimulator.isFinished())
+    {
+        if (abs(currentCrlCol - cycleSimulator.registerValueDuringCurrentCycle()) <= 1)
             currentCrtRowPixels += "#";
         else
             currentCrtRowPixels += ".";
 
-        // Finished cycle.
-        if (wasMidwayThroughAdd)
-        {
-            x += numberToAdd;
-        }
-        std::cout << "At end of cycle " << currentCycleNum << " x: " << x << " wasMidwayThroughAdd: " << wasMidwayThroughAdd << " isMidwayThroughAdd: " << isMidwayThroughAdd << endl;
-        // Begin next cycle.
-
         currentCrlCol++;
         if (currentCrlCol == 40)
         {
-        currentCrlCol = 0;
-        std::cout << "Row: " << currentCrtRowPixels << std::endl;
-        currentCrtRowPixels.clear();
+            currentCrlCol = 0;
+            std::cout << "Row: " << currentCrtRowPixels << std::endl;
+            currentCrtRowPixels.clear();
         }
 
-        currentCycleNum++;
+        cycleSimulator.computeNextCycle();
     }
-
-    std::cout << "totalSignalStrength: " <<totalSignalStrength << std::endl;
-
 }
