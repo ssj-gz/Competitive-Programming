66f078983c515ba3b2541c896dbb3cbc31a8d077
Simon St James <kdedevel@etotheipiplusone.com>
2024-05-29 17:48:04 +0100

    AOC19D25P1: clean up a little

--- adventofcode/2019/day25/day25_part1_solution.cpp
+++ adventofcode/2019/day25/day25_part1_solution.cpp
@@ -19,22 +19,17 @@ struct RoomInfo
 
 void inputAsciiToIntCode(IntCodeComputer& intCodeComputer, const std::string& inputLine)
 {
-    std::cout << "Telling IntCode: >" << inputLine << "<" << std::endl;
     vector<int64_t> input;
     for (const char letter : inputLine)
         input.push_back(static_cast<int64_t>(letter));
     input.push_back(10);
     intCodeComputer.addInputs(input);
-    const auto status = intCodeComputer.run();
-    //assert(status != IntCodeComputer::Terminated);
-
 };
 
 vector<string> readAsciiOutput(IntCodeComputer& intCodeComputer)
 {
     intCodeComputer.run();
     const auto output = intCodeComputer.takeOutput();
-    //assert(!output.empty());
     vector<string> lines;
     string currentLine;
     for (const auto letter : output)
@@ -48,7 +43,6 @@ vector<string> readAsciiOutput(IntCodeComputer& intCodeComputer)
         {
             currentLine.push_back(static_cast<char>(letter));
         }
-        //cout << static_cast<char>(letter);
     }
     lines.push_back(currentLine);
 
@@ -137,24 +131,14 @@ string oppositeDirection(const std::string& direction)
 
 void explore(IntCodeComputer& intCodeComputer, set<string>& roomsSeen, const vector<string>& directionsFollowed, int depth, map<string, vector<string>>& directionsToRoom)
 {
-    const std::string indent(depth, ' ');
-
-    auto printLines = [&indent](const vector<string>& lines)
-    {
-        for (const auto& line : lines) cout << indent << line << std::endl;
-    };
-
-
     const std::string directionFollowed = (directionsFollowed.empty() ? "" : directionsFollowed.back());
-    std::cout << indent << "Explore: depth " << depth << " directionFollowed:" << directionFollowed << std::endl;
-    const auto roomDescription = readAsciiOutput(intCodeComputer);
-    std::cout << indent << "roomDescription: " << std::endl;
-    printLines(roomDescription);
-    const RoomInfo roomInfo = parseRoomInfo(roomDescription);
+    const auto roomDescriptionText = readAsciiOutput(intCodeComputer);
+    const RoomInfo roomInfo = parseRoomInfo(roomDescriptionText);
 
     if (roomsSeen.contains(roomInfo.roomName))
     {
-        std::cout << indent << "Already been to " << roomInfo.roomName << "; backtracking.  directionFollowed: " << directionFollowed << std::endl;
+        // Already explored this room; backtrack to previous room by following the opposite direction to the one that brought
+        // us here.
         const string oppositeDir = oppositeDirection(directionFollowed);
         assert(std::find(roomInfo.directions.begin(), roomInfo.directions.end(), oppositeDir) != roomInfo.directions.end());
         inputAsciiToIntCode(intCodeComputer, oppositeDir);
@@ -168,34 +152,33 @@ void explore(IntCodeComputer& intCodeComputer, set<string>& roomsSeen, const vec
     for (const auto& item : roomInfo.items)
     {
         if (item == "infinite loop" || item == "molten lava" || item == "photons" || item == "giant electromagnet" || item == "escape pod")
+        {
+            // Skip the "deadly" items that I found by trial and error, Groundhog-day style!
             continue;
+        }
         inputAsciiToIntCode(intCodeComputer, "take " + item);
-        printLines(readAsciiOutput(intCodeComputer));
-        inputAsciiToIntCode(intCodeComputer, "inv");
-        printLines(readAsciiOutput(intCodeComputer));
+        readAsciiOutput(intCodeComputer); // Swallow output.
 
     }
 
+    // Explore further, but don't go back the way we came until 
+    // we've exhausted all other directions.
     for (const auto& direction : roomInfo.directions)
     {
         if (!directionFollowed.empty() && direction == oppositeDirection(directionFollowed))
             continue;
-        std::cout << indent << "Heading: >" << direction << "<" << std::endl;
         inputAsciiToIntCode(intCodeComputer, direction);
         vector<string> newDirectionFollowed = directionsFollowed;
         newDirectionFollowed.push_back(direction);
         explore(intCodeComputer, roomsSeen, newDirectionFollowed, depth + 1, directionsToRoom);
     }
 
-    std::cout << indent << "nothing more to do here; backtracking" << std::endl;
     if (depth != 0)
     {
+        // We've explored this room (and the rooms it leads to); go back the way we came.
         inputAsciiToIntCode(intCodeComputer, oppositeDirection(directionFollowed));
         readAsciiOutput(intCodeComputer); // Swallow output.
     }
-
-
-
 }
 
 void generateItemCombinations(const set<string>& allItems, set<string>::const_iterator itemsIter, set<string>& currentCombination, vector<set<string>>& itemsCombinations)
@@ -235,36 +218,6 @@ int main()
     const auto status = intCodeComputer.run();
     assert(status == IntCodeComputer::WaitingForInput);
 
-    //const auto lines = readAsciiOutput();
-    //printLines(lines);
-
-#if 0
-    string command;
-    while (std::getline(cin, command))
-    {
-        inputAsciiToIntCode(command);
-        const auto lines = readAsciiOutput();
-        std::cout << "IntCode response: " << std::endl;
-        printLines(lines);
-        std::cout << "End IntCode response" << std::endl;
-
-        RoomInfo roomInfo = parseRoomInfo(lines);
-
-        assert(!roomInfo.roomName.empty());
-        std::cout << "roomName: " << roomInfo.roomName << std::endl;
-        std::cout << "directions: " << std::endl;
-        for (const auto& direction : roomInfo.directions)
-        {
-            std::cout << " > " << direction << std::endl;
-        }
-        std::cout << "items: " << std::endl;
-        for (const auto& item : roomInfo.items)
-        {
-            std::cout << " > " << item << std::endl;
-        }
-
-    }
-#endif
     set<string> roomsSeen;
     map<string, vector<string>> directionsToRoom;
     vector<string> directionsFollowed;
