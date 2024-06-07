26a7a26adf3b8f4ff58060e76db9696a5d157c4d
Simon St James <kdedevel@etotheipiplusone.com>
2024-06-07 15:54:33 +0100

    AoC22D17P2: clean up a little

--- adventofcode/2022/day17/day17_part2_solution.cpp
+++ adventofcode/2022/day17/day17_part2_solution.cpp
@@ -48,18 +48,12 @@ int main()
     int jetStreamIndex = 0;
     int nextShapeIndex = 0;
 
-
     int numRocksLanded = 0;
     int longestFallCheck = -1;
     constexpr int longestFall = 42; // Empiricially derived.
     map<string, int> lastOccurenceOfTopPattern;
-    bool foundRepetition = false;
-    //const auto lcmThing = std::lcm(shapes.size(), jetBlastPatterns.size());
-    //const auto lcmThing = 280;
-    //const auto lcmThing = 35;
-    //std::cout << "lcmThing: " << lcmThing << std::endl;
     std::map<int, int> heightAfterNumRocks;
-    int potentialRepetitionAfterRocks = -1;
+    int potentialCycleAfterRocks = -1;
     while (true)
     {
         // Next shape appears.
@@ -69,57 +63,11 @@ int main()
         int shapeLeftX = 2;
         int shapeTopY = playArea.size() + 3 + shape.size() - 1;
         const int initialShapeTopY = shapeTopY;
-        //std::cout << "new shape appears" << std::endl;
-        //std::cout << "shapeLeftX: " << shapeLeftX << " shapeTopY: " << shapeTopY << " shapeWidth: " << shapeWidth << " shapeHeight: " << shapeHeight << std::endl;
 
-        auto drawPlayArea = [&]()
-        {
-            //return;
-            //std::cout << "drawPlayArea: shapeLeftX: " << shapeLeftX << " shapeTopY: " << shapeTopY << std::endl;
-            vector<string> playAreaView(max<int>(shapeTopY + 1, playArea.size() + 1), string(playAreaWidth, '.'));
-            for (int y = 0; y < playArea.size(); y++)
-            {
-                for (int x = 0; x < playAreaWidth; x++)
-                {
-                    playAreaView[y][x] = playArea[y][x];
-                }
-            }
-            for (int x = 0; x < shapeWidth; x++)
-            {
-                for (int y = 0; y < shapeHeight; y++)
-                {
-                    //std::cout << "y: " << y << std::endl;
-                    const int xInPlayAreaView = x + shapeLeftX;
-                    const int yInPlayAreaView = shapeTopY - y;
-                    //std::cout << "yInPlayAreaView: " << y + yInPlayAreaView << std::endl;
-
-                    assert(0 <= yInPlayAreaView);
-                    assert(yInPlayAreaView < playAreaView.size());
-                    assert(0 <= xInPlayAreaView);
-                    assert(xInPlayAreaView < playAreaView.front().size());
-
-                    if (shape[y][x] == '#')
-                        playAreaView[yInPlayAreaView][xInPlayAreaView] = '@';
-                    //else
-                        //playAreaView[yInPlayAreaView][xInPlayAreaView] = '.';
-
-                }
-            }
-            std::reverse(playAreaView.begin(), playAreaView.end());
-            std::cout << "Current: " << std::endl;
-            for (const auto& row : playAreaView)
-            {
-                std::cout << row << std::endl;
-            }
-        };
-
-        //std::cout << "Shape appears" << std::endl;
-        //drawPlayArea();
         while (true)
         {
             const int origShapeLeftX = shapeLeftX;
 
-            //std::cout << "Jet attempts to blow to " << jetBlastPatterns[jetStreamIndex] << std::endl;
             if (jetBlastPatterns[jetStreamIndex] == '<')
             {
                 shapeLeftX--;
@@ -137,7 +85,6 @@ int main()
                 {
                     for (int y = 0; y < shapeHeight; y++)
                     {
-                        //std::cout << "y: " << y << std::endl;
                         const int xInPlayAreaView = x + shapeLeftX;
                         const int yInPlayAreaView = shapeTopY - y;
                         if (xInPlayAreaView < 0 || xInPlayAreaView >= playAreaWidth)
@@ -160,24 +107,18 @@ int main()
             };
             if (!isShapePosValid())
             {
-                //std::cout << " stream didn't push us" << std::endl;
                 shapeLeftX = origShapeLeftX;
             }
-            //drawPlayArea();
-            //std::cout << "Attempting to descend" << std::endl;
             const int origShapeTopY = shapeTopY;
             shapeTopY--;
             if (!isShapePosValid())
             {
-                //std::cout << " couldn't settle" << std::endl;
                 shapeTopY = origShapeTopY;
                 // Inscribe in play area.
-                //std::cout << "shapeTopY: " << shapeTopY << std::endl;
                 for (int x = 0; x < shapeWidth; x++)
                 {
                     for (int y = 0; y < shapeHeight; y++)
                     {
-                        //std::cout << "y: " << y << std::endl;
                         const int xInPlayArea = x + shapeLeftX;
                         const int yInPlayArea = shapeTopY - y;
                         while (yInPlayArea >= playArea.size())
@@ -187,23 +128,25 @@ int main()
 
                     }
                 }
-                //std::cout << "Inscribed shape" << std::endl;
-                //drawPlayArea();
                 numRocksLanded++;
                 heightAfterNumRocks[numRocksLanded] = playArea.size() - 1;
-#if 1
-                const int distFell = (initialShapeTopY - shapeTopY);
-                //std::cout << "Fell: " << distFell << std::endl; 
-                if (longestFallCheck < distFell)
+
                 {
-                    longestFallCheck = distFell;
-                    std::cout << "new longestFallCheck: " << longestFallCheck << std::endl;
+                    // This was the code used to create longestFall; we now use it
+                    // to check that longestFall is correct.
+                    const int distFell = (initialShapeTopY - shapeTopY);
+                    if (longestFallCheck < distFell)
+                    {
+                        longestFallCheck = distFell;
+                        std::cout << "new longestFallCheck: " << longestFallCheck << std::endl;
+                    }
+                    assert(distFell <= longestFall);
                 }
-                assert(distFell <= longestFall);
-#endif
-#if 1
-                if (potentialRepetitionAfterRocks == -1 && playArea.size() - 1 >= longestFall)
+
+                if (potentialCycleAfterRocks == -1 && playArea.size() - 1 >= longestFall)
                 {
+                    // Calculate a string representing the top longestFall rows of the current play area,
+                    // and how many rocks ago we last saw it (if at all).
                     std::string topPattern;
                     auto rowIter = playArea.rbegin();
                     for (int i = 0; i < longestFall; i++)
@@ -213,66 +156,45 @@ int main()
                     }
                     if (lastOccurenceOfTopPattern.contains(topPattern))
                     {
-                        const int diff = (numRocksLanded - lastOccurenceOfTopPattern[topPattern]);
-                        std::cout << "Woohoo; numRocksLanded: " << numRocksLanded << " last occurence: " << lastOccurenceOfTopPattern[topPattern] << " diff: " << diff << std::endl;
-                        //assert(numRocksLanded - lastOccurenceOfTopPattern[topPattern] == 35);
-                        potentialRepetitionAfterRocks = std::lcm(shapes.size(), lcm(diff, jetBlastPatterns.size()));
-                        //potentialRepetitionAfterRocks = lcm(shapes.size(), lcm(diff, jetBlastPatterns.size()));
-                        std::cout << "potentialRepetitionAfterRocks: " << potentialRepetitionAfterRocks << std::endl;
-                        foundRepetition =  true;
-#if 0
-                        std::sting printTopPatterns = topPattern;
-                        while (!printTopPatterns.empty())
-                        {
-                            assert(printTopPatterns.size() >= playAreaWidth);
-                            std::cout << printTopPatterns.substr(0, playAreaWidth) << std::endl;
-                            printTopPatterns = topPattern.substr(playAreaWidth);
-                        }
-#endif
-                        std::cout << "Play area: " << std::endl;
-                        //drawPlayArea();
-                        //return 0;
-                    }
-                    else
-                    {
-                        assert(!foundRepetition);
+                        const int numRocksLandedSinceRepeat = (numRocksLanded - lastOccurenceOfTopPattern[topPattern]);
+                        // For a true cycle, we require all of:
+                        //    * # shapes
+                        //    * # jet blast patterns
+                        //    * this the number of rocks landed since we last saw this topPattern
+                        // all to coincide i.e. find the lcm of these three values.
+                        potentialCycleAfterRocks = std::lcm(shapes.size(), lcm(numRocksLandedSinceRepeat, jetBlastPatterns.size()));
                     }
                     lastOccurenceOfTopPattern[topPattern] = numRocksLanded;
-                    std::cout << "# lastOccurenceOfTopPattern: " << lastOccurenceOfTopPattern.size() << std::endl;
                 }
-#endif
 
-#if 1
-                if (potentialRepetitionAfterRocks != -1 && ((numRocksLanded % potentialRepetitionAfterRocks) == 0))
+                if (potentialCycleAfterRocks != -1 && ((numRocksLanded % potentialCycleAfterRocks) == 0))
                 {
+                    // Check for a cycle ending at the top of the current/ playArea, checking back <some multiple of>
+                    // potentialCycleAfterRocks rocks ago.
                     int multiplier = 1;
                     while (true)
                     {
-                        int64_t numRocksAgo = potentialRepetitionAfterRocks * multiplier;
+                        int64_t numRocksAgo = potentialCycleAfterRocks * multiplier;
                         if (numRocksAgo >= numRocksLanded)
                             break;
-                        int64_t blockSize = heightAfterNumRocks[numRocksLanded] - heightAfterNumRocks[numRocksLanded - numRocksAgo];
-                        std::cout << "blockSize: " << blockSize << " numRocksLanded: " << numRocksLanded << " numRocksAgo: " << numRocksAgo << std::endl;
+                        const int64_t numRowsToCheck = heightAfterNumRocks[numRocksLanded] - heightAfterNumRocks[numRocksLanded - numRocksAgo];
 
-                        assert(playArea.size() >= blockSize);
+                        assert(playArea.size() >= numRowsToCheck);
                         int recent = playArea.size() - 1;
-                        int earlier = recent - blockSize;
-                        bool match = true;
-                        for (int i = 0; i < blockSize; i++)
+                        int earlier = recent - numRowsToCheck;
+                        bool cycleDetected = true;
+                        for (int i = 0; i < numRowsToCheck; i++)
                         {
-                            //std::cout << "recent:  " << playArea[recent] << std::endl;
-                            //std::cout << "earlier: " << playArea[earlier] << std::endl;
                             if (playArea[recent] != playArea[earlier])
                             {
-                                match = false;
-                                std::cout << "mismatch after: " << i << std::endl;
+                                cycleDetected = false;
                                 break;
                             }
                             recent--;
                             earlier--;
 
                         }
-                        if (match)
+                        if (cycleDetected)
                         {
                             // We've found our cycle! Use this to quickly figure out the result of applying the remaining
                             // required rock landings.
@@ -295,38 +217,12 @@ int main()
 
 
                 }
-#endif
 
                 break;
             }
-            //std::cout << "Descent succeeded" << std::endl;
-            //drawPlayArea();
         }
-#if 0
-        if (playArea.back() == "#######")
-        {
-            break;
-        }
-#endif
-        if (numRocksLanded % 100'000 == 0)
-            std::cout << "numRocksLanded % 100'000: " << numRocksLanded << std::endl;
-        //if (numRocksLanded > 1'000'000)
-            //break;
-#if 0
-        if (numRocksLanded == 2022)
-        {
-            std::cout << "answer: " << playArea.size() - 1 /* Ignore "------" */ << std::endl;
-            break;
-        }
-#endif
-
 
         nextShapeIndex = (nextShapeIndex + 1) % shapes.size();
-        if (nextShapeIndex == 0)
-        {
-            
-
-        }
     }
 
 }
