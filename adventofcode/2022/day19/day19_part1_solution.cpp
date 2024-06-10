cbe8c209dc4b4082715f52c1c03772a23d0ca80f
Simon St James <kdedevel@etotheipiplusone.com>
2024-06-10 11:23:34 +0100

    AoC22D19P1: optimise - prune states if we don't think they can win
    Uses an approximation function, estimateMaxPossibleExtraGeodes, to
    (over)estimate the maximum number of possible geodes the current state can
    reach.  This will hopefully never been less than the real maximum.
    
    estimateMaxPossibleExtraGeodes uses a hugely simplified simulation (plus
    memoisation) to quickly produce this estimate.
    
    With new this optimisation, Part 1 now takes approx ~2.5 seconds on my
    UM690.

--- adventofcode/2022/day19/day19_part1_solution.cpp
+++ adventofcode/2022/day19/day19_part1_solution.cpp
@@ -15,6 +15,7 @@ std::string_view resourceTypeNames[] = { "Ore", "Clay", "Obsidian", "Geode" };
 struct Blueprint
 {
     std::array<int64_t, numResourceTypes> costOfRobot[numResourceTypes] = {};
+    int blueprintNumber = -1;
 };
 
 struct State
@@ -47,8 +48,39 @@ ostream& operator<<(ostream& os, const State& state)
     return os;
 };
 
+int64_t estimateMaxPossibleExtraGeodes(int initialNumObsidianRobots, int initialObsidian, int initialNumGeodeRobots, int timeRemaining, const Blueprint& bluePrint)
+{
+    if (timeRemaining == -1)
+        return 0;
+    static std::map<std::tuple<int, int, int, int, int>, int> lookup;
+    std::tuple<int, int, int, int, int> paramsAsTuple = { initialNumObsidianRobots, initialObsidian, initialNumGeodeRobots, timeRemaining, bluePrint.blueprintNumber };
+    if (lookup.contains(paramsAsTuple))
+        return lookup[paramsAsTuple];
+
+    const int64_t numNewGeodes = initialNumGeodeRobots;
+    // Assume we can buy an Obsidian robot every minute.
+    const int64_t extraFromBuyingObsidiean = estimateMaxPossibleExtraGeodes(initialNumObsidianRobots + 1, initialObsidian + initialNumObsidianRobots, initialNumGeodeRobots, timeRemaining - 1, bluePrint);
+    int64_t extraFromBuyingGeode = 0;
+    if (initialObsidian >= bluePrint.costOfRobot[Geode][Obsidian])
+    {
+        //std::cout << "timeRemaining: " << timeRemaining << " can buy extra geode robot!" << std::endl;
+        extraFromBuyingGeode = estimateMaxPossibleExtraGeodes(initialNumObsidianRobots, initialObsidian - bluePrint.costOfRobot[Geode][Obsidian] + initialNumObsidianRobots, initialNumGeodeRobots + 1, timeRemaining - 1, bluePrint);
+        //std::cout << " extraFromBuyingGeode: " <<  extraFromBuyingGeode  << std::endl;
+    }
+    else
+    {
+        //std::cout << "timeRemaining: " << timeRemaining << " CAN'T buy extra geode robot - costs " << bluePrint.costOfRobot[Geode][Obsidian] << " but only have: " << initialObsidian << std::endl;
+    }
+
+    int64_t result = numNewGeodes + std::max(extraFromBuyingGeode, extraFromBuyingObsidiean);
+    lookup[paramsAsTuple] = result;
+    return result;
+}
+
 int main()
 {
+
+
     vector<Blueprint> blueprints;
     string blueprintInfoLine;
     std::regex blueprintInfoRegex("^Blueprint\\s*(\\d+):\\sEach ore robot costs (\\d+) ore.\\s*Each clay robot costs (\\d+) ore.\\s*Each obsidian robot costs (\\d+) ore and (\\d+) clay.\\s*Each geode robot costs (\\d+) ore and (\\d+) obsidian\\.?\\s*$");
@@ -63,16 +95,18 @@ int main()
         blueprintInfo.costOfRobot[Clay] = { std::stoll(blueprintInfoMatch[3]), 0, 0, 0 };
         blueprintInfo.costOfRobot[Obsidian] = { std::stoll(blueprintInfoMatch[4]), std::stoll(blueprintInfoMatch[5]), 0, 0 };
         blueprintInfo.costOfRobot[Geode] = { std::stoll(blueprintInfoMatch[6]), 0, std::stoll(blueprintInfoMatch[7]), 0 };
+        blueprintInfo.blueprintNumber = blueprints.size() + 1;
 
         blueprints.push_back(blueprintInfo);
     }
+    // blueprint: 1time: 22 mostGeodes: 5 maxPossibleGeodes: 0 minCompetitorGeodes: 16 pruning state: State: amount of:  Ore = 4 Clay = 19 Obsidian = 11 Geode = 0 amount of robot:  Ore = 2 Clay = 5 Obsidian = 3 Geode = 0
+    std::cout << "blah: " << estimateMaxPossibleExtraGeodes(3, 11, 0, 2, blueprints[0]) << std::endl;
+    //return 0;
 
     int64_t qualitySum = 0;
-    int blueprintNumber = 1;
     constexpr int timeLimit = 24;
     for (const auto& bluePrint : blueprints)
     {
-        std::cout << "Here" << std::endl;
         int64_t maxAmountOfResourceToBuyRobot[numResourceTypes] = {};
             for (int resourceType = 0; resourceType < numResourceTypes; resourceType++)
         {
@@ -95,6 +129,7 @@ int main()
         set<State> seenStates;
         int time = 0;
         int64_t mostGeodes = 0;
+        int64_t earliestTimeOfMostGeodes = -1;
         while (!toExplore.empty())
         {
             std::cout << "time: " << time << " #toExplore: " << toExplore.size() << std::endl;
@@ -103,11 +138,24 @@ int main()
             {
                 if (seenStates.contains(state))
                     continue;
+                // Prune.
+                // For maxPossibleGeodes, assume we can buy a Geode-producing Robot every minute until the timeLimit.
+                const int64_t timeRemaining = timeLimit - time;
+                if ((mostGeodes > 0) && timeRemaining <= 10)
+                {
+                    const int64_t maxPossibleGeodes = state.amountOfResource[Geode] + estimateMaxPossibleExtraGeodes(state.amountOfRobot[Obsidian], state.amountOfResource[Obsidian], state.amountOfRobot[Geode], timeRemaining, bluePrint);
+                    const int64_t minCompetitorGeodes = mostGeodes + (timeLimit - earliestTimeOfMostGeodes);
+                    if (maxPossibleGeodes <= minCompetitorGeodes)
+                    {
+                        //std::cout << " blueprint: " << blueprintNumber << " time: " << time << " most (competitor) Geodes: " << mostGeodes << " earliestTimeOfMostGeodes: " << earliestTimeOfMostGeodes << " maxPossibleGeodes: " << maxPossibleGeodes << " minCompetitorGeodes: " << minCompetitorGeodes << " pruning state: " << state << std::endl;
+                        continue;
+                    }
+                }
                 seenStates.insert(state);
                 State nextState = state;
                 if (rand() % 100'000 == 0)
                 {
-                    cout << "time: " << time << " " << state << std::endl;
+                    cout << "time: " << time << " blueprint: " << bluePrint.blueprintNumber << " " << state << std::endl;
                 }
                 //cout << state << endl;
                 // Collect.
@@ -116,7 +164,7 @@ int main()
                     nextState.amountOfResource[resourceType] += nextState.amountOfRobot[resourceType];
                 }
                 // Cap - we don't need more of a given resource R than the cost to buy the most expensive
-                // resource (in terms of R) every single minute until the end.
+                // resource (in terms of R) every single minute (including this one) until the end.
                 for (int resourceType = 0; resourceType < numResourceTypes; resourceType++)
                 {
                     nextState.amountOfResource[resourceType] = std::min(nextState.amountOfResource[resourceType], (timeLimit - time + 1) * maxAmountOfResourceToBuyRobot[resourceType]);
@@ -163,6 +211,7 @@ int main()
                 if (mostGeodes < nextState.amountOfResource[Geode])
                 {
                     mostGeodes = nextState.amountOfResource[Geode];
+                    earliestTimeOfMostGeodes = time;
                     std::cout << "New mostGeodes: " << mostGeodes << std::endl;
                 }
             }
@@ -172,12 +221,11 @@ int main()
             if (time == timeLimit)
             {
                 std::cout << "mostGeodes: " << mostGeodes << std::endl;
-                qualitySum += mostGeodes * blueprintNumber;
+                qualitySum += mostGeodes * bluePrint.blueprintNumber;
                 break;
             }
         }
 
-        blueprintNumber++;
 
     }
     std::cout << "qualitySum: " << qualitySum << std::endl;
