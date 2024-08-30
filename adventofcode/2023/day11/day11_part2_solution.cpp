4431bf3a5ee29db50e13b253bc0179971c42f1a5
Simon St James <kdedevel@etotheipiplusone.com>
2024-08-30 18:02:36 +0100

    AoC23D11P2: clean up

--- adventofcode/2023/day11/day11_part2_solution.cpp
+++ adventofcode/2023/day11/day11_part2_solution.cpp
@@ -6,108 +6,29 @@
 
 using namespace std;
 
-const int64_t multiplier = 1'000'000;
+const int64_t emptyLineDistMultiplier = 1'000'000;
 
 int main()
 {
-    vector<string> debugMap;
+    vector<string> map;
     string mapLine;
     while (getline(cin, mapLine))
     {
-        debugMap.push_back(mapLine);
+        map.push_back(mapLine);
     }
-    const auto origMap = debugMap;
     struct Coord
     {
         int row = -1;
         int col = -1;
         auto operator<=>(const Coord& other) const = default;
     };
-#if 0
-    std::cout << "Original map: " << std::endl;
-    for (const auto& row : debugMap) cout << row << std::endl;
-    int width = debugMap.front().size();
-    int height = debugMap.size();
-    for (int row = 0; row < height; row++)
-    {
-        if (debugMap[row].find('#') == string::npos)
-        {
-            const int numRowsToAdd = multiplier - 1;
-            for (int i = 0; i < numRowsToAdd; i++)
-            {
-                debugMap.insert(debugMap.begin() + row, string(width, '.'));
-            }
-            row += numRowsToAdd;
-            height += numRowsToAdd;
-        }
-    }
-    std::cout << "After inserting rows: " << std::endl;
-    for (const auto& row : debugMap) cout << row << std::endl;
-    for (int col = 0; col < width; col++)
-    {
-        bool hasGalaxyInColumn = false;
-        for (int row = 0; row < height; row++)
-        {
-            if (debugMap[row][col] == '#')
-            {
-                hasGalaxyInColumn = true;
-                break;
-            }
-        }
-        if (!hasGalaxyInColumn)
-        {
-            std::cout << "inserting row" << std::endl;
-            const int numColsToAdd = multiplier - 1;
-            for (int row = 0; row < height; row++)
-            {
-                for (int i = 0; i < numColsToAdd; i++)
-                {
-                    debugMap[row].insert(debugMap[row].begin() + col, '.');
-                }
-            }
-            col += numColsToAdd;
-            width += numColsToAdd;
-        }
 
-    }
-    std::cout << "After inserting cols: " << std::endl;
-    for (const auto& row : debugMap) cout << row << std::endl;
-
-
-    int64_t debugResult = 0;
-    {
-        vector<Coord> galaxyCoords;
-        for (int col = 0; col < width; col++)
-        {
-            for (int row = 0; row < height; row++)
-            {
-                if (debugMap[row][col] == '#')
-                    galaxyCoords.push_back({row, col});
-            }
-        }
-
-
-        for (int galaxy1Index = 0; galaxy1Index < galaxyCoords.size(); galaxy1Index++)
-        {
-            for (int galaxy2Index = galaxy1Index + 1; galaxy2Index < galaxyCoords.size(); galaxy2Index++)
-            {
-                const auto galaxy1 = galaxyCoords[galaxy1Index];
-                const auto galaxy2 = galaxyCoords[galaxy2Index];
-                const int distance = abs(galaxy2.row - galaxy1.row) + abs(galaxy2.col - galaxy1.col);
-                std::cout << "Debug Distance between galaxies: " << galaxy1Index << " and " << galaxy2Index << " = " << distance << std::endl;
-                debugResult += distance;
-            }
-        }
-    }
-#endif
-
-    const int width = origMap.front().size();
-    const int height = origMap.size();
-    int64_t result = 0;
+    const int width = map.front().size();
+    const int height = map.size();
     set<int> emptyRowIndices;
     for (int row = 0; row < height; row++)
     {
-        if (origMap[row].find('#') == string::npos)
+        if (map[row].find('#') == string::npos)
         {
             emptyRowIndices.insert(row);
         }
@@ -118,7 +39,7 @@ int main()
         bool hasGalaxyInColumn = false;
         for (int row = 0; row < height; row++)
         {
-            if (origMap[row][col] == '#')
+            if (map[row][col] == '#')
             {
                 hasGalaxyInColumn = true;
                 break;
@@ -135,19 +56,18 @@ int main()
     {
         for (int row = 0; row < height; row++)
         {
-            if (origMap[row][col] == '#')
+            if (map[row][col] == '#')
                 galaxyCoords.push_back({row, col});
         }
     }
 
-
+    int64_t result = 0;
     for (int galaxy1Index = 0; galaxy1Index < galaxyCoords.size(); galaxy1Index++)
     {
         for (int galaxy2Index = galaxy1Index + 1; galaxy2Index < galaxyCoords.size(); galaxy2Index++)
         {
             const auto galaxy1 = galaxyCoords[galaxy1Index];
             const auto galaxy2 = galaxyCoords[galaxy2Index];
-            std::cout << "Computing Distance between galaxies: " << galaxy1Index << " and " << galaxy2Index << std::endl;
 
             int64_t distance = 0;
             {
@@ -155,18 +75,14 @@ int main()
                 int xEnd = galaxy2.col;
                 if (xEnd < xStart)
                     swap(xStart, xEnd);
-                std::cout << " xStart: " << xStart << " xEnd: " << xEnd << std::endl;
                 for (int x = xStart; x < xEnd; x++)
                 {
-                    //std::cout << "  x: " << x << std::endl;
                     if (emptyColIndices.contains(x))
                     {
-                        //std::cout << "    adding: " << multiplier << std::endl;
-                        distance += multiplier;
+                        distance += emptyLineDistMultiplier;
                     }
                     else
                     {
-                        //std::cout << "    adding: " << 1 << std::endl;
                         distance++;
                     }
                 }
@@ -176,28 +92,21 @@ int main()
                 int yEnd = galaxy2.row;
                 if (yEnd < yStart)
                     swap(yStart, yEnd);
-                std::cout << " yStart: " << yStart << " yEnd: " << yEnd << std::endl;
                 for (int y = yStart; y < yEnd; y++)
                 {
-                    //std::cout << "  y: " << y << std::endl;
                     if (emptyRowIndices.contains(y))
                     {
-                        //std::cout << "    adding: " << multiplier << std::endl;
-                        distance += multiplier;
+                        distance += emptyLineDistMultiplier;
                     }
                     else
                     {
-                        //std::cout << "    adding: " << 1 << std::endl;
                         distance++;
                     }
                 }
             }
-            std::cout << "Optimised Distance between galaxies: " << galaxy1Index << " and " << galaxy2Index << " = " << distance << std::endl;
             result += distance;
         }
     }
-    //std::cout << "debugResult: " << debugResult << std::endl;
     std::cout << "result: " << result << std::endl;
-    //assert(debugResult == result);
 }
 
