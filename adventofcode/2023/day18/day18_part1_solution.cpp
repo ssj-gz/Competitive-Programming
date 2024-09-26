55f0ff2e3f0fed77fef5ba53edffd1feca52c50b
Simon St James <kdedevel@etotheipiplusone.com>
2024-09-26 13:05:57 +0100

    AoC23D18P1: begin to optimise to deal with very large numToDig
    The eventual, optimised solution I come up with for this will ultimately
    form  the basis of the Part 1 solution, but it's easier to incubate it
    in Part 1's code as I'll be able to check the optimised solution against
    Part 1's original "brute force" implementation.
    
    For now, just fork the brute force implementation out into its own
    method, and add a means of storing the input data in sensible data
    structures that can be used by the brute force implementation and the
    upcoming optimised version (and also use int64_t instead of ints).

--- adventofcode/2023/day18/day18_part1_solution.cpp
+++ adventofcode/2023/day18/day18_part1_solution.cpp
@@ -7,61 +7,59 @@
 
 #include <cassert>
 
+#define BRUTE_FORCE
+
 using namespace std;
 
 struct Coord
 {
-    int x = -1;
-    int y = -1;
+    int64_t x = -1;
+    int64_t y = -1;
     auto operator<=>(const Coord& other) const = default;
 };
 
-int main()
+struct DigSection
 {
-    std::regex digPlanRegex(R"(^([URDL])\s*(\d+)\s*\((.*)\)\s*$)");
-    string digPlanLine;
+    enum Direction { Up, Left, Down, Right };
+    Direction direction = Up;
+    int64_t numToDig = -1; 
+    string colourRGB;
+};
 
+#ifdef BRUTE_FORCE
+int64_t lagoonSizeBruteForce(const vector<DigSection>& digPlan)
+{
     std::map<Coord, string> colourAtCoord;
-    int x = 0;
-    int y = 0;
-    while (getline(cin, digPlanLine))
+    int64_t x = 0;
+    int64_t y = 0;
+
+    for (const auto& digSection : digPlan)
     {
-        std::smatch digPlanMatch;
-
-        const bool matchSuccessful = std::regex_match(digPlanLine, digPlanMatch, digPlanRegex);
-        assert(matchSuccessful);
-
-        const auto dirChar = std::string(digPlanMatch[1])[0];
-        const int64_t numToDig = std::stoll(digPlanMatch[2]);
-        const string colourRGB = digPlanMatch[3];
-
-        std::cout << "dirChar: " << dirChar << " numToDig: " << numToDig << " colourRGB: " << colourRGB << std::endl;
-
-        for (int i = 0; i < numToDig; i++)
+        for (int64_t i = 0; i < digSection.numToDig; i++)
         {
-            switch (dirChar)
+            switch (digSection.direction)
             {
-                case 'U':
+                case DigSection::Up:
                     y--;
                     break;
-                case 'R':
+                case DigSection::Right:
                     x++;
                     break;
-                case 'D':
+                case DigSection::Down:
                     y++;
                     break;
-                case 'L':
+                case DigSection::Left:
                     x--;
                     break;
             }
-            colourAtCoord[{x, y}] = colourRGB;
+            colourAtCoord[{x, y}] = digSection.colourRGB;
         }
     }
 
-    int minX = std::numeric_limits<int>::max();
-    int minY = std::numeric_limits<int>::max();
-    int maxX = std::numeric_limits<int>::min();
-    int maxY = std::numeric_limits<int>::min();
+    int64_t minX = std::numeric_limits<int64_t>::max();
+    int64_t minY = std::numeric_limits<int64_t>::max();
+    int64_t maxX = std::numeric_limits<int64_t>::min();
+    int64_t maxY = std::numeric_limits<int64_t>::min();
     for (const auto& [coord, colour] : colourAtCoord)
     {
         minX = std::min(minX, coord.x);
@@ -69,22 +67,22 @@ int main()
         maxX = std::max(maxX, coord.x);
         maxY = std::max(maxY, coord.y);
     }
-    const int width = maxX - minX + 3;
-    const int height = maxY - minY + 3;
+    const int64_t width = maxX - minX + 3;
+    const int64_t height = maxY - minY + 3;
     assert(width > 0);
     assert(height > 0);
     vector<vector<char>> fillMap(width, vector<char>(height, '.'));
     for (const auto& [coord, colour] : colourAtCoord)
     {
-        const int x = coord.x - minX + 1;
-        const int y = coord.y - minY + 1;
+        const int64_t x = coord.x - minX + 1;
+        const int64_t y = coord.y - minY + 1;
         assert(x >= 0 && x < width);
         assert(y >= 0 && y < height);
         fillMap[x][y] = '#';
     }
-    for (int y = 0; y < height; y++)
+    for (int64_t y = 0; y < height; y++)
     {
-        for (int x = 0; x < width; x++)
+        for (int64_t x = 0; x < width; x++)
         {
             cout << fillMap[x][y];
         }
@@ -94,7 +92,7 @@ int main()
     vector<Coord> toExplore = { {0, 0} };
     set<Coord> seen = { {0, 0} };
 
-    int numOutsideCells = 0;
+    int64_t numOutsideCells = 0;
     while (!toExplore.empty())
     {
         std::cout << "#toExplore: " << toExplore.size() << std::endl;
@@ -111,8 +109,8 @@ int main()
                                          {0, +1}
                                          })
             {
-                const int newX = coord.x + dx;
-                const int newY = coord.y + dy;
+                const int64_t newX = coord.x + dx;
+                const int64_t newY = coord.y + dy;
                 if (newX < 0 || newX >= width || newY < 0 || newY >= height)
                     continue;
                 if (fillMap[newX][newY] == '#')
@@ -128,16 +126,60 @@ int main()
         toExplore = nextToExplore;
     }
     std::cout << "After fill: " << std::endl;
-    for (int y = 0; y < height; y++)
+    for (int64_t y = 0; y < height; y++)
     {
-        for (int x = 0; x < width; x++)
+        for (int64_t x = 0; x < width; x++)
         {
             cout << fillMap[x][y];
         }
         cout << endl;
     }
     std::cout << "numOutsideCells: " << numOutsideCells << std::endl;
-    const int64_t result = width * height - numOutsideCells; 
-    std::cout << "result: " << result << std::endl;
+    return width * height - numOutsideCells;
+}
+#endif
+
+int main()
+{
+    std::regex digPlanRegex(R"(^([URDL])\s*(\d+)\s*\((.*)\)\s*$)");
+    string digPlanLine;
+
+    vector<DigSection> digPlan;
+    while (getline(cin, digPlanLine))
+    {
+        std::smatch digPlanMatch;
+
+        const bool matchSuccessful = std::regex_match(digPlanLine, digPlanMatch, digPlanRegex);
+        assert(matchSuccessful);
+
+        const auto dirChar = std::string(digPlanMatch[1])[0];
+        const int64_t numToDig = std::stoll(digPlanMatch[2]);
+        const string colourRGB = digPlanMatch[3];
+
+        std::cout << "dirChar: " << dirChar << " numToDig: " << numToDig << " colourRGB: " << colourRGB << std::endl;
+
+        DigSection digSection;
+        digSection.numToDig = numToDig;
+        digSection.colourRGB;
+        switch (dirChar)
+        {
+            case 'U':
+                digSection.direction = DigSection::Up;
+                break;
+            case 'R':
+                digSection.direction = DigSection::Right;
+                break;
+            case 'D':
+                digSection.direction = DigSection::Down;
+                break;
+            case 'L':
+                digSection.direction = DigSection::Left;
+                break;
+        }
+        digPlan.push_back(digSection);
+    }
+
+    const int64_t resultBruteForce = lagoonSizeBruteForce(digPlan);
+    std::cout << "resultBruteForce: " << resultBruteForce << std::endl;
 
 }
