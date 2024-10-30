b204b8cd54bfd44054d91db01e2155d80198ce71
Simon St James <kdedevel@etotheipiplusone.com>
2024-10-30 10:31:10 +0000

    Add AoC 2018 Day 24 Part 1 input and (correct) solution

---
+++ adventofcode/2018/day24/day24_part1_solution.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <regex>
+#include <ranges>
+#include <sstream>
+
+#include <cassert>
+
+using namespace std;
+
+struct Group
+{
+    enum Type { Immune, Infection };
+    int id = -1;
+    Type type;
+    int unitCount = -1;
+    int hitPoints = -1;
+    int damageCount = -1;
+    string damageType;
+    int initiative = -1;
+    vector<string> weaknesses;
+    vector<string> immunities;
+    bool isAlive =  true;
+    int64_t effectivePower() const
+    {
+        return static_cast<int64_t>(unitCount) * damageCount;
+    }
+    int64_t effectiveDamageFrom(const auto* attacker) const
+    {
+        int64_t effectiveDamage = attacker->effectivePower();
+        if (std::find(weaknesses.begin(), weaknesses.end(), attacker->damageType) != weaknesses.end())
+        {
+            effectiveDamage *= 2;
+        }
+        else if (std::find(immunities.begin(), immunities.end(), attacker->damageType) != immunities.end())
+        {
+            effectiveDamage = 0;
+        }
+        return effectiveDamage;
+    }
+};
+
+ostream& operator<<(ostream& os, const Group& group)
+{
+    os << group.unitCount << " units each with " << group.hitPoints << " hit points "; 
+    if (!(group.weaknesses.empty() && group.immunities.empty()))
+    {
+        os << "(";
+        if (!group.weaknesses.empty())
+        {
+            os << "weak to";
+            for (auto weaknessIter = group.weaknesses.begin(); weaknessIter != group.weaknesses.end(); weaknessIter++)
+            {
+               os << " " << *weaknessIter; 
+               if (std::next(weaknessIter) != group.weaknesses.end())
+                   os << ",";
+            }
+        }
+        if (!group.immunities.empty())
+        {
+            if(!group.weaknesses.empty()) os << "; ";
+            os << "immune to";
+            for (auto immunityIter = group.immunities.begin(); immunityIter != group.immunities.end(); immunityIter++)
+            {
+                os << " " << *immunityIter; 
+                if (std::next(immunityIter) != group.immunities.end())
+                    os << ",";
+            }
+
+        }
+        os << ") ";
+    }
+    os << "with an attack that does " << group.damageCount << " " << group.damageType << " damage at initiative " << group.initiative;
+    return os;
+}
+
+int main()
+{
+    string immuneSystemHeader;
+    getline(cin, immuneSystemHeader);
+    assert(immuneSystemHeader == "Immune System:");
+    string groupDescrLine;
+    std::regex groupDescrRegex(R"(^(\d+) units each with (\d+) hit points (\((.*)\) )?with an attack that does (\d+) (\w+) damage at initiative (\d+)$)");
+    vector<Group> groups;
+
+    auto parseGroup = [&groupDescrRegex](Group::Type type, int id, const std::string& groupDescrLine)
+    {
+        std::smatch groupDescrMatch;
+        const bool matchSuccesful = std::regex_match(groupDescrLine, groupDescrMatch, groupDescrRegex);
+        assert(matchSuccesful);
+        const std::string weaknessesDescr = groupDescrMatch[4];
+        Group group;
+        group.type = type;
+        group.id = id;
+        group.unitCount = std::stoi(groupDescrMatch[1]);
+        group.hitPoints = std::stoi(groupDescrMatch[2]);
+        group.damageCount = std::stoi(groupDescrMatch[5]);
+        group.damageType = groupDescrMatch[6];
+        group.initiative = std::stoi(groupDescrMatch[7]);
+        // Trimming and splitting a string is still clunky as hell, here in YToOL 2024.
+        auto weaknessesDescrRange = std::views::split(std::string(weaknessesDescr), ';');
+        for (const auto& weaknessRange : weaknessesDescrRange)
+        {
+            string weaknessDescr(weaknessRange.begin(), weaknessRange.end());
+            //std::cout << " weaknessDescr: " << weaknessDescr << std::endl;
+            std::smatch weaknessMatch;
+            const bool matchSuccessful = std::regex_match(weaknessDescr, weaknessMatch, std::regex(R"(^\s*(weak|immune) to (.*)$)"));
+            assert(matchSuccessful);
+            const std::string weakOrImmune = weaknessMatch[1];
+            std::string attackTypesDescr = weaknessMatch[2];
+            attackTypesDescr.erase(std::remove(attackTypesDescr.begin(), attackTypesDescr.end(), ','), attackTypesDescr.end());
+            auto& attackList = (weakOrImmune == "weak") ? group.weaknesses : group.immunities;
+            auto attackTypesStream = istringstream(attackTypesDescr);
+            string attackType;
+            while (attackTypesStream >> attackType)
+                attackList.push_back(attackType);
+        }   
+        return group;
+    };
+    int id = 1;
+    while (getline(cin, groupDescrLine) && !groupDescrLine.empty())
+    {
+        std::cout << "groupDescrLine: " << groupDescrLine << std::endl;
+        const auto group = parseGroup(Group::Immune, id, groupDescrLine);
+        std::cout << "read group    : " << group << std::endl;
+        groups.push_back(group);
+        id++;
+    }
+
+    string infectionSystemHeader;
+    getline(cin, infectionSystemHeader);
+    assert(infectionSystemHeader == "Infection:");
+    id = 1;
+    while (getline(cin, groupDescrLine))
+    {
+        std::cout << "groupDescrLine: " << groupDescrLine << std::endl;
+        const auto group = parseGroup(Group::Infection, id, groupDescrLine);
+        std::cout << "read group    : " << group << std::endl;
+        groups.push_back(group);
+        id++;
+    }
+
+    vector<Group*> remainingGroups;
+    for (auto& group : groups)
+        remainingGroups.push_back(&group);
+
+    struct TargetResult
+    {
+        Group *attacker = nullptr;
+        Group *defender = nullptr;
+    };
+    std::cout << "Beginning combat" << std::endl;
+    while (true)
+    {
+        std::cout << "New round; #remainingGroups: " << remainingGroups.size() << std::endl;
+        // Target selection.
+        std::cout << "Target selection" << std::endl;
+        vector<TargetResult> targetResults;
+        sort(remainingGroups.begin(), remainingGroups.end(), [](const auto* lhsGroup, const auto* rhsGroup)
+                {
+                    const int64_t lhsEffectivePower = lhsGroup->effectivePower();
+                    const int64_t rhsEffectivePower = rhsGroup->effectivePower();
+                    if (lhsEffectivePower != rhsEffectivePower)
+                        return rhsEffectivePower < lhsEffectivePower;
+                    return rhsGroup->initiative < lhsGroup->initiative;
+                });
+        vector<Group*> targets = remainingGroups;
+        for (auto* attacker : remainingGroups)
+        {
+            vector<Group*> targetableEnemies;
+            for (auto* group : targets)
+            {
+                if (group->type != attacker->type)
+                    targetableEnemies.push_back(group);
+            }
+
+            sort(targetableEnemies.begin(), targetableEnemies.end(), [attacker](const auto* lhsGroup, const auto* rhsGroup)
+                    {
+                    const int64_t effectiveDamageLhs = lhsGroup->effectiveDamageFrom(attacker);
+                    const int64_t effectiveDamageRhs = rhsGroup->effectiveDamageFrom(attacker);
+                    if (effectiveDamageLhs != effectiveDamageRhs)
+                        return effectiveDamageRhs < effectiveDamageLhs;
+                    if (lhsGroup->effectivePower() != rhsGroup->effectivePower())
+                        return rhsGroup->effectivePower() < lhsGroup->effectivePower();
+                    return rhsGroup->initiative < lhsGroup->initiative;
+                    });
+            if (!targetableEnemies.empty())
+            {
+                for (auto* target : targetableEnemies)
+                {
+                    assert(target->isAlive);
+                    if (target->effectiveDamageFrom(attacker) == 0)
+                    {
+                        // "If it cannot deal any defending groups damage, it does not choose a target".
+                        // Ambiguous as to whether we include the defenders hitpoints, here(?)
+                        continue;
+                    }
+                    targetResults.push_back({attacker, target});
+                    targets.erase(std::find(targets.begin(), targets.end(), target));
+                    break;
+                }
+            }
+        }
+
+        std::cout << "Attack" << std::endl;
+        std::sort(targetResults.begin(), targetResults.end(), [](const auto& lhsTargetPair, const auto& rhsTargetPair)
+                {
+                    return rhsTargetPair.attacker->initiative < lhsTargetPair.attacker->initiative;
+                });
+        for (auto& [attacker, target] : targetResults)
+        {
+            if (!attacker->isAlive || !target->isAlive)
+                continue;
+            const int64_t effectiveDamageToTarget = target->effectiveDamageFrom(attacker);
+            const int64_t unitsLost = effectiveDamageToTarget / target->hitPoints;
+            std::cout << "Attacker: " << (attacker->type == Group::Immune ? "Immune" : "Infection") << " " << attacker->id << " attacks: " << (target->type == Group::Immune ? "Immune" : "Infection") << " " << target->id << " dealing " << effectiveDamageToTarget << " damage, killing " << std::min<int64_t>(unitsLost, target->unitCount) << " units" << std::endl;
+            target->unitCount -= unitsLost;
+            if (target->unitCount <= 0)
+                target->isAlive = false;
+        }
+        // Who is left alive?
+        remainingGroups.erase(std::remove_if(remainingGroups.begin(), remainingGroups.end(), [](const auto* group) { return !group->isAlive;}), remainingGroups.end());
+        const int numInfectionsAlive = std::count_if(remainingGroups.begin(), remainingGroups.end(), [](const auto* group) { return group->type == Group::Infection;});
+        const int numImmuneAlive = std::count_if(remainingGroups.begin(), remainingGroups.end(), [](const auto* group) { return group->type == Group::Immune;});
+        if (numImmuneAlive == 0 || numInfectionsAlive == 0)
+        {
+            break;
+        }
+    }
+
+    int64_t result = 0;
+    for (auto* group : remainingGroups)
+        result += group->unitCount;
+    std::cout << "result: " << result << std::endl;
+    return 0;
+}
