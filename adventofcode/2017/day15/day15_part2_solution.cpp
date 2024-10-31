078a57ef0fbe738bd5b2e6fa1e15b87bb652f27e
Simon St James <kdedevel@etotheipiplusone.com>
2024-10-31 11:19:38 +0000

    Add AoC 2017 Day 15 Part 2 (correct) solution

--- adventofcode/2017/day15/day15_part2_solution.cpp
+++ adventofcode/2017/day15/day15_part2_solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <regex>
 
 #include <cassert>
 
@@ -6,5 +7,47 @@ using namespace std;
 
 int main()
 {
+    std::regex generatorStartRegex(R"(^Generator [AB] starts with (\d+)$)");
+
+
+    string generatorAStartDesc;
+    std::getline(cin, generatorAStartDesc);
+    std::smatch generatorAStartMatch;
+    const bool generatorAMatched = std::regex_match(generatorAStartDesc, generatorAStartMatch, generatorStartRegex);
+    assert(generatorAMatched);
+    const int64_t generatorAStartVal = std::stoll(generatorAStartMatch[1]);
+
+    string generatorBStartDesc;
+    std::getline(cin, generatorBStartDesc);
+    std::smatch generatorBStartMatch;
+    const bool generatorBMatched = std::regex_match(generatorBStartDesc, generatorBStartMatch, generatorStartRegex);
+    assert(generatorBMatched);
+    const int64_t generatorBStartVal = std::stoll(generatorBStartMatch[1]);
+
+    std::cout << "generatorAStartVal: " << generatorAStartVal << " generatorBStartVal: " << generatorBStartVal << std::endl;
+
+    int64_t generatorA = generatorAStartVal;
+    int64_t generatorB = generatorBStartVal;
+    constexpr int64_t generatorAFactor = 16'807;
+    constexpr int64_t generatorBFactor = 48'271;
+    int64_t numMatches = 0;
+    for (int64_t count = 1; count <= 5'000'000; count++)
+    {
+        do
+        {
+            generatorA = (generatorA * generatorAFactor) % 2'147'483'647;
+        } while (generatorA % 4 != 0);
+        do
+        {
+            generatorB = (generatorB * generatorBFactor) % 2'147'483'647;
+        } while (generatorB % 8 != 0);
+        if ((generatorA & 0xFFFF) == (generatorB & 0xFFFF))
+        {
+            numMatches++;
+        }
+
+    }
+    std::cout << "numMatches: " << numMatches << std::endl;
+
     return 0;
 }
