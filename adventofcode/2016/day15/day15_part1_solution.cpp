eb44fcc93ce6c3ba63fcfaa2ac3a86c10b226d3c
Simon St James <kdedevel@etotheipiplusone.com>
2024-11-06 09:26:49 +0000

    Add AoC 2016 Day 15 Part 1 input and (correct) solution

---
+++ adventofcode/2016/day15/day15_part1_solution.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <regex>
+
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+    // Disc #1 has 5 positions; at time=0, it is at position 4.
+    std::regex discDescrRegex(R"(^Disc #(\d+) has (\d+) positions; at time=(\d+), it is at position (\d+)\.$)");
+    string discDescr;
+
+    struct Disc
+    {
+        int id = -1;
+        int numPositions = -1;
+        int startPosition = -1;
+    };
+
+    vector<Disc> discs;
+
+    while (getline(cin, discDescr))
+    {
+        std::smatch discDescrMatch;
+        const bool matchSuccessful = std::regex_match(discDescr, discDescrMatch, discDescrRegex);
+        assert(matchSuccessful);
+        Disc disc;
+        disc.id = std::stoi(discDescrMatch[1]);;
+        disc.numPositions = std::stoi(discDescrMatch[2]);
+        disc.startPosition = std::stoi(discDescrMatch[4]);
+
+        discs.push_back(disc);
+    }
+
+    int timeToWait = 0;
+    while (true)
+    {
+        std::cout << "Trying with timeToWait: " << timeToWait << std::endl;
+        vector<Disc> discsCopy = discs;
+        // Fast-forward.
+        for (auto& disc : discsCopy)
+        {
+            disc.startPosition = (disc.startPosition + timeToWait) % disc.numPositions;
+        }
+
+        bool passedThroughAllDiscs = true;
+        for (auto& disc : discsCopy)
+        {
+            const int timeToReachDisc = disc.id;
+            const int currentDiscPosition = (disc.startPosition + timeToReachDisc) % disc.numPositions;
+            if (currentDiscPosition != 0)
+            {
+                passedThroughAllDiscs = false;
+            }
+        }
+        if (passedThroughAllDiscs)
+        {
+            break;
+        }
+
+        timeToWait++;
+    }
+    std::cout << "result: " << timeToWait << std::endl;
+
+    return 0;
+}
