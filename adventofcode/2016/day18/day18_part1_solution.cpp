38870bef8eb739e724deda7aa3a3c7defd97bfd6
Simon St James <kdedevel@etotheipiplusone.com>
2024-11-06 10:50:02 +0000

    Add AoC 2016 Day 18 Part 1 input and (correct) solution

---
+++ adventofcode/2016/day18/day18_part1_solution.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+    string firstRow;
+    getline(cin, firstRow);
+
+    const int width = firstRow.size();
+
+    std::cout << "firstRow: " << firstRow << std::endl;
+    string previousRow = firstRow;
+    int numSafe = std::count(firstRow.begin(), firstRow.end(), '.');
+    constexpr int totalRows = 40;
+    for (int row = 1; row <= totalRows - 1; row++)
+    {
+        string newRow;
+        for (int x = 0; x < width; x++)
+        {
+            const auto leftIsTrap = (x >= 0 && previousRow[x - 1] == '^');
+            const auto centreIsTrap = (previousRow[x] == '^');
+            const auto rightIsTrap = (x + 1 < width && previousRow[x + 1] == '^');
+            if ((leftIsTrap && centreIsTrap && !rightIsTrap) ||
+                (centreIsTrap && rightIsTrap && !leftIsTrap) ||
+                (leftIsTrap && !centreIsTrap && !rightIsTrap) ||
+                (rightIsTrap && !centreIsTrap && !leftIsTrap))
+            {
+                newRow += '^';
+            }
+            else
+            {
+                newRow += '.';
+            }
+
+        }
+        std::cout << "newRow  : " << newRow << std::endl;
+        numSafe  += std::count(newRow.begin(), newRow.end(), '.');
+        previousRow = newRow;
+    }
+
+
+    std::cout << "numSafe: " << numSafe << std::endl;
+
+    return 0;
+}
