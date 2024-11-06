7d32bf2240c4c3a393d8d4c4db88b4ea73956b56
Simon St James <kdedevel@etotheipiplusone.com>
2024-11-06 13:55:48 +0000

    AoC16D20P2: clean up a little

--- adventofcode/2016/day20/day20_part2_solution.cpp
+++ adventofcode/2016/day20/day20_part2_solution.cpp
@@ -4,11 +4,6 @@
 
 #include <cassert>
 
-// TODO - remove this!
-#include <sys/time.h> // TODO - this is only for random testcase generation.  Remove it when you don't need new random testcases!
-
-//#define BRUTE_FORCE
-
 using namespace std;
 
 struct Range
@@ -18,28 +13,7 @@ struct Range
 };
 
 constexpr int64_t minIp = 0;
-constexpr int64_t maxIp = 4294967295; // TODO - reinstate this!
-//constexpr int64_t maxIp = 100;
-
-#ifdef BRUTE_FORCE
-int64_t calcNumAllowedIpsBruteForce(const vector<Range>& blockedRanges)
-{
-    int64_t numAllowedIps = 0;
-    for (int64_t ip = minIp; ip <= maxIp; ip++)
-    {
-        bool isAllowed = true;
-        for (const auto& blockedRange : blockedRanges)
-        {
-            if (ip >= blockedRange.start && ip <= blockedRange.end)
-                isAllowed = false;
-        }
-        if (isAllowed)
-            numAllowedIps++;
-        std::cout << " ip: " << ip << " isAllowed: " << isAllowed << std::endl;
-    }
-    return numAllowedIps;
-}
-#endif
+constexpr int64_t maxIp = 4294967295;
 
 int64_t calcNumAllowedIps(const vector<Range>& blockedRangesOrig)
 {
@@ -52,54 +26,40 @@ int64_t calcNumAllowedIps(const vector<Range>& blockedRangesOrig)
     {
         if (!currentMergedRange.has_value())
             return;
-        std::cout << "  flushing: " << currentMergedRange.value().start << " - " << currentMergedRange.value().end << std::endl;
         mergedBlockedRanges.push_back(currentMergedRange.value());
         currentMergedRange.reset();
     };
 
     for (const auto& blockedRange : blockedRanges)
     {
-        std::cout << "blockedRange: " << blockedRange.start << " - " << blockedRange.end << std::endl;
         if (!currentMergedRange.has_value())
         {
             currentMergedRange = blockedRange;
-            std::cout << " setting currentMergedRange to it" << std::endl;
         }
         else
         {
             if (blockedRange.end <= currentMergedRange.value().start)
             {
-                std::cout << " ignoring" << std::endl;
                 continue;
             }
             if (blockedRange.start > currentMergedRange.value().end + 1)
             {
                 flushCurrentMergedRange();
                 currentMergedRange = blockedRange;
-                std::cout << "  reset currentMergedRange; now: " << currentMergedRange.value().start << " - " << currentMergedRange.value().end << std::endl;
                 continue;
             }
 
             currentMergedRange.value().end = std::max(currentMergedRange.value().end, blockedRange.end);
-            std::cout << "  extended currentMergedRange; now: " << currentMergedRange.value().start << " - " << currentMergedRange.value().end << std::endl;
         }
     }
     flushCurrentMergedRange();
 
-    std::cout << "mergedBlockedRanges: " << std::endl;
-    for (const auto& blockedRange : mergedBlockedRanges)
-    {
-        std::cout << " " << blockedRange.start << " - " << blockedRange.end << std::endl;
-    }
     int64_t numAllowedIps = 0;
     int64_t prevValidBlockBegin = minIp;
-    std::cout << "computing numAllowedIps " << std::endl;
     for (const auto& blockedRange : mergedBlockedRanges)
     {
-        std::cout << " prevValidBlockBegin: " << prevValidBlockBegin << " blockedRange: " << blockedRange.start << " - " << blockedRange.end << std::endl;
         assert(prevValidBlockBegin == 0 || blockedRange.start > prevValidBlockBegin);
         numAllowedIps += blockedRange.start - prevValidBlockBegin;
-        std::cout <<" blockedRange.start - prevValidBlockBegin: " << (blockedRange.start - prevValidBlockBegin) << std::endl;
         prevValidBlockBegin = blockedRange.end + 1;
     }
     assert(!mergedBlockedRanges.empty());
@@ -115,30 +75,6 @@ int main()
 
     vector<Range> blockedRanges;
 
-#ifdef BRUTE_FORCE
-    struct timeval time;
-    gettimeofday(&time,NULL);
-    srand(static_cast<unsigned int>((time.tv_sec * 1000) + (time.tv_usec / 1000)));
-    const int numRanges = rand() % 10 + 1;
-    for (int i = 1; i <= numRanges; i++)
-    {
-        Range range;
-        range.start = rand() % (maxIp + 1);
-        range.end = rand() % (maxIp + 1);
-        if (range.start > range.end)
-            swap(range.start, range.end);
-        blockedRanges.push_back(range);
-    }
-    sort(blockedRanges.begin(), blockedRanges.end(), [](const auto& lhsRange, const auto& rhsRange) { return lhsRange.start < rhsRange.start; });
-    std::cout << "Blocked ranges: " << std::endl;
-    for (const auto x : blockedRanges) std::cout << " " << x.start << " - " << x.end << std::endl;
-    const int64_t numAllowedIpsBruteForce = calcNumAllowedIpsBruteForce(blockedRanges);
-    const int64_t numAllowedIps = calcNumAllowedIps(blockedRanges);
-    std::cout << "numAllowedIpsBruteForce: " << numAllowedIpsBruteForce << std::endl;
-    std::cout << "numAllowedIps          : " << numAllowedIps << std::endl;
-    assert(numAllowedIpsBruteForce == numAllowedIps);
-    return 0;
-#else
     while (getline(cin, rangeDescr))
     {
         std::smatch rangeMatch;
@@ -152,7 +88,6 @@ int main()
     }
     const int64_t numAllowedIps = calcNumAllowedIps(blockedRanges);
     std::cout << "numAllowedIps: " << numAllowedIps << std::endl;
-#endif
 
     return 0;
 }
