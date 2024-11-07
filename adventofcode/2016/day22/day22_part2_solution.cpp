a94982e6000b953c18f827fb61c018aa0c7aa28c
Simon St James <kdedevel@etotheipiplusone.com>
2024-11-07 15:51:41 +0000

    Add AoC 2016 Day 22 Part 2 (correct) solution
    An absolute undocumented mess that only gives the correct answer for the
    sample input by sheer fluke.
    
    I don't know how one would go about solving this problem for arbitrary
    puzzle input, but mine (and, I suspect, everyone elses') has the
    following quirks:
    
        * There are "immovable" nodes (those with used >= 490) that, as the name
          suggests, can never be moved
        * Every node has sufficient size (though not necessarily initial avail)
          to accommodate the used of any other node besides the "immovable"
          ones
        * However, no node - besides the "immovable" ones - has a size that can
          accommodate the combined used of *any* two other nodes
    
    Thus, in any reachable state, each node either:
    
        * contains the exact same data from start to finish (immovable)
        * contains no data
        * contains data from precisely one node (either its own initial data, or
          data from some other node)
    
    Thus, we normalise the initialNodeGrid by setting used = 2 and avail = 0
    for immovable, and used = 1 && avail == 0 for any non-empty nodes.
    
    This results in a hugely reduced state of possibilities and gives the
    correct answer in ~15 seconds on my UM690.

---
+++ adventofcode/2016/day22/day22_part2_solution.cpp
@@ -0,0 +1,481 @@
+#include <iostream>
+#include <regex>
+#include <set>
+#include <optional>
+
+#include <cassert>
+
+using namespace std;
+
+struct Coord
+{
+    int x = -1;
+    int y = -1;
+    auto operator<=>(const Coord& other) const = default;
+};
+
+struct Node
+{
+    Coord coord;
+    int used = -1;
+    int avail = -1;
+    int size = -1;
+    auto operator<=>(const Node& other) const = default;
+};
+
+int maxXForY0 = -1;
+int maxX = -1;
+int maxY = -1;
+
+
+struct State
+{
+    const vector<vector<Node>>* initialNodeGrid = nullptr;
+    Coord targetDataCoord;
+
+    map<Coord, Node> nodeGridOverride = {};
+    Node& nodeAt(const Coord& coord)
+    {
+        if (!nodeGridOverride.contains(coord))
+        {
+            nodeGridOverride[coord] = (*initialNodeGrid)[coord.x][coord.y];
+        }
+        return nodeGridOverride[coord];
+    }
+    const Node& nodeAt(const Coord& coord) const
+    {
+        if (nodeGridOverride.contains(coord))
+            return nodeGridOverride.find(coord)->second;
+        else
+            return (*initialNodeGrid)[coord.x][coord.y];
+    }
+    void discard()
+    {
+        auto overrideIter = nodeGridOverride.begin();
+        while (overrideIter != nodeGridOverride.end())
+        {
+            if (overrideIter->second == (*initialNodeGrid)[overrideIter->second.coord.x][overrideIter->second.coord.y])
+                overrideIter = nodeGridOverride.erase(overrideIter);
+            else
+                overrideIter++;
+        }
+    }
+    bool canDiscard() const
+    {
+        for (const auto& [coord, node] : nodeGridOverride)
+        {
+            if (node == (*initialNodeGrid)[coord.x][coord.y])
+                return true;
+        }
+        return false;
+    }
+#if 0
+    void normalise()
+    {
+        discard();
+        for (int x = 0; x <= maxX; x++)
+        {
+            for (int y = 0; y <= maxY; y++)
+            {
+                const auto& node = nodeAt({x, y});
+                if (node.used > 0)
+                    node.used = 1;
+            }
+        }
+
+    }
+#endif
+    auto operator<=>(const State& other) const = default;
+};
+
+map<std::tuple<int64_t, Coord, State>, vector<State>> blee;
+
+#if 0
+std::vector<State> makeSpace(const Coord& toMakeSpace, const int64_t spaceRequired, set<Coord>& processing, const State& state, string indent = "")
+{
+    const auto& node = state.nodeAt(toMakeSpace);
+    assert(!state.canDiscard());
+    //std::cout << indent << "request to make " << spaceRequired << " space at " << toMakeSpace.x << ", " << toMakeSpace.y << " used: " << node.used << " avail: " << node.avail << std::endl;
+    //indent += " ";
+    if (processing.contains(toMakeSpace))
+    {
+        //std::cout << indent << "already in progress; aborting" << std::endl;
+        return vector<State>();
+    }
+    if (blee.contains({spaceRequired, toMakeSpace, state}))
+    {
+        //std::cout << indent << "already done; returning cached value" << std::endl;
+
+        return blee[{spaceRequired, toMakeSpace, state}];
+    }
+    vector<State> returnValue;
+    const auto amountOfDataToMove = node.used;
+    if (node.avail + amountOfDataToMove < spaceRequired)
+    {
+        //std::cout << indent << "can't be done" << std::endl;
+        return vector<State>();
+    }
+    processing.insert(toMakeSpace);
+    for (const auto& [dx, dy] : std::initializer_list<std::pair<int, int>>{ {-1, 0},
+            {+1, 0},
+            {0, -1},
+            {0, +1}
+            })
+    {
+        const Coord neighbourCoord = {toMakeSpace.x + dx, toMakeSpace.y + dy};
+        if ( neighbourCoord.x  < 0 || neighbourCoord.x > maxX || neighbourCoord.y < 0 || neighbourCoord.y > maxY)
+            continue;
+        if (processing.contains(neighbourCoord))
+            continue;
+        const auto spaceAvailableAtNeighbour = state.nodeAt(neighbourCoord).avail;
+        if (spaceAvailableAtNeighbour >= spaceRequired)
+        {
+            State nextState = state;
+            assert(!nextState.canDiscard());
+            auto& from = nextState.nodeAt(toMakeSpace);
+            auto& to = nextState.nodeAt(neighbourCoord);
+
+            assert(from.used == amountOfDataToMove);
+            assert(to.avail >= amountOfDataToMove);
+
+            to.avail -= amountOfDataToMove;
+            to.used += amountOfDataToMove;
+
+            from.avail += amountOfDataToMove;
+            from.used = 0;
+
+            nextState.discard();
+            returnValue.push_back(nextState);
+        }
+        else
+        {
+            const auto& blahs = makeSpace(neighbourCoord, amountOfDataToMove, processing, state, indent + " ");
+            for (const auto& blah : blahs)
+            {
+                State nextState = blah;
+                assert(!nextState.canDiscard());
+                auto& from = nextState.nodeAt(toMakeSpace);
+                auto& to = nextState.nodeAt(neighbourCoord);
+
+                //std::cout << indent << "from.used: " << from.used << " toMakeSpace.used: " << node.used << " amountOfDataToMove:  " << amountOfDataToMove << std::endl;
+                assert(to.avail >= from.used);
+
+                to.avail -= amountOfDataToMove;
+                to.used += amountOfDataToMove;
+
+                from.avail += amountOfDataToMove;
+                from.used = 0;
+
+                nextState.discard();
+                returnValue.push_back(nextState);
+            }
+        }
+        
+    }
+    processing.erase(toMakeSpace);
+    if (returnValue.size() > 0)
+        std::cout << indent << "request to make " << spaceRequired << " space at: " << toMakeSpace.x << ", " << toMakeSpace.y << " yielded: " << returnValue.size() << " possibilities" << std::endl;
+    blee[{spaceRequired, toMakeSpace, state}] = returnValue;
+    return returnValue;
+}
+#endif
+
+
+int main()
+{
+    // root@ebhq-gridcenter# df -h
+    string dfCommand;
+    getline(cin, dfCommand);
+    // Filesystem              Size  Used  Avail  Use%
+    string headers;
+    getline(cin, headers);
+
+
+    std::regex dfRegex(R"(^/dev/grid/node-x(\d+)-y(\d+)\s*(\d+)T\s*(\d+)T\s*(\d+)T\s*(\d+)%$)");
+    string dfLine;
+    vector<Node> nodes;
+    int64_t totalUsed = 0;
+    int64_t totalSize = 0;
+    while (getline(cin, dfLine))
+    {
+        std::smatch dfMatch;
+        const bool matchSuccessful = std::regex_match(dfLine, dfMatch, dfRegex);
+        assert(matchSuccessful);
+        Node node;
+        node.coord.x = std::stoll(dfMatch[1]);
+        node.coord.y = std::stoll(dfMatch[2]);
+        node.size = std::stoll(dfMatch[3]);
+        assert(node.size > 0);
+        totalSize += node.size;
+        node.used = std::stoll(dfMatch[4]);
+        totalUsed += node.used;
+        node.avail = std::stoll(dfMatch[5]);
+        nodes.push_back(node);
+        if (node.coord.y == 0)
+            maxXForY0 = std::max(maxXForY0, node.coord.x);
+        maxX = std::max(maxX, node.coord.x);
+        maxY = std::max(maxY, node.coord.y);
+    }
+    std::cout << "totalUsed: " << totalUsed << " totalSize: " << totalSize << "slack: " << (totalSize - totalUsed) << std::endl;
+    std::cout << "maxXForY0: " << maxXForY0 << std::endl;
+    vector<vector<Node>> initialNodeGrid(maxX + 1, vector<Node>(maxY + 1));
+    for (auto& node : nodes)
+    {
+        initialNodeGrid[node.coord.x][node.coord.y] = node;
+    }
+    const auto initialNodeGridBak = initialNodeGrid;
+
+    for (int x = 0; x <= maxX; x++)
+    {
+        for (int y = 0; y <= maxY; y++)
+        {
+            auto& cell = initialNodeGrid[x][y];
+            bool canNeverMoveToNeighbour = true;
+            for (const auto& [dx, dy] : std::initializer_list<std::pair<int, int>>{ {-1, 0},
+                    {+1, 0},
+                    {0, -1},
+                    {0, +1}
+                    })
+            {
+                const int neighbourX = x + dx;
+                const int neighbourY = y + dy;
+                if (neighbourX < 0 || neighbourX > maxX || neighbourY < 0 || neighbourY > maxY)
+                    continue;
+                if (cell.used <= initialNodeGrid[neighbourX][neighbourY].size)
+                {
+                    canNeverMoveToNeighbour = false;
+                }
+            }
+
+            canNeverMoveToNeighbour = cell.used >= 490;
+            if (canNeverMoveToNeighbour)
+            {
+                cell.used = 2;
+                cell.avail = 0;
+                std::cout << "x: " << x << " y: " << y << " can never move to neighbour" << std::endl;
+            }
+            else
+            {
+                if (cell.used > 0)
+                    cell.used = 1;
+                cell.avail = (cell.used ? 0 : 1);
+            }
+        }
+    }
+
+    State initialState = { &initialNodeGrid, {maxXForY0, 0} };
+    //initialState.normalise();
+#if 1
+    vector<State> toExplore = { initialState };
+    set<State> seen = { initialState };
+
+    set<Coord> active;
+    int numSteps = 0;
+    std::optional<State> finalState;
+    map<State, State> predecessorOfState;
+    while (!toExplore.empty() && !finalState.has_value())
+    {
+        std::cout << "numSteps: " << numSteps << " #toExplore: " << toExplore.size() << std::endl;
+        vector<State> nextToExplore;
+        for (const auto& state : toExplore)
+        {
+            if (state.targetDataCoord == Coord{0, 0})
+            {
+                std::cout << "woo!" << std::endl;
+                finalState = state;
+                break;
+            }
+
+            auto calcActive = [](const auto& state)
+            {
+                set<Coord> active;
+                for (int x = 0; x <= maxX; x++)
+                {
+                    for (int y = 0; y <= maxY; y++)
+                    {
+                        const auto& node = state.nodeAt({x, y});
+                        const auto amountOfDataToMove = node.used;
+                        for (const auto& [dx, dy] : std::initializer_list<std::pair<int, int>>{ {-1, 0},
+                                {+1, 0},
+                                {0, -1},
+                                {0, +1}
+                                })
+                        {
+                            const int neighbourX = x + dx;
+                            const int neighbourY = y + dy;
+                            if (neighbourX < 0 || neighbourX > maxX || neighbourY < 0 || neighbourY > maxY)
+                                continue;
+                            const auto& neighbourNode = state.nodeAt({neighbourX, neighbourY});
+                            if (amountOfDataToMove <= neighbourNode.avail)
+                                active.insert({x, y});
+                        }
+                    }
+                }
+                return active;
+
+            };
+            auto calcFree = [](const auto& state)
+            {
+                int free = 0;
+                for (int x = 0; x <= maxX; x++)
+                {
+                    for (int y = 0; y <= maxY; y++)
+                    {
+                        const auto& node = state.nodeAt({x, y});
+                        if (node.used == 0)
+                            free++;
+                    }
+                }
+                return free;
+
+            };
+
+            //std::cout << "num active: "<< calcActive(state).size() << std::endl;
+            //std::cout << "num free: "<< calcFree(state) << std::endl;
+            assert(calcFree(state) == 1);
+            //const int origMobility = calcMobility(state);
+            //std::cout << "origMobility: " << origMobility << std::endl;
+            for (int x = 0; x <= maxX; x++)
+            {
+                for (int y = 0; y <= maxY; y++)
+                {
+                    const auto& node = state.nodeAt({x, y});
+                    const auto amountOfDataToMove = node.used;
+                    for (const auto& [dx, dy] : std::initializer_list<std::pair<int, int>>{ {-1, 0},
+                            {+1, 0},
+                            {0, -1},
+                            {0, +1}
+                            })
+                    {
+                        const int neighbourX = x + dx;
+                        const int neighbourY = y + dy;
+                        if (neighbourX < 0 || neighbourX > maxX || neighbourY < 0 || neighbourY > maxY)
+                            continue;
+                        const auto& neighbourNode = state.nodeAt({neighbourX, neighbourY});
+                        if (amountOfDataToMove <= neighbourNode.avail)
+                        {
+                            bool movesFromTarget = (x == state.targetDataCoord.x && y == state.targetDataCoord.y);
+                            if (movesFromTarget)
+                            {
+                                //std::cout << "moving from target: " << state.targetDataCoord.x << ", " << state.targetDataCoord.y << " to " << neighbourX << ", " << neighbourY << std::endl;
+                                //active.clear();
+                            }
+                            State nextState = state;
+                            auto& from = nextState.nodeAt({x, y});
+                            auto& to = nextState.nodeAt({neighbourX, neighbourY});
+                            assert(from.used == amountOfDataToMove);
+                            assert(to.avail >= amountOfDataToMove);
+
+                            to.avail -= amountOfDataToMove;
+                            to.used += amountOfDataToMove;
+
+                            from.avail += amountOfDataToMove;
+                            from.used = 0;
+
+                            if (movesFromTarget)
+                            {
+                                nextState.targetDataCoord = { neighbourX, neighbourY };
+                            }
+
+                            nextState.discard();
+
+#if 0
+                            bool hasNew = false;
+                            for (const auto& blah : calcActive(nextState))
+                            {
+                                if (!active.contains(blah))
+                                {
+                                    hasNew = true;
+                                    active.insert(blah);
+                                    //std::cout << "New active: " << blah.x << ", "  << blah.y << std::endl;
+                                }
+                            }
+                            //if (!hasNew && !movesFromTarget)
+                                //continue;
+#endif
+
+
+                            //nextState.normalise();
+                            if (!seen.contains(nextState))
+                            {
+                                seen.insert(nextState);
+                                predecessorOfState[nextState] = state;
+                                nextToExplore.push_back(nextState);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        toExplore = nextToExplore;
+        numSteps++;
+    }
+#endif
+    assert(finalState.has_value());
+    State currentState = finalState.value();
+    vector<std::pair<Coord, Coord>> moves;
+    while (true)
+    {
+        if (predecessorOfState.contains(currentState))
+        {
+            const auto& predecessor = predecessorOfState[currentState];
+            std::optional<Coord> from;
+            std::optional<Coord> to;
+            for (int x = 0; x <= maxX; x++)
+            {
+                for (int y = 0; y <= maxY; y++)
+                {
+                    auto& currentCell = currentState.nodeAt({x,y});
+                    auto& precessorCell = predecessor.nodeAt({x,y});
+                    if (currentCell.used == 0 && precessorCell.used != 0)
+                    {
+                        assert(!from.has_value());
+                        from = {x, y};
+                    }
+                    if (currentCell.used != 0 && precessorCell.used == 0)
+                    {
+                        assert(!to.has_value());
+                        to = {x, y};
+                    }
+                }
+            }
+
+            assert(from.has_value());
+            assert(to.has_value());
+            moves.push_back({from.value(), to.value()});
+            
+            currentState = predecessor;
+        }
+        else
+        {
+            assert(currentState == initialState);
+            break;
+        }
+
+    }
+    std::reverse(moves.begin(), moves.end());
+    auto nodeGrid = initialNodeGridBak;
+    for (const auto& move : moves)
+    {
+        const auto [fromX, fromY] = move.first;
+        const auto [toX, toY] = move.second;
+        std::cout << "move from: " << fromX << ", " << fromY << " to " << toX << ", " << toY << std::endl;
+        assert(nodeGrid[fromX][fromY].used > 0);
+        assert(nodeGrid[fromX][fromY].used <= nodeGrid[toX][toY].avail);
+        const auto amountOfDataToMove = nodeGrid[fromX][fromY].used;
+        nodeGrid[toX][toY].avail -= amountOfDataToMove;
+        nodeGrid[toX][toY].used += amountOfDataToMove;
+        nodeGrid[fromX][fromY].used = 0;
+        nodeGrid[fromX][fromY].avail += amountOfDataToMove;
+    }
+
+    
+#if 0
+    const Coord coordToClear = { maxXForY0, 0 };
+    set<Coord> processing = { };
+    makeSpace(coordToClear, initialNodeGrid[coordToClear.x][coordToClear.y].used, processing, initialState);
+#endif
+
+
+    return 0;
+}
