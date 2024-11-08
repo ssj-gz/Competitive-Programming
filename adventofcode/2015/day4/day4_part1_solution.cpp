53b8d24460d6516070812f7e45a4b97d56441459
Simon St James <kdedevel@etotheipiplusone.com>
2024-11-08 10:17:08 +0000

    Add AoC 2015 Day 4 Part 1 input and (correct) solution
    Using openssl's md5 as Boost, bizarrely, doesn't have a publically
    available one (and the internal one might not be reliable).
    
    Makefile included.  Only tested in Ubuntu 24.04.

---
+++ adventofcode/2015/day4/day4_part1_solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+#include <openssl/md5.h>
+
+#include <cassert>
+
+using namespace std;
+
+string md5AsHex(const std::string& input)
+{
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5((unsigned char*) input.data(), input.size(), result);
+    ostringstream md5HexOut;
+    for (auto byte : result)
+    {
+        md5HexOut << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(byte);
+    }
+    return md5HexOut.str();
+}
+
+int main()
+{
+    string secretkey;
+    cin >> secretkey;
+    assert(cin);
+
+    int number = 1;
+    while (md5AsHex(secretkey + std::to_string(number)).find("00000") != 0)
+    {
+        number++;
+    }
+    std::cout << "result: " << number << std::endl;
+    return 0;
+}
