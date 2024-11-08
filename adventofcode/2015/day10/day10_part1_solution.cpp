0dd546164a3368fbc77f06536b61c0c08093eab9
Simon St James <kdedevel@etotheipiplusone.com>
2024-11-08 14:21:20 +0000

    Add AoC 2015 Day 10 Part 1 input and (correct) solution

---
+++ adventofcode/2015/day10/day10_part1_solution.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+    string digits;
+    cin >> digits;
+
+    for (int i = 1; i <= 40; i++)
+    {
+        string nextDigits;
+        int prevDigit = -1;
+        int numInRun = 0;
+
+        auto flushRun = [&prevDigit, &numInRun, &nextDigits]()
+        {
+            if(prevDigit != -1)
+            {
+                nextDigits += std::to_string(numInRun);
+                nextDigits.push_back(prevDigit + '0');
+            }
+        };
+
+        for (const auto digitChar : digits)
+        {
+            const int digit = digitChar - '0';
+            if (digit != prevDigit)
+            {
+                flushRun();
+                prevDigit = digit;
+                numInRun = 1;
+            }
+            else
+            {
+                numInRun++;
+            }
+        }
+        flushRun();
+
+        digits = nextDigits;
+        std::cout << "After iteration " << i << " digits: " << nextDigits << std::endl;
+    }
+    std::cout << "result: " << digits.size() << std::endl;
+    return 0;
+}
